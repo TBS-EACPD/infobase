d254d2065d2e660b2220fca2f51d217a0af88dd5
AlexCLeduc <ledualex@gmail.com>
2017-12-21 14:35:21 -0500

    more progress on new header

---
+++ src/InfoBase/index_data.js
@@ -0,0 +1,80 @@
+const index_lang_lookups = {
+  page_title: {
+    en: "GC InfoBase",
+    fr: "L'InfoBase du GC",
+  },
+  logo_link: {
+    en: "https://www.canada.ca/en.html",
+    fr: "https://www.canada.ca/fr.html",
+  },
+  logo_a11y_label: {
+    en: "Government of Canada",
+    fr: "Gouvernement du Canada",
+  },
+  logo_a11y_label_2: {
+    en: "Symbol of the Government of Canada",
+    fr: "Symbole du gouvernement du Canada",
+  },
+  lang: {
+    en: "en",
+    fr: "fr",
+  },
+  meta_description: {
+    en: "Access various reports about Government of Canada finances and expenditures.",
+    fr: "Accédez à divers rapports portant sur la situation financière et les dépenses du gouvernement du Canada.",
+  },
+  other_lang_name: {
+    en: "Français",
+    fr: "English",
+  },
+  other_lang_href: {
+    en: "index-fra.html",
+    fr: "index-eng.html",
+  },
+  you_are_here: {
+    en: "You are here",
+    fr: "Vous êtes ici",
+  },
+  please_enable_js: {
+    en: "Please enable javascript",
+    fr: "Veuillez activer javascript",
+  },
+  app_requires_js: {
+    en: "This web application requires javascript",
+    fr: "L'utilisation de cette application web requiert javascript",
+  },
+  top_of_page: {
+    en: "Top of page",
+    fr: "Haut de la page",
+  },
+  about_this_site: {
+    en: "About this site",
+    fr: "À propos de ce site",
+  },
+  skip_to_main_content: {
+    en: "Skip to main content",
+    fr: "Passer au contenu principal",
+  },
+  footer_lis: {
+    en: `
+      <li><a href="https://www.canada.ca/en/social.html">Social media</a></li>
+      <li><a href="https://www.canada.ca/en/mobile.html">Mobile applications</a></li>
+      <li><a href="https://www1.canada.ca/en/newsite.html">About Canada.ca</a></li>
+      <li><a href="https://www.canada.ca/en/transparency/terms.html">Terms and conditions</a></li>
+      <li><a href="https://www.canada.ca/en/transparency/privacy.html">Privacy</a></li>
+
+    `,
+    fr: `
+      <li><a href="https://www.canada.ca/fr/sociaux.html">Médias sociaux</a></li>
+      <li><a href="https://www.canada.ca/fr/mobile.html">Applications mobiles</a></li>
+      <li><a href="https://www1.canada.ca/fr/nouveausite.html">À propos de Canada.ca</a></li>
+      <li><a href="https://www.canada.ca/fr/transparence/avis.html">Avis</a></li>
+      <li><a href="https://www.canada.ca/fr/transparence/confidentialite.html">Confidentialité</a></li>
+    `,
+  },
+};
+  
+
+module.exports = exports = {
+  index_lang_lookups,
+};
