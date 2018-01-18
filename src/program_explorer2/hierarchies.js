33a30f562b77d19d319d2677e4a47c10ee2c1eb2
Stephen-ONeil <stephenlevioneil@gmail.com>
2018-01-18 12:18:08 -0500

    By Shane's suggestion, add parent form groupings to org_info by inst form in the GaaG. Not sure if we'll keep it this way though, grouping's a bit boring.

--- src/program_explorer2/hierarchies.js
+++ src/program_explorer2/hierarchies.js
@@ -238,8 +238,8 @@ const orgs_to_inst_form_nodes = (orgs, only_orgs_with_data) => {
           description: glossary_entry_from_inst_form_type_id(type_id),
           name: InstForm.lookup(type_id).name,
           is: __type__ => __type__ === "inst_form",
-          plural:()=> text_maker("type"),
-          orgs: _.filter(orgs, org => !only_orgs_with_data || (org.tables && org.tables.length > 1)),
+          plural: ()=> text_maker("type"),
+          orgs: orgs,
         }) )
         .value()
     })
@@ -275,11 +275,26 @@ exports.create_org_info_inst_form_groups_hierarchy = function(value_attr,root_id
   return d4.hierarchy(Subject.gov,
     node => {
       if (node.is("gov")){
-        const orgs = _.chain(Subject.Ministry.get_all())
+        const inst_form_groups = _.chain(Subject.Ministry.get_all())
           .map(ministry => ministry.orgs)
           .flatten()
+          .reject("is_dead")
+          .filter(org => !only_orgs_with_data || (org.tables && org.tables.length > 1))
+          .groupBy("inst_form.parent_form.parent_form.id")
+          .map( (orgs, inst_form_group_id) => {
+            return {
+              id: inst_form_group_id,
+              name: InstForm.lookup(inst_form_group_id).name,
+              is: __type__ => __type__ === "inst_form_groups",
+              plural: ()=> text_maker("parent_form"),
+              orgs: orgs,
+            }
+          })
+          .flatten()
           .value();
-        return orgs_to_inst_form_nodes(orgs,only_orgs_with_data);
+        return inst_form_groups;
+      } else if (node.is("inst_form_groups")) {
+        return orgs_to_inst_form_nodes(node.orgs,only_orgs_with_data);
       } else if (node.is("inst_form")) {
         return node.orgs;
       }
