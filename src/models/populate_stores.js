cda74197d5f62ad218fda042f560bb6b37eeb421
AlexCLeduc <ledualex@gmail.com>
2018-02-13 11:03:21 -0500

    shut up the linter

--- src/models/populate_stores.js
+++ src/models/populate_stores.js
@@ -287,6 +287,8 @@ function populate_socr_tags(rows){
 };
 
 function populate_programs(rows){
+  //TODO what do we use is_crown for ? 
+  /* eslint-disable no-unused-vars */
   const [ dept_code, crso_id, activity_code, name, desc, is_crown, is_active, is_internal_service ] = [0,1,2,3,4,5,6,7];
   _.each(rows,row => {
     const crso = CRSO.lookup(row[crso_id]);
