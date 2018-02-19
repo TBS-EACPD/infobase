3be1607b44f895216a80a0172658838ba7440629
AlexCLeduc <ledualex@gmail.com>
2018-02-19 13:57:29 -0500

    style tweaks to about page

--- src/about/about.js
+++ src/about/about.js
@@ -7,9 +7,11 @@ const {text_maker} = require('../models/text');
 const { reactAdapter } = require('../core/reactAdapter.js');
 const { TextMaker }  = require('../util_components.js');
 
-const About = () => <div className="medium_panel_text">
-  <TextMaker el="div" text_key="about_body_text" />
-</div>;
+const About = () => (
+  <div className="medium_panel_text about-root">
+    <TextMaker el="div" text_key="about_body_text" />
+  </div>
+);
 
 
 ROUTER.add_container_route("about", "_about",function(container){
