1c69d34c77b1c7936f07ef3356901af449234354
Stephen-ONeil <stephenlevioneil@gmail.com>
2018-03-05 11:34:12 -0500

    Update GraphLegend component to stop using href='#', same as D3 graph legend changesi n previous commit

--- src/charts/declarative_charts.js
+++ src/charts/declarative_charts.js
@@ -234,15 +234,18 @@ const GraphLegend = ({
           }
           className="legend-color-checkbox"
         />
-        {React.createElement(  //only make it a link if clicking it does something
-          onClick ? 'a' : 'span',
-          { 
-            href: onClick ? "#" : null,
-            onClick: onClick ? ()=> { onClick(id) } : null,
-            role: onClick ? "button" : null, 
-          },
-          label
-        )} 
+        { onClick ?
+          <span
+            role="button"
+            tabIndex={0}
+            className="link-styled"
+            onClick={()=> onClick(id)}
+            onKeyPress={(e)=> e.charCode===13 && onClick(id)}
+          > 
+            label 
+          </span> : 
+          <span> label </span>
+        } 
       </li>
     )}
   </ul>
