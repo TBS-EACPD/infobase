f1d170075542f53590d950530ac634ca0435b281
Stephen-ONeil <stephenlevioneil@gmail.com>
2018-04-05 12:15:43 -0400

    Use LabeledBox for budget measures partition controls

--- src/partition/budget_measures_subapp/BudgetMeasuresControls.js
+++ src/partition/budget_measures_subapp/BudgetMeasuresControls.js
@@ -1,6 +1,11 @@
 import './BudgetMeasuresControls.scss';
+import './BudgetMeasuresControls.ib.yaml';
 
-import { RadioButtons } from '../../util_components.js';
+import { 
+  LabeledBox,
+  TextMaker,
+  RadioButtons,
+} from '../../util_components.js';
 
 export class BudgetMeasuresControls extends React.Component {
   constructor(){
@@ -15,15 +20,22 @@ export class BudgetMeasuresControls extends React.Component {
 
     return (
       <div className="budget-measures-partition-controls">
-        <RadioButtons
-          options={ _.map( items, ({id, display }) => ({ id, display, active: id === first_column }) ) }
-          onChange={ id => {
-            const new_path = `/budget-measures/${id}`;
-            if ( history.location.pathname !== new_path ){
-              // the first_column prop, and thus this button's active id, is updated through this route push
-              history.push(new_path);
-            }
-          }}
+        <LabeledBox 
+          label={ <TextMaker text_key="budget_measure_group_by_label" /> }
+          content={
+            <div className="centerer">
+              <RadioButtons
+                options={ _.map( items, ({id, display }) => ({ id, display, active: id === first_column }) ) }
+                onChange={ id => {
+                  const new_path = `/budget-measures/${id}`;
+                  if ( history.location.pathname !== new_path ){
+                    // the first_column prop, and thus this button's active id, is updated through this route push
+                    history.push(new_path);
+                  }
+                }}
+              />
+            </div>
+          }
         />
       </div>
     );
