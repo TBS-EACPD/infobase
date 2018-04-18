b65737c494acd1e3554d44a1f761a438a67bf4a6
Stephen-ONeil <stephenlevioneil@gmail.com>
2018-04-18 10:55:49 -0400

    Big rework of budget measure popup template, French version grammar should now be reasonable

--- src/partition/budget_measures_subapp/BudgetMeasuresPartition.js
+++ src/partition/budget_measures_subapp/BudgetMeasuresPartition.js
@@ -40,25 +40,39 @@ const get_level_headers = (first_column) => {
 const root_text_func = (displayed_measure_count, root_value) => text_maker("budget_measures_partition_root", {root_value, displayed_measure_count});
 
 const popup_template = node => {
+  const dept_is_first_column = (node.depth === 1 && node.data.type === "dept") || (node.depth === 2 && node.parent.data.type === "dept");
+  const is_dept = node.data.type === "dept";
+  const is_budget_measure = node.data.type === "budget_measure";
+
+  const is_first_column = node.depth === 1;
+
+  const dept_name = dept_is_first_column ? 
+    is_first_column ? node.data.name : node.parent.data.name :
+    !is_first_column ? node.data.name : node.parent.data.name;
+
+  const measure_name = !dept_is_first_column ? 
+    is_first_column ? node.data.name : node.parent.data.name :
+    !is_first_column ? node.data.name : node.parent.data.name;
+
   const popup_options = {
     year,
+    dept_is_first_column,
+    is_dept,
+    is_budget_measure,
+    dept_name,
+    measure_name,
+    is_first_column,
+    color: node.color,
     value: node.__value__,
     value_is_negative: node.__value__ < 0,
     value_is_zero: node.__value__ === 0,
     lang_formated_zero: window.lang === "en" ? "$0" : "0$",
-    parent_name: node.parent.data.name,
-    is_dept: node.data.type === "dept",
-    is_budget_measure: node.data.type === "budget_measure",
-    level: node.data.type,
-    id: node.data.id,
-    name: node.data.name,
-    color: node.color,
     description: !_.isUndefined(node.data.description) && !_.isEmpty(node.data.description) && node.data.description,
     notes: !_.isUndefined(node.data.notes) && !_.isEmpty(node.data.notes) && node.data.notes,
     chapter: !_.isUndefined(node.data.chapter_key) && budget_chapters[node.data.chapter_key].text,
     budget_link: !_.isUndefined(node.data.chapter_key) && node.data.chapter_key !== "oth" && !_.isEmpty(node.data.ref_id) && make_budget_link(node.data.chapter_key, node.data.ref_id),
-    is_first_column: node.depth === 1,
-    add_reference_to_parent: node.depth === 2 && node.data.type !== "net_adjust",
+    level: node.data.type,
+    id: node.data.id,
     focus_text: node.magnified ? text_maker("partition_unfocus_button") : text_maker("partition_focus_button"),
   };
   return text_maker("budget_measure_popup_template", popup_options);
