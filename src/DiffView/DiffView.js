f39e60df81c05014154c8c387cbee645510f5c49
AlexCLeduc <ledualex@gmail.com>
2018-05-11 11:47:11 -0400

    add DiffView route

---
+++ src/DiffView/DiffView.js
@@ -0,0 +1,286 @@
+import { combineReducers, createStore } from 'redux';
+import { Provider, connect } from 'react-redux';
+import { StandardRouteContainer } from '../core/NavComponents';
+import { infograph_href_template } from '../link_utils.js';
+
+import {
+  SpinnerWrapper,
+  TM,
+  FootnoteList,
+  HeightClipper,
+  Format,
+} from '../util_components.js';
+
+import { text_maker } from '../models/text.js';
+
+import { 
+  get_root,
+} from '../gen_expl/hierarchy_tools.js';
+
+
+import {
+  get_memoized_funcs,
+  initial_root_state,
+  root_reducer,
+  map_state_to_root_props_from_memoized_funcs,
+  map_dispatch_to_root_props,
+} from '../gen_expl/state_and_memoizing';
+
+import { ensure_loaded } from '../core/lazy_loader.js';
+import { Explorer } from '../components/ExplorerComponents.js';
+
+
+import {
+  estimates_diff_scheme,
+  col_defs,
+  initial_state as initial_scheme_state,
+} from './scheme.js';
+
+
+export class DevStuff extends React.Component {
+  constructor(){
+    super();
+    this.state = {loading: true};
+  }
+  componentWillMount(){
+    ensure_loaded({
+      table_keys: ["table8"],
+      footnotes_for: "estimates",
+    }).then(()=> {
+      this.setState({ loading: false });
+    })
+  }
+  render(){
+    
+    return (
+      <StandardRouteContainer
+        title={"dev"}
+        breadcrumbs={["dev"]}
+        description={"dev"}
+        route_key="_dev"
+      >
+        { this.state.loading ? 
+          <SpinnerWrapper /> :
+          <ExplorerContainer />
+        }
+      </StandardRouteContainer>
+    );
+
+
+  }
+}
+
+const map_state_to_props_from_memoized_funcs = memoized_funcs => {
+
+  const  { get_scheme_props } = memoized_funcs;
+  const mapRootStateToRootProps = map_state_to_root_props_from_memoized_funcs(memoized_funcs);
+
+  return state => _.immutate(
+    mapRootStateToRootProps(state),
+    get_scheme_props(state)
+  );
+}
+
+class ExplorerContainer extends React.Component {
+  componentWillMount(){
+    const scheme = estimates_diff_scheme;
+    const scheme_key = estimates_diff_scheme.key;
+
+    const reducer = combineReducers({
+      root: root_reducer, 
+      [scheme_key]: scheme.reducer,
+    });
+
+    const mapStateToProps = map_state_to_props_from_memoized_funcs(get_memoized_funcs([scheme]));
+
+    const mapDispatchToProps = dispatch => _.immutate(
+      map_dispatch_to_root_props(dispatch),
+      scheme.dispatch_to_props(dispatch)
+    );
+
+    const initialState = {
+      root: _.immutate(initial_root_state, {scheme_key}),
+      [scheme_key]: initial_scheme_state,
+    };
+
+    const connecter = connect(mapStateToProps, mapDispatchToProps);
+    const Container = connecter(EstimatesExplorer);
+    const store = createStore(reducer,initialState);
+
+    this.Container = Container;
+    this.store = store;
+
+  }
+  render(){
+    const { store, Container } = this;
+    return (
+      <Provider store={store}>
+        <Container />
+      </Provider>
+    );
+  }
+
+}
+
+const get_non_col_content = ({node}) => {
+  const subject = _.get(node, "data.subject");
+  const footnotes = _.get(node, "data.footnotes");
+  const last_year_mains = _.get(node, "data.last_year_mains");
+  if(!subject && _.isEmpty(footnotes)){
+    return null;
+  }
+  return (
+    <div>
+      <div>
+        Amounts appropriated in mains 2017-18 : <Format type="compact1" content={last_year_mains} />
+      </div>
+      {!_.isEmpty(footnotes) && 
+        <HeightClipper
+          allowReclip={true} 
+          clipHeight={150}
+        >
+          <header className="agnostic-header"><TM k="notes" /></header>
+          <FootnoteList
+            footnotes={_.map(footnotes, 'text')}
+          />
+        </HeightClipper>
+      }
+      { subject && 
+        <div className='ExplorerNode__BRLinkContainer'>
+          <a href={infograph_href_template(subject)}> 
+            <TM k="see_infographic" />
+          </a>
+        </div>
+      }
+    </div>
+  );
+}
+
+class EstimatesExplorer extends React.Component {
+  constructor(){
+    super()
+    this.state = { _query : "" };
+    this.debounced_set_query = _.debounce(this.debounced_set_query, 500);
+  }
+  handleQueryChange(new_query){
+    this.setState({
+      _query : new_query,
+      loading: new_query.length > 3 ? true : undefined,
+    });
+    this.debounced_set_query(new_query);
+  } 
+  debounced_set_query(new_query){
+    this.props.set_query(new_query);
+    setTimeout(()=>{
+      this.setState({
+        loading: false,
+      });
+    }, 500)
+  }
+  clearQuery(){
+    this.setState({_query : ""});
+    this.props.clear_query("");
+  }
+  render(){
+    const {
+      flat_nodes,
+      is_filtering,
+
+      set_query,
+      toggle_node,
+      
+      is_descending,
+      sort_col,
+      col_click,
+
+      //scheme props
+      show_stat,
+      toggle_stat_filter,
+    } = this.props;
+    const { loading } = this.state;
+
+    const root = get_root(flat_nodes);
+
+    const explorer_config = {
+      column_defs: col_defs,
+      onClickExpand: id => toggle_node(id),
+      is_sortable: true,
+      zebra_stripe: true,
+      col_click,
+      //un-used features...
+      get_non_col_content,
+      children_grouper: null,
+    }
+
+    return (
+      <div>
+        <div style={{marginTop: '15px'}}>
+          <div>
+            <label>
+              <input
+                type="checkbox"
+                checked={!show_stat}
+                onChange={toggle_stat_filter}
+                style={{ marginRight: '1rem' }}
+              />
+              Only show voted items
+            </label>
+          </div>
+          <form
+            style={{marginBottom: "15px"}}
+            onSubmit={evt => {
+              evt.preventDefault()
+              evt.stopPropagation()
+              set_query(evt.target.querySelector('input').value);
+              this.refs.focus_mount.focus();
+            }}
+          >
+            <input 
+              aria-label={text_maker("explorer_search_is_optional")}
+              className="form-control input-lg"
+              type="text"
+              style={{width:"100%", backgroundColor:"#fafafa"}}
+              placeholder={text_maker('everything_search_placeholder')}
+              onChange={evt => this.handleQueryChange(evt.target.value)}
+            />
+            {
+              window.is_a11y_mode &&
+              <input 
+                type="submit"
+                name="search"
+                value={text_maker("explorer_search")}
+              />
+            }
+          </form>
+        </div>
+        <div 
+          tabIndex={-1} 
+          ref="focus_mount" 
+          style={{position:'relative'}}
+          aria-label={text_maker("explorer_focus_mount")}
+        >
+          {loading && 
+            <div className="loading-overlay">
+              <div style={{height: '200px',position:'relative'}}>
+                <SpinnerWrapper scale={3} /> 
+              </div>
+            </div>
+          }
+          {is_filtering && _.isEmpty(root.children) &&
+            <div style={{fontWeight: '500', fontSize: '1.5em', textAlign:'center'}}>  
+              <TM k="search_no_results" />
+            </div>
+          }
+          <Explorer 
+            config={explorer_config}
+            root={root}
+            col_state={{
+              sort_col,
+              is_descending,
+            }}
+          />
+        </div>
+      </div>
+    );
+  }
+}
---
+++ src/DiffView/scheme.js
@@ -0,0 +1,231 @@
+import { createSelector } from 'reselect';
+import {
+  TM,
+  Format,
+} from '../util_components.js';
+import { Table } from '../core/TableClass.js';
+import { Dept } from '../models/subject.js';
+import FootNote from '../models/footnotes.js';
+
+import { convert_d3_hierarchy_to_explorer_hierarchy } from '../gen_expl/hierarchy_tools.js';
+
+
+
+
+const this_year_col = "{{est_in_year}}_estimates";
+const last_year_col = "{{est_last_year}}_estimates";
+
+
+//in this cpn, we don't care about supps
+const reduce_by_supps_dim = (rows) => _.chain(rows)
+  .groupBy( row => `${row.dept}-${row.votenum}-${row.desc}`)
+  .toPairs()
+  .map( ([_x, group]) => {
+    const [ first ] = group;
+    return {
+      dept: first.dept,
+      desc: first.desc,
+      this_year: _.sumBy(group, this_year_col) || 0,
+      last_year: _.sumBy(group, last_year_col) || 0,
+      last_year_mains: _.chain(group).filter({est_doc_code: "MAINS"}).sumBy(last_year_col).value(),
+    };
+  })
+  .value();
+
+
+function get_data(include_stat){
+  const data = _.chain(Table.lookup('table8').data)
+    .pipe(include_stat ? _.identity : rows => _.reject(rows, {votestattype: 999}))
+    .filter(obj => obj[this_year_col] || obj[last_year_col] )
+    .pipe( reduce_by_supps_dim )
+    .groupBy('dept')
+    .toPairs()
+    .map( ([org_id, rows]) => {
+
+      const org = Dept.lookup(org_id);
+      const this_year = _.sumBy(rows, "this_year") || 0;
+      const last_year  = _.sumBy(rows, "last_year") || 0;
+      const last_year_mains = _.sumBy(rows, "last_year_mains") || 0;
+      const inc = this_year-last_year;
+      const inc_pct = inc/last_year;
+
+      return {
+        id : org_id,
+        data: {
+          name: org.name,
+          subject: org,
+          this_year,
+          last_year,
+          last_year_mains,
+          inc,
+          inc_pct,
+          footnotes: FootNote.get_for_subject(
+            org,
+            [
+              "AUTH",
+              "EST_PROC",
+              "VOTED",
+              "STAT",
+            ]
+          ),
+        },
+        children: _.map(rows, row => {
+          const this_year = row["this_year"] || 0;
+          const last_year  = row["last_year"] || 0;
+          const inc = this_year-last_year;
+          const inc_pct = inc/last_year;
+
+          return {
+            id: `${org_id}-${row.desc}`,
+            data: {
+              noExpand: true, //prevents the ► character from being displayed
+              name: row.desc,
+              this_year,
+              last_year,
+              last_year_mains: row.last_year_mains,
+              inc,
+              inc_pct,
+            },
+          };
+        }),
+      };
+
+
+    })
+    .value();
+
+  const root = {
+    id: "root",
+    data: {},
+    children: data,
+  };
+
+  const d3_h7y = d3.hierarchy(root, _.property('children'));
+  return convert_d3_hierarchy_to_explorer_hierarchy(d3_h7y);
+
+}
+
+const Green = ({children}) => <span style={{color:"hsla(120, 100%, 25%, 1)"}}> {children} </span>
+const Red = ({children}) => <span style={{color:"hsla(0, 100%, 40%, 1)"}}> {children} </span>
+
+export const col_defs = [
+  {
+    id: 'name',
+    width: 250,
+    textAlign: "left",
+    header_display: <TM k="name" />,
+    get_val: ({data}) => data.name,
+  },
+  {
+    id: "this_year",
+    width: 150,
+    textAlign: "right",
+    header_display: "2018-19 Authorities",
+    get_val: node => _.get(node, "data.this_year"),
+    val_display: val => <Format type="compact1" content={val} />,
+  },
+  {
+    id: "inc",
+    width: 150,
+    textAlign: "right",
+    header_display: "Change from last year",
+    get_val: node => _.get(node, "data.inc"),
+    val_display: val => {
+      const content = <Format type="compact1" content={val} />;
+      if(val>0){
+        return <Green>{content}</Green>;
+      } else if(val === 0){
+        return content;
+      } else {
+        return <Red>{content}</Red>;
+      }
+
+    },
+  },
+  {
+    id: "inc_pct",
+    width: 150,
+    textAlign: "right",
+    header_display: "Change from last year (%)",
+    get_val: node => _.get(node, "data.inc_pct"),
+    val_display: val => {
+      let content;
+      if( Math.abs( -1-val ) < 0.001){
+        content = <strong>Old</strong>
+      } else if(val === Infinity){
+        content = <strong>New</strong>;
+      } else {
+        content= <Format type="percentage1" content={val} />;
+      }
+      if(val>0){
+        return <Green>{content}</Green>
+      } else if(val===0){
+        return content;
+      } else {
+        return <Red>{content}</Red>
+      }
+    },
+  },
+];
+
+
+const scheme_key = "estimates_diff";
+export const initial_state = {
+  sort_col: "inc",
+  is_descending: true,
+  show_stat: true,
+};
+export const estimates_diff_scheme = {
+  key: scheme_key,
+  get_sort_func_selector: () => {
+    return createSelector(
+      [
+        aug_state => aug_state[scheme_key].is_descending, 
+        aug_state => aug_state[scheme_key].sort_col, 
+      ],
+      (is_descending, sort_col) => {
+  
+        const attr_getter = _.find(col_defs, { id:sort_col }).get_val;
+  
+        return list => _.chain(list) //sort by search relevance, than the initial sort func
+          .sortBy(attr_getter)
+          .pipe( is_descending ? _.reverse : _.identity )
+          .value();
+      }
+    );
+  },
+  get_props_selector: () => augmented_state => _.clone(augmented_state.estimates_diff),
+  dispatch_to_props: dispatch => ({ 
+    col_click : col_key => dispatch({type: 'column_header_click', payload: col_key }),
+    toggle_stat_filter: ()=> dispatch({type: "toggle_stat_filter"}),
+  }),
+  reducer: (state=initial_state, action) => {
+    const { type, payload } = action;
+    if(type === "toggle_stat_filter"){
+      return _.immutate(state, {
+        show_stat: !state.show_stat,
+      });
+    }
+    if(type === 'column_header_click'){
+      const { is_descending, sort_col } = state;
+      const clicked_col = payload;
+      const mods = (
+        clicked_col === sort_col ?
+        { is_descending : !is_descending } :
+        { is_descending: true, sort_col : clicked_col }
+      );
+      return _.immutate(state, mods);
+    } else {
+      return state;
+    }
+  
+  },
+  get_base_hierarchy_selector: () => createSelector(
+    _.property("estimates_diff.show_stat"),
+    should_show_stat => get_data(should_show_stat)
+  ),
+  shouldUpdateFlatNodes(oldSchemeState, newSchemeState){
+    return oldSchemeState.show_stat !== newSchemeState.show_stat;
+  },
+};
+
