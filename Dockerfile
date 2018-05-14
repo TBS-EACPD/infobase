490ab11332948a5867d8a3a5ec7e27d0d0677111
AlexCLeduc <ledualex@gmail.com>
2018-05-14 15:22:04 -0400

    add tests and ci tests

--- Dockerfile
+++ Dockerfile
@@ -2,4 +2,8 @@ FROM google/cloud-sdk:latest
 run curl -sL https://deb.nodesource.com/setup_9.x | bash && \
     apt-get install -y nodejs && \
     apt-get install -y build-essential
-
+RUN apt-get --assume-yes install wget && \
+    wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add - && \
+    sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list' && \
+    apt-get --assume-yes update && \
+    apt-get --assume-yes install -y google-chrome-stable
---
+++ tests/fr-test.js
@@ -0,0 +1,15 @@
+import { Selector } from 'testcafe'; // first import testcafe selectors
+
+fixture `French test`// declare the fixture
+    .page `http://localhost:8080/build/InfoBase/index-fra.html#start`;  // specify the start page
+
+
+
+const financial_link_sel = "#app-focus-root > div > div > div > div > div.intro-box > div.equal-height-row.equal-height-row--home-row > div:nth-child(1) > div > a > div"
+
+//then create a test and place your code there
+test('(French) app boots and loads home page data', async t => {
+    await t
+        // Use the assertion to check if the actual header text is equal to the expected one
+        .expect(Selector(financial_link_sel).innerText).contains("246,0 milliards");
+});
