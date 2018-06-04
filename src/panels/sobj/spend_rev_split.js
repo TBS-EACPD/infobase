d0552c49c59715e3bffe1e5e0a94e97017927012
AlexCLeduc <ledualex@gmail.com>
2018-06-04 13:38:54 -0400

    move spend_rev_split to new panel/text api

--- src/panels/sobj/spend_rev_split.js
+++ src/panels/sobj/spend_rev_split.js
@@ -1,12 +1,19 @@
-import './spend_rev_split.ib.yaml';
-
+import text from './spend_rev_split.yaml';
 import {
+  create_text_maker,
   Statistics,
   formats,
-  text_maker,
+  TM as StdTM,
   PanelGraph,
-  charts_index,
+  declarative_charts,
+  StdPanel,
+  Col,
 } from "../shared";
+
+const text_maker = create_text_maker(text);
+const TM = props => <StdTM tmf={text_maker} {...props} />;
+
+const { Bar } = declarative_charts;
 
 const is_revenue = so_num => +so_num > 19;
 const last_year_col = "{{pa_last_year}}";
@@ -33,9 +40,16 @@ const rows_to_rev_split = rows => {
 };
 
 
-function render(panel,calculations, options) {
-  const { graph_args } = calculations;       
+const text_keys_by_level = {
+  dept: "dept_spend_rev_split_text",
+  program: "program_spend_rev_split_text",
+  tag: "tag_spend_rev_split_text",
+};
+
+function render({calculations, footnotes, sources}) {
+  const { graph_args, info, subject } = calculations;       
   const { neg_exp, gross_exp, net_exp } = graph_args; 
+  
   const  series = { "": [ gross_exp, neg_exp] };
   const  _ticks = [ 'gross', 'revenues'  ];
   // if neg_exp is 0, then no point in showing the net bar
@@ -45,42 +59,53 @@ function render(panel,calculations, options) {
   }
   const ticks = _ticks.map(text_maker);
 
+  let graph_content;
   if(window.is_a11y_mode){
     //all information is contained in text
-    return;
+    graph_content = null;
   } else {
-    new charts_index.BAR.bar(panel.areas().graph.node(),{
-      series,
-      ticks,
-      add_xaxis : true,
-      add_yaxis : false,
-      add_labels : true,                                  
-      x_axis_line : true,                                
-      colors : infobase_colors(),
-      formater : formats.compact1_raw,
-      margin : {top: 20, right:20, left: 60, bottom: 80} ,
-    }).render();
+    graph_content = (
+      <Bar
+        {...{
+          series,
+          ticks,
+          add_xaxis : true,
+          add_yaxis : false,
+          add_labels : true,                                  
+          x_axis_line : true,                                
+          colors : infobase_colors(),
+          formater : formats.compact1_raw,
+          margin : {top: 20, right:20, left: 60, bottom: 80},
+        }}
+      />
+    );
+      
   }
+
+  return (
+    <StdPanel
+      title={text_maker("spend_rev_split_title")}
+      {...{footnotes,sources}}
+    >
+      <Col size={5} isText>
+        <TM k={text_keys_by_level[subject.level]}  args={info} />
+      </Col>
+      <Col size={7} isGraph>
+        {graph_content}
+      </Col>
+    </StdPanel>
+  );
   
 }
 
 const key = "spend_rev_split";
-const title = "spend_rev_split_title";
-const layout =  {
-  full: {text: 5, graph: 7},
-  half : {text: 12, graph: 12},
-};
 
 new PanelGraph({
-  is_old_api: true,
   key,
   depends_on : ["table4","table5"],
-  layout,
   footnotes : false,
   level : "dept",
   info_deps : ["table5_dept_info","table4_dept_info"],
-  title,
-  text :  "dept_spend_rev_split_text",
 
   calculate(subject,info,options){
     if ( info.dept_pa_last_year_rev === 0 ){
@@ -164,15 +189,10 @@ Statistics.create_and_register({
 });
 
 new PanelGraph({
-  is_old_api: true,
   key,
   depends_on : ["table305"],
   info_deps : ["program_revenue"],
-  layout,
   level : "program",
-  title,
-  text :   "program_spend_rev_split_text",
-
   calculate(subject,info,options){ 
     const {table305} = this.tables;
     const prog_rows = table305.programs.get(subject);
@@ -187,15 +207,10 @@ new PanelGraph({
 });
 
 new PanelGraph({
-  is_old_api: true,
   key,
   depends_on : ["table305"],
-  layout,
   level : "tag",
   info_deps : ["tag_revenue"],
-  title,
-  text :   "tag_spend_rev_split_text",
-
   calculate(subject,info,options){
     const {table305} = this.tables;
     const prog_rows =  table305.q(subject).data;
