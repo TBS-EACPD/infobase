540a8194b0a6fb35f33703ae830d234d65dbc441
AlexCLeduc <ledualex@gmail.com>
2018-06-04 12:41:24 -0400

    simplify and get rid of old panel

--- src/panels/sobj/spend_by_so_hist.js
+++ src/panels/sobj/spend_by_so_hist.js
@@ -13,90 +13,71 @@ const { sos } = business_constants;
 const { std_years } = years;
 const { Format } = util_components;
 
-function spend_by_so_hist_render (panel,calculations){
-  const { graph_args } = calculations;
-  const {ticks, data} = graph_args;
-  const legend_title = "so";
 
-  if(window.is_a11y_mode){
-    charts_index.create_a11y_table({
-      container: panel.areas().graph,
-      data: data.map( ({label, data}) => ({
-        label,
-        /* eslint-disable react/jsx-key */
-        data: data.map(amt => <Format type="compact1" content={amt} />),
-      })),
-      label_col_header: text_maker("so"),
-      data_col_headers: ticks, 
-    })
-  } else {
-    charts_index.create_graph_with_legend.call({panel},{
-      get_data: _.property('data'), 
-      data,
-      ticks,
-      y_axis : "$",
-      yaxis_formatter: formats.compact1_raw,
-      sort_data: false,
-      legend_title: legend_title,
-    });
-  }
+new PanelGraph({
+  is_old_api: true,
+  level: "dept",
+  key : "spend_by_so_hist",
+  depends_on: ['table5'],
+  footnotes : [ "SOBJ", "EXP"],
 
+  info_deps: [ 'table5_dept_info', 'table5_gov_info' ],
+  layout : {
+    full : {text : 12, graph: [4,8]},
+    half: {text : 12, graph: [12,12]},
+  },
+
+  title: "dept_fin_spend_by_so_hist_title",
+  text: "dept_fin_spend_by_so_hist_text",
+
+  calculate (subject,info){
+    const {table5} = this.tables;
+    return  {
+      data: (
+        _.chain(sos)
+          .sortBy(sobj => sobj.so_num )
+          .map(sobj => 
+            ({
+              "label": sobj.text,
+              "data": std_years.map( year => table5.so_num(year,subject)[sobj.so_num]),
+              "active": false,
+            })
+          )
+          .filter(d => d3.sum(d.data) )
+          .value()
+      ),
+      ticks: info.last_years,
+    };
+  },
+  render(panel,calculations){
+    const { graph_args } = calculations;
+    const {ticks, data} = graph_args;
+    const legend_title = "so";
   
-};      
-
-_.each(['gov', 'dept'], lvl => {
-  new PanelGraph({
-    is_old_api: true,
-    level: lvl,
-    key : "spend_by_so_hist",
-    depends_on: ['table5'],
-    footnotes : [ "SOBJ", "EXP"],
-
-    info_deps: (
-      lvl === 'dept' ?  
-      [ 'table5_dept_info', 'table5_gov_info' ] : 
-      ['table5_gov_info']
-    ),
-
-    height : 375,
-
-    layout : {
-      full : {text : 12, graph: [4,8]},
-      half: {text : 12, graph: [12,12]},
-    },
-
-    title : (
-      lvl === 'dept' ? 
-      "dept_fin_spend_by_so_hist_title"  : 
-      "gov_fin_spend_by_so_hist_title"
-    ),
-
-    text : (
-      lvl === 'dept' ? 
-       "dept_fin_spend_by_so_hist_text" :
-       "gov_fin_spend_by_so_hist_text"
-    ),
-
-    calculate (subject,info){
-      const {table5} = this.tables;
-      return  {
-        data: (
-          _.chain(sos)
-            .sortBy(sobj => sobj.so_num )
-            .map(sobj => 
-              ({
-                "label": sobj.text,
-                "data": std_years.map( year => table5.so_num(year,subject)[sobj.so_num]),
-                "active": false,
-              })
-            )
-            .filter(d => d3.sum(d.data) )
-            .value()
-        ),
-        ticks: info.last_years,
-      };
-    },
-
-    render : spend_by_so_hist_render,
-  });
+    if(window.is_a11y_mode){
+      charts_index.create_a11y_table({
+        container: panel.areas().graph,
+        data: data.map( ({label, data}) => ({
+          label,
+          /* eslint-disable react/jsx-key */
+          data: data.map(amt => <Format type="compact1" content={amt} />),
+        })),
+        label_col_header: text_maker("so"),
+        data_col_headers: ticks, 
+      })
+    } else {
+      charts_index.create_graph_with_legend.call({panel},{
+        get_data: _.property('data'), 
+        data,
+        ticks,
+        y_axis : "$",
+        yaxis_formatter: formats.compact1_raw,
+        sort_data: false,
+        legend_title: legend_title,
+      });
+    }
+  
+    
+  },   
+  
 });
