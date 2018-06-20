86d4a4d2bc1c83c7fc9a42a0c86b0e61718006d3
Stephen-ONeil <stephenlevioneil@gmail.com>
2018-06-20 12:43:39 -0400

    Add Selector(...).exists to browser tests, should properly fail when target is missing on CI (I think?)

--- tests/fr-test.js
+++ tests/fr-test.js
@@ -1,7 +1,7 @@
 import { Selector } from 'testcafe'; // first import testcafe selectors
 
 fixture `French test`// declare the fixture
-    .page `http://localhost:8080/build/InfoBase/index-fra.html#start`;  // specify the start page
+  .page `http://localhost:8080/build/InfoBase/index-fra.html#start`;  // specify the start page
 
 
 
@@ -9,7 +9,10 @@ const financial_link_sel = "#app-focus-root > div > div > div > div > div.intro-
 
 //then create a test and place your code there
 test('(French) app boots and loads home page data', async t => {
-    await t
-        // Use the assertion to check if the actual header text is equal to the expected one
-        .expect(Selector(financial_link_sel).innerText).contains("246,0 milliards");
+  const target_selector = Selector(financial_link_sel);
+  await t
+    // First assure target exists, just testing innerText seems faulty in container when element doesn't exist
+    .expect(target_selector.exists).ok()
+    // Use the assertion to check if the actual target text is equal to the expected one
+    .expect(target_selector.innerText).contains("246,0 milliards");
 });
--- tests/test.js
+++ tests/test.js
@@ -1,7 +1,7 @@
 import { Selector } from 'testcafe'; // first import testcafe selectors
 
 fixture `Main app tests`// declare the fixture
-    .page `http://localhost:8080/build/InfoBase/index-eng.html#start`;  // specify the start page
+  .page `http://localhost:8080/build/InfoBase/index-eng.html#start`;  // specify the start page
 
 
 
@@ -9,7 +9,10 @@ const financial_link_sel = "#app-focus-root > div > div > div > div > div.intro-
 
 //then create a test and place your code there
 test('App boots and loads home page data', async t => {
-    await t
-        // Use the assertion to check if the actual header text is equal to the expected one
-        .expect(Selector(financial_link_sel).innerText).contains("$246.0");
+  const target_selector = Selector(financial_link_sel);
+  await t
+    // First assure target exists, just testing innerText seems faulty in container when element doesn't exist
+    .expect(target_selector.exists).ok()
+    // Use the assertion to check if the actual target text is equal to the expected one
+    .expect(target_selector.innerText).contains("$246.0");
 });
