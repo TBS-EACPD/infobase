162009e59cd719ea1fe65e1976b816cac2944f21
Stephen-ONeil <stephenlevioneil@gmail.com>
2018-06-22 12:54:04 -0400

    Fix (?) home page flag line width being bad on mobile, try updating browser tests

--- tests/fr-test.js
+++ tests/fr-test.js
@@ -5,11 +5,12 @@ fixture `French test`// declare the fixture
 
 
 
-const financial_link_sel = "#app-focus-root > div > div > div > div > div.intro-box > div.equal-height-row.equal-height-row--home-row > div:nth-child(1) > div > a > div > div > div.h-img-card__text"
+const financial_link_sel = "#app-focus-root > div > div > div.outer-container-escape-hatch > " +
+  "div.inner-container-escape-hatch > div.home-root > div.intro-box > div.container > h2 > span";
 
 //then create a test and place your code there
 test('(French) app boots and loads home page data', async t => {
     await t
-        // Use the assertion to check if the actual header text is equal to the expected one
-        .expect(Selector(financial_link_sel).innerText).contains("246,0 milliards");
+       // Use the assertion to check if the actual header text is equal to the expected one
+       .expect(Selector(financial_link_sel).innerText).contains("Traduit");
 });
--- tests/test.js
+++ tests/test.js
@@ -1,15 +1,17 @@
 import { Selector } from 'testcafe'; // first import testcafe selectors
 
 fixture `Main app tests`// declare the fixture
-    .page `http://localhost:8080/build/InfoBase/index-eng.html#start`;  // specify the start page
+  .page `http://localhost:8080/build/InfoBase/index-eng.html#start`;  // specify the start page
 
 
 
-const financial_link_sel = "#app-focus-root > div > div > div > div > div.intro-box > div.equal-height-row.equal-height-row--home-row > div:nth-child(1) > div > a > div > div > div.h-img-card__text"
+const financial_link_sel = = "#app-focus-root > div > div > div.outer-container-escape-hatch > " +
+  "div.inner-container-escape-hatch > div.home-root > div.intro-box > div.container > h2 > span";
+
 
 //then create a test and place your code there
 test('App boots and loads home page data', async t => {
-    await t
-        // Use the assertion to check if the actual header text is equal to the expected one
-        .expect(Selector(financial_link_sel).innerText).contains("$246.0");
+  await t
+    // Use the assertion to check if the actual header text is equal to the expected one
+    .expect(Selector(financial_link_sel).innerText).contains("Transforming");
 });
