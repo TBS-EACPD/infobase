8ffac632ef3cd0911f9128adefdb5bec8d26c9df
Stephen-ONeil <stephenlevioneil@gmail.com>
2018-07-16 15:15:45 -0400

    Also need no-cache on index html files

--- deploy_build_scripts/push_to_gcloud_bucket.sh
+++ deploy_build_scripts/push_to_gcloud_bucket.sh
@@ -18,5 +18,6 @@ gsutil -m -h Content-Encoding:gzip rsync -d -a public-read -c -r gzip/InfoBase $
 gsutil -m rsync -a public-read -c -r non-gzip/InfoBase $GCLOUD_BUCKET_URL
 #This will make sure any new files immediately get public read access
 
+gsutil setmeta -h "Cache-Control:no-cache" $GCLOUD_BUCKET_URL/index-*.html
 gsutil setmeta -h "Cache-Control:no-cache" $GCLOUD_BUCKET_URL/app/a*-[ef][nr].min.js
-# set no-cache on main .js files, always needs to be fresh to guarantee the rest of the cache-busting will work as intended
+# set no-cache on html and js entry files, always needs to be fresh to guarantee the rest of the cache-busting will work as intended
