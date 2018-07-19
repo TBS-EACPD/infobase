83db1eb66324be61e40ad6f08e7e220c0f8187e2
Stephen-ONeil <stephenlevioneil@gmail.com>
2018-07-19 14:04:30 -0400

    Duh, forgot InsertRuntimeFooterLinks' render was going to get called on each route change, was inserting footer links over and over.

--- src/core/InsertRuntimeFooterLinks.js
+++ src/core/InsertRuntimeFooterLinks.js
@@ -8,15 +8,21 @@ const footer_link_items = [
 ];
 
 export class InsertRuntimeFooterLinks extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      static_footer_links: document.querySelector('#footer_survey_link_area').innerHTML,
+    }
+  }
   render(){
     const footer_survey_link_ul = document.querySelector('#footer_survey_link_area');
-
+  
     const links_to_insert = _.chain(footer_link_items)
       .map( link_item => `<li><a href="${link_item.href}">${link_item.text}</a></li>`)
-      .reduce( (memo, link_html) => memo + link_html, "")
+      .reduce( (memo, link_html) => memo + link_html, this.state.static_footer_links)
       .value();
-
-    footer_survey_link_ul.innerHTML += links_to_insert;
+  
+    footer_survey_link_ul.innerHTML = links_to_insert;
 
     return null;
   }
