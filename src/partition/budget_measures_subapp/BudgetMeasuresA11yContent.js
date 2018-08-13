86a9d721f9c858d82624ff8180644c53808b15e4
Stephen-ONeil <stephenlevioneil@gmail.com>
2018-08-13 11:27:48 -0400

    Start switching budget tracker a11y table to dispaly the overview hierarchy

--- src/partition/budget_measures_subapp/BudgetMeasuresA11yContent.js
+++ src/partition/budget_measures_subapp/BudgetMeasuresA11yContent.js
@@ -1,5 +1,4 @@
 import { formats } from '../../core/format.js';
-import { run_template } from '../../models/text.js';
 import {
   text_maker,
   TextMaker,
@@ -8,136 +7,120 @@ import { budget_measures_hierarchy_factory } from './budget_measures_hierarchy_f
 import { businessConstants } from '../../models/businessConstants.js';
 import { Subject } from '../../models/subject';
 
-const { budget_chapters } = businessConstants;
+const { budget_values } = businessConstants;
 const { BudgetMeasure } = Subject;
-const year = run_template("{{planning_year_2}}");
 
-const name_and_value_cell_formatter = node => {
-  const in_billions = node.value >= 1*Math.pow(10,9);
+const value_formatter = value => {
+  const in_billions = value >= 1*Math.pow(10,9);
   const format = in_billions ? formats.compact1 : formats.compact;
-  return node.data.name + " (" + format(node.value, {raw: true}) + ")";
+  return format(value, {raw: true});
 }
+const name_and_value_cell_formatter = node => `${node.data.name} (${value_formatter(node.value)})`;
 
 export function BudgetMeasuresA11yContent(){
-  const hierarchical_budget_measures_data = budget_measures_hierarchy_factory("funding","budget-measure", []);
-
-  const budget_measures_data_by_chapter = _.groupBy(
-    hierarchical_budget_measures_data.children, 
-    budget_measure_node => budget_measure_node.data.chapter_key
-  );
-
+  const hierarchical_budget_measures_overview = budget_measures_hierarchy_factory("overview","budget-measure", []);
+  
   const ordered_col_header_text_keys = [
     "budget_measures",
     "budget_measure_descriptions",
     "budget_measure_link_header",
-    "orgs_funded_by_budget_measure",
+    "funding_decisions_header",
+    //"program_allocations",
   ];
 
   return (
     <div style={{overflow: "auto"}}>
-      {
-        _.map(_.keys(budget_chapters), chapter_key => {
-          const budget_measures_for_chapter = budget_measures_data_by_chapter[chapter_key];
-  
-          return <table 
-            className="table table-striped table-bordered" 
-            key={chapter_key}
-          >
-            <caption>
-              <TextMaker 
-                text_key="budget_measures_partition_a11y_chapter_table_caption" 
-                args={{
-                  value: _.reduce(budget_measures_for_chapter, (sum, child_node) => sum + child_node.value, 0), 
-                  chapter: budget_chapters[chapter_key].text,
-                  year,
-                }} 
-              />
-              <br/>
-              <TextMaker text_key="notes"/> <TextMaker text_key="budget_measure_description_values_clarification"/>
-            </caption>
-            <thead>
-              <tr>
-                {_.map(ordered_col_header_text_keys, col_text_key =>
-                  <th 
-                    scope="col"
-                    key={ col_text_key }
-                  >
-                    { text_maker(col_text_key) }
-                  </th>
-                )}
-              </tr>
-            </thead>
-            <tbody>
-              {
-                _.map(budget_measures_for_chapter, budget_measure => {
-                  const has_children = !_.isUndefined(budget_measure.children) && budget_measure.children.length > 0;
-                  const rows_to_span = has_children ? budget_measure.children.length : 1;
+      <table className="table table-striped table-bordered" >
+        <caption>
+          <TextMaker text_key="budget_measures_partition_a11y_chapter_table_caption" />
+          <br/>
+          <TextMaker text_key="notes"/>: <TextMaker text_key="budget_measure_description_values_clarification"/>
+        </caption>
+        <thead>
+          <tr>
+            {_.map(ordered_col_header_text_keys, col_text_key =>
+              <th 
+                scope="col"
+                key={ col_text_key }
+              >
+                { text_maker(col_text_key) }
+              </th>
+            )}
+          </tr>
+        </thead>
+        <tbody>
+          {
+            _.map(hierarchical_budget_measures_overview.children, budget_measure => {
+              const has_children = !_.isUndefined(budget_measure.children) && budget_measure.children.length > 0;
+              const rows_to_span = has_children ? budget_measure.children.length : 1;
 
-                  const main_row = <tr key={ "measure" + budget_measure.data.id }>
-                    <th
-                      scope="row"
-                      rowSpan={ rows_to_span }
-                    >
-                      { name_and_value_cell_formatter(budget_measure) }
-                    </th>
-                    <td
-                      key={ "measure_description" + budget_measure.data.id }
-                      rowSpan={ rows_to_span }
-                    >
-                      { !_.isEmpty(budget_measure.data.description) && 
-                        <div dangerouslySetInnerHTML={{__html: budget_measure.data.description}} />
-                      }
-                    </td>
-                    <td
-                      key={ "measure_link" + budget_measure.data.id }
-                      rowSpan={ rows_to_span }
-                    >
-                      { ( (budget_measure.data.chapter_key === "oth" && budget_measure.data.type !== "net_adjust") || !_.isEmpty(budget_measure.data.ref_id) ) && 
-                        <a
-                          href = { BudgetMeasure.make_budget_link(budget_measure.data.chapter_key, budget_measure.data.ref_id) }
-                        >
-                          { text_maker("link") }
-                        </a>
-                      }
-                      { budget_measure.data.chapter_key !== "oth" && _.isEmpty(budget_measure.data.ref_id) && 
-                        text_maker("not_found_in_budget_text") 
-                      }
-                    </td>
-                    { has_children &&
-                      <td 
-                        key={ "measure" + budget_measure.data.id + "-org" + budget_measure.children[0].data.id }
-                      >
-                        { name_and_value_cell_formatter(budget_measure.children[0]) }
-                      </td>
-                    }
-                    { !has_children && <td></td> }
-                  </tr>;
-  
-                  if ( !has_children || budget_measure.children.length === 1 ){
-                    return main_row;
-                  } else {
-                    const sub_rows = _.chain(budget_measure.children)
-                      .tail()
-                      .map( org => 
-                        <tr key={ "measure" + budget_measure.data.id + "-org" + org.data.id }>
-                          <td>
-                            { name_and_value_cell_formatter(org) }
-                          </td>
-                        </tr>
-                      )
-                      .value();
-  
-                    return [
-                      main_row,
-                      ...sub_rows,
-                    ];
+              const main_row = <tr key={ "measure" + budget_measure.data.id }>
+                <th
+                  scope="row"
+                  rowSpan={ rows_to_span }
+                >
+                  { name_and_value_cell_formatter(budget_measure) }
+                </th>
+                <td
+                  key={ "measure_description" + budget_measure.data.id }
+                  rowSpan={ rows_to_span }
+                >
+                  { !_.isEmpty(budget_measure.data.description) && 
+                    <div dangerouslySetInnerHTML={{__html: budget_measure.data.description}} />
                   }
-                })
+                </td>
+                <td
+                  key={ "measure_link" + budget_measure.data.id }
+                  rowSpan={ rows_to_span }
+                >
+                  { ( (budget_measure.data.chapter_key === "oth" && budget_measure.data.type !== "net_adjust") || !_.isEmpty(budget_measure.data.ref_id) ) && 
+                    <a
+                      href = { BudgetMeasure.make_budget_link(budget_measure.data.chapter_key, budget_measure.data.ref_id) }
+                    >
+                      { text_maker("link") }
+                    </a>
+                  }
+                  { budget_measure.data.chapter_key !== "oth" && _.isEmpty(budget_measure.data.ref_id) && 
+                    text_maker("not_found_in_budget_text") 
+                  }
+                </td>
+                { has_children &&
+                  <td 
+                    key={ "measure" + budget_measure.data.id + "-org" + budget_measure.children[0].data.id }
+                  >
+                    { name_and_value_cell_formatter(budget_measure.children[0]) }
+                  </td>
+                }
+                { !has_children && 
+                  <td>
+                    { `${budget_values.remaining.text} (${value_formatter(budget_measure.value)})` }
+                  </td> 
+                }
+              </tr>;
+  
+              if ( !has_children || budget_measure.children.length === 1 ){
+                return main_row;
+              } else {
+                const sub_rows = _.chain(budget_measure.children)
+                  .tail()
+                  .map( org => 
+                    <tr key={ "measure" + budget_measure.data.id + "-org" + org.data.id }>
+                      <td>
+                        { name_and_value_cell_formatter(org) }
+                      </td>
+                    </tr>
+                  )
+                  .value();
+  
+                return [
+                  main_row,
+                  ...sub_rows,
+                ];
               }
-            </tbody>
-          </table>
-        }) 
-      }
+            })
+          }
+        </tbody>
+      </table>
     </div>
   );
 }
