9086dc06d0f8f361fda70ab2569e6347f7b758c2
Ellen Li <eli030@uottawa.ca>
2018-08-13 14:33:19 -0400

    work

--- src/rpb/rpb_text_provider.js
+++ src/rpb/rpb_text_provider.js
@@ -2,8 +2,8 @@
 import rpb_text from './rpb.yaml';
 import { 
   create_tm_cmpnt,
-  TrivialTextMaker,
+  TextMaker as StandardTextMaker
 } from '../util_components.js';
 
 export const [ text_maker, TM ] = create_tm_cmpnt(rpb_text);
-export const TextMaker = TrivialTextMaker;
+export const TextMaker = props => <StandardTextMaker text_maker_func={text_maker} {...props} />;
