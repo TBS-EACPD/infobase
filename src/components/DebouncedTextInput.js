df8374f8ce5bd357653edd5b2338a9e18469ef5f
Stephen-ONeil <stephenlevioneil@gmail.com>
2018-08-15 14:30:43 -0400

    Have DebouncedTextInput component cancel its debounced callbacks on unmount. Also fix it so it preoperly accepts and uses a debounceTime prop with a 500ms default

--- src/components/DebouncedTextInput.js
+++ src/components/DebouncedTextInput.js
@@ -1,23 +1,32 @@
-const DebouncedTextInput = ({
-  placeHolder,
-  defaultValue,
-  debounce_time,
-  updateCallback,
-}) => {
-  const debounced_callback = _.debounce(event => updateCallback(event.target.value), 500);
-  const handle_change = event => {
-    event.persist();
-    debounced_callback(event);
-  }
+class DebouncedTextInput extends React.Component {
+  render(){
+    const {
+      placeHolder,
+      defaultValue,
+      debounceTime,
+      updateCallback,
+    } = this.props;
 
-  return <input  
-    type = "text"
-    className = "form-control search"
-    placeholder = { placeHolder || "" }
-    defaultValue = { defaultValue || undefined }
-    onChange = { handle_change }
-  >
-  </input>
-};
+    this.debounced_callback = _.debounce(event => updateCallback(event.target.value), debounceTime);
+    const handle_change = event => {
+      event.persist();
+      this.debounced_callback(event);
+    }
+
+    return <input  
+      type = "text"
+      className = "form-control search"
+      placeholder = { placeHolder || "" }
+      defaultValue = { defaultValue || undefined }
+      onChange = { handle_change }
+    >
+    </input>;
+  }
+  componentWillUnmount(){
+    !_.isUndefined(this.debounced_callback) && this.debounced_callback.cancel();
+  }
+}
+
+DebouncedTextInput.defaultProps= {debounceTime: 500};
 
 export { DebouncedTextInput }
