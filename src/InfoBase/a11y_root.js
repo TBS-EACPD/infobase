8fc5ff539ca52338706a2ca928380f2726af16e9
Stephen-ONeil <stephenlevioneil@gmail.com>
2018-09-28 14:56:39 -0400

    Remove jQuery, just leaving a dummy file in external-dependencies until the prod index html files are updated

--- src/InfoBase/a11y_root.js
+++ src/InfoBase/a11y_root.js
@@ -1,11 +1,9 @@
 import pre_load from './pre_load';
 
-$(()=>{
-  window.is_a11y_mode = true;
-  const kill_spinner = pre_load();
-  import('./bootstrap.js').then( ({bootstrap}) => {
-    import('./A11Y_App.js').then( ({App, app_reducer }) => {
-      bootstrap(App, app_reducer, kill_spinner);
-    });
+window.is_a11y_mode = true;
+const kill_spinner = pre_load();
+import('./bootstrap.js').then( ({bootstrap}) => {
+  import('./A11Y_App.js').then( ({App, app_reducer }) => {
+    bootstrap(App, app_reducer, kill_spinner);
   });
 });
