43861c0b5150cb200534f8c3204697afc999be9b
Stephen-ONeil <stephenlevioneil@gmail.com>
2018-10-05 13:44:42 -0400

    Have ComponentLoader attempt to catch errors due to outdated client code by comparing the client sha to the CDN sha and reloading the page without cache if they do not match. Otherwise, displays error component

--- src/core/ComponentLoader.js
+++ src/core/ComponentLoader.js
@@ -1,35 +1,55 @@
+import { get_static_url, make_request } from './request_utils.js';
+
 import { SpinnerWrapper } from '../util_components.js';
 
 
-const DefaultErrorComponent = _.constant(
-  window.lang === "en" ? 
-  "An error occured" :
-  "Une erreur est survenue"
-);
+class DefaultErrorComponent extends React.Component {
+  render(){
+    return (
+      window.lang === "en" ? 
+        "An error occured" :
+        "Une erreur est survenue"
+    );
+  }
+}
 
 export const ComponentLoader = (get_component, LoadingElement, errorElement)  => class ComponentLoader_ extends React.Component {
   constructor(){
     super();
     this.state = { loading: true };
 
-    get_component().then( Component => {
-      
-      this.Component = Component;
-      this.timedOutStateChange = setTimeout(()=>{ //less janky if we force a timeout
-        this.setState({loading: false});
-      }, 250);
+    get_component()
+      .then( Component => {
+        
+        this.Component = Component;
+        this.timedOutStateChange = setTimeout(()=>{ //less janky if we force a timeout
+          this.setState({loading: false});
+        }, 250);
+  
+      })
+      .catch( (error) => {
+  
+        this.Component = (
+          errorElement ?
+            () => errorElement :
+            DefaultErrorComponent
+        );
 
-    }).catch(()=>{
+        // Attempts to identify and handle most common production error case, where the client's out of sync with the CDN content
+        // Does this by compairing the client's sha to the CDN's build sha
+        // Reloads the page if the sha's are mismatched, otherwise display the error component
+        const unique_query_param = Date.now() + Math.random();
+        make_request( get_static_url('build_sha', unique_query_param) )
+          .then( build_sha => {
+            const local_sha_matches_remote_sha = build_sha.search(`^${window.sha}`) !== -1;
 
-      this.Component = (
-        errorElement ?
-        () => errorElement :
-        DefaultErrorComponent
-      );
-      this.setState({loading: false});
-    })
-    
-
+            if (local_sha_matches_remote_sha){
+              this.setState({loading: false}); // Display error component
+            } else {
+              window.location.reload(true); // Force reload without cache
+            }
+          })
+      });
   }
   componentWillUnmount(){
     clearTimeout(this.timedOutStateChange);
--- src/core/request_utils.js
+++ src/core/request_utils.js
@@ -1,8 +1,10 @@
 const static_url = CDN_URL;
 
 //no URL should start with "./" or "/"
-export const get_static_url =  url => {
-  return `${static_url}/${url}?v=${window.sha}`
+export const get_static_url = (url, version_query) => {
+  const query_string = version_query || window.sha;
+
+  return `${static_url}/${url}?v=${query_string}`;
 }
 
 export const make_request = url => fetch(
