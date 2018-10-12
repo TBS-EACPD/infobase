485188d80991cf4cc3bf908a85b88a5bf66c3a2e
Stephen-ONeil <stephenlevioneil@gmail.com>
2018-10-12 17:57:22 -0400

    Finish up budget tracker footer

--- src/partition/budget_measures_subapp/BudgetMeasuresFooter.js
+++ src/partition/budget_measures_subapp/BudgetMeasuresFooter.js
@@ -1,21 +1,48 @@
 import './BudgetMeasuresFooter.scss';
 
-import {
-  text_maker,
-  TextMaker,
-} from './budget_measure_text_provider.js';
+import { TextMaker } from './budget_measure_text_provider.js';
 
-import { LabeledBox } from '../../util_components.js';
+import { 
+  LabeledBox, 
+  FancyUL,
+} from '../../util_components.js';
+
+import { sources } from '../../metadata/data_sources.js';
+
+const { BUDGET: budget_source } = sources;
+
+const FancyULBudgetSourceRow = (source_item, open_data_link) => (
+  <span key={source_item.id} className="fancy-ul-span-flex">
+    <a href={"#metadata/BUDGET"}>
+      {source_item.text}
+    </a>
+    <a 
+      target="_blank" 
+      className="btn btn-xs btn-ib-primary btn-responsive-fixed-width" 
+      href={open_data_link}>
+      <TextMaker text_key="open_data_link"/>
+    </a>
+  </span>
+);
 
 export class BudgetMeasuresFooter extends React.Component {
   render(){
+    const open_data_link = budget_source.open_data[lang];
+    
     return (
       <div className="budget-measures-partition-footer">
         <LabeledBox 
           label = { <TextMaker text_key="data_sources" /> }
           content = {
-            <div>
-              { "TODO" }
+            <div className="budget-measure-fancy-ul-container">
+              <FancyUL>
+                {
+                  _.map(
+                    budget_source.items(),
+                    (source_item) => FancyULBudgetSourceRow(source_item, open_data_link)
+                  )
+                }
+              </FancyUL>
             </div>
           }
         />
