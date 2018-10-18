4239ae12025eea09c5187f836135a335c23652b1
Stephen-ONeil <stephenlevioneil@gmail.com>
2018-10-18 11:17:37 -0400

    In prod_deploy_script, move exporting of CLOUDFLARE_KEY to earlier in the process, since it's a pain to have the lpass password dialog popup again right before the final step. Meant babysitting the deploy all the way through

--- deploy_build_scripts/prod_deploy_script.sh
+++ deploy_build_scripts/prod_deploy_script.sh
@@ -10,14 +10,16 @@ function cleanup {
 }
 trap cleanup EXIT
 
+export CDN_URL="https://cdn-rdc.ea-ad.ca/InfoBase"
+export GCLOUD_BUCKET_URL="gs://cdn-rdc.ea-ad.ca/InfoBase"
+
+export CLOUDFLARE_KEY=$(lpass show CLOUDFLARE_KEY --notes)
 echo $(lpass show IB_SERVICE_KEY --notes) | base64 -D > $scratch/key.json
+
 gcloud auth activate-service-account --key-file=$scratch/key.json
 
 gcloud config set project infobase-prod
 gcloud config set compute/zone northamerica-northeast1-a
-
-export CDN_URL="https://cdn-rdc.ea-ad.ca/InfoBase"
-export GCLOUD_BUCKET_URL="gs://cdn-rdc.ea-ad.ca/InfoBase"
 
 #build everything
 ./deploy_build_scripts/build_all.sh
@@ -26,7 +28,6 @@ export GCLOUD_BUCKET_URL="gs://cdn-rdc.ea-ad.ca/InfoBase"
 ./deploy_build_scripts/push_to_gcloud_bucket.sh
 
 #clear certain cloudflare caches
-export CLOUDFLARE_KEY=$(lpass show CLOUDFLARE_KEY --notes)
 ./deploy_build_scripts/selectively_clear_cloudflare_cache.sh
 
 cleanup
