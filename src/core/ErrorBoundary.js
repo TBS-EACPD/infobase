4cead008db323256aba1a41b2a0948648c1b06dc
Stephen-ONeil <stephenlevioneil@gmail.com>
2018-10-25 14:43:39 -0400

    Clean up ErrorBoundary a bit

--- src/core/ErrorBoundary.js
+++ src/core/ErrorBoundary.js
@@ -7,39 +7,53 @@ export class ErrorBoundary extends React.Component {
 
     this.state = {
       has_error: false,
-    }
+      testing_for_stale_client: false,
+    };
   }
   static getDerivedStateFromError(error) {
-    return { hasError: true };
-  }
-  componentDidCatch(error, info) {
-    this.handle_stale_client_error_case();
+    return {
+      has_error: true,
+      testing_for_stale_client: true,
+    };
   }
   handle_stale_client_error_case(){
     const unique_query_param = Date.now() + Math.random().toString().replace('.','');
 
     // Stale clients are our most likely production errors, always check for and attempt to handle them
-    // Reloads the page if the client/CDN sha's are mismatched, otherwise displays the error component
+    // That is, reload the page without cache if the client/CDN sha's are mismatched (and the build is non-DEV)
+    // Otherwise, log the error (again, if non-DEV) and display error component
     make_request( get_static_url('build_sha', unique_query_param) )
       .then( build_sha => {
         const local_sha_matches_remote_sha = build_sha.search(`^${window.sha}`) !== -1;
     
         if (!local_sha_matches_remote_sha && !DEV) {
-          window.location.reload(true); // Force reload without cache, unless in dev
+          window.location.reload(true);
+        } else {
+          this.log_error_and_display_error_page();
         }
       })
       .catch( () => {
-        if (!DEV){
-          log_standard_event({
-            SUBAPP: window.location.hash.replace('#',''),
-            MISC1: "ERROR_IN_PROD",
-          });
-        }
+        this.log_error_and_display_error_page();
       });
+  }
+  log_error_and_display_error_page(){
+    if (!DEV){
+      log_standard_event({
+        SUBAPP: window.location.hash.replace('#',''),
+        MISC1: "ERROR_IN_PROD",
+      });
+    }
+
+    this.setState({
+      testing_for_stale_client: false,
+    });
   }
   render(){
     if (!this.state.has_error){
       return this.props.children;
+    } else if (this.state.testing_for_stale_client){
+      this.handle_stale_client_error_case();
+      return null;
     } else {
       return (
         <div
