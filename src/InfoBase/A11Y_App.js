597f7fbfbc8622cae8882ee3521d4dc7eb96ccef
Stephen-ONeil <stephenlevioneil@gmail.com>
2018-10-25 15:29:46 -0400

    Correct a11y app routes

--- src/InfoBase/A11Y_App.js
+++ src/InfoBase/A11Y_App.js
@@ -14,11 +14,9 @@ import { ReactUnmounter } from '../core/NavComponents';
 import { EasyAccess } from '../core/EasyAccess';
 import { SpinnerWrapper } from '../components/SpinnerWrapper.js';
 
-const Home = React.lazy( () => import('../home/home.js') );
+const Home = React.lazy( () => import('../home/A11Y_home.js') );
 const GraphInventory = React.lazy( () => import('../graph_route/GraphInventory.js') );
-const PartitionRoute = React.lazy( () => import('../partition/partition_subapp/PartitionRoute.js') );
 const BudgetMeasuresRoute = React.lazy( () => import('../partition/budget_measures_subapp/BudgetMeasuresRoute.js') );
-const BubbleExplore = React.lazy( () => import("../dept_explore/dept_explore.js") );
 const About = React.lazy( () => import('../about/about.js') );
 const MetaData = React.lazy( () => import('../metadata/metadata.js') );
 const IgocExplorer = React.lazy( () => import('../igoc_explorer/igoc_explorer.js') );
@@ -37,9 +35,9 @@ export class App extends React.Component {
     return (
       <div tabIndex={-1} id="app-focus-root">
         <ErrorBoundary>
+          <DevFip />
           <ReactUnmounter />
           <InsertRuntimeFooterLinks />
-          <DevFip />
           <EasyAccess />
           <Suspense fallback={<SpinnerWrapper scale={3} />}> 
             <Switch>
@@ -48,13 +46,12 @@ export class App extends React.Component {
               <Route path="/resource-explorer/:hierarchy_scheme?/:doc?" component={ResourceExplorer} />
               <Route path="/orgs/:level/:subject_id/infograph/:bubble?/" component={InfoGraph} />
               <Route path="/glossary/:active_key?" component={Glossary} />
-              <Route path="/partition/:perspective?/:data_type?" component={PartitionRoute} />
               <Route path="/budget-measures/:first_column?/:selected_value?" component={BudgetMeasuresRoute} />
-              <Route path="/explore-:perspective?" component={BubbleExplore} />
               <Route path="/rpb/:config?" component={ReportBuilder} />
               <Route path="/about" component={About} />
               <Route path="/graph/:level?/:graph?/:id?" component={GraphInventory} />
               <Route path="/compare_estimates" component={EstimatesComparison} />
+              <Route path="/start/:no_basic_equiv?" component={Home} />
               <Route path="/" component={Home} />
             </Switch>
           </Suspense>
