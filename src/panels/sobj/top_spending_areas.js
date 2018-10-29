7e33de15ad053acc5fc582ae8ff20fffd9bf7c15
Stephen-ONeil <stephenlevioneil@gmail.com>
2018-10-29 18:41:03 -0400

    Fix markdown in top_spending_areas panel text

--- src/panels/sobj/top_spending_areas.js
+++ src/panels/sobj/top_spending_areas.js
@@ -18,7 +18,7 @@ const { std_years } =  years;
 const { Format } = util_components;
 const { A11YTable } = declarative_charts;
 
-const { text_maker, TM } = create_text_maker_component(text);3
+const { text_maker, TM } = create_text_maker_component(text);
 
 const is_non_revenue = d => +(d.so_num) < 19;
 
@@ -30,57 +30,53 @@ const common_cal = (programs,table305) => {
   if (rows_by_so.length <= 1) {
     return false;
   }
+
   const top_3_sos = _.take(rows_by_so,cut_off_index);
-  const  remainder = (
-    top_3_sos.length > cut_off_index -1 ? 
+  const remainder = top_3_sos.length > cut_off_index -1 ? 
     {
-      label : text_maker("other"), 
-      value : d3.sum(_.tail(rows_by_so,cut_off_index),_.property("value")),
+      label: text_maker("other"), 
+      value: d3.sum( _.tail(rows_by_so,cut_off_index), _.property("value") ),
     } : 
-    []
-  );
+    [];
 
-  return top_3_sos.concat(remainder);       
-
+  return top_3_sos.concat(remainder);
 };
 
 Statistics.create_and_register({
   id: 'program_std_obj', 
-  table_deps: [ 'table305'],
+  table_deps: ['table305'],
   level: 'program',
   compute: (subject, tables, infos, add, c) => {
     const {table305} = tables;
 
     const rows_by_so = collapse_by_so([subject], table305, is_non_revenue);
     const { 
-      label : top_so_name, 
-      value : top_so_amount,
+      label: top_so_name, 
+      value: top_so_amount,
     } = _.first(rows_by_so);
 
-    const total_spent =  d3.sum(rows_by_so, _.property('value'))
-
+    const total_spent =  d3.sum( rows_by_so, _.property('value') );
     const top_so_pct = top_so_amount/total_spent;
 
     add("top_so_name", top_so_name);
     add("total_spent", total_spent);
     add("top_so_spent", top_so_amount);
     add("top_so_pct", top_so_pct);
-
   },
 })
 
 
 Statistics.create_and_register({
   id: 'tag_std_obj', 
-  table_deps: [ 'table305'],
+  table_deps: ['table305'],
   level: 'tag',
   compute: (subject, tables, infos, add, c) => {
     const {table305} = tables;
 
     const rows_by_so = collapse_by_so(subject.programs, table305, is_non_revenue);
     const { 
-      label : top_so_name, 
-      value : top_so_amount,
+      label: top_so_name, 
+      value: top_so_amount,
     } = _.first(rows_by_so);
 
     const total_spent =  d3.sum(rows_by_so, _.property('value'))
@@ -91,7 +87,6 @@ Statistics.create_and_register({
     add("total_spent", total_spent);
     add("top_so_spent", top_so_amount);
     add("top_so_pct", top_so_pct);
-
   },
 })
 
@@ -102,7 +97,7 @@ const render_w_options = ({text_key}) => ({calculations, footnotes, sources}) =>
   return (
     <StdPanel
       title={text_maker("top_spending_areas_title")}
-      {...{footnotes,sources}}
+      {...{footnotes, sources}}
     >
       <Col isText size={5}>
         <TM k={text_key} args={info} />
@@ -130,39 +125,34 @@ const render_w_options = ({text_key}) => ({calculations, footnotes, sources}) =>
       </Col>
     </StdPanel>
   )
-
-
 }
-
 
 new PanelGraph({
   key: 'top_spending_areas',
-  depends_on : ['table305'],
-  info_deps : ["program_std_obj"],
-  level : "program",
+  depends_on: ['table305'],
+  info_deps: ["program_std_obj"],
+  level: "program",
+  footnotes: ["SOBJ"],
 
   calculate(subject,info,options){ 
-    if(_.isEmpty(this.tables.table305.programs.get(subject))){
+    if (_.isEmpty( this.tables.table305.programs.get(subject) ) ){
       return false;
     }
     return  common_cal([subject], this.tables.table305);
   },
 
-  footnotes : ["SOBJ"],
   render: render_w_options({text_key: "program_top_spending_areas_text"}),
 });
 
 new PanelGraph({
   key: 'top_spending_areas',
-  info_deps : ["tag_std_obj"],
-  depends_on : ['table305'],
-  level : "tag",
-  footnotes : ["SOBJ"],
-  text :  "tag_top_spending_areas_text",
+  info_deps: ["tag_std_obj"],
+  depends_on: ['table305'],
+  level: "tag",
+  footnotes: ["SOBJ"],
 
   calculate(subject,info,options){ 
-
-    return  common_cal(subject.programs, this.tables.table305);
+    return common_cal(subject.programs, this.tables.table305);
   },
 
   render: render_w_options({text_key: "tag_top_spending_areas_text"}),
