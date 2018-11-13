38f2c714ed9f81be9331faee848f46f5454cec52
Stephen-ONeil <stephenlevioneil@gmail.com>
2018-11-13 10:18:29 -0500

    Cleaning up and refactoring what I'd started last week

--- browser-tests/route-load-tests-config.js
+++ browser-tests/route-load-tests-config.js
@@ -3,17 +3,17 @@ module.exports.route_load_tests_config = [
     name: "Homepage",
     route: "",
     selector: {
-      nona11y: "div.home-root > div.intro-box > div.container > h2 > span",
-      a11y: "#app-focus-root > div > div > section:nth-child(2) > ul > li:nth-child(1) > a",
+      app: "div.home-root > div.intro-box > div.container > h2 > span",
+      a11y_app: "#app-focus-root > div > div > section:nth-child(2) > ul > li:nth-child(1) > a",
     },
     expected_text: {
-      en: {
-        nona11y: "Find",
-        a11y: "Learn about government finances",
+      app: {
+        en: "Find",
+        fr: "Trouver",
       },
-      fr: {
-        nona11y: "Trouver",
-        a11y: "Apprendre au sujet des finances du gouvernement",
+      a11y_app: {
+        en: "Learn about government finances",
+        fr: "Apprendre au sujet des finances du gouvernement",
       },
     },
   },
@@ -21,29 +21,12 @@ module.exports.route_load_tests_config = [
     name: "About",
     route: "about",
     selector: {
-      common: "div.medium_panel_text.about-root > div > p"
+      common: "div.medium_panel_text.about-root > div > p",
     },
     expected_text: {
-      en: {
-        common: "interactive",
-      },
-      fr: {
-        common: "interactif",
-      },
-    },
-  },
-  {
-    name: "About",
-    route: "about",
-    selector: {
-      common: "div.medium_panel_text.about-root > div > p"
-    },
-    expected_text: {
-      en: {
-        common: "interactive",
-      },
-      fr: {
-        common: "interactif",
+      common: {
+        en: "interactive",
+        fr: "interactif",
       },
     },
   },
@@ -51,14 +34,12 @@ module.exports.route_load_tests_config = [
     name: "Government at a glance",
     route: "partition/dept/exp",
     selector: {
-      nona11y: "div.partition-controls > form.form-horizontal > div.partition-control-element > text",
+      app: "div.partition-controls > form.form-horizontal > div.partition-control-element > text",
     },
     expected_text: {
-      en: {
-        nona11y: "Select",
-      },
-      fr: {
-        nona11y: "Sélectionner",
+      app: {
+        en: "Select",
+        fr: "Sélectionner",
       },
     },
   },
--- browser-tests/route-load-tests.js
+++ browser-tests/route-load-tests.js
@@ -7,44 +7,57 @@ const createTestCafe = require('testcafe');
 
 const route_load_tests_config = require('./route-load-tests-config.js');
 
-const args = process.argv;
 
-function choose(name){
-  return (args.indexOf(name) > -1) && name;
-}
+const route_load_tests = (config) => {
+  const args = process.argv;
+  const options = get_options_from_args(args);
 
-const run_optional_tests = !!choose('RUN_OPTIONAL');
-const headless = !!choose('HEADLESS');
-const chrome = !!choose('CHROME');
-const chromium = !!choose('CHROMIUM');
+  // Make a temp directory to hold the test files to be generated from the config 
+  const temp_dir = fs.mkdtempSync(path.join(os.tmpdir(), 'route-tests-'));
 
 
-const build_dir_name = process.env.BUILD_DIR || "build";
-
-// Make a temp directory to hold the test files generated using route-load-tests-config.json
-const temp_dir = fs.mkdtempSync(path.join(os.tmpdir(), 'route-tests-'));
-
+  // Run all tests in temp_dir, test report sent to stdout
+  run_tests(temp_dir, options);
+} 
 
 
+const get_options_from_args = (args) => ({
+  run_optional_tests: !!choose(args, 'RUN_OPTIONAL'),
+  chrome: !!choose(args, 'CHROME'),
+  chromium: !!choose(args, 'CHROMIUM'),
+  headless: !!choose(args, 'HEADLESS'),
+});
+const choose = (args, arg_name) => (args.indexOf(arg_name) > -1) && arg_name;
 
 
-let testcafe = null;
-createTestCafe('localhost', 8080)
-  .then(tc => {
-    testcafe = tc;
-    const runner = testcafe.createRunner();
 
-    return runner
-      .src(temp_dir)
-      .browsers( 
-        _.filter([
-          chrome && `chrome${headless ? ':headless' : ''}`, 
-          chromium && `chromium${headless ? ':headless' : ''}`,
-        ])
-      )
-      .run();
-  })
-  .then(failedCount => {
-    testcafe.close();
-    fs.rmdir(temp_dir);
-  });
+const run_tests = (test_dir, options) => {
+  let testcafe = null;
+  createTestCafe('localhost', 8080)
+    .then(
+      tc => {
+        testcafe = tc;
+        const runner = testcafe.createRunner();
+    
+        return runner
+          .src(test_dir)
+          .browsers( 
+            _.filter([
+              options.chrome && `chrome${options.headless ? ':headless' : ''}`, 
+              options.chromium && `chromium${options.headless ? ':headless' : ''} --no-sandbox`,
+            ])
+          )
+          .reporter('spec') // the default testcafe reporter, sending to stdout
+          .run();
+      }
+    )
+    .finally( 
+      () => {
+        !_.isNull(testcafe) && testcafe.close();
+        test_dir && fs.rmdir(test_dir);
+      } 
+    );
+};
+
+
+route_load_tests(route_load_tests_config);
