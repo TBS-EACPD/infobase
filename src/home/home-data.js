5ce5ed576f11e270bc154eb0d6dc24057df54cd1
Stephen-ONeil <stephenlevioneil@gmail.com>
2018-11-20 17:17:01 -0500

    Update quick links... probably not final names/set of links, looks a bit silly right now

--- src/home/home-data.js
+++ src/home/home-data.js
@@ -3,6 +3,59 @@ import { rpb_link } from '../rpb/rpb_link.js';
 export default function(a11y_mode){
   const featured_content_items = [
     {
+      text_key: 'quick_link_DRR_1718',
+      href: '#orgs/gov/gov/infograph/results',
+      is_new: true,
+    },
+    {
+      text_key: 'quick_link_auth_and_exp',
+      href: rpb_link({ 
+        table: 'table4', 
+        mode: 'details',
+      }),
+      is_new: true,
+    },
+    {
+      text_key: 'quick_link_exp_by_so',
+      href: rpb_link({ 
+        table: 'table5', 
+        mode: 'details',
+      }),
+      is_new: true,
+    },
+    {
+      text_key: 'quick_link_spending_by_program',
+      href: rpb_link({ 
+        table: 'table6', 
+        mode: 'details',
+      }),
+      is_new: true,
+    },
+    {
+      text_key: 'quick_link_transfer_payment',
+      href: rpb_link({ 
+        table: 'table7', 
+        mode: 'details',
+      }),
+      is_new: true,
+    },
+    { 
+      text_key: 'quick_link_prog_by_vote_stat',
+      href: rpb_link({ 
+        table: 'table300', 
+        mode: 'details',
+      }),
+      is_new: true,
+    },
+    { 
+      text_key: 'quick_link_prog_by_so',
+      href: rpb_link({ 
+        table: 'table305', 
+        mode: 'details',
+      }),
+      is_new: true,
+    },
+    {
       text_key: "supps_a",
       href: (
         window.is_a11y_mode ? 
@@ -11,55 +64,12 @@ export default function(a11y_mode){
             columns: [ "{{est_in_year}}_estimates"], 
             dimension: 'by_estimates_doc', 
             filter: ({ //TODO: D.R.Y this against table8
-              "en": "Supp. Estimates A",
-              "fr": "Budget supp. A",
+              en: "Supp. Estimates A",
+              fr: "Budget supp. A",
             })[window.lang],
           }) :
           "#compare_estimates"
       ),
-      is_new: true,
-    },
-    {
-      text_key: "home_people_data_quicklink",
-      href: "#orgs/gov/gov/infograph/people",
-      is_new: true,
-    },
-    {
-      text_key: "home_who_we_help_quicklink",
-      href: "#resource-explorer/WWH/dp18",
-      is_new: true,
-    },
-    {
-      text_key: "DP_1819",
-      href: rpb_link({ 
-        table: 'table6', 
-        columns: ['{{planning_year_1}}'], 
-        dimension: "gov_goco",
-      }),
-    },
-    {
-      text_key: "main_estimates",
-      href: (
-        window.is_a11y_mode ? 
-          rpb_link({ 
-            table: 'table8', 
-            columns: [ "{{est_in_year}}_estimates"], 
-            dimension: 'by_estimates_doc', 
-          }) :
-          "#partition/est_doc_mains/planned_exp"
-      ),
-    },
-    {
-      text_key: "DRR_1617",
-      href: rpb_link({ 
-        table: 'table12', 
-        columns: ['{{pa_last_year}}'], 
-        dimension: "gov_goco",
-      }),
-    },
-    {
-      text_key: "igoc",
-      href: "#igoc",
     },
   ];
 
