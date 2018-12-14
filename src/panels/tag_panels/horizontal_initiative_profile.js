d2ac72be9b013a9e4ead6c9c506e371bafc6361b
Stephen-ONeil <stephenlevioneil@gmail.com>
2018-12-14 13:13:17 -0500

    Clean up the hi profile panel mock up a bit

--- src/panels/tag_panels/horizontal_initiative_profile.js
+++ src/panels/tag_panels/horizontal_initiative_profile.js
@@ -3,7 +3,11 @@ import {
   PanelGraph, 
   util_components, 
   TextPanel,
+  Subject,
+  formats,
 } from '../shared';
+
+const { Dept } = Subject;
 
 const { 
   create_text_maker_component,
@@ -20,12 +24,24 @@ new PanelGraph({
   render({calculations}){
     const { subject } = calculations;
 
-    const labels_and_items = _.chain(subject.lookups)
-      .map(
-        (value, key) => [key, value]
-      )
-      .filter( ([label, item]) => item && !_.isArray(item) && !_.isObject(item) )
-      .value()
+    const hi_lookups = subject.lookups || {};
+
+    const labels_and_items = _.chain(
+      [
+        ["hi_lead_dept", Dept.lookup(hi_lookups.lead_dept) && `${Dept.lookup(hi_lookups.lead_dept).name} (${Dept.lookup(hi_lookups.lead_dept).fancy_acronym})`],
+        ["hi_governance", hi_lookups.governance],
+        ["hi_start_year", hi_lookups.start_year],
+        ["hi_renewal_year", hi_lookups.renewal_year],
+        ["hi_eval_year", hi_lookups.eval_year],
+        ["hi_end_year", hi_lookups.end_year],
+        ["hi_spending_planned", hi_lookups.spending_planned && formats.compact_raw(hi_lookups.spending_planned)],
+        ["hi_spending_actual", hi_lookups.spending_actual && formats.compact_raw(hi_lookups.spending_actual)],
+        ["hi_spending_cumulative", hi_lookups.spending_cumulative && formats.compact_raw(hi_lookups.spending_cumulative)],
+      ]
+    )
+      .map( ([label_key, item]) => [text_maker(label_key), item] )
+      .filter( ([key, item]) => item )
+      .value();
 
     return (
       <TextPanel title={text_maker("horizontal_initiative_profile")}>
