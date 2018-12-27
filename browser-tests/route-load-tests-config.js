d4e8bdc65bfbc0a9d287b557f4564ef16a471963
Stephen-ONeil <stephenlevioneil@gmail.com>
2018-12-27 15:50:14 -0500

    Relabel RPB url route load test as legacy RPB url, add new rpb url to test config

--- browser-tests/route-load-tests-config.js
+++ browser-tests/route-load-tests-config.js
@@ -175,8 +175,13 @@ module.exports.route_load_tests_config = [
     test_on: ["eng", "basic-eng"],
   },
   {
-    name: "Report Builder - Report",
+    name: "Report Builder - Report - Legacy URL",
     route: "rpb/~(columns~(~'thisyearexpenditures)~subject~'gov_gov~mode~'simple~dimension~'major_voted_stat~table~'table1~preferDeptBreakout~true~descending~false~filter~'All)",
+    test_on: ["eng", "basic-eng"],
+  },
+  {
+    name: "Report Builder - Report",
+    route: "rpb/~(columns~(~'thisyearexpenditures)~subject~'gov_gov~mode~'simple~dimension~'major_voted_stat~table~'orgVoteStatQfr~preferDeptBreakout~true~sort_col~'dept~descending~false~filter~'All)",
     test_on: ["eng", "basic-eng"],
   },
   {
