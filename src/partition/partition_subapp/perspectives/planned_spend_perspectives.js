4eaa7def24374b372fbaff25304abfeb7e253736
Stephen-ONeil <stephenlevioneil@gmail.com>
2019-01-15 16:44:56 -0500

    Fix glossary tooltip in interim est GaaG page, fix up dates int est and sups b GaaG perspectives

--- src/partition/partition_subapp/perspectives/planned_spend_perspectives.js
+++ src/partition/partition_subapp/perspectives/planned_spend_perspectives.js
@@ -363,6 +363,8 @@ const get_year = (presentation_scheme) => {
   switch (presentation_scheme){
     case "est_doc_mains" : return "{{est_in_year}}";
     case "est_doc_sea" : return "{{est_in_year}}";
+    case "est_doc_seb" : return "{{est_in_year}}";
+    case "est_doc_im" : return "{{est_next_year}}";
     default : return "{{est_last_year}}";
   }
 }
