41a6a682897f00188f362ca5cf82cf84a2a755c3
Kate Cook <kathleen.cook@tbs-sct.gc.ca>
2019-01-18 10:44:55 -0500

    cleaning up concept filter to remove repetition

--- src/rpb/table_picker_concept_filter.js
+++ src/rpb/table_picker_concept_filter.js
@@ -1,38 +1,24 @@
+const concept_categories = {
+  timing: ["QFR", "PA", "EST_PROC"],
+  money: ["AUTH", "EXP", "VOTED", "STAT", "PLANNED_EXP", "SOBJ10"],
+  people: ["PEOPLE", "FTE", "FPS", "GENDER", "AGE", "FOL"],
+  organization: ["GOV_FRAM", "SOBJ", "PROG", "GEO"],
+}
 
-const concept_whitelist = [
-  "QFR",
-  "AUTH",
-  "EXP",
-  "VOTED",
-  "STAT",
-  "PEOPLE",
-  "SOBJ",
-  "PA",
-  "GOV_FRAM",
-  "PLANNED_EXP",
-  "PROG",
-  "SOBJ10",
-  "EST_PROC",
-  "FPS",
-  "FTE",
-  "GEO",
-  "AGE",
-  "FOL",
-  "GENDER",
-];
+/* some tables have tags that we don't want to show, so establish a whitelist */
+const concept_whitelist = _.chain(concept_categories)
+  .flatMap()
+  .uniq()
+  .value();
 
 const concept_filter = concept_key => _.includes(concept_whitelist, concept_key);
 
+const categories = _.keys(concept_categories);
 
-const concept_categories = {
-  timing: ["QFR","PA","EST_PROC"],
-  money: ["AUTH","EXP","VOTED","STAT","PLANNED_EXP","SOBJ10"],
-  people: ["PEOPLE","FTE","FPS","GENDER","AGE","FOL"],
-  organization: ["GOV_FRAM","SOBJ","PROG","GEO"],
-}
-
-const categories = ["people","money","timing","organization"];
-const concepts_by_category = _.fromPairs(_.map(categories,cat => [cat,concept_categories[cat]]));
+const concepts_by_category = _.chain(categories)
+  .map(cat => [ cat, concept_categories[cat] ])
+  .fromPairs()
+  .value();
 
 export {
   categories,
