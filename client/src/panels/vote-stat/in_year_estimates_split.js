cf9e0e1bdfec6cae921b9ce62235b217021aa535
meganhng <meganhong17@gmail.com>
2019-01-28 10:02:51 -0500

    converting original bar graphs to nivo bar graphs

--- client/src/panels/vote-stat/in_year_estimates_split.js
+++ client/src/panels/vote-stat/in_year_estimates_split.js
@@ -1,6 +1,8 @@
 import { text_maker, TM } from './vote-stat-text-prodiver.js';
 import { ResponsiveBar } from '@nivo/bar';
 
+import { Table } from '../../core/TableClass.js';
+import { ensure_loaded } from '../../core/lazy_loader.js';
 import {
   formats,
   PanelGraph,
@@ -80,6 +82,16 @@ const estimates_split_render_w_text_key = text_key => ({calculations, footnotes,
       return result;
     });
 
+
+    // const static_bar_args = {
+    //   add_xaxis: true,
+    //   x_axis_line: true,
+    //   add_yaxis: false,
+    //   add_labels: true,
+    //   colors: infobase_colors(),
+    //   margin: {top: 20, right: 20, left: 20, bottom: 80},
+    //   formater: formats.compact1,
+    // };
     content = (
       <div className="keenansucks" style={{
         height: "400px",
@@ -102,12 +114,14 @@ const estimates_split_render_w_text_key = text_key => ({calculations, footnotes,
           axisBottom={{
             "tickSize": 3,
             "tickRotation": -15,
+
           }}
           axisLeft={null}
           labelTextColor="inherit:darker(1.6)"
           motionStiffness={90}
           motionDamping={15}    
           tooltipFormat={d=> `\n$${formats.big_int_real(d, {raw: true})}`}          
+
         />
       </div>
     );
