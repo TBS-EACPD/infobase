4fa91ceb17cac7a28655f2666e73b8f1e853189f
Stephen-ONeil <stephenlevioneil@gmail.com>
2019-02-08 12:48:18 -0500

    Tweak server logging to make sure queries in both request bodies and in request query strings will be logged, turn off logging in dev since all it does is output junk to the console

--- server/src/app.js
+++ server/src/app.js
@@ -2,24 +2,24 @@ import express from 'express';
 import body_parser from 'body-parser';
 import expressGraphQL from 'express-graphql'; 
 import compression from 'compression';
-import depthLimit from 'graphql-depth-limit'
+import depthLimit from 'graphql-depth-limit';
 
 import {
   create_models,
   create_schema,
 } from './models/index.js';
-import { connect_db } from "./db.js"
+import { connect_db } from "./db.js";
 import { connect } from 'net';
 
 const log_query = (req) => {
-  /* eslint-disable no-console */
-  req.body.query && 
-    console.log(
+  const request_content = (!_.isEmpty(req.body) && req.body) || (!_.isEmpty(req.query) && req.query);
+  request_content && 
+    console.log( /* eslint-disable-line no-console */
       `${
-        req.body.query
+        request_content.query
       }${
-        req.body.variables ? 
-          `\n\n${req.body.variables}` : 
+        request_content.variables ? 
+          `\n\n${request_content.variables}` : 
           ''
       }`
     );
@@ -29,11 +29,10 @@ const log_query = (req) => {
 create_models();
 connect_db();
 const schema = create_schema();
-
 const app = express();
 
 app.use( body_parser.json({ limit: '50mb' }) );
-app.use(compression())
+app.use( compression() );
 
 app.use("/", function (req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
@@ -41,7 +40,7 @@ app.use("/", function (req, res, next) {
   if (req.method === 'OPTIONS') {
     res.sendStatus(200);
   } else {
-    log_query(req);
+    !global.IS_DEV_SERVER && log_query(req);
     next();
   }
 });
