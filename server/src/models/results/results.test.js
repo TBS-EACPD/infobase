2cf32788498fbd84da96cb6f166f7d1c89ad0dcd
Stephen-ONeil <stephenlevioneil@gmail.com>
2019-02-26 15:46:16 -0500

    Update API results tests, test that pre-rolled-up results counts match query-rolled-up counts

--- server/src/models/results/results.test.js
+++ server/src/models/results/results.test.js
@@ -1,3 +1,4 @@
+import { _ } from 'lodash';
 const { execQuery } = global;
 
 const dept_results_data_query = `
@@ -63,11 +64,102 @@ fragment everything on Org {
   }
 }
 `;
+
+const all_results_counts_query = `
+query results_counts {
+	root(lang:"en") {
+    gov {
+      all_target_counts_summary {
+        subject_id
+        drr17_results
+        drr17_indicators_met
+        drr17_indicators_not_available
+        drr17_indicators_not_met
+        drr17_indicators_future
+        dp18_results
+        dp18_indicators
+      }
+    }
+  }
+}
+`;
+const dept_results_count_query = `
+query test_org_count {
+	root(lang:"en") {
+    TBC: org(org_id:"326") {
+      ...everything
+    }
+    ND: org(org_id:"133") {
+      ...everything
+    }
+    CPCC: org(org_id:"552") {
+      ...everything
+    }
+  }
+}
+
+fragment result_counts on ResultCount {
+  results
+
+  indicators_dp
+
+  indicators_met
+  indicators_not_available
+  indicators_not_met
+  indicators_future
+}
+fragment everything on Org {
+  drr17: target_counts(doc: "drr17") {
+    ...result_counts
+  }
+  dp18: target_counts(doc: "dp18") {
+    ...result_counts
+  }
+}
+`;
+
 describe("results data", function(){
 
-  it("Test departments result snapshot", async ()=> {
+  it("Test departments result snapshot", async () => {
     const data = await execQuery(dept_results_data_query);
     return expect(data).toMatchSnapshot();
+  });
+
+  it("Test departments ResultCount snapshot", async () => {
+    const dept_result_counts = await execQuery(dept_results_count_query);
+    return expect(dept_result_counts).toMatchSnapshot();
+  });
+
+  it("Test that AllDocResultCount match rolled up ResultCount values", async () => {
+    const all_result_counts_result = await execQuery(all_results_counts_query);
+    const dept_result_counts_result = await execQuery(dept_results_count_query);
+    
+    const counts_by_dept_code = _.chain(all_result_counts_result.data.root.gov.all_target_counts_summary)
+      .map( counts => [counts.subject_id, _.omit(counts, 'subject_id')] )
+      .fromPairs()
+      .value();
+
+    const combine_docs_and_rekey = ({drr17, dp18}) => _.mapValues(
+      {
+        ..._.chain(drr17)
+          .omit("indicators_dp")
+          .map( (value, key) => [`drr17_${key}`, value] )
+          .fromPairs()
+          .value(),
+        dp18_results: dp18.results,
+        dp18_indicators: dp18.indicators_dp,
+      },
+      (value) => value === 0 ? null : value 
+    );
+
+    const result_counts_match = _.chain(dept_result_counts_result.data.root)
+      .mapValues( combine_docs_and_rekey )
+      .every(
+        (counts, dept_code) => _.isEqual(counts_by_dept_code[dept_code], counts)
+      )
+      .value();
+   
+    return expect(result_counts_match).toEqual(true);
   });
 })
 
