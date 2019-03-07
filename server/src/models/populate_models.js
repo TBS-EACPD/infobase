a7bbc2756dc09a843e64e3556f7aba16dfc114cb
Stephen-ONeil <stephenlevioneil@gmail.com>
2019-03-07 11:54:03 -0500

    Should make populate_models fail fast in CI instead of requiring a manual cancel on unhandled promise rejections

--- server/src/models/populate_models.js
+++ server/src/models/populate_models.js
@@ -1,6 +1,10 @@
 import { create_models, populate_models } from "./index.js";
 import { connect_db, drop_db } from "../db.js";
 global.IS_DEV_SERVER = !(process.env.SHOULD_USE_REMOTE_DB);
+
+// Properly exit on any unhandled promise rejections, for failing-fast in CI
+process.on('unhandledRejection', unhandledRejectionException => { throw unhandledRejectionException });
+
 (async ()=> {
   await connect_db();
   await drop_db();
