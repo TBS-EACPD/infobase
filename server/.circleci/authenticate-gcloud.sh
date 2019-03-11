41cacaf4682a1296cd6162ea1f9febf5116fe981
Stephen-ONeil <stephenlevioneil@gmail.com>
2019-03-11 17:07:28 -0400

    Delete old server/.circleci

--- server/.circleci/authenticate-gcloud.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo $GCLOUD_JSON_AUTH > ${HOME}/gcloud-service-key.json
-#try if gcloud doesn't work, try sudo /opt/google-cloud-sdk/bin/gcloud
-# gcloud components update
-gcloud auth activate-service-account --key-file=${HOME}/gcloud-service-key.json
-gcloud config set project ib-api-first
-gcloud config set compute/zone northamerica-northeast1-a
-gcloud container clusters get-credentials ib-api-cluster
--- server/.circleci/deploy.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-IMAGE_URL="gcr.io/ib-api-first/ib-api:${CIRCLE_SHA1}"
-docker build -t $IMAGE_URL .
-gcloud docker -- push $IMAGE_URL
-kubectl set image deployment/ib-api ib-api=$IMAGE_URL
