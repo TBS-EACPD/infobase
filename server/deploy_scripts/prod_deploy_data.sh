201f276bb923d2545b0fc2d963ab331f05f1cea2
Stephen-ONeil <stephenlevioneil@gmail.com>
2019-03-12 15:05:38 -0400

    Cleanup of package.json's, add repo root package.json for centrally organizing certain scripts

--- server/deploy_scripts/prod_deploy_data.sh
+++ server/deploy_scripts/prod_deploy_data.sh
@@ -2,9 +2,9 @@
 # must run this scripts in root of api project
 set -e # will exit if any command has non-zero exit value
 
-source scripts/set_prod_env_vars.sh
-source scripts/set_transient_data_secrets.sh # these trap EXIT to handle their own cleanup
+source deploy_scripts/set_prod_env_vars.sh
+source deploy_scripts/set_transient_data_secrets.sh # these trap EXIT to handle their own cleanup
 
 babel-node src/models/populate_models.js
 
-source scripts/unset_prod_env_vars.sh
+source deploy_scripts/unset_prod_env_vars.sh
