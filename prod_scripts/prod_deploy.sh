9420f3a7eacd583bdb4f16b6545c018b7c18bcbb
Stephen-ONeil <stephenlevioneil@gmail.com>
2019-03-13 17:31:26 -0400

    Correct relative file paths in /prod_script scripts

--- prod_scripts/prod_deploy.sh
+++ prod_scripts/prod_deploy.sh
@@ -3,13 +3,13 @@ set -e # will exit if any command has non-zero exit value
 
 MDB_NAME=prod-db-$(git rev-parse HEAD | cut -c1-7)
 
-(cd ../server && sh deploy_scripts/prod_deploy_data.sh)
-(cd ../server && sh deploy_scripts/prod_deploy_function.sh)
+(cd server && sh deploy_scripts/prod_deploy_data.sh)
+(cd server && sh deploy_scripts/prod_deploy_function.sh)
 
-(cd ../client && sh deploy_scripts/prod_deploy_client.sh)
+(cd client && sh deploy_scripts/prod_deploy_client.sh)
 
 # --eval seems to be the go-to way to passing args in to a JS mongo script
 mongo $(lpass show MDB_SHELL_CONNECT_STRING --notes) \
   --username $(lpass show MDB_WRITE_USER --notes) --password $(lpass show MDB_WRITE_PW --notes) \
   --eval "const new_prod_db_name = '$MDB_NAME';" \ 
-  mongo_post_deploy_cleanup.js
+  prod_scripts/mongo_post_deploy_cleanup.js
--- prod_scripts/prod_rollback.sh
+++ prod_scripts/prod_rollback.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e # will exit if any command has non-zero exit value
 
-(cd ../client && sh deploy_scripts/prod_rollback_client.sh) # moving to an old client deploy points to an old function and thus old data, rollbacks all at once
+(cd client && sh deploy_scripts/prod_rollback_client.sh) # moving to an old client deploy points to an old function and thus old data, rollbacks all at once
 
 mongo $(lpass show MDB_SHELL_CONNECT_STRING --notes) \
   --username $(lpass show MDB_WRITE_USER --notes) --password $(lpass show MDB_WRITE_PW --notes) \
-  mongo_post_rollback_cleanup.js
+  prod_scripts/mongo_post_rollback_cleanup.js
