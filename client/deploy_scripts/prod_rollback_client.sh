096e801f8c696ece8bd2bb9f097ee16c0413e453
Stephen-ONeil <stephenlevioneil@gmail.com>
2019-03-14 09:57:12 -0400

    Align client rollback script with mongo metadata rollback script (a second rollback will roll back the first rollback now)

--- client/deploy_scripts/prod_rollback_client.sh
+++ client/deploy_scripts/prod_rollback_client.sh
@@ -4,7 +4,12 @@ set -e # will exit if any command has non-zero exit value
 source ./deploy_scripts/set_transient_secrets.sh
 source ./deploy_scripts/create_prod_env_vars.sh
 
+# Move rollback bucket contents to prod
 gsutil -m rsync -a public-read -r -d $ROLLBACK_BUCKET $PROD_BUCKET
+
+# Move staging contents to rollback (staging would be the same as prod before rollback was run)
+# NOTE: this means a consequitive rollback rolls back the first rollback
+gsutil -m rsync -a public-read -r -d $STAGING_BUCKET $ROLLBACK_BUCKET
 
 #clear certain cloudflare caches
 ./deploy_scripts/selectively_clear_cloudflare_cache.sh
--- prod_scripts/mongo_post_rollback_cleanup.js
+++ prod_scripts/mongo_post_rollback_cleanup.js
@@ -14,7 +14,3 @@ metadata_db.metadata.insertOne({
   prod: current_metadata.rollback,
   rollback: current_metadata.prod,
 });
-// Note: the previous prod is re-identified as the new rollback, but running the rollback script a second time
-// will not actually put it back in production correctly since the client doesn't swap the same way.
-// Re-identifying the previous prod as the current rollback here is mainly so that the next deploy will
-// automatically drop it to clean up.
