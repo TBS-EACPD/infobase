8c016c59ac4c9104015522bd69a34eda795de69e
Stephen-ONeil <stephenlevioneil@gmail.com>
2019-03-22 09:51:13 -0400

    Fix snapshot test set_up_tests.js to actually populate the database with test data

--- server/src/set_up_tests.js
+++ server/src/set_up_tests.js
@@ -1,5 +1,6 @@
 const { graphql } = require("graphql");
 
+const { connect_db, drop_db } = require("./db.js");
 
 const {
   create_models,
@@ -7,17 +8,19 @@ const {
   create_schema,
 } = require('./models/index.js');
 
-const { connect_db } = require("./db.js");
 
 global.USE_TEST_DATA = true;
 global.IS_DEV_SERVER = true;
 
-create_models();
-connect_db();
-// populate_models();
+(async ()=> {
+  await connect_db();
+  await drop_db();
+  create_models();
+  await populate_models();
+  console.log("done"); /* eslint-disable-line no-console */
+})();
+
 const schema = create_schema();
-
-
 global.execQuery = async function(query, vars={}){
   if(!vars.lang){
     vars = { lang: "en", ...vars };
