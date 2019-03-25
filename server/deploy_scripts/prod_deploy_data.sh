7e30d8e4f122afbd7bab56dcad383acfe44c3d53
Stephen-ONeil <stephenlevioneil@gmail.com>
2019-03-25 14:41:17 -0400

    Explicitly use local node_modules copy of babel-node in server/deploy_scripts/prod_deploy_data.sh (so that it no longer relies on, coincidentally, having the correct version of babel-node installed globally)

--- server/deploy_scripts/prod_deploy_data.sh
+++ server/deploy_scripts/prod_deploy_data.sh
@@ -5,7 +5,7 @@ set -e # will exit if any command has non-zero exit value
 source deploy_scripts/set_prod_env_vars.sh
 source deploy_scripts/set_transient_data_secrets.sh # these trap EXIT to handle their own cleanup
 
-babel-node src/models/populate_models.js
+node_modules/@babel/node/bin/babel-node.js src/models/populate_models.js
 
 source deploy_scripts/unset_prod_env_vars.sh
 
