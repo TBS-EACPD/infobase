cc2c0621e99fe630fdcdfac7290ed576ebed98ad
Stephen-ONeil <stephenlevioneil@gmail.com>
2019-04-05 09:47:17 -0400

    Update quick links

--- client/src/home/home-data.js
+++ client/src/home/home-data.js
@@ -10,34 +10,13 @@ const featured_content_items = _.compact([
     is_link_out: true,
     is_youtube: true,
   },
-  !window.is_a11y_mode && {
-    text_key: "treemap_home_title",
-    href: "#treemap",
-    is_new: true,
-  },
   //{ HI_TODO
   //  text_key: 'quick_link_hi_tags',
   //  href: '#resource-explorer/HI/drr17',
   //  is_new: true,
   //},
   {
-    text_key: "interim_mains",
-    href: (
-      window.is_a11y_mode ? 
-        rpb_link({ 
-          table: 'table8', 
-          columns: ["{{est_next_year}}_estimates"], 
-          dimension: 'by_estimates_doc', 
-          filter: ({ //TODO: D.R.Y this against table8
-            en: "Interim Estimates",
-            fr: "Budget provisoire des dépenses",
-          })[window.lang],
-        }) :
-        "#partition/est_doc_ie/estimates"
-    ),
-  },
-  {
-    text_key: "supps_b",
+    text_key: "quick_link_main_estimates",
     href: (
       window.is_a11y_mode ? 
         rpb_link({ 
@@ -45,12 +24,41 @@ const featured_content_items = _.compact([
           columns: ["{{est_in_year}}_estimates"], 
           dimension: 'by_estimates_doc', 
           filter: ({ //TODO: D.R.Y this against table8
-            en: "Supp. Estimates B",
-            fr: "Budget supp. B",
+            en: "Main Estimates",
+            fr: "Budget principal des dépenses",
           })[window.lang],
         }) :
         "#compare_estimates"
     ),
+    is_new: true,
+  },
+  {
+    text_key: 'quick_link_DP_1920',
+    href: '#orgs/gov/gov/infograph/results',
+    is_new: true,
+  },
+  {
+    text_key: 'quick_link_planned_spending_by_program',
+    href: rpb_link({ 
+      table: 'programSpending', 
+      columns: ["{{planning_year_1}}","{{planning_year_2}}","{{planning_year_3}}"],
+      mode: 'details',
+    }),
+    is_new: true,
+  },
+  {
+    text_key: 'quick_link_planned_ftes_by_program',
+    href: rpb_link({ 
+      table: 'programFtes', 
+      columns: ["{{planning_year_1}}","{{planning_year_2}}","{{planning_year_3}}"],
+      mode: 'details',
+    }),
+    is_new: true,
+  },
+  !window.is_a11y_mode && {
+    text_key: "treemap_home_title",
+    href: "#treemap",
+    is_new: true,
   },
   {
     text_key: 'quick_link_DRR_1718',
@@ -60,20 +68,6 @@ const featured_content_items = _.compact([
     text_key: 'quick_link_auth_and_exp',
     href: rpb_link({ 
       table: 'orgVoteStatPa', 
-      mode: 'details',
-    }),
-  },
-  {
-    text_key: 'quick_link_exp_by_so',
-    href: rpb_link({ 
-      table: 'orgSobjs', 
-      mode: 'details',
-    }),
-  },
-  {
-    text_key: 'quick_link_spending_by_program',
-    href: rpb_link({ 
-      table: 'programSpending', 
       mode: 'details',
     }),
   },
