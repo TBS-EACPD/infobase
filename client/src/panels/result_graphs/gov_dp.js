430cf0cc72020d60c4ff0f18b96041f0bcb44c1a
Stephen-ONeil <stephenlevioneil@gmail.com>
2019-04-09 18:53:37 -0400

    Replace search bar in gov dp panel with HorizontalStatusTable

--- client/src/panels/result_graphs/gov_dp.js
+++ client/src/panels/result_graphs/gov_dp.js
@@ -3,21 +3,21 @@ import {Fragment} from 'react';
 import text from './gov_dp_text.yaml';
 
 import {
+  Subject,
   create_text_maker_component,
   PanelGraph,
   Panel,
   rpb_link,
-  infograph_href_template,
   get_source_links,
 } from "../shared";
-
-import { DeptSearch } from '../../util_components.js';
-
 
 import {
   ResultCounts,
   result_docs,
 } from './results_common.js';
+import { HorizontalStatusTable } from './result_components.js';
+
+const { Dept } = Subject;
 
 const get_dp_rpb_links = () => ({
   spend: rpb_link({
@@ -35,10 +35,16 @@ const get_dp_rpb_links = () => ({
 
 const { text_maker, TM } = create_text_maker_component(text);
 
-const ResultsIntroPanel = ({counts, depts_with_dps}) => <Fragment>
+const latests_dp_doc = _.chain(result_docs)
+  .keys()
+  .filter( doc_key => /dp/.test(doc_key) )
+  .last()
+  .value();
+
+const ResultsIntroPanel = ({counts, verbose_gov_counts, counts_by_dept}) => <Fragment>
   <div className="frow middle-xs">
     <div className="fcol-md-7 medium_panel_text">
-      <TM k="gov_dp_text" args={{...counts, depts_with_dps}} />
+      <TM k="gov_dp_text" args={{...counts, depts_with_dps: counts_by_dept.length}} />
     </div>
     {!window.is_a11y_mode &&
       <div className="fcol-md-5">
@@ -58,25 +64,15 @@ const ResultsIntroPanel = ({counts, depts_with_dps}) => <Fragment>
       </div>
     }
   </div>
-  {!window.is_a11y_mode && 
-    <div
-      style={{
-        padding: "0 100px",
-        margin: "40px auto",
-      }}
-    >
-      <div className="medium_panel_text">
-        <TM k="gov_dp_above_search_bar_text" />
-      </div>
-      <div>
-        <DeptSearch
-          only_include_dp
-          href_template={ subj => infograph_href_template(subj, "results", true) }
-          placeholder={text_maker("gov_dp_search_bar_placeholder")}
-        />
-      </div>
-    </div>
-  }
+  <HorizontalStatusTable 
+    counts_by_dept={counts_by_dept}
+    gov_counts={verbose_gov_counts}
+    status_columns={{
+      [`${latests_dp_doc}_results`]: text_maker("results"),
+      [`${latests_dp_doc}_indicators`]: text_maker("indicators"),
+    }}
+    doc={latests_dp_doc}
+  />
 </Fragment>;
   
 
@@ -85,25 +81,29 @@ new PanelGraph({
   requires_result_counts: true,
   key: "gov_dp",
   calculate: () => {
-    const latests_dp_doc = _.chain(result_docs)
-      .keys()
-      .filter( doc_key => /dp/.test(doc_key) )
-      .last()
+    const verbose_gov_counts = ResultCounts.get_gov_counts();
+    
+    const dept_counts = _.filter(ResultCounts.get_all_dept_counts(), row => row[`${latests_dp_doc}_results`] > 0 );
+    const counts_by_dept = _.chain(dept_counts)
+      .map( row => ({ 
+        subject: Dept.lookup(row.id),
+        counts: row,
+      }))
+      .map( obj => ({...obj, total: d3.sum(_.values(obj.counts)) } ) )
       .value();
 
-    const depts_with_dps = _.filter( 
-      ResultCounts.get_all_dept_counts(),
-      counts => counts[`${latests_dp_doc}_results`]
-    ).length;
-
-    return { depts_with_dps };
+    return { 
+      verbose_gov_counts,
+      counts_by_dept,
+    };
   },
   footnotes: false,
   source: (subject) => get_source_links(["DP"]),
   render({ calculations, sources}){
     const {
       graph_args: {
-        depts_with_dps,
+        verbose_gov_counts,
+        counts_by_dept,
       },
     } = calculations;
     const counts = ResultCounts.get_gov_counts();
@@ -117,7 +117,8 @@ new PanelGraph({
       >
         <ResultsIntroPanel 
           counts={counts}
-          depts_with_dps={depts_with_dps}
+          verbose_gov_counts={verbose_gov_counts}
+          counts_by_dept={counts_by_dept}
           spend_link={spend}
           fte_link={ftes}
         />
