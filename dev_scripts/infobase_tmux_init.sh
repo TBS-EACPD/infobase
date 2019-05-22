86954b5cb797e486ab8ddaf665c3c13651055a21
Stephen-ONeil <stephenlevioneil@gmail.com>
2019-05-22 19:13:49 -0400

    Add a handy dev script (for myself at least), starts my standard InfoBase dev tmux setup and runs all the dev start-up commands. I recommend putting a sym link to it in your path to keep it easy to run (assumes you have bash and tmux installed, and have placed the infobase repo at /home/<usr>/documents/InfoBase)

---
+++ dev_scripts/infobase_tmux_init.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+existing_IB_session=$(tmux list-sessions 2> /dev/null | grep -oh ^IB-[0-9] | head -1)
+
+cd /home/*/documents/InfoBase
+
+if [[ -n $existing_IB_session ]]; then
+ tmux attach-session -t "$existing_IB_session"
+else
+  tmux new-session -t "IB" \; \
+    split-window -h \; \
+    send-keys 'cd client && npm run IB_base && npm run IB_q' C-m \; \
+    split-window -v \; \
+    send-keys 'cd client && npm run serve-loopback' C-m \; \
+    split-window -v \; \
+    send-keys 'cd server && npm start' C-m \; \
+    split-window -v \; \
+    send-keys 'cd server && mongod' C-m \; \
+    selectp -t 0 \; \
+    send-keys 'cd server && npm run populate_db && cd ..' C-m \;
+fi
