a7ecefbd55ced82143fafc17c5c3a94bc330e2dc
Stephen-ONeil <stephenlevioneil@gmail.com>
2019-06-03 15:29:27 -0400

    Planning comments in email_server.js, but might be feature-creeping myself

--- email_server/src/email_server.js
+++ email_server/src/email_server.js
@@ -1,12 +1,21 @@
 import express from 'express';
+import body_parser from 'body-parser';
 import compression from 'compression';
 
-const log_email_request = (req) => {
-  const request_content = (!_.isEmpty(req.body) && req.body) || (!_.isEmpty(req.query) && req.query);
-  request_content && 
+const log_email_request = (request) => {
+  const request_is_empty = (_.isEmpty(request.body) || !request.body) && (_.isEmpty(request.query) || !request.query);
+  if (request_is_empty){
     console.log( /* eslint-disable-line no-console */
-      request_content
+      "Empty request"
     );
+  } else {
+    console.log( /* eslint-disable-line no-console */
+      `
+        body: ${request.body},
+        query: ${request.query},
+      `
+    );
+  }
 };
 
 
@@ -14,14 +23,38 @@ const email_server = express();
 
 email_server.use( body_parser.json({ limit: '50mb' }) );
 email_server.use( compression() );
+email_server.use(function (request, response, next) {
 
-email_server.use("/", function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
-
-  !global.IS_DEV_SERVER && log_email_request(req);
+  response.header('Access-Control-Allow-Origin', '*');
+  response.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
   
-  //TODO
+  if (request.method === 'OPTIONS') {
+    response.sendStatus(200);
+  } else {
+    !global.IS_DEV_SERVER && log_email_request(request);
+    next();
+  }
 });
+
+
+// TODO: what can I do to prevent spam hammering? Is that in-scope right now?
+
+
+email_server.get(
+  'email_template',
+  (request, response) => "TODO" //TODO, function from some other module, take query param as argument for name of template
+);
+
+
+email_server.post(
+  'email', 
+  (request, response) => {
+    // request body will be JSON, containing name of template and filled out template
+    // 1) validate template (check against template, also security/content check?)
+    // 2) if valid, construct email string from template, else log and respond with rejected
+    // 3) send and respond with accepted
+  }
+);
+
 
 export { email_server };
