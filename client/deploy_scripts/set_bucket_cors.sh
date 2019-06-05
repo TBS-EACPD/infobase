5dcbd55c6b0dd9cc87e344125e506a6bd3d9bb2c
Stephen-ONeil <stephenlevioneil@gmail.com>
2019-06-05 17:20:30 -0400

    Move CORS rules to lastpass, add client deploy script for setting CORS metadata on the google storage

---
+++ client/deploy_scripts/set_bucket_cors.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e # will exit if any command has non-zero exit value
+
+scratch=$(mktemp -d -t tmp.XXXXXXXXXX)
+function cleanup {
+  rm -rf "$scratch"
+}
+trap cleanup EXIT
+
+echo $(lpass show INFOBASE_CORS_JSON --notes) | base64 --decode > $scratch/cors-json-file.json
+
+gsutil cors set $scratch/cors-json-file.json gs://cdn-rdc.ea-ad.ca
