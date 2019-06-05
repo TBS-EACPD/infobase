0a0f428e3dcb0d46bedc2d01435980e669e2f7a2
Stephen-ONeil <stephenlevioneil@gmail.com>
2019-06-05 17:23:22 -0400

    Add dev bucket to the buckets CORS rules are set on by the script, uses same rules as prod bucket. Note, to run this script you need to (temporarily) sign in to the gcloud CLI as a user with appropriate permissions on both buckets

--- client/deploy_scripts/set_bucket_cors.sh
+++ client/deploy_scripts/set_bucket_cors.sh
@@ -10,3 +10,4 @@ trap cleanup EXIT
 echo $(lpass show INFOBASE_CORS_JSON --notes) | base64 --decode > $scratch/cors-json-file.json
 
 gsutil cors set $scratch/cors-json-file.json gs://cdn-rdc.ea-ad.ca
+gsutil cors set $scratch/cors-json-file.json $(lpass show INFOBASE_DEV_BUCKET --notes)
