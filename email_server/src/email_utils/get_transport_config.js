cef825998398984eac968b700c356ce8f95e3287
Stephen-ONeil <stephenlevioneil@gmail.com>
2019-06-11 16:44:55 -0400

    Simplify get_transport_config.js

--- email_server/src/email_utils/get_transport_config.js
+++ email_server/src/email_utils/get_transport_config.js
@@ -16,31 +16,10 @@ const get_auth = async () => {
 };
 
 
-const get_host = () => process.env.IS_PROD_SERVER ? process.env.PROD_HOST : "smtp.ethereal.email";
-
-
-const get_service = () => (
-  process.env.IS_PROD_SERVER ?
-    {
-      service: "gmail",
-    } : 
-    {}
-);
-
-const get_port_and_secure_flag = () => (
-  process.env.IS_PROD_SERVER ?
-    {} : 
-    {
-      port: 587,
-      secure: false,
-    }
-);
-
-
 const get_transport_config = async () => ({
-  host: get_host,
-  ... get_service(),
-  ... get_port_and_secure_flag(),
+  host: process.env.IS_PROD_SERVER ? "smtp.gmail.com" : "smtp.ethereal.email",
+  port: process.env.IS_PROD_SERVER ? 465 : 587,
+  secure: !!(process.env.IS_PROD_SERVER),
   auth: await get_auth(),
 });
 
