f4eddf65f28977f1bc3c76656698eff4ca7b18d3
Stephen-ONeil <stephenlevioneil@gmail.com>
2019-06-11 15:35:42 -0400

    Complete get_templates.js, add email server start and debug scripts to package.json

--- email_server/src/index.js
+++ email_server/src/index.js
@@ -1,11 +1,11 @@
 // entry-point, for both dev and GCF
 import { make_email_server } from './email_server.js';
-import {
+import { get_templates } from './template_utils';
+
+const email_server = (async () => {
+  const templates = await get_templates();
   
-} from './template_utils';
-
-const templates = {}; //todo
-
-const email_server = make_email_server(templates);
+  return make_email_server(templates);
+})();
 
 export { email_server };
--- email_server/src/template_utils/get_templates.js
+++ email_server/src/template_utils/get_templates.js
@@ -1,5 +1,34 @@
-const get_templates = () => {
-  // TODO: get all template files from ../../templates, return object representations
+import _ from 'lodash';
+import { readdirSync, readFile } from 'fs';
+import { promisify } from 'util';
+import path from 'path';
+
+const readFileAsync = promisify(readFile);
+
+const get_templates = async () => {
+  const templates_path = path.join(__dirname, '../../templates');
+
+  const json_template_names = readdirSync(
+    templates_path,
+    (files) => _.chain(files)
+      .filter( (file_name) => /\.json$/.test(file_name) )
+      .reduce( (file_name) => file_name.replace(/\.json$/, '') )
+      .value()
+  );
+  const template_name_content_pairs = await Promise.all(
+    _.map(
+      json_template_names,
+      (json_template_name) => readFileAsync(`${templates_path}/${json_template_name}`, "utf8")
+        .then( (json) => {
+          return [
+            json_template_name,
+            JSON.parse(json),
+          ];
+        })
+    )
+  );
+
+  return _.fromPairs(template_name_content_pairs);
 };
 
 export { get_templates };
