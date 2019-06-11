2b8cee0d6e8b77bc8b6221f0605f3b7760dbb008
Stephen-ONeil <stephenlevioneil@gmail.com>
2019-06-11 17:03:33 -0400

    Add temporary return values for unimplemented template_utils modules, far past time for me to put this through a real test (and finish setting up the account credentials and etc)

--- email_server/src/template_utils/make_email_body_from_completed_template.js
+++ email_server/src/template_utils/make_email_body_from_completed_template.js
@@ -1,5 +1,6 @@
 const make_email_body_from_completed_template = (original_template, completed_template) => {
   // TODO, as the name says
+  return "Temporary testing placeholder";
 };
 
 export { make_email_body_from_completed_template };
--- email_server/src/template_utils/validate_completed_template.js
+++ email_server/src/template_utils/validate_completed_template.js
@@ -1,5 +1,6 @@
 const validate_completed_template = (original_template, completed_template) => {
   //TODO validate a completed_template against the corresponding template
+  return true;
 };
 
 export { validate_completed_template };
