996c96639b1e0b040909b9c2e4f3c4856e3797c4
sranc046 <sranc046@uottawa.ca>
2019-06-14 15:29:34 -0400

    Accessibility improvements

--- client/src/components/ShareModal.js
+++ client/src/components/ShareModal.js
@@ -14,11 +14,8 @@ import {
 } from 'react-share';
 import text from "./ShareModal.yaml" ;
 import { create_text_maker } from '../models/text.js' ;
-import { get_static_url } from '../request_utils.js';
-import svgs from './ShareModalSVG.yaml';
 
 const text_maker = create_text_maker(text);
-const svg_maker = create_text_maker(svgs);
 
 export class ShareModal extends React.Component {
 
@@ -26,31 +23,17 @@ export class ShareModal extends React.Component {
     super(props);
 
     this.onBlur = this.onBlur.bind(this);
-    this.check = this.check.bind(this);
-    this.counter = 0;
   }
 
   onBlur(e) {
     var currentTarget = e.currentTarget;
     setTimeout(() => {
       if (!currentTarget.contains(document.activeElement)) {
-        //this.props.modalOff();
-        this.activeModal.onHide=this.props.modalOff();
+        this.props.modalOff();
       }
     }, 0);
   } ;
 
-  componentWillUpdate() {
-    this.counter = 0;
-  }
-  
-  check() {
-    if (this.counter > 0) {
-      this.props.modalOff();
-    }
-    this.counter++;
-  }
-  
   render() {
     const {
       subject,
@@ -64,34 +47,35 @@ export class ShareModal extends React.Component {
     acronym = acronym == '' ? subject.name : acronym.toUpperCase();
 
     return (
-      <Modal ref={modal => this.activeModal = modal} show={show} onHide={modalOff} onBlur={this.onBlur}>
-        <div tabIndex='0' ref={div => this.checkFocus = div} onFocus={this.check}></div>
-        <Modal.Header>
-          <Modal.Title style={{fontSize: '130%'}}><img src='./svg/shareGrey.svg'/> {text_maker("share")}</Modal.Title>
-          <Modal.Title style={{fontSize: '100%', marginTop: '7px'}}>{acronym} — {title}</Modal.Title>
-        </Modal.Header>
+      <Modal show={show} onHide={modalOff}>
+        <div onBlur={this.onBlur}>
+          <Modal.Header>
+            <Modal.Title style={{fontSize: '130%'}}><img src='./svg/shareGrey.svg'/> {text_maker("share")}</Modal.Title>
+            <Modal.Title style={{fontSize: '100%', marginTop: '7px'}}>{acronym} — {title}</Modal.Title>
+          </Modal.Header>
 
-        <Modal.Body>
-          <FacebookShareButton className='share-icons' url={url} >
-            <FacebookIcon size={32}></FacebookIcon>
-          </FacebookShareButton> 
-          <TwitterShareButton className='share-icons' url={url}>
-            <TwitterIcon size={32}></TwitterIcon >
-          </TwitterShareButton> 
-          <EmailShareButton className='share-icons' url={url}> 
-            <EmailIcon size={32}></EmailIcon>
-          </EmailShareButton>
-          <LinkedinShareButton className='share-icons' url={url}>
-            <LinkedinIcon size={32}></LinkedinIcon>
-          </LinkedinShareButton> 
-          <RedditShareButton className='share-icons' url={url} title={title}>
-            <RedditIcon size={32}></RedditIcon>
-          </RedditShareButton>
-        </Modal.Body>
+          <Modal.Body>
+            <FacebookShareButton className='share-icons' url={url} >
+              <FacebookIcon size={32}></FacebookIcon>
+            </FacebookShareButton> 
+            <TwitterShareButton className='share-icons' url={url}>
+              <TwitterIcon size={32}></TwitterIcon >
+            </TwitterShareButton> 
+            <EmailShareButton className='share-icons' url={url}> 
+              <EmailIcon size={32}></EmailIcon>
+            </EmailShareButton>
+            <LinkedinShareButton className='share-icons' url={url}>
+              <LinkedinIcon size={32}></LinkedinIcon>
+            </LinkedinShareButton> 
+            <RedditShareButton className='share-icons' url={url} title={title}>
+              <RedditIcon size={32}></RedditIcon>
+            </RedditShareButton>
+          </Modal.Body>
 
-        <Modal.Footer>
-          <Button bsStyle="primary" onClick={modalOff}>Cancel</Button>
-        </Modal.Footer>
+          <Modal.Footer>
+            <Button bsStyle="primary" onClick={modalOff}>Cancel</Button>
+          </Modal.Footer>
+        </div>
       </Modal>
     )
   }
