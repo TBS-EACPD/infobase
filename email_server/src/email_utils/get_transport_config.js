8a449fe0dd901ca4fb65feb2de201f0d3a57f36d
Stephen-ONeil <stephenlevioneil@gmail.com>
2019-06-19 17:11:05 -0400

    Properly set up get_prod_auth for the email server

--- email_server/src/email_utils/get_transport_config.js
+++ email_server/src/email_utils/get_transport_config.js
@@ -1,15 +1,39 @@
 import nodemailer from 'nodemailer';
+import { google } from 'googleapis';
+const OAuth2 = google.auth.OAuth2;
 
-const get_prod_auth = () => ({
-  user: process.env.PROD_EMAIL_USER,
-  pass: process.env.PROD_EMAIL_PASS,
-});
+const get_prod_auth = async () => {
+  const {
+    REPORT_A_PROBLEM_EMAIL_ADDRESS: email_address,
+    REPORT_A_PROBLEM_CLIENT_ID: client_id,
+    REPORT_A_PROBLEM_CLIENT_SECRET: client_secret,
+    REPORT_A_PROBLEM_REFRESH_TOKEN: refresh_token,
+  } = process.env;
+
+  const oauth2Client = new OAuth2(
+    client_id,
+    client_secret,
+    "https://developers.google.com/oauthplayground"
+  );
+  oauth2Client.setCredentials({refresh_token});
+
+  const tokens = await oauth2Client.refreshAccessToken();
+
+  return {
+    type: "OAuth2",
+    user: email_address, 
+    clientId: client_id,
+    clientSecret: client_secret,
+    refreshToken: refresh_token,
+    accessToken: tokens.credentials.access_token,
+  };
+};
 
 const get_dev_auth = async () => await nodemailer.createTestAccount();
 
 const get_auth = async () => {
   if (process.env.IS_PROD_SERVER){
-    return get_prod_auth();
+    return await get_prod_auth();
   } else {
     return await get_dev_auth();
   }
