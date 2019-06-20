f62250827cf80d1a0cbfe62f427d7b01c8f3427e
sranc046 <sranc046@uottawa.ca>
2019-06-20 09:43:53 -0400

    small_inline config works + color change (although not ideal)

--- client/src/components/LeafSpinner.js
+++ client/src/components/LeafSpinner.js
@@ -21,19 +21,30 @@ export const LeafSpinner = ({config_name, faded}) => {
     tabbed_panel: {
       transform: 'scale(1)',
       position: 'absolute',
+      top: '40px',
     },
     small_inline: {
-      transform: 'scale(0.3)',
-      color: '#fff',
+      transform: 'scale(0.25)',
+      position: 'absolute',
+      top: '9px',
+      left: '-50%',
     },
   };
+
+  let loader = leaf_loading_spinner;
+  
+  if (config_name === 'small_inline') {
+    loader = leaf_loading_spinner.replace('stroke="#26374A"', 'stroke="#FFF"');
+    loader = loader.replace('stroke="#2C70C9"', 'stroke="#FFF"');
+    loader = loader.replace('fill="#FF0000"', 'fill="#FFF"');
+  }
 
   return (
     <div className={`faded-loading-container--${faded}`} style={{top: topOffset}}>
       <div 
         className="leaf-spinner-container" 
         style={leaf_spinner_configs[config_name]}
-        dangerouslySetInnerHTML={{__html: leaf_loading_spinner}}
+        dangerouslySetInnerHTML={{__html: loader}}
       />
     </div>
   )
