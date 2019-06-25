5ca4f4d8a4efb12f9db4363f324be21429733282
Stephen-ONeil <stephenlevioneil@gmail.com>
2019-06-25 17:36:49 -0400

    Small style cleanup in LeafSpinner.scss and panel-components.js, tweak LeafSpinner.js configs and their usage

--- client/src/components/LeafSpinner.js
+++ client/src/components/LeafSpinner.js
@@ -3,50 +3,56 @@ import leaf_loading_spinner from '../svg/leaf-loading-spinner.svg';
 
 export const LeafSpinner = ({config_name}) => {
 
+  const default_spinner_config_form = (scale) => ({
+    outer_positioning: 'default',
+    spinner_container_style: {
+      transform: `scale(${scale})`,
+      position: 'fixed',
+    },
+    svg_modifier: _.identity,
+  });
+
   const leaf_spinner_configs = {
-    initial: {
-      transform: 'scale(2)',
-      position: 'fixed',
-    },
-    route: {
-      transform: 'scale(2)',
-      position: 'fixed',
-    },
-    sub_route: {
-      transform: 'scale(2)',
-      position: 'fixed',
-    },
+    initial: default_spinner_config_form(2),
+    route: default_spinner_config_form(2),
+    sub_route: default_spinner_config_form(2),
     tabbed_content: {
-      transform: 'scale(1)',
-      position: 'absolute',
-      top: '50%',
+      outer_positioning: 'default',
+      spinner_container_style: {
+        transform: `scale(1)`,
+        position: 'absolute',
+      },
+      svg_modifier: _.identity,
     },
     small_inline: {
-      transform: 'scale(0.25)',
-      position: 'absolute',
-      top: '9px',
-      left: '-50%',
+      outer_positioning: 'relative',
+      spinner_container_style: {
+        transform: 'scale(0.25)',
+        position: 'absolute',
+        top: '9px',
+        left: '-50%',
+      },
+      svg_modifier: (svg) => svg
+        .replace(`stroke="${window.infobase_color_constants.primaryColor}"`, 'stroke="#FFF"')
+        .replace(`stroke="${window.infobase_color_constants.secondaryColor}"`, 'stroke="#FFF"')
+        .replace('fill="#FF0000"', 'fill="#FFF"')
+        .replace('faded-background--true', 'faded-background--false'),
     },
   };
 
-  let loader = leaf_loading_spinner;
-  let setPosition = '';
-  
-  if (config_name === 'small_inline') {
-    loader = leaf_loading_spinner.replace(`stroke="${window.infobase_color_constants.primaryColor}"`, 'stroke="#FFF"');
-    loader = loader.replace(`stroke="${window.infobase_color_constants.secondaryColor}"`, 'stroke="#FFF"');
-    loader = loader.replace('fill="#FF0000"', 'fill="#FFF"');
-    loader = loader.replace('faded-background--true', 'faded-background--false');
-    setPosition = 'relative';
-  }
+  const {
+    outer_positioning,
+    spinner_container_style,
+    svg_modifier,
+  } = leaf_spinner_configs[config_name];
 
   return (
-    <div style={{position: setPosition}}>
+    <div style={{position: outer_positioning}}>
       <div 
         className="leaf-spinner-container" 
-        style={leaf_spinner_configs[config_name]}
-        dangerouslySetInnerHTML={{__html: loader}}
+        style={spinner_container_style}
+        dangerouslySetInnerHTML={{__html: svg_modifier(leaf_loading_spinner)}}
       />
     </div>
-  )
+  );
 };
