85f65eb8e0596a609ebc0cfadd6a07a7ced5d7c1
Stephen-ONeil <stephenlevioneil@gmail.com>
2019-06-27 11:48:54 -0400

    Handle get /email_template errors better on client

--- client/src/email_backend_utils.js
+++ client/src/email_backend_utils.js
@@ -2,13 +2,26 @@ const email_backend_url = window.is_dev && !window.is_ci ?
   `http://${ window.local_ip || "127.0.0.1" }:7331` :
   "https://us-central1-report-a-problem-email-244220.cloudfunctions.net/prod-email-backend";
 
+const format_error_as_email_template = (error_message) => ({
+  error: {
+    required: "true",
+    value_type: "error",
+    form_type: "error",
+    form_label: {
+      en: `An error has occured (${error_message})`,
+      fr: `Une erreur est survenue (${error_message})`,
+    },
+  },
+});
+
 const get_email_template_names = () => fetch(
   `${email_backend_url}/email_template_names`,
   {
     method: 'GET',
     mode: "cors",
   }
-).then( resp => resp.text() )
+)
+  .then( resp => resp.text() )
   .catch( error => {
     console.log(error); // eslint-disable-line no-console
     return [];
@@ -20,20 +33,13 @@ const get_email_template = (template_name) => fetch(
     method: 'GET',
     mode: "cors",
   }
-).then( (resp) => resp.json() )
-  .catch( error => {
-    return {
-      error: {
-        required: "true",
-        value_type: "error",
-        form_type: "error",
-        form_label: {
-          en: `An error has occured (${error})`,
-          fr: `Une erreur est survenue (${error})`,
-        },
-      },
-    };
-  });
+)
+  .then( 
+    (resp) => /2[0-9][0-9]/.test(resp.status) ? 
+      resp.json() : 
+      resp.text().then(format_error_as_email_template) 
+  )
+  .catch(format_error_as_email_template);
 
 const send_completed_email_template = (template_name, completed_template) => fetch(
   `${email_backend_url}/submit_email`,
@@ -46,8 +52,9 @@ const send_completed_email_template = (template_name, completed_template) => fet
       completed_template,
     }),
   }
-).then( ({status}) => /2[0-9][0-9]/.test(status) ) // CORS preflight
-  .then( ({status}) => /2[0-9][0-9]/.test(status) ); // actually sending the email
+)
+  .then( ({status}) => /2[0-9][0-9]/.test(status) ) // CORS preflight
+  .then( ({status}) => {debugger; /2[0-9][0-9]/.test(status)} ); // actually sending the email
 
 export {
   get_email_template_names,
