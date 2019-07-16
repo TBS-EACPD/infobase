a35efdd136d89a62531e0a64cc6e9740c23b2db8
Stephen-ONeil <stephenlevioneil@gmail.com>
2019-07-16 10:39:18 -0400

    Introduce error in authenticate-server-gcloud.sh to double check behaviour of failing CI scripts when being sileced

--- ci_scripts/authenticate-server-gcloud.sh
+++ ci_scripts/authenticate-server-gcloud.sh
@@ -3,7 +3,7 @@ echo $API_GCLOUD_JSON_AUTH > ${HOME}/server-gcloud-service-key.json
 
 source ci_scripts/silence_risky_logging.sh "mute"
 
-gcloud auth activate-service-account --key-file=${HOME}/server-gcloud-service-key.json
+gcloud auth activate-service-account --key-file=${HOME}/sfsdfg.json
 gcloud config set project $DEV_API_PROJECT_ID
 gcloud config set compute/zone us-central1
 
