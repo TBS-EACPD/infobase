f8311710f887891378eadb6e94614f87a171b28b
Stephen-ONeil <stephenlevioneil@gmail.com>
2019-07-17 13:04:28 -0400

    Fix authenticte-server-gcloud.sh, don't need it broken for testing purposes anymore

--- ci_scripts/authenticate-server-gcloud.sh
+++ ci_scripts/authenticate-server-gcloud.sh
@@ -4,7 +4,7 @@ echo $API_GCLOUD_JSON_AUTH > ${HOME}/server-gcloud-service-key.json
 
 source ci_scripts/redact_env_vars_from_logging.sh "redact-start"
 
-gcloud auth activate-service-account --key-file=${HOME}/sfsdfg.json
+gcloud auth activate-service-account --key-file=${HOME}/server-gcloud-service-key.json
 gcloud config set project $DEV_API_PROJECT_ID
 gcloud config set compute/zone us-central1
 
