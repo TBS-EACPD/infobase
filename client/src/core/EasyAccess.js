bc57336c3abbcfa3aa5071dc58f821a9224c3746
Kate Cook <kathleen.cook@tbs-sct.gc.ca>
2019-07-17 16:13:46 -0400

    add icons to top links

--- client/src/core/EasyAccess.js
+++ client/src/core/EasyAccess.js
@@ -2,6 +2,7 @@ import { TrivialTM } from '../util_components.js';
 import { trivial_text_maker } from '../models/text.js';
 import { get_static_url } from '../request_utils.js';
 import { Fragment } from 'react';
+import { text_maker } from '../tables/table_common.js';
 
 export class EasyAccess extends React.Component {
   render(){ return null; }
@@ -18,28 +19,39 @@ const TM = TrivialTM;
 const EasyAccess_ = () =>
   <Fragment>
     <a href='#glossary'>
+      <img
+        className="mrgn-rght-sm"
+        title={text_maker("glossary")}
+        style={{width: "20px", height: "20px", verticalAlign: "-4px"}}
+        src={get_static_url("svg/glossary-icon.svg")} 
+      />
       <TM k="glossary" />
     </a>
-    <a
-      href="#metadata"
-      className="mrgn-lft-md"
-    >
+    <a href="#metadata" className="mrgn-lft-md">
+      <img
+        className="mrgn-rght-sm"
+        title={text_maker("metadata")}
+        style={{width: "20px", height: "20px", verticalAlign: "-4px"}}
+        src={get_static_url("svg/metadata-icon.svg")} 
+      />
       <TM k="metadata" />
     </a>
-    <a
-      href="#about"
-      className="mrgn-lft-md"
-    >
+    <a href="#about" className="mrgn-lft-md">
+      <img
+        className="mrgn-rght-sm"
+        title={text_maker("about_title")}
+        style={{width: "20px", height: "20px", verticalAlign: "-4px"}}
+        src={get_static_url("svg/aboutus-icon.svg")} 
+      />
       <TM k="about_title" />
     </a>
-    <a
-      href={trivial_text_maker("survey_link_href")}
-      className="mrgn-lft-md"
-    >
-      <TM k="survey_link_text" />
+    <a href={trivial_text_maker("survey_link_href")} className="mrgn-lft-md">
       <img
+        className="mrgn-rght-sm"
+        title={text_maker("survey_link_text")}
         style={{width: "20px", height: "20px", verticalAlign: "-4px"}}
         src={get_static_url("svg/feedback-icon.svg")} 
       />
+      <TM k="survey_link_text" />
     </a>
   </Fragment>;
