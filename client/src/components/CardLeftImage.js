9397c822019d08f556bb8310e12c3c3f09ff0cd6
Kate Cook <kathleen.cook@tbs-sct.gc.ca>
2019-07-17 13:35:14 -0400

    turn CardLeftImage back into a link

--- client/src/components/CardLeftImage.js
+++ client/src/components/CardLeftImage.js
@@ -6,43 +6,58 @@ const CardLeftImage = ({
   img_src,
   title_key,
   text_key,
-  link_key,
+  button_text_key,
   link_href,
   text_args,
   tmf,
 }) => (
-  <div className="left-img-card">
-    { img_src &&
-      <div className="left-img-card__left">
-        <a className="left-img-card__img-link" href={link_href}>
+  <a className="link-unstyled" href={link_href}>
+    <div className="left-img-card">
+      { img_src &&
+        <div className="left-img-card__left">
           <img
             aria-hidden="true"
             src={img_src}
             className="left-img-card__img" 
           />
-        </a>
-      </div>
-    }
-    <div className="left-img-card__right-container">
-      <div className="left-img-card__right">
-        <header className="left-img-card__title">
-          <TM k={title_key} tmf={tmf} args={text_args} />
-        </header>
-        <div className="left-img-card__text">
-          <TM k={text_key} tmf={tmf} args={text_args} />
         </div>
-        { link_key &&
-            <div className="left-img-card__bottom-right">
-              <a className="link-unstyled" href={link_href}>
+      }
+      <div className="left-img-card__right-container">
+        <div className="left-img-card__right">
+          <header className="left-img-card__title">
+            <TM k={title_key} tmf={tmf} args={text_args} />
+          </header>
+          <div className="left-img-card__text">
+            <TM k={text_key} tmf={tmf} args={text_args} />
+          </div>
+          { button_text_key &&
+              <div className="left-img-card__bottom-right">
                 <div tabIndex={0} className="left-img-card__badge">
-                  <TM k={link_key} tmf={tmf} args={text_args} />
+                  <TM k={button_text_key} tmf={tmf} args={text_args} />
                 </div>
-              </a>
-            </div>
-        }
+              </div>
+          }
+        </div>
+      </div>
+      <div className="left-img-card__right-container">
+        <div className="left-img-card__right">
+          <header className="left-img-card__title">
+            <TM k={title_key} tmf={tmf} args={text_args} />
+          </header>
+          <div className="left-img-card__text">
+            <TM k={text_key} tmf={tmf} args={text_args} />
+          </div>
+          <div className="left-img-card__bottom-right">
+            <a className="link-unstyled" href={link_href}>
+              <div tabIndex={0} className="left-img-card__badge">
+                <TM k={button_text_key} tmf={tmf} args={text_args} />
+              </div>
+            </a>
+          </div>
+        </div>
       </div>
     </div>
-  </div>
+  </a>
 );
 
 export { CardLeftImage };
