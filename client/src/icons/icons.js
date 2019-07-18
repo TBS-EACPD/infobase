e3321b06a4d34ff1a1c86abe6f36b66c6f473f6d
Kate Cook <kathleen.cook@tbs-sct.gc.ca>
2019-07-18 15:59:49 -0400

    make the home icon css styling work

--- client/src/icons/icons.js
+++ client/src/icons/icons.js
@@ -1,18 +1,38 @@
-
+import './icons.scss';
 
 const IconHome = (props) => {
-  const umbrellafill = props.umbrellafill || 'orangered'
+  const {
+    title,
+  } = props;
 
   return (
-    <svg className="umbrella" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32" aria-labelledby="title">
-      <title id="title">Umbrella</title>
-      <path fill={umbrellafill} d="M27 14h5c0-1.105-1.119-2-2.5-2s-2.5 0.895-2.5 2v0zM27 14c0-1.105-1.119-2-2.5-2s-2.5 0.895-2.5 2c0-1.105-1.119-2-2.5-2s-2.5 0.895-2.5 2v0 14c0 1.112-0.895 2-2 2-1.112 0-2-0.896-2-2.001v-1.494c0-0.291 0.224-0.505 0.5-0.505 0.268 0 0.5 0.226 0.5 0.505v1.505c0 0.547 0.444 0.991 1 0.991 0.552 0 1-0.451 1-0.991v-14.009c0-1.105-1.119-2-2.5-2s-2.5 0.895-2.5 2c0-1.105-1.119-2-2.5-2s-2.5 0.895-2.5 2c0-1.105-1.119-2-2.5-2s-2.5 0.895-2.5 2c0-5.415 6.671-9.825 15-9.995v-1.506c0-0.283 0.224-0.499 0.5-0.499 0.268 0 0.5 0.224 0.5 0.499v1.506c8.329 0.17 15 4.58 15 9.995h-5z"/>
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className="icon--svg-inline" aria-labelledby={title}>
+      <path fill="none" d="M0 0h24v24H0V0z"/>
+      <path className="svg-fill" d="M12 5.69l5 4.5V18h-2v-6H9v6H7v-7.81l5-4.5M12 3L2 12h3v8h6v-6h2v6h6v-8h3L12 3z"/>
     </svg>
   );
 };
 
 
+const IconFeedBack = (props) => {
+  const {
+    title,
+  } = props;
+  
+  return (
+    <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" viewBox="0 0 600 600" style="enable-background:new 0 0 600 600;" aria-labelledby={title}>
+      <path className="svg-fill" d="M549.9,503.3c2.3-0.5-8.6-13.2-10.8-17.7l-42.3-68.5c20.8-17.4,54.2-67.8,52.3-113.1
+        c-4.2-100.9-112-182.8-250.2-182.8S48.7,203,48.7,304s112,182.9,250.2,182.9c45.4,0,81.3-4.8,118-20.2
+        C520.2,497.5,536.1,506.3,549.9,503.3z"/>
+    </svg>
+
+  );
+};
+  
+
+
 export {
   IconHome,
+  IconFeedBack,
 };
   
