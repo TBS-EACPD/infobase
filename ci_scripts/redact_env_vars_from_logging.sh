b7a0fc1c6bda28b5840685710b0f7e7250ddc2d8
Stephen-ONeil <stephenlevioneil@gmail.com>
2019-07-18 10:09:02 -0400

    Add a couple more comments to redact env vars script

--- ci_scripts/redact_env_vars_from_logging.sh
+++ ci_scripts/redact_env_vars_from_logging.sh
@@ -29,6 +29,7 @@ elif [[ $CIRCLECI && $REDACT_LOGS && ($1 == "redact-end") ]] ; then
     env_map_file=$(mktemp -t env_vals.XXXXXXXXXX)
     sorted_env_map_file=$(mktemp -t sorted_env_vals.XXXXXXXXXX)
 
+    # bunch of small, generic (eg. short numbers) CI env vars we don't want to over-redact for. Filter them out
     env_whitelist_pattern="(CI|CIRCLECI|CIRCLE_BRANCH|CIRCLE_BUILD_NUM|CIRCLE_BUILD_URL|CIRCLE_COMPARE_URL|CIRCLE_JOB|CIRCLE_NODE_INDEX|CIRCLE_NODE_TOTAL|CIRCLE_PREVIOUS_BUILD_NUM|CIRCLE_PROJECT_REPONAME|CIRCLE_PROJECT_USERNAME|CIRCLE_REPOSITORY_URL|CIRCLE_SHA1|CIRCLE_SHELL_ENV|CIRCLE_STAGE|CIRCLE_USERNAME|CIRCLE_WORKFLOW_ID|CIRCLE_WORKFLOW_JOB_ID|CIRCLE_WORKFLOW_UPSTREAM_JOB_IDS|CIRCLE_WORKFLOW_WORKSPACE_ID|CIRCLE_WORKING_DIRECTORY|SHLVL)"
     env | grep -Eov "^[^$env_whitelist_pattern]=" > $filtered_env_file
 
@@ -37,13 +38,14 @@ elif [[ $CIRCLECI && $REDACT_LOGS && ($1 == "redact-end") ]] ; then
 
     paste $env_vals_file $env_names_file > $env_map_file
 
-    # sort the maping file by the length of the env var value, don't want to redact a sub-string of a longer env var by coincidence
+    # sort the maping file by the length of the env var value, don't want to only redact a sub-string of a longer env var by coincidence
     awk -F $'\t' '{print $0"\t"length($2)}' $env_map_file | sort -k3rn | sed -e 's/ [0-9]*$//' > $sorted_env_map_file
     
     env_map_length=$( cat $sorted_env_map_file | wc -l )
 
     redacted_file=$(mktemp -t redacted_file.XXXXXXXXXX)
 
+    # do the redacting using the env map file
     awk -v map_length="${env_map_length}" -F $'\t' '
       BEGIN {
         redacted_target_file = ""
