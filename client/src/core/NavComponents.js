eb7363d2ef450da86bdad33dae1e39e2a9c2c2ee
Kate Cook <kathleen.cook@tbs-sct.gc.ca>
2019-07-26 13:45:07 -0400

    one more tweak to the beta banner

--- client/src/core/NavComponents.js
+++ client/src/core/NavComponents.js
@@ -159,7 +159,7 @@ export class StandardRouteContainer extends React.Component {
         <DocumentDescription description_str={description} />
         <BreadCrumbs crumbs={breadcrumbs} />
         <HeaderBanner route_filter={_.constant(false)} />
-        {beta && <HeaderBanner route_filter={_.constant(true)} banner_content={trivial_text_maker("beta_banner_content")} banner_class={classNames("alert","alert-info")}/>}
+        {beta && <HeaderBanner route_filter={_.constant(true)} banner_content={trivial_text_maker("beta_banner_content")} banner_class={classNames("alert", "alert-info", "beta-banner")}/>}
         <AnalyticsSynchronizer route_key={route_key} />
         { shouldSyncLang !== false &&
           <LangSynchronizer /> 
