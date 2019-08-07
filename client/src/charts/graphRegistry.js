75ccf55c2d2ad86fd8bafa5752d7e5927bcd106b
Stephen-ONeil <stephenlevioneil@gmail.com>
2019-08-07 15:57:28 -0400

    Clean up GraphRegistry class a little bit more, fix resize and hasChange events and delete some old special case logic

--- client/src/charts/graphRegistry.js
+++ client/src/charts/graphRegistry.js
@@ -9,19 +9,20 @@ class GraphRegistry {
     this.registry = [];
     this.window_width_last_updated_at = window.innerWidth;
 
+    const that = this;
     window.addEventListener(
       "hashchange", 
       _.debounce(function(){ 
-        GraphRegistry.update_registry();
+        that.update_registry();
       }, 250)
     );
 
     window.addEventListener(
       "resize", 
       _.debounce(function(){
-        if ( this.should_graphs_update() ){
-          GraphRegistry.update_registry();
-          GraphRegistry.update_graphs();
+        if ( that.should_graphs_update() ){
+          that.update_registry();
+          that.update_graphs();
         }
       }, 250)
     );
@@ -32,7 +33,7 @@ class GraphRegistry {
   }
 
   update_registry(){
-    var new_registry = this.registry.filter(
+    const new_registry = this.registry.filter(
       (graph_obj) => document.body.contains( graph_obj.html.node() ) 
     );
     this.registry = new_registry;
@@ -45,28 +46,26 @@ class GraphRegistry {
       graph_obj.outside_width = graph_obj.html.node().offsetWidth;
       graph_obj.outside_height = graph_obj.options.height || 400;
 
-      // Remove all labels associated with the graph (that is, all siblings of the svg's container).
-      // Graphs with "preserve_labels_on_update" must be re-rendered with all labels intact, so nothing is removed here.
       const html_container = graph_obj.html.node();
-      const preserve_labels = !_.isNull( html_container.querySelector("[preserve_labels_on_update]") );
-      if (!preserve_labels){
-        // forEach directly on this nodeList is spoty, mapping it through to an array first works consistently though
-        _.map(html_container.childNodes, _.identity) 
-          .forEach(
-            child => {
-              if ( !_.isUndefined(child) && !child.className.includes("__svg__") ){
-                html_container.removeChild(child);
-              }
+
+      // forEach directly on a nodeList has spoty support even with polyfils, 
+      // mapping it through to an array first works consistently though
+      _.map(html_container.childNodes, _.identity)
+        .forEach(
+          child => {
+            // Remove all labels associated with the graph (that is, all siblings of the svg's container).
+            if ( !_.isUndefined(child) && !child.className.includes("__svg__") ){
+              html_container.removeChild(child);
             }
-          );
-      }
+          }
+        );
 
       graph_obj.render(graph_obj.options);
     });
   }
 
   setup_graph_instance(instance, container, options = {}){
-    var base_dispatch_events = [
+    const base_dispatch_events = [
       "renderBegin",
       "renderEnd",
       'dataMouseEnter',
