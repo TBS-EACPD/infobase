cccf28c154858b2d0cc597bd600a871e0086182f
Stephen-ONeil <stephenlevioneil@gmail.com>
2019-08-12 12:29:53 -0400

    Make reusable StatelessModal component, replace ShareModal with it

---
+++ client/src/components/StatelessModal.js
@@ -0,0 +1,55 @@
+import './StatelessModal.scss';
+
+import { Modal } from 'react-bootstrap';
+import { trivial_text_maker } from '../models/text.js';
+
+export class StatelessModal extends React.Component {
+  constructor(props){
+    super(props);
+
+    this.onBlur = this.onBlur.bind(this);
+  }
+
+  onBlur(e){
+    var currentTarget = e.currentTarget;
+    setTimeout(() => {
+      if (!currentTarget.contains(document.activeElement)) {
+        this.props.closeModal();
+      }
+    }, 0);
+  }
+
+  render(){
+    const {
+      show,
+      on_close_callback,
+      title,
+      subtitle,
+      body,
+      close_text,
+    } = this.props;
+
+    return (
+      <Modal show={show} onHide={on_close_callback}>
+        <div onBlur={this.onBlur}>
+          <Modal.Header>
+            <Modal.Title style={{fontSize: '130%'}}>{title}</Modal.Title>
+            <Modal.Title style={{fontSize: '100%', marginTop: '7px'}}>{subtitle}</Modal.Title>
+          </Modal.Header>
+
+          <Modal.Body>
+            {body}
+          </Modal.Body>
+
+          <Modal.Footer>
+            <button className="btn btn-ib-primary" onClick={on_close_callback}>{close_text}</button>
+          </Modal.Footer>
+          <div tabIndex='0' onFocus={on_close_callback} />
+        </div>
+      </Modal>
+    );
+  }
+}
+StatelessModal.defaultProps = {
+  close_text: trivial_text_maker("cancel"),
+};
