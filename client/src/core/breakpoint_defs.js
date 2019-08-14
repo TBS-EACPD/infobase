eb62868e9a52d9d66fdc831a8ee80997a1a092ba
sranc046 <sranc046@uottawa.ca>
2019-08-14 14:08:10 -0400

    breakpoint_def.js + small fixes

---
+++ client/src/core/breakpoint_defs.js
@@ -0,0 +1,21 @@
+// Media breakpoint constants for use in the infobase. Should be kept in sync with _common-variables.scss
+// These are global variables throughout the infobase. Modify with caution!!!
+
+/*media breakpoints*/
+const smallDevice = '768px';
+const mediumDevice = '992px';
+const largeDevice = '1200px'; 
+
+/*media breakpoints +1 for min-width*/
+const minSmallDevice = '769px';
+const minMediumDevice = '993px';
+const minLargeDevice = '1201px'; 
+
+export {
+  smallDevice,
+  mediumDevice,
+  largeDevice,
+  minSmallDevice,
+  minMediumDevice,
+  minLargeDevice,
+};
