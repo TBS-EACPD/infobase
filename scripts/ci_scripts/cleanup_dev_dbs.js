cc128412d2626dbd3ab5f2f775a52799aa54815f
Stephen-ONeil <stephenlevioneil@gmail.com>
2019-08-21 19:13:28 -0400

    Remove use of Promises in cleanup_dev_dbs.js, looks like mongo's JS engine doesn't support them

--- scripts/ci_scripts/cleanup_dev_dbs.js
+++ scripts/ci_scripts/cleanup_dev_dbs.js
@@ -2,14 +2,10 @@ const active_db_names = active_branches.replace(/^origin\//g, "").replace(/\nori
 
 const all_dev_db_names = db.adminCommand( { listDatabases: 1, nameOnly: true} );
 
-Promise.all(
-  all_dev_db_names.map(
-    (dev_db_name)  => {
-      if ( !active_db_names.includes(dev_db_name) ){
-        return db.getSiblingDB(dev_db_name).dropDatabase();
-      } else {
-        return Promise.resolve();
-      }
+all_dev_db_names.forEach(
+  (dev_db_name)  => {
+    if ( !active_db_names.includes(dev_db_name) ){
+      db.getSiblingDB(dev_db_name).dropDatabase();
     }
-  )
+  }
 );
