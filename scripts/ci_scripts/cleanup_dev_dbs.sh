3ae193b7bc470d991a49de394809bcdd1a7d1829
Stephen-ONeil <stephenlevioneil@gmail.com>
2019-08-21 17:01:55 -0400

    Switch to proper mongo shell connect string in cleanup_dev_dbs.sh

--- scripts/ci_scripts/cleanup_dev_dbs.sh
+++ scripts/ci_scripts/cleanup_dev_dbs.sh
@@ -5,7 +5,7 @@ active_branches=(git branch -r)
 
 source scripts/ci_scripts/redact_env_vars_from_logging.sh "redact-start"
 
-mongo $MDB_CONNECT_STRING_3 --username $MDB_USERNAME --password $MDB_PW \
+mongo $MDB_SHELL_CONNECT_STRING --username $MDB_USERNAME --password $MDB_PW \
   --eval "const active_branches = '$active_branches';" \
   scripts/prod_scripts/cleanup_dev_dbs.js
 
