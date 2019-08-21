01f8cd21b2045a60d8b1b379afdd481849f558c0
Stephen-ONeil <stephenlevioneil@gmail.com>
2019-08-21 19:05:13 -0400

    Fix path typo in cleanup_dev_dbs.sh

--- scripts/ci_scripts/cleanup_dev_dbs.sh
+++ scripts/ci_scripts/cleanup_dev_dbs.sh
@@ -3,10 +3,12 @@ set -e
 
 active_branches=(git branch -r)
 
+echo "Starting dev link MongoDB cleanup..."
+
 source scripts/ci_scripts/redact_env_vars_from_logging.sh "redact-start"
 
 mongo $MDB_SHELL_CONNECT_STRING --username $MDB_USERNAME --password $MDB_PW \
   --eval "const active_branches = '$active_branches';" \
-  scripts/prod_scripts/cleanup_dev_dbs.js
+  scripts/ci_scripts/cleanup_dev_dbs.js
 
 source scripts/ci_scripts/redact_env_vars_from_logging.sh "redact-end"
