a75194b737f435fa131abfa4c2228f3dc5d15332
Stephen-ONeil <stephenlevioneil@gmail.com>
2019-08-21 16:55:20 -0400

    Ah, switch cleanup_dev_dbs container image to one with mongo installed, also make cleanup one word there for consistency

--- scripts/ci_scripts/cleanup_dev_dbs.sh
+++ scripts/ci_scripts/cleanup_dev_dbs.sh
@@ -7,6 +7,6 @@ source scripts/ci_scripts/redact_env_vars_from_logging.sh "redact-start"
 
 mongo $MDB_CONNECT_STRING_3 --username $MDB_USERNAME --password $MDB_PW \
   --eval "const active_branches = '$active_branches';" \
-  scripts/prod_scripts/clean_up_dev_dbs.js
+  scripts/prod_scripts/cleanup_dev_dbs.js
 
 source scripts/ci_scripts/redact_env_vars_from_logging.sh "redact-end"
