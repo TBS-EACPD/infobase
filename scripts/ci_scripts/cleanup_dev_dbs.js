09f658c8d6516ef4c8bf2a7b4029e79d4bf08ba5
Stephen-ONeil <stephenlevioneil@gmail.com>
2019-08-21 19:55:20 -0400

    add some logging, disable the database dropping in cleanup_dev_dbs.js to test things out

--- scripts/ci_scripts/cleanup_dev_dbs.js
+++ scripts/ci_scripts/cleanup_dev_dbs.js
@@ -1,10 +1,15 @@
 const active_db_names = active_branches.replace(/^origin\//g, "").replace(/\norigin\//g, ",").split(",");
 
+print(active_branches);
+
+print(active_db_names);
+
 const all_dev_db_names = db.adminCommand( { listDatabases: 1, nameOnly: true} ).databases;
 
 for (i=0; i < all_dev_db_names.length; i++){
-  const db_name = all_dev_db_names[i].name
+  const db_name = all_dev_db_names[i].name;
   if ( !active_db_names.includes(db_name) ){
-    db.getSiblingDB(db_name).dropDatabase();
+    print(db_name);
+    //db.getSiblingDB(db_name).dropDatabase();
   }
 }
