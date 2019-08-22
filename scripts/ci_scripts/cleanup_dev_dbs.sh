e00fbe5beee60d41d9b414d402bfb73866c12034
Stephen-ONeil <stephenlevioneil@gmail.com>
2019-08-21 20:32:51 -0400

    Iron out cleanup_dev_dbs.sh issues

--- scripts/ci_scripts/cleanup_dev_dbs.sh
+++ scripts/ci_scripts/cleanup_dev_dbs.sh
@@ -8,7 +8,7 @@ echo "Starting dev link MongoDB cleanup..."
 source scripts/ci_scripts/redact_env_vars_from_logging.sh "redact-start"
 
 mongo $MDB_SHELL_CONNECT_STRING --username $MDB_USERNAME --password $MDB_PW \
-  --eval "const active_branches = '$active_branches';" \
+  --eval "const active_branches = \`$active_branches\`;" \
   scripts/ci_scripts/cleanup_dev_dbs.js
 
 source scripts/ci_scripts/redact_env_vars_from_logging.sh "redact-end"
