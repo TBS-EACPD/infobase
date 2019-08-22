62174016873f840fab9a4b6baade36455018a421
Stephen-ONeil <stephenlevioneil@gmail.com>
2019-08-21 20:32:51 -0400

    Iron out cleanup_dev_dbs.sh issues

--- scripts/ci_scripts/cleanup_dev_dbs.js
+++ scripts/ci_scripts/cleanup_dev_dbs.js
@@ -1,15 +1,15 @@
-const active_db_names = active_branches.replace(/origin\//g, "").split(" ");
-
-print(active_branches);
-
-print(active_db_names);
+const active_db_names = active_branches.replace(/^[ ]?origin\//g, "").replace(/\n[ ]?origin\//g, ",").split(",");
 
 const all_dev_db_names = db.adminCommand( { listDatabases: 1, nameOnly: true} ).databases;
 
-for (i=0; i < all_dev_db_names.length; i++){
-  const db_name = all_dev_db_names[i].name;
-  print(db_name);
-  if ( !active_db_names.includes(db_name) ){
-    //db.getSiblingDB(db_name).dropDatabase();
+if (all_dev_db_names.length === 0){
+  print("No stale dev DBs to drop");
+} else {
+  for (i=0; i < all_dev_db_names.length; i++){
+    const db_name = all_dev_db_names[i].name;
+    if ( !active_db_names.includes(db_name) ){
+      print(`Droping stale dev DB "${db_name}"`);
+      db.getSiblingDB(db_name).dropDatabase();
+    }
   }
 }
