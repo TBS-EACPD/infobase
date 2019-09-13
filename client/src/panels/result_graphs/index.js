d155612091377056d411887621974694f63194a4
Kate Cook <kathleen.cook@tbs-sct.gc.ca>
2019-09-13 16:43:01 -0400

    convert to new panel definition structure, only show on subjects with drr data

--- client/src/panels/result_graphs/index.js
+++ client/src/panels/result_graphs/index.js
@@ -1,10 +1,12 @@
 import { declare_explore_results_panel } from './result_drilldown.js';
+import { declare_results_table_panel } from './result_flat_table.js';
 import { declare_drr_summary_panel } from './drr_summary.js';
 import { declare_gov_drr_panel } from './gov_drr.js';
 import { declare_gov_dp_panel } from './gov_dp.js';
 
 export {
   declare_explore_results_panel,
+  declare_results_table_panel,
   declare_drr_summary_panel,
   declare_gov_drr_panel,
   declare_gov_dp_panel,
