ce61e93d48d2300a7ca8e54f86ef18c83c4fe31d
Kate Cook <kathleen.cook@tbs-sct.gc.ca>
2019-09-18 14:15:14 -0400

    argh, still broken, but whatever

--- client/src/panels/IndividualPanelRoute.js
+++ client/src/panels/IndividualPanelRoute.js
@@ -21,23 +21,27 @@ function url_template(subject, panel){
 }
 
 
+const defaultSubjectKeys = {
+  dept: '1',
+  program: 'AGR-AAA00', //business risk management
+  tag: 'GOC001',
+  crso: "TBC-BXA00",
+};
+
 const get_subject = (level, id) => {
   let subject;
   switch(level){
-    case 'ind':
-      subject = Indicator.lookup(id);
-      break;
     case 'dept':
-      subject = Dept.lookup(id);
+      subject = Dept.lookup(id) || Dept.lookup(defaultSubjectKeys.dept);
       break;
     case 'tag':
-      subject = Tag.lookup(id);
+      subject = Tag.lookup(id) || Tag.lookup(defaultSubjectKeys.tag);
       break;
     case 'program':
-      subject = Program.lookup(id);
+      subject = Program.lookup(id) || Program.lookup(defaultSubjectKeys.program);
       break;
     case 'crso':
-      subject = CRSO.lookup(id);
+      subject = CRSO.lookup(id) || CRSO.lookup( defaultSubjectKeys.crso );
       break;
     default:
       subject = Gov;
@@ -46,9 +50,72 @@ const get_subject = (level, id) => {
 
 };
 
-<ReactPanelGraph 
-graph_key={graph_key}
-subject={subject}
-bubble={active_bubble_id}
-key={graph_key + subject.guid}
-/>
+export default class IsolatedPanel extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      loading: true,
+    };
+  }
+  loadDeps(subject_id,level,panel_key){
+    const subject = get_subject(level, subject_id);
+    ensure_loaded({
+      graph_keys: [ panel_key ],
+      subject_level: subject.level,
+      subject,
+      footnotes_for: subject,
+    }).then(()=>{
+      this.setState({loading: false});
+    });
+  }
+  componentDidMount(){
+    const { panel_key, subject_id, level } = this.props;
+    Promise.all(
+      _.chain(["gov", "dept", "crso", "program", "tag"])
+        .map(level => get_subject(level))
+        .map(get_panels_for_subject)
+        .value(),
+      this.loadDeps(subject_id,level,panel_key),
+    ).then( () => this.setState({loading: false}) );
+  }
+  componentDidUpdate(){
+    if(this.state.loading){
+      const { panel_key, subject_id, level } = this.props;
+      const subject = get_subject(level, subject_id);
+      this.loadDeps(subject,panel_key);
+    }
+  }
+
+  render(){
+    const { loading } = this.state; 
+    if(loading){
+      return <SpinnerWrapper config_name={"sub_route"} />
+    } else {
+      const { panel_key, subject_id, level } = this.props;
+      const subject = get_subject(level, subject_id);
+      const graph_obj = PanelGraph.lookup(panel_key, subject.level);
+      debugger;
+      return (
+        <StandardRouteContainer 
+          title={"TODO"}
+          breadcrumbs={"TODO"}
+          description={null}
+          route_key={"panel"}
+        >
+          <div id="main">
+            <ReactPanelGraph 
+              graph_key={panel_key}
+              subject={subject}
+              key={`${panel_key}-${subject.guid}`}
+            />
+          </div>
+        </StandardRouteContainer>
+      );
+    }
+  }
+}
+IsolatedPanel.defaultProps = {
+  level: 'dept',
+  panel_key: 'drr_summary',
+  subject_id: '1',
+};
