8680bf659d93ac51922092cfe57f882e6f9afdaf
Stephen-ONeil <stephenlevioneil@gmail.com>
2019-10-04 16:14:09 -0400

    Save bundle-stats json output to a google cloud bucket from CI

---
+++ scripts/ci_scripts/store_bundle_stats.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# used in CI after the client has been built, stores the bundle-stats json output in 
+
+set -e # will exit if any command has non-zero exit value
+
+source ../scripts/ci_scripts/redact_env_vars_from_logging.sh "redact-start"
+
+gsutil cp ./client/$BUILD_DIR/InfoBase/bundle-stats.json $GCLOUD_BUNDLE_STATS_BUCKET_URL/$CIRCLE_BRANCH-$CIRCLE_SHA1-$(date +%s).json
+
+source ../scripts/ci_scripts/redact_env_vars_from_logging.sh "redact-end"
