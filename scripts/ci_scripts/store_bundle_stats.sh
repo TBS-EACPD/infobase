5477abb6196c8b673e89ba1f4cf2b1180679ed9e
Stephen-ONeil <stephenlevioneil@gmail.com>
2019-10-04 17:08:08 -0400

    In store_bundle_stats.sh, store a second copy named <branch>-head.json for easy reference. For now, only have CI save bundle stats from master, since they're somewhat large files

--- scripts/ci_scripts/store_bundle_stats.sh
+++ scripts/ci_scripts/store_bundle_stats.sh
@@ -5,6 +5,10 @@ set -e # will exit if any command has non-zero exit value
 
 source ./scripts/ci_scripts/redact_env_vars_from_logging.sh "redact-start"
 
+# save a copy for trend analysis purposes
 gsutil cp ./client/$BUILD_DIR/InfoBase/bundle-stats.json $GCLOUD_BUNDLE_STATS_BUCKET_URL/$CIRCLE_BRANCH-$(echo "$CIRCLE_SHA1" | cut 1-7).json
+
+# save a copy as <branch>-head for easy reference when making comparisons
+gsutil cp ./client/$BUILD_DIR/InfoBase/bundle-stats.json $GCLOUD_BUNDLE_STATS_BUCKET_URL/$CIRCLE_BRANCH-head.json
 
 source ./scripts/ci_scripts/redact_env_vars_from_logging.sh "redact-end"
