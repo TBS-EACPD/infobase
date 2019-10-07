67f4868ce0dfb49cd81021d943e6bfa7cfcbd0ba
Stephen-ONeil <stephenlevioneil@gmail.com>
2019-10-07 11:07:29 -0400

    Be more careful with the existence check before deleting previous builds in client/deploy_scripts/build_all.sh

--- client/deploy_scripts/build_all.sh
+++ client/deploy_scripts/build_all.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e # will exit if any command has non-zero exit value
-[ -e $BUILD_DIR ] && rm -r $BUILD_DIR/InfoBase
+[ -e $BUILD_DIR/InfoBase ] && rm -r $BUILD_DIR/InfoBase
 npm run IB_base
 npm run IB_prod_no_watch
 npm run a11y_prod_no_watch
