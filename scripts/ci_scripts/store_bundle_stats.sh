9a8a88bf8367ea7fa42b7b85472b52e0d1735659
Stephen-ONeil <stephenlevioneil@gmail.com>
2019-10-07 09:38:08 -0400

    Move the bundle-stats output dir back a level and avoid deleting it's output when build_all.sh is run

--- scripts/ci_scripts/store_bundle_stats.sh
+++ scripts/ci_scripts/store_bundle_stats.sh
@@ -6,9 +6,9 @@ set -e # will exit if any command has non-zero exit value
 source ./scripts/ci_scripts/redact_env_vars_from_logging.sh "redact-start"
 
 # save a copy for trend analysis purposes
-gsutil cp ./client/$BUILD_DIR/InfoBase/bundle-stats.json $GCLOUD_BUNDLE_STATS_BUCKET_URL/$CIRCLE_BRANCH-$(echo "$CIRCLE_SHA1" | cut 1-7).json
+gsutil cp ./client/$BUILD_DIR/bundle-stats.json $GCLOUD_BUNDLE_STATS_BUCKET_URL/$CIRCLE_BRANCH-$(echo "$CIRCLE_SHA1" | cut 1-7).json
 
 # save a copy as <branch>-head for easy reference when making comparisons
-gsutil cp ./client/$BUILD_DIR/InfoBase/bundle-stats.json $GCLOUD_BUNDLE_STATS_BUCKET_URL/$CIRCLE_BRANCH-head.json
+gsutil cp ./client/$BUILD_DIR/bundle-stats.json $GCLOUD_BUNDLE_STATS_BUCKET_URL/$CIRCLE_BRANCH-head.json
 
 source ./scripts/ci_scripts/redact_env_vars_from_logging.sh "redact-end"
