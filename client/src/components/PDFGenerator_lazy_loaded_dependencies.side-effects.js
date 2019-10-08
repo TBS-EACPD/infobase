d47d47ea6e9f7e6e1c81fd9f770598f491f4b537
Stephen-ONeil <stephenlevioneil@gmail.com>
2019-10-08 13:07:56 -0400

    Lazy load the extra heavy dependencies of the PDFGenerator component, delaying all that downloading until if/when the pdf download button is actually clicked

---
+++ client/src/components/PDFGenerator_lazy_loaded_dependencies.side-effects.js
@@ -0,0 +1,8 @@
+import * as jsPDF from 'jspdf';
+import 'jspdf-autotable';
+
+import * as qrcode from 'qrcode-generator';
+
+import * as html2canvas from 'html2canvas';
+
+export {jsPDF, qrcode, html2canvas};
