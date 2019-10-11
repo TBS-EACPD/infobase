95fa2fb3c5c9ba0ce9b73682cdac92a15c4bf942
Taewan Kang <tkang042@uottawa.ca>
2019-10-11 14:20:50 -0400

    6 new financial models/schemas in db

--- server/src/models/finances/populate.js
+++ server/src/models/finances/populate.js
@@ -2,10 +2,80 @@ import _ from "lodash";
 import {
   get_standard_csv_file_rows,
   create_program_id,
+  check_ngtive_nan,
 } from '../load_utils.js';
 
 export default async function({models}){
-  const { ProgramSpending, ProgramFte } = models;
+  const {
+    OrgVoteStatPa,
+    OrgVoteStatQfr,
+    OrgVoteStatEstimates,
+    OrgTransferPayments,
+    ProgramSobjs,
+    ProgramVoteStat,
+    ProgramSpending,
+    ProgramFte,
+  } = models;
+
+  const orgVoteStatPa_records = _.chain(get_standard_csv_file_rows("org_vote_stat_pa.csv"))
+    .map(obj => ({
+      ...obj,
+      pa_last_year_5_auth: check_ngtive_nan(obj.pa_last_year_5_auth),
+      pa_last_year_4_auth: check_ngtive_nan(obj.pa_last_year_4_auth),
+      pa_last_year_3_auth: check_ngtive_nan(obj.pa_last_year_3_auth),
+      pa_last_year_2_auth: check_ngtive_nan(obj.pa_last_year_2_auth),
+      pa_last_year_auth: check_ngtive_nan(obj.pa_last_year_auth),
+    
+      pa_last_year_5_exp: check_ngtive_nan(obj.pa_last_year_5_exp),
+      pa_last_year_4_exp: check_ngtive_nan(obj.pa_last_year_4_exp),
+      pa_last_year_3_exp: check_ngtive_nan(obj.pa_last_year_3_exp),
+      pa_last_year_2_exp: check_ngtive_nan(obj.pa_last_year_2_exp),
+      pa_last_year_exp: check_ngtive_nan(obj.pa_last_year_exp),
+    }))
+    .map(obj => new OrgVoteStatPa(obj))
+    .value();
+  
+  const orgVoteStatQfr_records = _.chain(get_standard_csv_file_rows("org_vote_stat_qfr.csv"))
+    .map(obj => new OrgVoteStatQfr(obj))
+    .value();
+
+  const orgVoteStatEstimates_records = _.chain(get_standard_csv_file_rows("org_vote_stat_estimates.csv"))
+    .map(obj => new OrgVoteStatEstimates(obj))
+    .value();
+
+  const orgTransferPayments_records = _.chain(get_standard_csv_file_rows("org_transfer_payments.csv"))
+    .map(obj => ({
+      ...obj,
+      pa_last_year_5_auth: check_ngtive_nan(obj.pa_last_year_5_auth),
+      pa_last_year_4_auth: check_ngtive_nan(obj.pa_last_year_4_auth),
+      pa_last_year_3_auth: check_ngtive_nan(obj.pa_last_year_3_auth),
+      pa_last_year_2_auth: check_ngtive_nan(obj.pa_last_year_2_auth),
+      pa_last_year_1_auth: check_ngtive_nan(obj.pa_last_year_1_auth),
+    
+      pa_last_year_5_exp: check_ngtive_nan(obj.pa_last_year_5_exp),
+      pa_last_year_4_exp: check_ngtive_nan(obj.pa_last_year_4_exp),
+      pa_last_year_3_exp: check_ngtive_nan(obj.pa_last_year_3_exp),
+      pa_last_year_2_exp: check_ngtive_nan(obj.pa_last_year_2_exp),
+      pa_last_year_1_exp: check_ngtive_nan(obj.pa_last_year_1_exp),
+    }))
+    .map(obj => new OrgTransferPayments(obj))
+    .value();
+    
+  const programSobjs_records = _.chain(get_standard_csv_file_rows("program_sobjs.csv"))
+    .map(obj => ({
+      ...obj,
+      program_id: create_program_id(obj),
+    }))
+    .map(obj => new ProgramSobjs(obj))
+    .value();
+
+  const programVoteStat_records = _.chain(get_standard_csv_file_rows("program_vote_stat.csv"))
+    .map(obj => ({
+      ...obj,
+      program_id: create_program_id(obj),
+    }))
+    .map(obj => new ProgramVoteStat(obj))
+    .value();
 
   const programSpending_records = _.chain(get_standard_csv_file_rows("program_spending.csv"))
     .map(obj => ({
@@ -24,6 +94,12 @@ export default async function({models}){
     .value();
 
   return await Promise.all([
+    OrgVoteStatPa.insertMany(orgVoteStatPa_records),
+    OrgVoteStatQfr.insertMany(orgVoteStatQfr_records),
+    OrgVoteStatEstimates.insertMany(orgVoteStatEstimates_records),
+    OrgTransferPayments.insertMany(orgTransferPayments_records),
+    ProgramSobjs.insertMany(programSobjs_records),
+    ProgramVoteStat.insertMany(programVoteStat_records),
     ProgramSpending.insertMany(programSpending_records),
     ProgramFte.insertMany(programFte_records),
   ]);
