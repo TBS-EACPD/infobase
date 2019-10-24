edbd1edfeb02be75f4359744e32be2bd7b995492
Kate Cook <kathleen.cook@tbs-sct.gc.ca>
2019-10-24 12:17:23 -0400

    fix label, make stateless

--- client/src/components/CheckboxSelector.js
+++ client/src/components/CheckboxSelector.js
@@ -1,6 +1,6 @@
 import './CheckboxSelector.scss';
 
-export class CheckboxSelector extends React.Component {
+export class FancyCheckboxSelector extends React.Component {
   constructor(props){
     super(props);
   }
@@ -10,41 +10,33 @@ export class CheckboxSelector extends React.Component {
     const { items, handleToggle } = this.props;
     return (
       _.map(items, (item, key) =>
-        <Checkbox key={key} toggleHandler={handleToggle} {...item}/>
+        <FancyCheckbox key={key} toggleHandler={handleToggle} {...item}/>
       )
     );
   }
 }
 
-export class Checkbox extends React.Component {
+export class FancyCheckbox extends React.Component {
   constructor(props){
     super(props);
-    this.state = {
-      checked: props.active,
-    };
   }
   render(){
-    const {id, label, color} = this.props;
-    const checked = this.state.checked;
-    const onClickHandler = () => {
-      const newChecked = !this.state.checked;
-      this.props.toggleHandler(this.props.id,newChecked);
-      this.setState({checked: newChecked});
-    };
+    const {id, label, color, active} = this.props;
+    const onClickHandler = () => this.props.toggleHandler(this.props.id,!active);
     const borderStyle = {borderColor: color};
     return (
       <div className="checkbox-selector">
         <button
           className={"checkbox-selector--box"}
-          style={checked ? { backgroundColor: color, ...borderStyle} : borderStyle }
+          style={active ? { backgroundColor: color, ...borderStyle} : borderStyle }
           tabIndex={0}
-          aria-pressed={checked}
+          aria-pressed={active}
           aria-labelledby={"checkboxLabel_"+id}
           onClick={onClickHandler}
         />
         <label
           className="checkbox-selector--label"
-          id={"checkboxLabel"+id}
+          id={"checkboxLabel_"+id}
           onClick={onClickHandler}
         >
           {label}
