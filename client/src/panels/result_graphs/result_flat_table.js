3af27c724bdf6bea30df4e13a53f27da15cac56e
Kate Cook <kathleen.cook@tbs-sct.gc.ca>
2019-10-24 15:52:15 -0400

    don't need check for mobile

--- client/src/panels/result_graphs/result_flat_table.js
+++ client/src/panels/result_graphs/result_flat_table.js
@@ -70,7 +70,7 @@ const subject_link = (node) => {
         {node.data.name}
         {` (${text_maker("a_masc")} `}
         <a
-          href={ (window.is_a11y_mode || window.feature_detection.is_mobile()) && glossary_href("SSP")}
+          href={window.is_a11y_mode && glossary_href("SSP")}
           className="nowrap glossary-tooltip-link"
           tabIndex={0}
           aria-hidden="true"
