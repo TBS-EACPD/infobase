2b5b0d0ec7447c9756280d29685008e8476cd141
Stephen-ONeil <stephenlevioneil@gmail.com>
2019-10-30 12:08:35 -0400

    Aha, forgot about the hacky bit in the CI config where I edited the server jest config in place to point it from testing src/ to testing transpilied_build/. Move that to an env var flag

--- server/snapshot_tests.config.js
+++ server/snapshot_tests.config.js
@@ -1,5 +1,7 @@
+const test_target = !process.env.TEST_AGAINST_TRANSPILIED ? 'src' : 'transpiled_build';
+
 module.exports = {
-  setupTestFrameworkScriptFile: "./snapshot_tests_setup.js",
-  testRegex: "src\\/.+\\.snapshot_tests\\.js?$",
-  testEnvironment: "node",
+  setupTestFrameworkScriptFile: `./${test_target}/snapshot_tests_setup.js`,
+  testRegex: `${test_target}\\/.+\\.snapshot_tests\\.js$`,
+  testEnvironment: 'node',
 };
