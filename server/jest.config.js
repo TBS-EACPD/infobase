40db5cae4cf793e26b2a795154c4d5932c10d722
Stephen-ONeil <stephenlevioneil@gmail.com>
2019-10-30 11:32:03 -0400

    Identify snapshot tests as such in the server code base, to open up room for having some actual worthwhile unit tests separate from them

--- server/jest.config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  setupTestFrameworkScriptFile: "./src/set_up_tests.js",
-  testRegex: "src\\/.+\\.test\\.js?$",
-  testEnvironment: "node",
-};
---
+++ server/snapshot_tests.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  setupTestFrameworkScriptFile: "./snapshot_tests_setup.js",
+  testRegex: "src\\/.+\\.snapshot_tests\\.js?$",
+  testEnvironment: "node",
+};
