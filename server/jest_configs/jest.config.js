b1640a7a8c7f7852a598a6e545697e71c4699eb7
Stephen-ONeil <stephenlevioneil@gmail.com>
2019-10-31 15:29:01 -0400

    Add env var to tell server jest configs to not print coverage to console

--- server/jest_configs/jest.config.js
+++ server/jest_configs/jest.config.js
@@ -1,9 +1,10 @@
 const test_target = !process.env.TEST_AGAINST_TRANSPILIED ? 'src' : 'transpiled_build';
+const reporters = !process.env.DO_NOT_PRINT_COVERAGE ? ["json", "text"] : ['json'];
 
 module.exports = {
   rootDir: `../${test_target}`,
   testRegex: `\\.test\\.js$`,
   testEnvironment: 'node',
   coverageDirectory: "../coverage/unit_test_coverage",
-  coverageReporters: ["json", "text"],
+  coverageReporters: reporters,
 };
