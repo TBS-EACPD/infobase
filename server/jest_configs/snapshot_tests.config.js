340712b469e389d67a475e304002df75f3f7364d
Stephen-ONeil <stephenlevioneil@gmail.com>
2019-10-31 15:38:11 -0400

    Rename server snapshot tests for consistency

--- server/jest_configs/snapshot_tests.config.js
+++ server/jest_configs/snapshot_tests.config.js
@@ -4,7 +4,7 @@ const reporters = !process.env.DO_NOT_PRINT_COVERAGE ? ["json", "text"] : ['json
 module.exports = {
   rootDir: `../${test_target}`,
   setupTestFrameworkScriptFile: './snapshot_tests_setup.js',
-  testRegex: `\\.snapshot_tests\\.js$`,
+  testRegex: `\\.snapshot-test\\.js$`,
   testEnvironment: 'node',
   coverageDirectory: "../coverage/snapshot_test_coverage",
   coverageReporters: reporters,
