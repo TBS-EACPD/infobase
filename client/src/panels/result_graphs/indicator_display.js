b49022c8d9a76ceb553328beaf2561e5ea0fe2e1
Kate Cook <kathleen.cook@tbs-sct.gc.ca>
2019-11-01 11:35:52 -0400

    have the panel make its own query

--- client/src/panels/result_graphs/indicator_display.js
+++ client/src/panels/result_graphs/indicator_display.js
@@ -1,5 +1,6 @@
-
-import { ensure_loaded } from '../../core/lazy_loader.js';
+import { get_client } from '../../graphql_utils.js';
+import gql from 'graphql-tag';
+import { log_standard_event } from '../../core/analytics.js';
 import { StandardRouteContainer } from '../../core/NavComponents.js';
 
 import { TM, text_maker } from './result_text_provider.js';
@@ -11,6 +12,86 @@ import {
 
 import { Indicator } from '../../models/results.js';
 import { SingleIndicatorDisplay } from './result_components.js';
+
+
+const indicators_fields_fragment = `  id
+  stable_id
+  result_id
+  name
+  doc
+
+  target_year
+  target_month
+
+  is_reporting_discontinued
+
+  target_type
+  target_min
+  target_max
+  target_narrative
+  measure
+  seeking_to
+
+  previous_year_target_type
+  previous_year_target_min
+  previous_year_target_max
+  previous_year_target_narrative
+  previous_year_measure
+  previous_year_seeking_to
+
+  target_explanation
+  result_explanation
+
+  actual_datatype
+  actual_result
+  
+  status_key
+
+  methodology
+`;
+
+const get_indicator_query = gql`
+query($lang: String!, $id: String) {
+  root(lang: $lang) {
+    indicator(id: $id) {
+      ${indicators_fields_fragment}
+    }
+  }
+}
+`;
+
+const process_indicator = (indicator) => {
+  indicator.target_year = _.isEmpty(indicator.target_year) ? null : parseInt(indicator.target_year);
+  indicator.target_month = _.isEmpty(indicator.target_month) ? null : parseInt(indicator.target_month);
+  return indicator;
+};
+
+const query_api = (id) => {
+  const time_at_request = Date.now();
+  const client = get_client();
+  const query = get_indicator_query;
+  return client.query({
+    query,
+    variables: {
+      lang: window.lang, 
+      id,
+      _query_name: 'results_bundle',
+    },
+  })
+    .then( (response) => {
+      Indicator.create_and_register(process_indicator(response.data.root.indicator));
+      return Promise.resolve();
+    })
+    .catch(function(error){
+      const resp_time = Date.now() - time_at_request;     
+      log_standard_event({
+        SUBAPP: window.location.hash.replace('#',''),
+        MISC1: "API_QUERY_FAILURE",
+        MISC2: `Results, took  ${resp_time} ms - ${error.toString()}`,
+      });
+      throw error;
+    });
+};
 
 export default class IndicatorPanel extends React.Component {
   constructor(props) {
@@ -24,11 +105,7 @@ export default class IndicatorPanel extends React.Component {
         params: { id },
       },
     } = this.props;
-    ensure_loaded({
-      subject: {id: id, level: "indicator"},
-      results: true,
-    })
-      .then( () => this.setState({loading: false}) );
+    query_api(id).then( () => this.setState({loading: false}) );
   }
 
   render(){
