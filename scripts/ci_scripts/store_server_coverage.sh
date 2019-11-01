a1147c87f632c2e77771646512b10d8aeaf51eb3
Stephen-ONeil <stephenlevioneil@gmail.com>
2019-11-01 15:13:12 -0400

    Only deploy new server coverage status badge on master and, for now, only save coverage json on master too

--- scripts/ci_scripts/store_server_coverage.sh
+++ scripts/ci_scripts/store_server_coverage.sh
@@ -8,7 +8,7 @@ source ./scripts/ci_scripts/redact_env_vars_from_logging.sh "redact-start"
 # save a copy for trend analysis purposes
 gsutil cp ./server/coverage/coverage-final.json $GCLOUD_SERVER_COVERAGE_BUCKET_URL/$CIRCLE_BRANCH-$(echo "$CIRCLE_SHA1" | cut -c 1-7).json
 
-if [[ $CIRCLE_BRANCH == "coverage-badge" ]]; then
+if [[ $CIRCLE_BRANCH == "master" ]]; then
   # ... the baseline stats lives in a weird place, node_modules/.cache. No options in the API for controling this, pain to manage
   gsutil cp ./server/coverage/coverage-shield-badge.svg $GCLOUD_SERVER_COVERAGE_BUCKET_URL/server-coverage-badge.svg
   gsutil setmeta -h "Cache-Control:no-cache" $GCLOUD_SERVER_COVERAGE_BUCKET_URL/server-coverage-badge.svg
