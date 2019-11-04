8b361f6d5cc6c7609ef397e0e1fcb00c33a2bc6d
Stephen-ONeil <stephenlevioneil@gmail.com>
2019-11-04 09:18:11 -0500

    Update some comments in store_server_coverage.sh

--- scripts/ci_scripts/store_server_coverage.sh
+++ scripts/ci_scripts/store_server_coverage.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# used during the server deploy CI step to save the test coverage json and status-badge created durring the test step
+# used during the server deploy CI step to save the test coverage json and update the server coverage status-badge (both created during the test job)
 
 set -e # will exit if any command has non-zero exit value
 
@@ -9,7 +9,6 @@ source ./scripts/ci_scripts/redact_env_vars_from_logging.sh "redact-start"
 gsutil cp ./server/coverage/coverage-final.json $GCLOUD_SERVER_COVERAGE_BUCKET_URL/$CIRCLE_BRANCH-$(echo "$CIRCLE_SHA1" | cut -c 1-7).json
 
 if [[ $CIRCLE_BRANCH == "master" ]]; then
-  # ... the baseline stats lives in a weird place, node_modules/.cache. No options in the API for controling this, pain to manage
   gsutil cp ./server/coverage/coverage-shield-badge.svg $GCLOUD_SERVER_COVERAGE_BUCKET_URL/server-coverage-badge.svg
   gsutil setmeta -h "Cache-Control:no-cache" $GCLOUD_SERVER_COVERAGE_BUCKET_URL/server-coverage-badge.svg
 fi
