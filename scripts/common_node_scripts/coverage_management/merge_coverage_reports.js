a111f9d0ee40b554c24918b9a78c6488de4b697a
Stephen-ONeil <stephenlevioneil@gmail.com>
2019-11-08 17:25:33 -0500

    Ok\! Teased that out, got the istanbul text reporter outputting to a file WITHOUT a hacky patch

--- scripts/common_node_scripts/coverage_management/merge_coverage_reports.js
+++ scripts/common_node_scripts/coverage_management/merge_coverage_reports.js
@@ -2,8 +2,7 @@
 const fs = require('fs-extra');
 const yargs = require('yargs');
 const { createCoverageMap } = require('istanbul-lib-coverage');
-const { createReporter } = require('istanbul-api');
-
+const { createReporter, config } = require('istanbul-api');
 
 function main(){
   // Original based on https://github.com/facebook/jest/issues/2418#issuecomment-478932514
@@ -30,10 +29,22 @@ function main(){
     map.merge(r);
   });
 
-  const reporter = createReporter();
+  // first arg is the config (false sets it to default), second is an overwrites object
+  const report_config = config.loadFile(
+    false,
+    {
+      reporting: {
+        'report-config': {
+          text: {file: 'coverage-final.txt'}
+        }
+      }
+    }
+  );
+
+  const reporter = createReporter(report_config);
   reporter.addAll(reporters);
   reporter.write(map);
-  console.log('Created a merged coverage report at ./coverage/coverage-final.json'); // eslint-disable-line no-console
+  console.log('Created merged coverage report(s) in ./coverage/'); // eslint-disable-line no-console
 }
 
 try {
