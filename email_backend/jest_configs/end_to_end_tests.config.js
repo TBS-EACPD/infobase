a6b8e26683ce9b5d62d5e09e543472906690080d
Stephen-ONeil <stephenlevioneil@gmail.com>
2019-11-08 18:08:57 -0500

    Update email_backend jest configs, split up unit and end-to-end tests, use common_node_scripts to manage its coverage files

---
+++ email_backend/jest_configs/end_to_end_tests.config.js
@@ -0,0 +1,8 @@
+const common_config_rules = require('./common_config.js');
+
+module.exports = {
+  ...common_config_rules,
+
+  testRegex: '\\.end-to-end-test\\.js$',
+  coverageDirectory: '../coverage/end_to_end_test_coverage',
+};
