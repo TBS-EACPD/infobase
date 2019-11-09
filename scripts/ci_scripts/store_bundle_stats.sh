d7ab12ed41f0764c9033f00c0c6cf00b3c048371
Stephen-ONeil <stephenlevioneil@gmail.com>
2019-11-09 17:52:32 -0500

    Use path relative to ~ when sourcing redact_env_vars_from_logging in CI scripts

--- scripts/ci_scripts/store_bundle_stats.sh
+++ scripts/ci_scripts/store_bundle_stats.sh
@@ -3,7 +3,7 @@
 
 set -e # will exit if any command has non-zero exit value
 
-source ./scripts/ci_scripts/redact_env_vars_from_logging.sh "redact-start"
+source ~/InfoBase/scripts/ci_scripts/redact_env_vars_from_logging.sh "redact-start"
 
 # save a copy for trend analysis purposes
 gsutil cp ./client/$BUILD_DIR/bundle-stats.json $GCLOUD_BUNDLE_STATS_BUCKET_URL/$CIRCLE_BRANCH-$(echo "$CIRCLE_SHA1" | cut -c 1-7).json
@@ -13,4 +13,4 @@ if [[ $CIRCLE_BRANCH == "master" ]]; then
   gsutil cp ./client/node_modules/.cache/bundle-stats/baseline.json $GCLOUD_BUNDLE_STATS_BUCKET_URL/baseline.json
 fi
 
-source ./scripts/ci_scripts/redact_env_vars_from_logging.sh "redact-end"
+source ~/InfoBase/scripts/ci_scripts/redact_env_vars_from_logging.sh "redact-end"
