98ece9f4a30c87124d785c0e872183acace54555
Stephen-ONeil <stephenlevioneil@gmail.com>
2019-11-28 11:35:08 -0500

    Fancy up scripts/spinner.sh a little

--- scripts/spinner.sh
+++ scripts/spinner.sh
@@ -1,18 +1,25 @@
 #!/bin/bash
 
-# starts a spinner (on stderr), reports the spinners PID on stdout,
-# use the PID to kill the spinner
+# Starts a spinner and reports the spinners PID on stdout (the spinner itself is on stderr, to be out of the way of the PID)
+# Keep track of that PID, you need to use it to kill the spinner when you want it stopped
 
 (
   # based on https://unix.stackexchange.com/a/512170
   i=0
+  echo "\n"
   while [ true ]; do
-    c=`expr ${i} % 4`
+    c=`expr ${i} % 9`
     case ${c} in
-      0) echo "/\c" ;;
-      1) echo "-\c" ;;
-      2) echo "\\ \b\c" ;;
-      3) echo "|\c" ;;
+      0) echo "⠋\c" ;;
+      1) echo "⠙\c" ;;
+      2) echo "⠹\c" ;;
+      3) echo "⠸\c" ;;
+      4) echo "⠼\c" ;;
+      5) echo "⠴\c" ;;
+      6) echo "⠦\c" ;;
+      7) echo "⠧\c" ;;
+      8) echo "⠇\c" ;;
+      9) echo "⠏\c" ;;
     esac
     i=`expr ${i} + 1`
     # change the speed of the spinner by altering the 1 below
