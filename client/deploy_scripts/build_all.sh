ae95d027030aa1e805f359ff777998a7e353de3d
Stephen-ONeil <stephenlevioneil@gmail.com>
2019-11-28 09:39:52 -0500

    Run standard and a11y prod builds in parallel in client/deploy_Scripts/build_all.sh. Consistently shaves a minute off total time locally (down from 3m34s to 2m36s between my last two tests)

--- client/deploy_scripts/build_all.sh
+++ client/deploy_scripts/build_all.sh
@@ -3,5 +3,15 @@ set -e # will exit if any command has non-zero exit value
 [ -e $BUILD_DIR/InfoBase ] && rm -r $BUILD_DIR/InfoBase # clean up build dir
 
 npm run IB_base
-npm run IB_prod_no_watch
-npm run a11y_prod_no_watch
+
+npm run IB_prod_no_watch > /tmp/ib_prod_build_output &
+ib_prod_pid=$!
+
+npm run a11y_prod_no_watch > /tmp/a11y_prod_build_output &
+a11y_prod_pid=$!
+
+wait $ib_prod_pid
+cat /tmp/ib_prod_build_output
+
+wait $a11y_prod_pid
+cat /tmp/ia11y_prod_build_output
