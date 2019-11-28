90a420fe2d518eff62148a8bc452ec20fb76d3ac
Stephen-ONeil <stephenlevioneil@gmail.com>
2019-11-28 14:16:01 -0500

    Have distinct prod build scripts for each lang and version, try running all four in parallel. Tried once locally and it was slightly longer than the average for the previous two parallel build version (so either my weak CPU's bottle necking or the cumulative webpack overhead/lost optimization from grouping builds out weighs the parallelism, pushing so I can see if CI's resources handles it better)

--- client/deploy_scripts/build_all.sh
+++ client/deploy_scripts/build_all.sh
@@ -9,11 +9,18 @@ npm run IB_base
 # Run standard and a11y builds in parallel as background processes, store thieir stdout and stderr in temp files and hold on to their pids
 scratch=$(mktemp -d -t captured_build_output.XXXXXXXXXX)
 
-npm run IB_prod_no_watch > $scratch/ib_prod_build_out 2> $scratch/ib_prod_build_err &
-ib_prod_pid=$!
+npm run IB_prod_no_watch_en > $scratch/ib_prod_en_build_out 2> $scratch/ib_prod_en_build_err &
+ib_prod_en_pid=$!
 
-npm run a11y_prod_no_watch > $scratch/a11y_prod_build_out 2> $scratch/a11y_prod_build_err &
-a11y_prod_pid=$!
+npm run IB_prod_no_watch_fr > $scratch/ib_prod_fr_build_out 2> $scratch/ib_prod_fr_build_err &
+ib_prod_fr_pid=$!
+
+npm run a11y_prod_no_watch_en > $scratch/a11y_prod_en_build_out 2> $scratch/a11y_prod_en_build_err &
+a11y_prod_en_pid=$!
+
+npm run a11y_prod_no_watch_fr > $scratch/a11y_prod_fr_build_out 2> $scratch/a11y_prod_fr_build_err &
+a11y_prod_fr_pid=$!
+
 
 # Start a spinner going in the terminal meanwhile, hold on to its pid to stop it later
 spinner_pid=$(sh ../scripts/spinner.sh)
@@ -23,15 +30,23 @@ spinner_pid=$(sh ../scripts/spinner.sh)
 function print_captured_output {
   kill -9 $spinner_pid
 
-  cat $scratch/ib_prod_build_out
-  cat $scratch/ib_prod_build_err
+  cat $scratch/ib_prod_en_build_out
+  cat $scratch/ib_prod_en_build_err
 
-  cat $scratch/a11y_prod_build_out
-  cat $scratch/a11y_prod_build_err
+  cat $scratch/ib_prod_fr_build_out
+  cat $scratch/ib_prod_fr_build_err
+
+  cat $scratch/a11y_prod_en_build_out
+  cat $scratch/a11y_prod_en_build_err
+
+  cat $scratch/a11y_prod_fr_build_out
+  cat $scratch/a11y_prod_fr_build_err
 }
 trap print_captured_output EXIT
 
 
-wait $ib_prod_pid
-wait $a11y_prod_pid
+wait $ib_prod_en_pid
+wait $ib_prod_fr_pid
+wait $a11y_prod_en_pid
+wait $a11y_prod_fr_pid
 exit
