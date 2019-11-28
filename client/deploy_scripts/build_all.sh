31a2fbb8315e537e8938a40251acb5e2ac03348f
Stephen-ONeil <stephenlevioneil@gmail.com>
2019-11-28 10:48:08 -0500

    Capture and print stderr from backgrounded buils in build_all.sh. Important to always print both stdout and stderr output because the actuall webpack errors are in stdout...

--- client/deploy_scripts/build_all.sh
+++ client/deploy_scripts/build_all.sh
@@ -1,19 +1,29 @@
 #!/bin/bash
 set -e # will exit if any command has non-zero exit value 
+
 [ -e $BUILD_DIR/InfoBase ] && rm -r $BUILD_DIR/InfoBase # clean up build dir
 
 npm run IB_base
 
-scratch=$(mktemp -d -t build_all_logs.XXXXXXXXXX)
 
-npm run IB_prod_no_watch > $scratch/ib_prod_build_output &
+scratch=$(mktemp -d -t captured_build_output.XXXXXXXXXX)
+
+npm run IB_prod_no_watch > $scratch/ib_prod_build_out 2> $scratch/ib_prod_build_err &
 ib_prod_pid=$!
 
-npm run a11y_prod_no_watch > $scratch/a11y_prod_build_output &
+npm run a11y_prod_no_watch > $scratch/a11y_prod_build_out 2> $scratch/a11y_prod_build_err &
 a11y_prod_pid=$!
 
-wait $ib_prod_pid
-cat $scratch/ib_prod_build_output
 
+function print_captured_output {
+  cat $scratch/ib_prod_build_out
+  cat $scratch/ib_prod_build_err
+
+  cat $scratch/a11y_prod_build_out
+  cat $scratch/a11y_prod_build_err
+}
+trap print_captured_output EXIT
+
+
+wait $ib_prod_pid
 wait $a11y_prod_pid
-cat $scratch/a11y_prod_build_output
