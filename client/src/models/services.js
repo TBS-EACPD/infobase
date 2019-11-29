6a9390e770a899b633ce6af6a84cbe5f09972841
Kate Cook <kathleen.cook@tbs-sct.gc.ca>
2019-11-29 13:35:11 -0500

    add service model

---
+++ client/src/models/services.js
@@ -0,0 +1,63 @@
+import { 
+  mix,
+  staticStoreMixin,
+  PluralSingular, 
+  SubjectMixin,
+} from './storeMixins.js';
+import { trivial_text_maker } from '../models/text.js';
+
+const static_subject_store = () => mix().with(staticStoreMixin, PluralSingular, SubjectMixin);
+
+
+const Service = class Service extends static_subject_store(){
+  static get type_name(){ return 'service'; }
+  static get singular(){ return trivial_text_maker("service"); }
+  static get plural(){ return trivial_text_maker("services"); }
+  get level(){ return "service"; }
+  get guid(){ return `service_${this.id}`; }
+
+  static create_and_register(def){
+    const {id, serv} = def;
+    const inst = new Service(serv);
+    this.register(id, inst );
+  }
+
+  constructor(serv){
+    super();
+
+    _.assign(
+      this,
+      {
+        ...serv,
+      }
+    );
+  }
+};
+
+
+const ServiceStandard = class ServiceStandard extends static_subject_store(){
+  static get type_name(){ return 'service_standard'; }
+  static get singular(){ return trivial_text_maker("service_standard"); }
+  static get plural(){ return trivial_text_maker("service_standards"); }
+  get level(){ return "service_standard"; }
+  get guid(){ return `standard_${this.id}`; }
+
+  static create_and_register(def){
+    const {id, serv} = def;
+    const inst = new Service(serv);
+    this.register(id, inst );
+  }
+
+  constructor(serv){
+    super();
+
+    _.assign(
+      this,
+      {
+        ...serv,
+      }
+    );
+  }
+};
+
+export { Service, ServiceStandard };
