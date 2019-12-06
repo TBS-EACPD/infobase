0be66aed3169b28f1d15ab95b58687ae92a04702
Kate Cook <kathleen.cook@tbs-sct.gc.ca>
2019-12-06 11:15:10 -0500

    finish fleshing out standards

--- client/src/models/services.js
+++ client/src/models/services.js
@@ -6,6 +6,8 @@ import {
 } from './storeMixins.js';
 import { trivial_text_maker } from '../models/text.js';
 
+import { Program } from './organizational_entities.js';
+
 // dependencies are tangled up too much here, disable it for the whole file
 /* eslint-disable no-use-before-define */
 
@@ -20,14 +22,18 @@ const Service = class Service extends static_subject_store(){
 
   static create_and_register(def){
     const { service_id } = def;
-    // duplicate service_id as id since it makes sense
+    // duplicate service_id as id since it makes sense for each subject-like object to have an id
     def.id = def.service_id;
     const inst = new Service(def);
     this.register(service_id, inst );
   }
 
+  get contributing_programs(){
+    return _.map(this.program_ids, prog_id => Program.lookup(prog_id) );
+  }
+
   get standards(){
-    return ServiceStandard.lookup(this.id);
+    return service_indexed_standards[this.id];
   }
 
   constructor(serv){
@@ -42,7 +48,7 @@ const Service = class Service extends static_subject_store(){
   }
 };
 
-
+const service_indexed_standards = []
 const ServiceStandard = class ServiceStandard extends static_subject_store(){
   static get type_name(){ return 'service_standard'; }
   static get singular(){ return trivial_text_maker("service_standard"); }
@@ -51,9 +57,21 @@ const ServiceStandard = class ServiceStandard extends static_subject_store(){
   get guid(){ return `standard_${this.id}`; }
 
   static create_and_register(def){
-    const {id, serv} = def;
-    const inst = new Service(serv);
-    this.register(id, inst );
+    const { standard_id, service_id } = def;
+    // duplicate service_id as id since it makes sense for each subject-like object to have an id
+    def.id = def.standard_id;
+
+    const inst = new ServiceStandard(def);
+    this.register(standard_id, inst);
+
+    if(!service_indexed_standards[service_id]){
+      service_indexed_standards[service_id] = [];
+    } 
+    service_indexed_standards[service_id].push(inst);
+  }
+
+  get service(){
+    return Service.lookup(this.service_id);
   }
 
   constructor(serv){
