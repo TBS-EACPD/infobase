668af7e5ed8f8d0b40bb2725b12b0d503535c81f
Stephen-ONeil <stephenlevioneil@gmail.com>
2019-12-16 12:10:05 -0500

    Add logging to client GraphiQL, store queries in a route parameter

--- client/src/graphql_utils/GraphiQL.js
+++ client/src/graphql_utils/GraphiQL.js
@@ -30,20 +30,51 @@ export default class _GraphiQL extends React.Component {
   componentDidMount(){
     get_api_url()
       .then( 
-        (api_url) => this.setState({
-          fetcher: (graphql_params) => fetch(
-            api_url, 
-            {
-              method: 'post',
-              headers: { 'Content-Type': 'application/json' },
-              body: JSON.stringify(graphql_params),
+        (api_url) => {
+          
+          const fetcher = (graphql_params) => {
+            // start the fetch right away
+            const fetch_promise = fetch(
+              api_url, 
+              {
+                method: 'post',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify(graphql_params),
+              }
+            ).then( (response) => response.json() );
+
+
+            if (graphql_params.operationName !== "IntrospectionQuery"){
+              // side-effects of the query being made (logging, updating route params, etc.)
+              // skipped for introspection queries because one is made on every load to populate the doc explorer drawer
+              log_standard_event({
+                SUBAPP: window.location.hash.replace('#',''),
+                MISC1: "GraphiQL query",
+                MISC2: graphql_params,
+              });
+
+              this.props.history.push(`/graphiql/${encodeURIComponent(graphql_params.query)}`);
             }
-          ).then( response => response.json() ),
-        }) 
-      );
+
+            // return the promise from the fetch
+            return fetch_promise;
+          };
+
+          this.setState({fetcher});
+        });
   }
   render(){
     const { fetcher } = this.state;
+
+    const {
+      match: {
+        params: {
+          encoded_query,
+        },
+      },
+    } = this.props;
+
+    const decoded_query = !_.isEmpty(encoded_query) ? decodeURIComponent(encoded_query) : undefined;
 
     return (
       <StandardRouteContainer
@@ -68,6 +99,7 @@ export default class _GraphiQL extends React.Component {
             >
               <GraphiQL
                 fetcher={fetcher}
+                query={decoded_query}
                 defaultQuery={default_query}
                 docExplorerOpen={true}
               />
