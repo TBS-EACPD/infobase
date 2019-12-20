ac9ef85be289d3de869a8375e80eb034e1431a24
Stephen-ONeil <stephenlevioneil@gmail.com>
2019-12-20 10:10:19 -0500

    Run resource doc year templates centrally

--- client/src/gen_expl/resource-explorer-common.js
+++ client/src/gen_expl/resource-explorer-common.js
@@ -28,7 +28,7 @@ export const get_col_defs = ({doc}) => [
             'actual_spending_header' 
         }
         args={{
-          year: result_docs[doc].primary_resource_year,
+          year: result_docs[doc].primary_resource_year_written,
         }}
       />
     ),
@@ -47,7 +47,7 @@ export const get_col_defs = ({doc}) => [
             'actual_ftes_header' 
         }
         args={{
-          year: result_docs[doc].primary_resource_year,
+          year: result_docs[doc].primary_resource_year_written,
         }}
       />
     ),
