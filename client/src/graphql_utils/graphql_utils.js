6eddab5f6ba9b4a6a891aae9ec19932b434abc2b
Stephen-ONeil <stephenlevioneil@gmail.com>
2020-01-02 15:51:37 -0500

    Hotfix persistent issues with the client get_api_url function, in dev builds, not properly switching to the local host fallback when unable to connect to the local API server. Annoyingly, it was a transiplation issue

--- client/src/graphql_utils/graphql_utils.js
+++ client/src/graphql_utils/graphql_utils.js
@@ -7,34 +7,30 @@ import { compressToBase64 } from 'lz-string';
 
 const prod_api_url = `https://us-central1-ib-serverless-api-prod.cloudfunctions.net/prod-api-${window.sha}/graphql`;
 
-let api_url;
 export const get_api_url = async () => {
-  if (!api_url){
-    if(window.is_ci){
-      api_url = `hacky_target_text_for_ci_to_replace_with_test_and_deploy_time_api_urls`;
-    } else if (window.is_dev){
-      api_url = `http://${window.local_ip}:1337/graphql`;
+  if(window.is_ci){
+    return `hacky_target_text_for_ci_to_replace_with_test_and_deploy_time_api_urls`;
+  } else if (window.is_dev){
+    const local_dev_api_url = `http://${window.local_ip}:1337/graphql`;
 
-      // Need to be careful if local IP's changed since the local_ip env var was set (last time
-      // webpack process was restarted), if it has then fall back to using local host.
-      // Only give the query 1 second to respond before going to fallback.
-      const controller = new AbortController();
-      const id_for_test_timeout = setTimeout(() => controller.abort(), 1000);
-      api_url = await fetch(`${api_url}?query={ root(lang: "en") { non_field } }&query_name=dev_connection_test`, {signal: controller.signal})
-        .then( (response ) => {
-          clearTimeout(id_for_test_timeout);
-          return api_url;
-        })
-        .catch( (error) => {
-          clearTimeout(id_for_test_timeout);
-          return "http://127.0.0.1:1337/graphql";
-        });
-    } else {
-      api_url = prod_api_url;
-    }
+    // Need to be careful if the local IP's changed since the local_ip env var was last set (last time
+    // webpack process was restarted), if it has then it will fail as an API URL.
+    // Fall back to using local host in that case. Only give the query 1 second to respond before going to fallback.
+    // Can't just always use local host though, or else we can't locally serve dev builds to other devices
+    const controller = new AbortController();
+    const id_for_test_timeout = setTimeout(() => controller.abort(), 1000);
+    return await fetch(`${local_dev_api_url}?query={ root(lang: "en") { non_field } }&query_name=dev_connection_test`, {signal: controller.signal})
+      .then( (response ) => {
+        clearTimeout(id_for_test_timeout);
+        return local_dev_api_url;
+      })
+      .catch( (error) => {
+        clearTimeout(id_for_test_timeout);
+        return "http://127.0.0.1:1337/graphql";
+      });
+  } else {
+    return prod_api_url;
   }
-  
-  return api_url;
 };
 
 // Makes our GET requests tolerant of long queries, sufficient but may not work for arbitrarily long queries
