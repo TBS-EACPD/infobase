4fae07ed81fee6db89be5752ee084ec2c784f40b
Stephen-ONeil <stephenlevioneil@gmail.com>
2020-01-02 10:44:49 -0500

    Be more explicit when identifying subject entities with infographics in smart_href_template

--- client/src/link_utils.js
+++ client/src/link_utils.js
@@ -1,7 +1,11 @@
 import { infograph_href_template } from './infographic/infographic_link.js';
 import { rpb_link } from './rpb/rpb_link.js';
 import { Table } from './core/TableClass.js';
+import { Subject } from './models/subject.js';
 import { GlossaryEntry } from './models/glossary.js';
+
+const {Gov, Dept, CRSO, Program, Tag} = Subject;
+const subject_classes_with_infographics = [Gov, Dept, CRSO, Program, Tag];
 
 const glossary_href = (subject_or_id, first_character = '#') => {
   const id = _.isString(subject_or_id) ? subject_or_id : subject_or_id.id;
@@ -11,12 +15,12 @@ const glossary_href = (subject_or_id, first_character = '#') => {
 };
 
 const smart_href_template = (entity, first_character) => {
-  if(entity.level){
-    return infograph_href_template(entity, null, first_character);
-  } else if(entity.table.constructor === Table && entity.table){
+  if(entity.table && entity.table.constructor === Table){
     return rpb_link({ table: entity.table.id }, first_character);
   } else if(entity.constructor === GlossaryEntry){
     return glossary_href(entity, first_character);
+  } else if( _.includes(subject_classes_with_infographics, entity.constructor) ){
+    return infograph_href_template(entity, null, first_character);
   } else {
     throw new Error(`${entity} does not belong to a class with a known href template`);
   }
