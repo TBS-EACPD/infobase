9536b6ae99406c4a4d317f24ce5f504379acadbb
Stephen-ONeil <stephenlevioneil@gmail.com>
2020-01-07 10:34:43 -0500

    Ahh, introduced an error by accidentally marking the hierarchy scheme argument as required in the resource-explorer to tag-explorer route redirect

--- client/src/InfoBase/App.js
+++ client/src/InfoBase/App.js
@@ -64,7 +64,7 @@ export class App extends React.Component {
               <Route path="/igoc/:grouping?" component={IgocExplorer} />
               <Redirect 
                 from="/resource-explorer/:hierarchy_scheme?/:doc?"
-                to="/tag-explorer/:hierarchy_scheme"
+                to="/tag-explorer/:hierarchy_scheme?"
               />
               <Route path="/tag-explorer/:hierarchy_scheme?/:period?" component={TagExplorer} />
               <Route path="/orgs/:level/:subject_id/infograph/:active_bubble_id?/:options?/" component={Infographic} />
