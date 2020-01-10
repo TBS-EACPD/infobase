0362d82b26712ca0f52c4ed74e1061924c864b55
Stephen-ONeil <stephenlevioneil@gmail.com>
2020-01-10 13:55:27 -0500

    Add optional branch name argument to refresh_pipeline_data.sh so it can be used to grab data from non-master pipeline branches

--- scripts/dev_scripts/refresh_pipeline_data.sh
+++ scripts/dev_scripts/refresh_pipeline_data.sh
@@ -1,13 +1,21 @@
 #!/bin/sh
 
 # Assumes a clone of the EACPD pipeline repo exists in a dir named "pipeline" as a sibling of the infobase repo.
-# Pulls in the latest version of the pipeline on master and copies its InfoBase output in to the infobase repo data dir.
+# Pulls in the latest version of the pipeline on the argument branch (default master) and copies its InfoBase output in to the infobase repo data dir.
 
 repo_path=$(pwd)
 
+branch_name=$(
+  if [ -z "$var" ]; then
+    echo "$1"
+  else
+    echo "master"
+  fi
+)
+
 cd ../pipeline
 
-git checkout master
-git pull origin master
+git checkout $branch_name
+git pull origin $branch_name
 
 cp -f outputs/Infobase/* $repo_path/data
