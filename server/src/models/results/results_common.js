d535b540829931407c8c84aeb2f003c950e41ebb
Stephen-ONeil <stephenlevioneil@gmail.com>
2020-01-13 09:54:17 -0500

    Have the server use old doc years to correspond to the test data/snapshots when in test data mode

--- server/src/models/results/results_common.js
+++ server/src/models/results/results_common.js
@@ -1,8 +1,20 @@
-export const drr_docs = [
+const current_drr_docs = [
   "drr17",
   "drr18",
 ];
-
-export const dp_docs = [
+const current_dp_docs = [
   "dp19",
 ];
+
+
+const snapshot_test_drr_docs = [
+  "drr17",
+];
+const snapshot_test_dp_docs = [
+  "dp18",
+  "dp19",
+];
+
+
+export const drr_docs = process.env.USE_TEST_DATA ? snapshot_test_drr_docs : current_drr_docs;
+export const dp_docs = process.env.USE_TEST_DATA ? snapshot_test_dp_docs : current_dp_docs;
