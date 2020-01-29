0a7dd585425ecbeba7f140454229aeff634919e1
Taewan Kang <tkang042@uottawa.ca>
2020-01-29 10:59:36 -0500

    initial functionality of panel filter

--- client/src/panels/PanelRenderer.js
+++ client/src/panels/PanelRenderer.js
@@ -9,16 +9,19 @@ export const panel_context = React.createContext(null);
 export const PanelRenderer = withRouter(
   class PanelRenderer_ extends React.Component {
     render(){
-      let {
+      const {
         subject,
         panel_key,
         history,
         active_bubble_id,
-      } = this.props;  
+        panel_filter,
+      } = this.props;
 
       const panel_obj = PanelRegistry.lookup(panel_key, subject.level);
 
       const panel_options = {history};
+      const panel_filter_arr = _.map(panel_filter, (value, key) => panel_filter[key] && key);
+      const isFiltered = _.intersection(panel_filter_arr, panel_obj.depends_on).length > 0;
 
       const { Provider } = panel_context;
 
@@ -28,6 +31,7 @@ export const PanelRenderer = withRouter(
         return null;
       }
       return (
+        isFiltered &&
         <div id={panel_key} tabIndex="0">
           <Provider value={ {active_bubble_id, panel_key, subject} }>
             { panel_obj.render(calculations, panel_options) }
@@ -36,7 +40,7 @@ export const PanelRenderer = withRouter(
       );
     }
     shouldComponentUpdate(nextProps){
-      return !shallowEqualObjectsOverKeys(nextProps, this.props, ['subject','panel_key']);
+      return !shallowEqualObjectsOverKeys(nextProps, this.props, ['subject', 'panel_key', 'panel_filter']);
     } 
   }
 );
