d23c7ee62903ec28005775934134725a39908b09
Stephen-ONeil <stephenlevioneil@gmail.com>
2020-02-13 12:32:36 -0500

    Add auto-kill of already open server mongod's when server/mongo/start_mongod.sh is run

--- server/mongo/start_mongod.sh
+++ server/mongo/start_mongod.sh
@@ -4,4 +4,13 @@ if [[ ! -e ./mongo/db ]]; then
   mkdir ./mongo/db
 fi
 
+if [[ -e ./mongo/db/mongod.lock ]]; then 
+  running_mongod_pid=$(lsof ./mongo/db/mongod.lock | awk 'NR == 2 {print $2}')
+
+  # sticks in loop until the process is actually dead, otherwise it's a race between the subsequent mongod starting and this old one finishing its cleanup
+  while kill $running_mongod_pid; do 
+    sleep 1
+  done
+fi
+
 mongod --config ./mongo/mongod.conf
