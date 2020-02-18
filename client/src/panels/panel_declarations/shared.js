121123c17748df6d63acd8e0a4f5a5d0468b1463
Kate Cook <kathleen.cook@tbs-sct.gc.ca>
2020-02-18 16:30:09 -0500

    move nivocharts into subdirectory and set up a shared within that

--- client/src/panels/panel_declarations/shared.js
+++ client/src/panels/panel_declarations/shared.js
@@ -29,7 +29,7 @@ import {
   AverageSharePie,
   get_formatter,
   infobase_colors_smart,
-} from '../../charts/NivoCharts.js';
+} from '../../charts/nivo/NivoCharts.js/index.js';
 import { Canada } from '../../charts/canada.js';
 import { FlatTreeMapViz } from '../../charts/flat_treemap/FlatTreeMapViz.js';
 
