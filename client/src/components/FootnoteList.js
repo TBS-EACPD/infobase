c1034ec80ce3dbac2e4187a1ea3734a5f2fe8494
Stephen-ONeil <stephenlevioneil@gmail.com>
2020-03-09 18:47:51 -0400

    Horrible code, but I've got the warning panel content doubling as dynamic footnotes like I wanted (big mess though, giant circular dependency too)

--- client/src/components/FootnoteList.js
+++ client/src/components/FootnoteList.js
@@ -5,20 +5,30 @@ import { sanitized_dangerous_inner_html } from '../general_utils.js';
 import { FancyUL } from './misc_util_components.js';
 
 
-const FootnoteList = ({ footnotes }) => <div className={"footnote-list"}>
-  <FancyUL>
-    {_.chain(footnotes)
-      .uniqBy("text")
-      .map( 
-        ({text}, ix) => <div 
-          className={"footnote-list__note"} 
-          key={ix} 
-          dangerouslySetInnerHTML={sanitized_dangerous_inner_html(text)}
-        />
-      )
-      .value()
-    }
-  </FancyUL>
-</div>;
+const FootnoteList = ({ footnotes }) => (
+  <div className={"footnote-list"}>
+    <FancyUL>
+      {_.chain(footnotes)
+        .uniqWith( ({text_1}, {text_2}) => !_.isUndefined(text_1) && text_1 === text_2)
+        .map( 
+          ({text, component}, ix) => {
+            const props = component ? 
+              ({ children: component }) :
+              ({ dangerouslySetInnerHTML: sanitized_dangerous_inner_html(text) });
+  
+            return (
+              <div 
+                {...props}
+                className={"footnote-list__note"} 
+                key={ix} 
+              />
+            );
+          }
+        )
+        .value()
+      }
+    </FancyUL>
+  </div>
+);
 
 export { FootnoteList };
