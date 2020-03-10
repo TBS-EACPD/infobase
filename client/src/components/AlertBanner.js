f8669bfe3a4a54482fa0c8978e8c97ac98957c88
Stephen-ONeil <stephenlevioneil@gmail.com>
2020-03-10 17:01:30 -0400

    AlertBanner (and LateDepartmentsBanner) style tweaks

---
+++ client/src/components/AlertBanner.js
@@ -0,0 +1,18 @@
+import './AlertBanner.scss';
+
+import classNames from 'classnames';
+
+const banner_classes = ['info', 'success', 'warning', 'danger'];
+export const AlertBanner = ({children, banner_class, additional_class_names, style}) => {
+  if ( banner_class && !_.includes(banner_classes, banner_class) ){
+    throw `AlertBanner received invalid banner_class prop of ${banner_class}`;
+  }
+
+  const banner_class_name = `alert-${banner_class || 'info'}`;
+
+  return (
+    <div className={classNames('ib-alert alert alert-no-symbol alert--is-bordered', banner_class_name, additional_class_names)} style={style}>
+      { children }
+    </div>
+  );
+};
