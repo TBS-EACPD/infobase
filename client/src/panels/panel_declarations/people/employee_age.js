e8d2011601f3ee9bf9a71e56554c28f91d6f7339
Stephen-ONeil <stephenlevioneil@gmail.com>
2020-03-11 14:07:11 -0400

    Remove airia-hidden from TabbedContent component, may have only been there because the employee_age panel, specifically, needed it (better to totally hide the controls there since the only content of the tabs are graphs that are aria-hidden themselves)

--- client/src/panels/panel_declarations/people/employee_age.js
+++ client/src/panels/panel_declarations/people/employee_age.js
@@ -166,28 +166,30 @@ export const declare_employee_age_panel = () => declare_panel({
             <TM k={level+"_employee_age_text"} args={info} />
           </Col>
           { !window.is_a11y_mode &&
-            <Col size={12} isGraph extraClasses="zero-padding"> 
-              <TabbedContent
-                tab_keys={["age_group", "avgage"]}
-                tab_labels={{
-                  age_group: text_maker("age_group"),
-                  avgage: text_maker("avgage"),
-                }}
-                tab_pane_contents={{
-                  age_group: (
-                    <div id={"emp_age_tab_pane"}>
-                      <LineBarToggleGraph {...age_group_options} />
-                      <div className='clearfix'></div>
-                    </div>
-                  ), 
-                  avgage: (
-                    <div id={"emp_age_tab_pane"}>
-                      <LineBarToggleGraph {...avg_age_options} />
-                      <div className='clearfix'></div>
-                    </div>
-                  ),
-                }}
-              />
+            <Col size={12} isGraph extraClasses="zero-padding">
+              <div aria-hidden="true">
+                <TabbedContent
+                  tab_keys={["age_group", "avgage"]}
+                  tab_labels={{
+                    age_group: text_maker("age_group"),
+                    avgage: text_maker("avgage"),
+                  }}
+                  tab_pane_contents={{
+                    age_group: (
+                      <div id={"emp_age_tab_pane"}>
+                        <LineBarToggleGraph {...age_group_options} />
+                        <div className='clearfix'></div>
+                      </div>
+                    ), 
+                    avgage: (
+                      <div id={"emp_age_tab_pane"}>
+                        <LineBarToggleGraph {...avg_age_options} />
+                        <div className='clearfix'></div>
+                      </div>
+                    ),
+                  }}
+                />
+              </div>
             </Col>
           }
           { window.is_a11y_mode &&
