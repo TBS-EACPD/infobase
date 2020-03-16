4d7f74c2f7cdb3523ae9fd1e7c48bcefb9b57392
Stephen-ONeil <stephenlevioneil@gmail.com>
2020-03-16 16:40:45 -0400

    Bunch of code refactoring in tp_by_region.js

--- client/src/panels/panel_declarations/finances/transfer_payments/tp_by_region.js
+++ client/src/panels/panel_declarations/finances/transfer_payments/tp_by_region.js
@@ -23,23 +23,23 @@ const { provinces, provinces_with_article } = businessConstants;
 const { A11YTable } = declarative_charts;
 
 
-const loadPopulation = () => {
-  const parse_csv_string = csv_string => _.tail( d3.csvParseRows( _.trim(csv_string) ) );
-  
-  return make_request( get_static_url(`csv/canadian_population_estimates_by_province.csv`) )
-    .then( csv_string => parse_csv_string(csv_string) )
-    .then((population_data) =>
-      _.chain(population_data)
-        .keyBy(_.first)
-        .mapValues(row => _.chain(row)
-          .tail()
-          .map(value => parseInt(value))
-          .value()
-        )
-        .value()
-    );
-};
+const load_population_data = () => make_request( get_static_url(`csv/canadian_population_estimates_by_province.csv`) )
+  .then(
+    (csv_string) => _.chain(csv_string)
+      .trim()
+      .thru( d3.csvParseRows )
+      .tail()
+      .map( ([prov_code, ...values]) => [prov_code, _.map(values, _.toInteger)] )
+      .fromPairs()
+      .value()
+  );
 
+
+const group_prov_data_by_year = (data_by_prov) => _.chain(data_by_prov)
+  .map( (values, prov_code) => _.map(values, (value) => [prov_code, value]) )
+  .unzip()
+  .map( _.fromPairs )
+  .value();
 
 const prepare_data_for_a11y_table = (data) => _.chain(data)
   .flatMap( _.keys )
@@ -65,55 +65,58 @@ class TPMap extends React.Component {
 
     this.state = {
       loading: true,
-      population: {},
+      population_data: {},
     };
   }
   componentDidMount(){
-    loadPopulation()
+    load_population_data()
       .then( 
-        (transformed) => this.setState({
+        (population_data) => this.setState({
           loading: false,
-          population: transformed,
+          population_data,
         })
       );
   }
   render(){
     const { calculations, footnotes, sources } = this.props;
-    const { loading, population } = this.state;
+    const { loading, population_data } = this.state;
 
     if (loading) {
       return (
         <div style = {{position: "relative", height: "80px", marginBottom: "-10px"}}>
           <SpinnerWrapper config_name={"sub_route"} />
-        </div>);
-    } else {
-      const tp_table = calculations.panel_args.table;
-
-      const get_subject_data_for_year = (year) => tp_table.prov_code(
-        year,
-        calculations.subject.level === 'dept' && calculations.subject.id
+        </div>
       );
+    } else {
+      const {
+        subject,
+        panel_args: {
+          table: transfer_payments_table,
+        },
+      } = calculations;
 
-      const data_tp = std_years.map(get_subject_data_for_year);
-      const data_tppc = std_years.map((year, i) => {
-        const single_year_tp_data = get_subject_data_for_year(year);
-        return _.chain( _.keys(single_year_tp_data) )
-          .pullAll(["na", "abroad"])
-          .map((prov) => {
-            const in_year_prov_transfer_payments = single_year_tp_data[prov];
-            const in_year_prov_population = population[prov][i];
-            return [
-              prov,
-              in_year_prov_transfer_payments/in_year_prov_population,
-            ];
-          })
-          .fromPairs()
-          .value();
-      });
+      const transfer_payments_data_by_prov = transfer_payments_table.prov_code(
+        std_years,
+        subject.level === 'dept' && subject.id
+      );
+      const per_capita_data_by_prov = _.chain(transfer_payments_data_by_prov)
+        .omitBy( (values, prov_code) => _.isUndefined(population_data[prov_code]) )
+        .mapValues(
+          (transfer_payment_values, prov_code) => _.chain(transfer_payment_values)
+            .zipWith(
+              population_data[prov_code],
+              (transfer_payment, population) => transfer_payment/population
+            )
+            .value()
+        )
+        .value();
+
+      const transfer_payment_data = group_prov_data_by_year(transfer_payments_data_by_prov);
+      const per_capita_data = group_prov_data_by_year(per_capita_data_by_prov);
       
       //REGULAR TP VERSION
-      const current_year_data_tp = _.last(data_tp);
-      const max_tp = _.chain(data_tp)
+      const current_year_data_tp = _.last(transfer_payment_data);
+      const max_tp = _.chain(transfer_payment_data)
         .last()
         .values()
         .max()
@@ -132,20 +135,20 @@ class TPMap extends React.Component {
       );
       const percent_of_total_tp = current_year_data_tp[largest_prov_tp] / total_sum_tp;
       const text_args_tp = {
+        subject,
         largest_prov: provinces_with_article[largest_prov_tp].text,
         largest_amount: formatter(current_year_data_tp[largest_prov_tp]),
         total_sum: formatter(total_sum_tp),
         percent_of_total: formats["percentage1_raw"](percent_of_total_tp),
-        subject: calculations.subject,
         show_per_capita: false,
       };
 
-      const should_per_capita_tab_be_disabled = _.every(data_tppc, _.isEmpty);
+      const should_per_capita_tab_be_disabled = _.every(per_capita_data, _.isEmpty);
       
       //TP PER CAPITA VERSION
-      const current_year_data_tppc = _.last(data_tppc);
+      const current_year_data_tppc = _.last(per_capita_data);
       
-      const max_tppc = _.chain(data_tppc)
+      const max_tppc = _.chain(per_capita_data)
         .last()
         .values()
         .max()
@@ -159,12 +162,12 @@ class TPMap extends React.Component {
         .maxBy( (prov) => current_year_data_tppc[prov] )
         .value();
       const text_args_tppc = {
+        subject,
         largest_prov: provinces_with_article[largest_prov_tppc].text,
         largest_amount: formatter(current_year_data_tp[largest_prov_tppc]),
         largest_per_capita: formatter(current_year_data_tppc[largest_prov_tppc]),
         total_sum: formatter(total_sum_tp),
         percent_of_total: formats["percentage1_raw"](percent_of_total_tp),
-        subject: calculations.subject,
         show_per_capita: true,
       };
 
@@ -189,7 +192,7 @@ class TPMap extends React.Component {
                     {!window.is_a11y_mode && 
                       <Canada
                         graph_args={{
-                          data: data_tp,
+                          data: transfer_payment_data,
                           color_scale: color_scale_tp,
                           years: std_years,
                           formatter: formatter,
@@ -205,7 +208,7 @@ class TPMap extends React.Component {
                     {!window.is_a11y_mode && 
                       <Canada
                         graph_args={{
-                          data: data_tppc,
+                          data: per_capita_data,
                           color_scale: color_scale_tppc,
                           years: std_years,
                           formatter: formatter,
@@ -223,12 +226,12 @@ class TPMap extends React.Component {
               <A11YTable
                 label_col_header = {text_maker("tp_a11y_table_title")}
                 data_col_headers = {_.map( std_years, y => run_template(y) )}
-                data = { prepare_data_for_a11y_table(data_tp) }
+                data = { prepare_data_for_a11y_table(transfer_payment_data) }
               />
               <A11YTable
                 label_col_header = {text_maker("tp_pc_a11y_table_title")}
                 data_col_headers = {_.map( std_years, y => run_template(y) )}
-                data = { prepare_data_for_a11y_table(data_tppc) }
+                data = { prepare_data_for_a11y_table(per_capita_data) }
               />
             </Col>
           }
