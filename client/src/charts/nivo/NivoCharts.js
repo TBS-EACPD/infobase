b88dc9dc8e59c9e2e485536e586b4b32579ee71d
Kate Cook <kathleen.cook@tbs-sct.gc.ca>
2020-04-02 10:39:15 -0400

    fix build errors introduced from rebasing

--- client/src/charts/nivo/NivoCharts.js
+++ client/src/charts/nivo/NivoCharts.js
@@ -2,6 +2,7 @@ import { NivoResponsiveBar, NivoResponsiveHBar } from './nivo_bar.js';
 import { NivoResponsiveLine } from './nivo_line.js';
 import { NivoResponsivePie } from './nivo_pie.js';
 import { LineBarToggleGraph } from './nivo_misc.js';
+import { CircleProportionChart } from './nivo_bubble.js';
 
 export {
   NivoResponsiveBar,
@@ -9,4 +10,5 @@ export {
   NivoResponsiveLine,
   NivoResponsivePie,
   LineBarToggleGraph,
+  CircleProportionChart,
 };
