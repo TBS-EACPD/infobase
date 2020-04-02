d7f3f54c74d7ab1b41657e2a61218262d2731469
Stephen-ONeil <stephenlevioneil@gmail.com>
2020-04-02 16:59:50 -0400

    Fix that last hotfix to deployed cloud function creds

--- server/deploy_scripts/prod_deploy_function.sh
+++ server/deploy_scripts/prod_deploy_function.sh
@@ -8,7 +8,7 @@ source deploy_scripts/set_prod_env_vars.sh
 source deploy_scripts/set_transient_function_secrets.sh # these trap EXIT to handle their own cleanup
 
 gcloud functions deploy prod-api-${CURRENT_SHA} --project ${PROJECT} --region us-central1 --entry-point app --stage-bucket prod-api-staging-bucket --runtime nodejs8 --trigger-http --env-vars-file $scratch/envs.yaml > /dev/null
-gcloud functions add-iam-policy-binding prod-api-${CURRENT_SHA} --member allUsers --role roles/cloudfunctions.invoker
+gcloud alpha functions add-iam-policy-binding prod-api-${CURRENT_SHA} --project ${PROJECT} --region us-central1 --member allUsers --role roles/cloudfunctions.invoker
 
 source deploy_scripts/unset_prod_env_vars.sh
 
