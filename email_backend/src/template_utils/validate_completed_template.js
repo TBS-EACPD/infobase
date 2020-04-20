98f4875767d0500dc16e4657469b93f7ed135eb4
Stephen-ONeil <stephenlevioneil@gmail.com>
2020-04-20 16:45:07 -0400

    Clarify some variable names in validate_completed_template.js

--- email_backend/src/template_utils/validate_completed_template.js
+++ email_backend/src/template_utils/validate_completed_template.js
@@ -20,30 +20,30 @@ const verify_required_fields_present = (field_templates, completed_fields) => _.
 
 const verify_values_are_expected_and_match_value_types = (field_templates, completed_fields) => _.chain(completed_fields)
   .map(
-    (field_value, field_key) => {
+    (submitted_value, field_key) => {
       const expected_type = _.get(field_templates, `${field_key}.value_type`);
       const is_required = _.get(field_templates, `${field_key}.required`);
 
       if (expected_type){
         switch (expected_type){
           case "string":
-            return _.isString(field_value);
+            return _.isString(submitted_value);
           case "number":
-            return _.isNumber(field_value);
+            return _.isNumber(submitted_value);
           case "json":
-            return _.isObject(field_value);
+            return _.isObject(submitted_value);
           case "enums":
             return _.chain(field_templates)
               .get(`${field_key}.enum_values`)
               .keys()
               .thru(
                 (enum_values) => {
-                  const some_submitted_keys_are_not_enum_options = _.without(field_value, ...enum_values).length > 0;
-                  const required_field_but_no_valid_values = is_required && _.intersection(field_value, enum_values).length === 0;
-                  const radio_form_but_multiple_values = field_templates[field_key].form_type === "radio" && field_value.length > 1;
+                  const some_submitted_values_are_not_enum_options = _.without(submitted_value, ...enum_values).length > 0;
+                  const required_field_but_no_valid_values = is_required && _.intersection(submitted_value, enum_values).length === 0;
+                  const radio_form_but_multiple_values = field_templates[field_key].form_type === "radio" && submitted_value.length > 1;
 
                   return (
-                    !some_submitted_keys_are_not_enum_options && 
+                    !some_submitted_values_are_not_enum_options && 
                     !required_field_but_no_valid_values &&
                     !radio_form_but_multiple_values
                   );
