f9a961038185609bb3f492d11220bffe89631bdb
Stephen-ONeil <stephenlevioneil@gmail.com>
2020-04-24 16:59:04 -0400

    Attempt permissions on CI deployed API functions

--- server/deploy_scripts/ci_deploy_function.sh
+++ server/deploy_scripts/ci_deploy_function.sh
@@ -12,5 +12,6 @@ echo "USE_REMOTE_DB: '1'" >> ./envs.yaml
 source ../scripts/ci_scripts/redact_env_vars_from_logging.sh "redact-start"
 
 gcloud functions deploy $CIRCLE_BRANCH --entry-point app --stage-bucket api-staging-bucket --runtime nodejs8 --trigger-http --env-vars-file ./envs.yaml
+gcloud alpha functions add-iam-policy-binding $CIRCLE_BRANCH --member allUsers --role roles/cloudfunctions.invoker
 
 source ../scripts/ci_scripts/redact_env_vars_from_logging.sh "redact-end"
