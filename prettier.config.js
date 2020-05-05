ed5ebc6c59872a6a700a0f2eb8999a1874ec8395
AlexCLeduc <ledualex@gmail.com>
2020-05-05 10:47:43 -0400

    - add prettier as top-level dependency - configure prettier - add npm commands

---
+++ prettier.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  printWidth: 95,
+  useTabs: false,
+  tabWidth: 2,
+  semi: true,
+  quoteProps: "as-needed",
+  trailingComma: "all",
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  arrowParens: "avoid",
+  proseWrap: "never",
+  endOfLine: "auto",
+};
