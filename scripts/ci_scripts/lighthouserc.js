5d3aec9ad4d991120cad35b48f19be4bad392180
Taewan Kang <tkang042@uottawa.ca>
2020-05-06 10:54:49 -0400

    add list of routes from browser tests

--- scripts/ci_scripts/lighthouserc.js
+++ scripts/ci_scripts/lighthouserc.js
@@ -1,9 +1,11 @@
+const { route_load_tests_config } = require('../../client/browser-tests/route-load-tests-config.js');
+
 module.exports = {
   "ci": {
     "collect": {
-      "url": [
-        `https://dev.ea-ad.ca/${process.env.CIRCLE_BRANCH}/index-eng.html#start`
-      ]
+      "url": _.map(route_load_tests_config, ({route}) => 
+        `https://dev.ea-ad.ca/${process.env.CIRCLE_BRANCH}/index-eng.html#${route}`
+      )
     },
     "upload": {
       "target": "temporary-public-storage"
