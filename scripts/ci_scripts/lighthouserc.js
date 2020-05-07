478b37d23d0580674f50079cb3b77d095c0f8f52
Taewan Kang <tkang042@uottawa.ca>
2020-05-07 11:00:01 -0400

    run each tests only once

--- scripts/ci_scripts/lighthouserc.js
+++ scripts/ci_scripts/lighthouserc.js
@@ -4,6 +4,7 @@ const { route_load_tests_config } = require('../../client/browser-tests/route-lo
 module.exports = {
   "ci": {
     "collect": {
+      "numberOfRuns": 1,
       "url": _.map(route_load_tests_config, ({route}) => 
         `https://dev.ea-ad.ca/${process.env.CIRCLE_BRANCH}/index-eng.html#${route}`
       )
