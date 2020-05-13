72a0e277dc2b240d20ebf139890e564a9e68a698
Taewan Kang <tkang042@uottawa.ca>
2020-05-13 10:50:11 -0400

    separate util component, css refinement

---
+++ client/src/components/DisplayTableUtils.js
@@ -0,0 +1,28 @@
+import "./DisplayTableUtils.scss";
+
+import { WriteToClipboard } from "./WriteToClipboard.js";
+import { IconCopy } from "../icons/icons.js";
+
+export class DisplayTableUtils extends React.Component {
+  render() {
+    const { data_to_csv_string } = this.props;
+
+    return (
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "row-reverse",
+          paddingBottom: 5,
+          width: "99%",
+        }}
+      >
+        <WriteToClipboard
+          text_to_copy={data_to_csv_string}
+          button_class_name={"display-table-heading-utils"}
+          IconComponent={IconCopy}
+          icon_color={window.infobase_color_constants.secondaryColor}
+        />
+      </div>
+    );
+  }
+}
