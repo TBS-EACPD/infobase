0d8d951ebc36829e1e9e3dbda91227f00e144382
Stephen-ONeil <stephenlevioneil@gmail.com>
2020-05-19 16:23:56 -0400

    Cleanup in LabeledTable, throw out an old, unused code branch for a version that was used as a hacky navigation menu. Throw out a bunch of old styles and generally clean up the scss module

--- client/src/components/LabeledTable.js
+++ client/src/components/LabeledTable.js
@@ -5,59 +5,29 @@ export class LabeledTable extends React.Component {
   render() {
     const { title, content, styles } = this.props;
     return (
-      <div className="labeled-table">
+      <section className="labeled-table">
         <div className="labeled-table__header">{title}</div>
         <div className="labeled-table__items">
-          {_.map(content, (item, ix) =>
-            item.href ? (
-              <a
-                href={item.href}
-                role="radio"
-                title={title}
-                className={classNames(
-                  "labeled-table__item",
-                  item.active && "active"
+          {_.map(content, (item, ix) => (
+            <div className="labeled-table__item" key={ix}>
+              <div className="labeled-table__item-label">
+                {styles && styles.header ? (
+                  <span className={styles.header}>{item.name}</span>
+                ) : (
+                  item.name
                 )}
-                tabIndex={0}
-                aria-checked={item.active}
-                key={ix}
-              >
-                <div className="labeled-table__item-label">
-                  {styles && styles.header ? (
-                    <span className={styles.header}>{item.name}</span>
-                  ) : (
-                    item.name
-                  )}
-                </div>
-                <div className="labeled-table__item-description">
-                  {styles && styles.desc ? (
-                    <span className={styles.desc}>{item.desc}</span>
-                  ) : (
-                    item.desc
-                  )}
-                </div>
-              </a>
-            ) : (
-              <div className="labeled-table__item" key={ix}>
-                <div className="labeled-table__item-label">
-                  {styles && styles.header ? (
-                    <span className={styles.header}>{item.name}</span>
-                  ) : (
-                    item.name
-                  )}
-                </div>
-                <div className="labeled-table__item-description">
-                  {styles && styles.desc ? (
-                    <span className={styles.desc}>{item.desc}</span>
-                  ) : (
-                    item.desc
-                  )}
-                </div>
               </div>
-            )
-          )}
+              <div className="labeled-table__item-description">
+                {styles && styles.desc ? (
+                  <span className={styles.desc}>{item.desc}</span>
+                ) : (
+                  item.desc
+                )}
+              </div>
+            </div>
+          ))}
         </div>
-      </div>
+      </section>
     );
   }
 }
