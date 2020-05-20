83e8912fbc9c59e508970869c05da4a09e2ef23d
Stephen-ONeil <stephenlevioneil@gmail.com>
2020-05-20 17:34:18 -0400

    Added check to FancyUL to enforce the title prop being a string... second time I've done this, really should add prop types to the project.

--- client/src/components/FancyUL.js
+++ client/src/components/FancyUL.js
@@ -2,16 +2,24 @@ import "./FancyUL.scss";
 
 import classNames from "classnames";
 
-export const FancyUL = ({ className, title, children }) => (
-  <ul className={classNames("fancy-ul", className)} aria-label={title}>
-    {title && (
-      <li className={"fancy-ul__title"} aria-hidden={true}>
-        {title}
-      </li>
-    )}
-    {_.chain(children)
-      .compact()
-      .map((item, i) => <li key={i}>{item}</li>)
-      .value()}
-  </ul>
-);
+export const FancyUL = ({ className, title, children }) => {
+  if (!_.isString(title)) {
+    throw new Error(
+      "FancyUL title prop must be a string, for use in its aria-label attribute."
+    );
+  }
+
+  return (
+    <ul className={classNames("fancy-ul", className)} aria-label={title}>
+      {title && (
+        <li className={"fancy-ul__title"} aria-hidden={true}>
+          {title}
+        </li>
+      )}
+      {_.chain(children)
+        .compact()
+        .map((item, i) => <li key={i}>{item}</li>)
+        .value()}
+    </ul>
+  );
+};
