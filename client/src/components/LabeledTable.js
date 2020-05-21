771c00f00b76c685352480c5d89d209f76a0842a
Stephen-ONeil <stephenlevioneil@gmail.com>
2020-05-21 09:52:30 -0400

    Ah, we did have prop-types, we just weren't using it. Should do that more. Fix up prop type checks on FancyUL and LAbeledTable, tweak their flexibility with a prop for TitleComponent

--- client/src/components/LabeledTable.js
+++ client/src/components/LabeledTable.js
@@ -1,42 +1,46 @@
 import "./LabeledTable.scss";
 
-export class LabeledTable extends React.Component {
-  render() {
-    const { title, contents } = this.props;
+import PropTypes from "prop-types";
 
-    const title_and_labels_are_strings = !_.some(
-      [title, ..._.map(contents, "label")],
-      !_.isString
+export const LabeledTable = ({ title, TitleComponent, contents }) => (
+  <section className="labeled-table" aria-label={title}>
+    {title && (
+      <div className="labeled-table__header" aria-hidden={true}>
+        {TitleComponent ? <TitleComponent>{title}</TitleComponent> : title}
+      </div>
+    )}
+    <div className="labeled-table__items">
+      {_.map(contents, ({ id, label, content }, ix) => (
+        <div
+          className="labeled-table__item"
+          key={id || ix}
+          id={id}
+          tabIndex={0}
+          aria-label={label}
+        >
+          <div className="labeled-table__item-label" aria-hidden={true}>
+            {label}
+          </div>
+          <div className="labeled-table__item-description">{content}</div>
+        </div>
+      ))}
+    </div>
+  </section>
+);
+
+LabeledTable.propTypes = {
+  title: PropTypes.string,
+  contents: (props, propName, componentName) => {
+    const contents = props[propName];
+    const all_labels_are_strings = !_.some(
+      contents,
+      ({ label }) => !_.isString(label)
     );
 
-    if (!title_and_labels_are_strings) {
-      throw new Error(
-        "LabeledTable component requires a title and content labels that are strings. Content may be string or component."
+    if (!all_labels_are_strings) {
+      return new Error(
+        `\`${componentName}\`: \`${propName}\` prop's \`label\` values must be strings`
       );
     }
-
-    return (
-      <section className="labeled-table" aria-label={title}>
-        <div className="labeled-table__header" aria-hidden={true}>
-          {title}
-        </div>
-        <div className="labeled-table__items">
-          {_.map(contents, ({ id, label, content }, ix) => (
-            <div
-              className="labeled-table__item"
-              key={id || ix}
-              id={id}
-              tabIndex={0}
-              aria-label={label}
-            >
-              <div className="labeled-table__item-label" aria-hidden={true}>
-                {label}
-              </div>
-              <div className="labeled-table__item-description">{content}</div>
-            </div>
-          ))}
-        </div>
-      </section>
-    );
-  }
-}
+  },
+};
