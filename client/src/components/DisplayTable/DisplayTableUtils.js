cc174f6849291bc68a368ab1d568cb518e431b2f
Stephen-ONeil <stephenlevioneil@gmail.com>
2020-05-21 17:10:20 -0400

    Rearrange DisplayTableUtils code and HTML structure, clear up disjointment between header util container and table by putting them within a shared parent (which owns the background colour and rounded border)

--- client/src/components/DisplayTable/DisplayTableUtils.js
+++ client/src/components/DisplayTable/DisplayTableUtils.js
@@ -1,49 +1,47 @@
 import "./DisplayTableUtils.scss";
 import text from "./DisplayTable.yaml";
-import { create_text_maker_component } from "../misc_util_components.js";
 
+import { Fragment } from "react";
+
+import { create_text_maker_component } from "../misc_util_components.js";
 import { WriteToClipboard } from "../WriteToClipboard.js";
 import { IconCopy, IconDownload } from "../../icons/icons.js";
 
 const { text_maker } = create_text_maker_component(text);
 
-export class DisplayTableUtils extends React.Component {
-  downloadCsv() {
-    const { csv_string, table_name } = this.props;
-    const uri = "data:text/csv;charset=UTF-8," + encodeURIComponent(csv_string);
+export const DisplayTableCopy = ({ csv_string }) => (
+  <WriteToClipboard
+    text_to_copy={csv_string}
+    button_class_name={"display_table__util-icon"}
+    button_description={text_maker("copy_table_data_desc")}
+    IconComponent={IconCopy}
+    icon_color={window.infobase_color_constants.backgroundColor}
+  />
+);
 
-    const temporary_anchor = document.createElement("a");
-    temporary_anchor.setAttribute(
-      "download",
-      `${table_name ? table_name : "table"}.csv`
-    );
-    temporary_anchor.setAttribute("href", uri);
-    temporary_anchor.dispatchEvent(new MouseEvent("click"));
-  }
-  render() {
-    const { csv_string } = this.props;
+const download_csv = (csv_string, table_name) => {
+  const uri = "data:text/csv;charset=UTF-8," + encodeURIComponent(csv_string);
 
-    return (
-      <div className="dp-utils-heading-container">
-        {!window.feature_detection.is_IE() && (
-          <button
-            onClick={() => this.downloadCsv()}
-            className={"dp-utils-heading"}
-          >
-            <IconDownload
-              title={text_maker("download_table_data_desc")}
-              color={window.infobase_color_constants.backgroundColor}
-            />
-          </button>
-        )}
-        <WriteToClipboard
-          text_to_copy={csv_string}
-          button_class_name={"dp-utils-heading"}
-          button_description={text_maker("copy_table_data_desc")}
-          IconComponent={IconCopy}
-          icon_color={window.infobase_color_constants.backgroundColor}
+  const temporary_anchor = document.createElement("a");
+  temporary_anchor.setAttribute(
+    "download",
+    `${table_name ? table_name : text_maker("table")}.csv`
+  );
+  temporary_anchor.setAttribute("href", uri);
+  temporary_anchor.dispatchEvent(new MouseEvent("click"));
+};
+export const DisplayTableDownload = ({ csv_string, table_name }) => (
+  <Fragment>
+    {!window.feature_detection.is_IE() && (
+      <button
+        onClick={() => download_csv(csv_string, table_name)}
+        className={"display_table__util-icon"}
+      >
+        <IconDownload
+          title={text_maker("download_table_data_desc")}
+          color={window.infobase_color_constants.backgroundColor}
         />
-      </div>
-    );
-  }
-}
+      </button>
+    )}
+  </Fragment>
+);
--- client/src/components/DisplayTable/index.js
+++ client/src/components/DisplayTable/index.js
@@ -1,3 +1,6 @@
 export { DisplayTable } from "./DisplayTable.js";
 
-export { default_dept_name_sort_func, sort_func_template } from "./utils";
+export {
+  default_dept_name_sort_func,
+  sort_func_template,
+} from "./sort_utils.js";
