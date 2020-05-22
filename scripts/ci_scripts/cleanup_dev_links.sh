f0a59865581f85290bde7b57376d612d62135e2b
Stephen-ONeil <stephenlevioneil@gmail.com>
2020-05-22 16:30:40 -0400

    Start of auto-cleanup of stale dev links, pushing to confirm portability of the bash I've started writing

---
+++ scripts/ci_scripts/cleanup_dev_links.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+
+echo "Starting dev link MongoDB cleanup..."
+
+source ~/InfoBase/scripts/ci_scripts/redact_env_vars_from_logging.sh "redact-start"
+
+active_branches=git branch -r | sed -E 's/(.*\/)(.*)/\2/g'
+
+dev_link_buckets=gsutil ls $GCLOUD_BUCKET_ROOT | /
+  sed -E 's/(.*\/)(.*)(\/$)/\2/g' | /
+  sed -e '/(^__)|(^archived__).*/d'
+
+inactive_dev_link_buckets=grep -Fxvf <(active_branches) <(dev_link_buckets)
+
+echo $inactive_dev_link_buckets # log to test portability in CI
+
+source ~/InfoBase/scripts/ci_scripts/redact_env_vars_from_logging.sh "redact-end"
