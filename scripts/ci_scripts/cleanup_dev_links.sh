d2d1154eb41ea835e01ccf1877392d92599b7e82
Stephen-ONeil <stephenlevioneil@gmail.com>
2020-05-22 19:43:06 -0400

    Finish cleanup_dev_links.sh, placeholder for dead_dev_link.html

--- scripts/ci_scripts/cleanup_dev_links.sh
+++ scripts/ci_scripts/cleanup_dev_links.sh
@@ -3,17 +3,19 @@ set -e
 
 echo "Starting dev link bucket cleanup..."
 
-#source ~/InfoBase/scripts/ci_scripts/redact_env_vars_from_logging.sh "redact-start"
-GCLOUD_BUCKET_ROOT="gs://dev.ea-ad.ca"
+source ~/InfoBase/scripts/ci_scripts/redact_env_vars_from_logging.sh "redact-start"
 
 active_branches=$(git branch -r | sed -E 's/(.*\/)(.*)/\2/g')
 
-dev_link_buckets=$(gsutil ls $GCLOUD_BUCKET_ROOT | \
+branches_with_dev_link_buckets=$(gsutil ls $GCLOUD_BUCKET_ROOT | \
   sed -E 's/(.*\/)(.*)(\/$)/\2/g' | \
   sed -E '/(^__)|(^archived__).*/d')
 
-inactive_dev_link_buckets=$(grep -Fxvf <(echo "$active_branches") <(echo "$dev_link_buckets"))
+inactive_branches_with_dev_link_buckets=$(grep -Fxvf <(echo "$active_branches") <(echo "$branches_with_dev_link_buckets"))
 
-echo $inactive_dev_link_buckets
+while IFS= read -r branch_name;
+  gsutil cp -a public-read "./scripts/ci_scripts/dead_dev_link.html" "$GCLOUD_BUCKET_ROOT/$branch_name/index-eng.html"
+  gsutil cp -a public-read "./scripts/ci_scripts/dead_dev_link.html" "$GCLOUD_BUCKET_ROOT/$branch_name/index-fra.html"
+done <<< "$inactive_branches_with_dev_link_buckets"
 
-#source ~/InfoBase/scripts/ci_scripts/redact_env_vars_from_logging.sh "redact-end"
+source ~/InfoBase/scripts/ci_scripts/redact_env_vars_from_logging.sh "redact-end"
