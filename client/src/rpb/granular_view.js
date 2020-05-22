9292538b293fb15140b7ba8712c4360983ec942f
Taewan Kang <tkang042@uottawa.ca>
2020-05-22 14:51:44 -0400

    moving around config items, removing filter row and other minor style updates and code clean up

--- client/src/rpb/granular_view.js
+++ client/src/rpb/granular_view.js
@@ -1,11 +1,5 @@
 import { TextMaker, text_maker } from "./rpb_text_provider.js";
-import {
-  ReportDetails,
-  ReportDatasets,
-  ExportButton,
-  ShareReport,
-  NoDataMessage,
-} from "./shared.js";
+import { ReportDetails, ReportDatasets, NoDataMessage } from "./shared.js";
 import {
   TwoLevelSelect,
   LabeledBox,
@@ -22,54 +16,9 @@ const PAGE_SIZE = 600;
 
 class GranularView extends React.Component {
   render() {
-    const {
-      subject,
-      table,
-      dimension,
-      filter,
-
-      flat_data,
-      filters_by_dimension,
-
-      on_set_filter,
-    } = this.props;
+    const { subject, table } = this.props;
     return (
       <div>
-        <LabeledBox label={<TextMaker text_key="rpb_table_controls" />}>
-          <div className="row">
-            <div className="col-md-6">
-              <div className="rpb-config-item">
-                <label className="rpb-config-header" htmlFor="filt_select">
-                  Filter
-                </label>
-                <TwoLevelSelect
-                  className="form-control form-control-ib"
-                  id="filt_select"
-                  onSelect={(id) => {
-                    const [dim_key, filt_key] = id.split("__");
-                    on_set_filter({ filter: filt_key, dimension: dim_key });
-                  }}
-                  selected={dimension + "__" + filter}
-                  grouped_options={_.mapValues(
-                    filters_by_dimension,
-                    (filter_by_dim) => ({
-                      ...filter_by_dim,
-                      children: _.sortBy(filter_by_dim.children, "display"),
-                    })
-                  )}
-                />
-              </div>
-              <div className="rpb-config-item">
-                <ExportButton
-                  id="export_button"
-                  get_csv_string={() => this.get_csv_string()}
-                />
-              </div>
-              <ShareReport />
-            </div>
-            <div className="clearfix" />
-          </div>
-        </LabeledBox>
         <LabeledBox
           label={
             <TextMaker
@@ -91,18 +40,12 @@ class GranularView extends React.Component {
           <ReportDatasets {...this.props} />
         </LabeledBox>
         {table.rpb_banner && <AlertBanner>{table.rpb_banner}</AlertBanner>}
-        <div id="rpb-main-content">
-          {_.isEmpty(flat_data) ? (
-            <NoDataMessage />
-          ) : (
-            this.get_plain_table_content()
-          )}
-        </div>
+        <div id="rpb-main-content">{this.get_table_content()}</div>
       </div>
     );
   }
 
-  get_plain_table_content(excel_mode = false) {
+  get_table_content(excel_mode = false) {
     const {
       columns: data_columns,
       page_num,
@@ -110,6 +53,10 @@ class GranularView extends React.Component {
       sorted_key_columns,
 
       on_set_page,
+      on_set_filter,
+      filters_by_dimension,
+      dimension,
+      filter,
     } = this.props;
 
     const pages = _.chunk(flat_data, PAGE_SIZE);
@@ -154,88 +101,91 @@ class GranularView extends React.Component {
 
     return (
       <div>
-        <DisplayTable data={table_data} column_configs={column_configs} />
-
-        {!excel_mode && pages.length > 1 && (
-          <div className="pagination-container">
-            {window.is_a11y_mode && (
-              <p>
-                <TextMaker
-                  text_key="pagination_a11y"
-                  args={{ current: page_num, total: pages.length }}
-                />
-              </p>
+        <div className="rpb-config-item">
+          <label className="rpb-config-header" htmlFor="filt_select">
+            {text_maker("filter_data")}
+          </label>
+          <TwoLevelSelect
+            className="form-control form-control-ib"
+            id="filt_select"
+            onSelect={(id) => {
+              const [dim_key, filt_key] = id.split("__");
+              on_set_filter({ filter: filt_key, dimension: dim_key });
+            }}
+            selected={`${dimension}__${filter}`}
+            grouped_options={_.mapValues(
+              filters_by_dimension,
+              (filter_by_dim) => ({
+                ...filter_by_dim,
+                children: _.sortBy(filter_by_dim.children, "display"),
+              })
             )}
-            <ul className="pagination">
-              {_.map(pages, (data, ix) => (
-                <li
-                  key={ix}
-                  className={classNames(ix === page_num && "active")}
-                >
-                  <span
-                    tabIndex={0}
-                    style={
-                      ix === page_num
-                        ? {
-                            color:
-                              window.infobase_color_constants.textLightColor,
-                          }
-                        : null
-                    }
-                    disabled={page_num === ix}
-                    role="button"
-                    onClick={
-                      ix === page_num
-                        ? null
-                        : () => {
-                            on_set_page(ix);
-                            this.refs.table.focus();
-                          }
-                    }
-                    onKeyDown={
-                      ix === page_num
-                        ? null
-                        : (e) => {
-                            if (e.keyCode === 13 || e.keyCode === 32) {
-                              on_set_page(ix);
-                              this.refs.table.focus();
-                            }
-                          }
-                    }
-                  >
-                    {ix + 1}
-                  </span>
-                </li>
-              ))}
-            </ul>
+          />
+        </div>
+        {_.isEmpty(flat_data) ? (
+          <NoDataMessage />
+        ) : (
+          <div>
+            <DisplayTable data={table_data} column_configs={column_configs} />
+            {!excel_mode && pages.length > 1 && (
+              <div className="pagination-container">
+                {window.is_a11y_mode && (
+                  <p>
+                    <TextMaker
+                      text_key="pagination_a11y"
+                      args={{ current: page_num, total: pages.length }}
+                    />
+                  </p>
+                )}
+                <ul className="pagination">
+                  {_.map(pages, (data, ix) => (
+                    <li
+                      key={ix}
+                      className={classNames(ix === page_num && "active")}
+                    >
+                      <span
+                        tabIndex={0}
+                        style={
+                          ix === page_num
+                            ? {
+                                color:
+                                  window.infobase_color_constants
+                                    .textLightColor,
+                              }
+                            : null
+                        }
+                        disabled={page_num === ix}
+                        role="button"
+                        onClick={
+                          ix === page_num
+                            ? null
+                            : () => {
+                                on_set_page(ix);
+                                this.refs.table.focus();
+                              }
+                        }
+                        onKeyDown={
+                          ix === page_num
+                            ? null
+                            : (e) => {
+                                if (e.keyCode === 13 || e.keyCode === 32) {
+                                  on_set_page(ix);
+                                  this.refs.table.focus();
+                                }
+                              }
+                        }
+                      >
+                        {ix + 1}
+                      </span>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            )}
           </div>
         )}
       </div>
     );
-  }
-
-  get_csv_string() {
-    const { flat_data, sorted_key_columns, columns } = this.props;
-
-    const non_dept_key_columns = _.reject(sorted_key_columns, { nick: "dept" });
-
-    const cols = ["dept", ...non_dept_key_columns, ...columns];
-
-    const headers = [
-      text_maker("org"),
-      ..._.map([...non_dept_key_columns, ...columns], "fully_qualified_name"),
-    ];
-
-    const array_based_rows = _.chain(flat_data)
-      .map((row) =>
-        _.map(cols, (col) =>
-          col === "dept" ? Dept.lookup(row.dept).name : row[col.nick]
-        )
-      )
-      .value();
-
-    const headers_and_rows = [headers].concat(array_based_rows);
-    return d3.csvFormatRows(headers_and_rows);
   }
 }
 
