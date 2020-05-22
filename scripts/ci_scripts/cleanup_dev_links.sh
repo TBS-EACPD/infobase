28e5709f83138e071aaf8d7025b4d8e4e55fb459
Stephen-ONeil <stephenlevioneil@gmail.com>
2020-05-22 17:08:32 -0400

    Authenticate the correct service worker in cleanup_dev_links

--- scripts/ci_scripts/cleanup_dev_links.sh
+++ scripts/ci_scripts/cleanup_dev_links.sh
@@ -11,7 +11,7 @@ dev_link_buckets=$(gsutil ls $GCLOUD_BUCKET_ROOT | \
   sed -E 's/(.*\/)(.*)(\/$)/\2/g' | \
   sed -e '/(^__)|(^archived__).*/d')
 
-inactive_dev_link_buckets=$(grep -Fxvf <(active_branches) <(dev_link_buckets))
+inactive_dev_link_buckets=$(grep -Fxvf <($active_branches) <($dev_link_buckets))
 
 echo $inactive_dev_link_buckets # log to test portability in CI
 
