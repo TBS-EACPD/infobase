ab16b706f91ee11251af74fc4f0f6b07e48186d1
Stephen-ONeil <stephenlevioneil@gmail.com>
2020-05-22 17:00:37 -0400

    bash syntax fix

--- scripts/ci_scripts/cleanup_dev_links.sh
+++ scripts/ci_scripts/cleanup_dev_links.sh
@@ -5,13 +5,13 @@ echo "Starting dev link MongoDB cleanup..."
 
 source ~/InfoBase/scripts/ci_scripts/redact_env_vars_from_logging.sh "redact-start"
 
-active_branches=git branch -r | sed -E 's/(.*\/)(.*)/\2/g'
+active_branches=$(git branch -r | sed -E 's/(.*\/)(.*)/\2/g')
 
-dev_link_buckets=gsutil ls $GCLOUD_BUCKET_ROOT | /
-  sed -E 's/(.*\/)(.*)(\/$)/\2/g' | /
-  sed -e '/(^__)|(^archived__).*/d'
+dev_link_buckets=$(gsutil ls $GCLOUD_BUCKET_ROOT | \
+  sed -E 's/(.*\/)(.*)(\/$)/\2/g' | \
+  sed -e '/(^__)|(^archived__).*/d')
 
-inactive_dev_link_buckets=grep -Fxvf <(active_branches) <(dev_link_buckets)
+inactive_dev_link_buckets=$(grep -Fxvf <(active_branches) <(dev_link_buckets))
 
 echo $inactive_dev_link_buckets # log to test portability in CI
 
