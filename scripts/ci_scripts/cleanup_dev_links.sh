4528e48cb502f582aae80334cae4a468343c611d
Stephen-ONeil <stephenlevioneil@gmail.com>
2020-05-22 18:35:42 -0400

    Fix bad process substitution

--- scripts/ci_scripts/cleanup_dev_links.sh
+++ scripts/ci_scripts/cleanup_dev_links.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 set -e
 
-echo "Starting dev link MongoDB cleanup..."
+echo "Starting dev link bucket cleanup..."
 
-source ~/InfoBase/scripts/ci_scripts/redact_env_vars_from_logging.sh "redact-start"
+#source ~/InfoBase/scripts/ci_scripts/redact_env_vars_from_logging.sh "redact-start"
+GCLOUD_BUCKET_ROOT="gs://dev.ea-ad.ca"
 
 active_branches=$(git branch -r | sed -E 's/(.*\/)(.*)/\2/g')
 
 dev_link_buckets=$(gsutil ls $GCLOUD_BUCKET_ROOT | \
   sed -E 's/(.*\/)(.*)(\/$)/\2/g' | \
-  sed -e '/(^__)|(^archived__).*/d')
+  sed -E '/(^__)|(^archived__).*/d')
 
-inactive_dev_link_buckets=$(grep -Fxvf <($active_branches) <($dev_link_buckets))
+inactive_dev_link_buckets=$(grep -Fxvf <(echo "$active_branches") <(echo "$dev_link_buckets"))
 
-echo $inactive_dev_link_buckets # log to test portability in CI
+echo $inactive_dev_link_buckets
 
-source ~/InfoBase/scripts/ci_scripts/redact_env_vars_from_logging.sh "redact-end"
+#source ~/InfoBase/scripts/ci_scripts/redact_env_vars_from_logging.sh "redact-end"
