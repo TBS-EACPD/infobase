2cc5fbd70c831142ef3be05782c3e147d6ee9092
Stephen-ONeil <stephenlevioneil@gmail.com>
2020-05-29 17:10:33 -0400

    Centralize footnote topic text keys, throw error if a new topic key is used without an associated text key. Use in FootnoteList FootnoteMetaTopics

--- client/src/rpb/rpb_text_provider.js
+++ client/src/rpb/rpb_text_provider.js
@@ -1,10 +1,14 @@
 import rpb_text from "./rpb.yaml";
+import footnote_topic_text from "../models/footnotes/footnote_topics.yaml";
 import {
   create_text_maker_component,
   TextMaker as StandardTextMaker,
 } from "../components/index.js";
 
-export const { text_maker, TM } = create_text_maker_component(rpb_text);
+export const { text_maker, TM } = create_text_maker_component([
+  rpb_text,
+  footnote_topic_text,
+]);
 export const TextMaker = (props) => (
   <StandardTextMaker text_maker_func={text_maker} {...props} />
 );
