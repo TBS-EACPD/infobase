9b13ff1283ce32b4acbef3ecf03b4e66a20a50d4
Stephen-ONeil <stephenlevioneil@gmail.com>
2020-06-01 15:57:07 -0400

    Update home quick links post-SEA

--- client/src/home/home-data.js
+++ client/src/home/home-data.js
@@ -1,27 +1,24 @@
 const featured_content_items = _.compact([
   {
-    text_key: "quick_link_DP_2021",
-    href: "#orgs/gov/gov/infograph/results/.-.-(panel_key.-.-'gov_dp)",
-    is_new: "true",
-  },
-  {
-    text_key: "quick_link_main_estimates",
+    text_key: "supps_a",
     href: "#compare_estimates",
     is_new: "true",
   },
   {
+    text_key: "quick_link_DP_2021",
+    href: "#orgs/gov/gov/infograph/results/.-.-(panel_key.-.-'gov_dp)",
+  },
+  {
+    text_key: "quick_link_main_estimates",
+    href: "#partition/est_doc_mains/estimates",
+  },
+  {
     text_key: "quick_link_DRR_1819",
     href: "#orgs/gov/gov/infograph/results/.-.-(panel_key.-.-'gov_drr)",
-    is_new: "true",
   },
   {
     text_key: "supps_b",
-    href: {
-      en:
-        "#rpb/.-.-(subject.-.-'gov_gov.-.-preferDeptBreakout.-.-true.-.-mode.-.-'simple.-.-table.-.-'orgVoteStatEstimates.-.-columns.-.-(.-.-'*7b*7best_last_year*7d*7d_estimates).-.-dimension.-.-'by_estimates_doc.-.-filter.-.-'Supp.*20Estimates*20B.-.-sort_col.-.-'dept.-.-descending.-.-false)",
-      fr:
-        "#rpb/.-.-(subject.-.-'gov_gov.-.-preferDeptBreakout.-.-true.-.-mode.-.-'simple.-.-table.-.-'orgVoteStatEstimates.-.-columns.-.-(.-.-'*7b*7best_last_year*7d*7d_estimates).-.-dimension.-.-'by_estimates_doc.-.-filter.-.-'Budget*20supp.*20B.-.-sort_col.-.-'dept.-.-descending.-.-false)",
-    },
+    href: "#partition/est_doc_seb/estimates",
   },
   {
     text_key: "quick_link_spending_by_program",
