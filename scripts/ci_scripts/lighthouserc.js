85df822eb49b2f8bc96a678e8aef2ae9b7505e02
Taewan Kang <tkang042@uottawa.ca>
2020-06-12 12:01:32 -0400

    porting over lhci to skip prettier integration headache

---
+++ scripts/ci_scripts/lighthouserc.js
@@ -0,0 +1,23 @@
+const _ = require("lodash");
+
+const {
+  route_load_tests_config,
+} = require("../../client/browser-tests/route-load-tests-config.js");
+
+module.exports = {
+  ci: {
+    collect: {
+      numberOfRuns: 1,
+      url: _.map(
+        route_load_tests_config,
+        ({ route }) =>
+          `https://dev.ea-ad.ca/${process.env.CIRCLE_BRANCH}/index-eng.html#${route}`
+      ),
+    },
+    upload: {
+      target: "lhci",
+      serverBaseUrl: "https://infobase-lhci.herokuapp.com/",
+      token: "554e44e8-3fcb-46ba-8444-9cb54850bc72", // could also use LHCI_TOKEN variable instead
+    },
+  },
+};
