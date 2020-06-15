cf00ff9241816a53d8ba07473252e0b3e9da18f6
GarvinH <garvinh2001@hotmail.com>
2020-06-15 11:21:51 -0400

    Copy contact info for tbs from canada.ca

--- client/src/contact/contact.js
+++ client/src/contact/contact.js
@@ -7,7 +7,7 @@ const text_maker = create_text_maker(contact_us_bundle)
 
 export default class Contact extends React.Component {
   render() {
-    console.log("test")
+    console.log("tes")
     return (
       <StandardRouteContainer
         title={text_maker("contact_us_title")}
@@ -18,7 +18,11 @@ export default class Contact extends React.Component {
         <div className="medium_panel_text text-only-page-root">
           <TM tmf={text_maker} el="h1" k="contact_us_title"/>
           <TM tmf={text_maker} el="div" k="contact_us_intro"/>
-          {/* <TM tmf={text_maker} el="div" k="contact_us_questions_email"/> */}
+          <TM tmf={text_maker} el="h2" k="general_enquiries_title"/>
+          <TM tmf={text_maker} el="div" k="general_enquiries_sub_text_1"/>
+          <TM tmf={text_maker} el="div" k="general_enquiries_sub_text_2"/>
+          <TM tmf={text_maker} el="div" k="general_enquiries_sub_text_3"/>
+          <TM tmf={text_maker} el="div" k="general_enquiries_sub_text_4"/>
         </div>
       </StandardRouteContainer>
     );
