dfb320a83164b80a845b56db943cc6ebd2d30f29
Stephen-ONeil <stephenlevioneil@gmail.com>
2020-06-19 16:33:24 -0400

    Argh, haha, finally found the issue with cleanup_dev_links.sh. When no inactive branches have dev links and the grep matches nothing, grep exits with error code 1 (forgot that was grep's behaviour, thought it returned empty for some reason). Things fell apart because of that.

--- scripts/ci_scripts/cleanup_dev_links.sh
+++ scripts/ci_scripts/cleanup_dev_links.sh
@@ -13,7 +13,8 @@ branches_with_live_dev_links=$(gsutil ls "$GCLOUD_BUCKET_ROOT/*/favicon.ico" | \
   sed -E 's/(.*\/)(.*)(\/favicon.ico$)/\2/g' | \
   sed -E '/(^__)|(^archived__).*/d')
 
-inactive_branches_with_live_dev_links=$(grep -Fxvf <(echo "$active_branches") <(echo "$branches_with_live_dev_links"))
+# reminder: grep exits with code 1 if no matches found. Using || echo "" to return an empty string instead
+inactive_branches_with_live_dev_links=$(grep -Fxvf <(echo "$active_branches") <(echo "$branches_with_live_dev_links") || echo "")
 
 if [[ ! -z "$inactive_branches_with_live_dev_links" ]]; then
   # the index html replacements used for dead dev links are stored in google cloud and must be updated there. Note that
