f148ff85b1669f43fb01eeea5b14ecbdbb8dcc3a
Taewan Kang <tkang042@uottawa.ca>
2020-06-25 16:38:57 -0400

    service overview panel

---
+++ client/src/charts/gauge.js
@@ -0,0 +1,32 @@
+import "./gauge.scss";
+import { Format } from "../components";
+
+export default class Gauge extends React.Component {
+  render() {
+    const { total_value, value, color } = this.props;
+    const percentage = value / total_value;
+    return (
+      <div>
+        <h4 style={{ textAlign: "center" }}>
+          {value} / {total_value}
+        </h4>
+        <div
+          style={{
+            backgroundColor:
+              color || window.infobase_color_constants.successDarkColor,
+          }}
+          className="gauge"
+        >
+          <div
+            style={{ transform: `rotate(${percentage * 180}deg)` }}
+            className="gauge_percentage"
+          ></div>
+          <div className="gauge_mask"></div>
+          <h2 className="gauge_value">
+            <Format type={"percentage"} content={percentage} />
+          </h2>
+        </div>
+      </div>
+    );
+  }
+}
