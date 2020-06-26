5918b6ca3cb3e34aa28f71fcf111d12cf6d1a475
Taewan Kang <tkang042@uottawa.ca>
2020-06-26 15:55:36 -0400

    service channel and standard panels

--- client/src/panels/panel_declarations/services/index.js
+++ client/src/panels/panel_declarations/services/index.js
@@ -1,2 +1,4 @@
 export { declare_provided_services_list_panel } from "./provided_services_list.js";
 export { ServiceOverview } from "./service_overview.js";
+export { ServiceChannels } from "./service_channels.js";
+export { ServiceStandards } from "./service_standards.js";
