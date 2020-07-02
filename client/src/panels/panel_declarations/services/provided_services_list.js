2dfdeab78525202f1a1caa8b028f13498c2ee899
GarvinH <garvinh2001@hotmail.com>
2020-07-02 16:07:44 -0400

    Display service as a tag

--- client/src/panels/panel_declarations/services/provided_services_list.js
+++ client/src/panels/panel_declarations/services/provided_services_list.js
@@ -5,11 +5,13 @@ import {
   InfographicPanel,
   declare_panel,
 } from "../shared.js";
+import { FancyUL } from "../../../components";
 
 import { FancyUL } from "../../../components";
 
 const { text_maker, TM } = create_text_maker_component(text);
 
+<<<<<<< HEAD
 class ProvidedServicesListPanel extends React.Component {
   constructor(props) {
     super(props);
@@ -78,6 +80,33 @@ class ProvidedServicesListPanel extends React.Component {
     );
   }
 }
+=======
+const ProvidedServicesListPanel = ({ panel_args }) => (
+  <div>
+    <TM
+      k="list_of_provided_services_desc"
+      args={{
+        subject_name: panel_args.subject.name,
+        num_of_services: panel_args.services.length,
+      }}
+    />
+    <FancyUL>
+      {_.map(
+        panel_args.services,
+        ({ name, id, org_id, service_type, description }) => (
+          <React.Fragment key={id}>
+            <a href={`#dept/${org_id}/service-panels/${id}`}>{name}</a>
+            <p>{description}</p>
+            <div style={{ display: "flex", fontSize: "14px" }}>
+              <div className="tag-badge">{service_type}</div>
+            </div>
+          </React.Fragment>
+        )
+      )}
+    </FancyUL>
+  </div>
+);
+>>>>>>> 9fa26043... Display service as a tag
 
 export const declare_provided_services_list_panel = () =>
   declare_panel({
