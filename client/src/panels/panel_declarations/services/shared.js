6a7cc3dcb9e1241a902374491e4658ab20978d22
Taewan Kang <tkang042@uottawa.ca>
2020-07-02 15:09:19 -0400

    more panels and restructures

---
+++ client/src/panels/panel_declarations/services/shared.js
@@ -0,0 +1,18 @@
+const digital_status_keys = [
+  "account_reg_digital",
+  "application_digital",
+  "authentication",
+  "decision_digital",
+  "issuance_digital",
+  "issue_res_digital",
+];
+const service_channels_keys = [
+  "in_person_applications",
+  "mail_applications",
+  "online_applications",
+  "other_channel_applications",
+  "telephone_enquires",
+  "website_visits",
+];
+
+export { digital_status_keys, service_channels_keys };
