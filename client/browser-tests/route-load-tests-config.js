b2092e4d13b093fbfbd2586b73c8a382dd23b2be
Taewan Kang <tkang042@uottawa.ca>
2020-07-03 12:22:30 -0400

    add services for route-load and lighthouse

--- client/browser-tests/route-load-tests-config.js
+++ client/browser-tests/route-load-tests-config.js
@@ -101,6 +101,11 @@ module.exports.route_load_tests_config = [
     test_on: ["eng", "fra", "basic-eng"],
   },
   {
+    name: "Infographic - Dept - Services",
+    route: "orgs/dept/326/infograph/services",
+    test_on: ["eng", "basic-eng"],
+  },
+  {
     name: "Infographic - Dept - Results",
     route: "orgs/dept/326/infograph/results",
     test_on: ["eng", "basic-eng"],
