2e396c26401412c5a202c37bdf9a05ac6cc2dd63
Taewan Kang <tkang042@uottawa.ca>
2020-07-04 21:25:16 -0400

    code cleanups, id panel, changes to nivo pie

--- client/src/panels/panel_declarations/services/services_id_methods.js
+++ client/src/panels/panel_declarations/services/services_id_methods.js
@@ -1,21 +1,81 @@
-import text from "./provided_services_list.yaml";
+import text from "./services.yaml";
 import { Service } from "../../../models/services.js";
-
 import {
   create_text_maker_component,
   InfographicPanel,
   declare_panel,
-  NivoResponsiveBar,
+  NivoResponsivePie,
+  newIBCategoryColors,
 } from "../shared.js";
 
 const { text_maker, TM } = create_text_maker_component(text);
-//TODO WIP
-const ServicesIdMethodsPanel = ({ panel_args }) => {
-  const data_keys = ["sin_is_identifier", "cra_buisnss_number_is_identifier"];
-  const label_keys = ["uses_this_id", "doesnt_use_id"];
-  const colors = infobase_colors();
 
-  return <div>TODO</div>;
+const ServicesIdMethodsPanel = ({ panel_args }) => {
+  const services = panel_args.services;
+  const colors = d3
+    .scaleOrdinal()
+    .domain(["uses_identifier", "does_not_identifier"])
+    .range(_.take(newIBCategoryColors, 2));
+
+  const sin_id = _.countBy(services, "sin_is_identifier");
+  const cra_id = _.countBy(services, "cra_buisnss_number_is_identifier");
+
+  const sin_data = [
+    {
+      id: "uses_identifier",
+      label: text_maker("uses_sin_as_identifier"),
+      value: sin_id.true,
+    },
+    {
+      id: "does_not_identifier",
+      label: text_maker("does_not_use_sin_as_identifier"),
+      value: sin_id.false,
+    },
+  ];
+  const cra_data = [
+    {
+      id: "uses_identifier",
+      label: text_maker("uses_cra_as_identifier"),
+      value: cra_id.true,
+    },
+    {
+      id: "does_not_identifier",
+      label: text_maker("does_not_use_cra_as_identifier"),
+      value: cra_id.false,
+    },
+  ];
+
+  const nivo_common_props = {
+    is_money: false,
+    colorBy: (d) => colors(d.id),
+  };
+
+  return (
+    <div className={"col-container"}>
+      <div className="fcol-md-6 p-20">
+        <TM className="id-method-text" k="sin_sub_title" el="h4" />
+        <NivoResponsivePie
+          {...nivo_common_props}
+          custom_legend_items={_.map(sin_data, (row) => ({
+            ...row,
+            color: colors(row.id),
+          }))}
+          data={sin_data}
+        />
+      </div>
+      <div className="fcol-md-6 p-20">
+        <TM className="id-method-text" k="cra_sub_title" el="h4" />
+        <NivoResponsivePie
+          {...nivo_common_props}
+          custom_legend_items={_.map(cra_data, (row) => ({
+            ...row,
+            color: colors(row.id),
+          }))}
+          data={cra_data}
+        />
+      </div>
+    </div>
+  );
 };
 
 export const declare_services_id_methods_panel = () =>
@@ -24,18 +84,18 @@ export const declare_services_id_methods_panel = () =>
     levels: ["dept"],
     panel_config_func: (level, panel_key) => ({
       requires_services: true,
-      calculate: (subject) => {
-        const services =
-          level === "dept"
-            ? Service.get_by_dept(subject.id)
-            : Service.get_all();
-        return { subject, services };
-      },
+      calculate: (subject) => ({
+        subject,
+        services: Service.get_by_dept(subject.id),
+      }),
       footnotes: false,
       render({ calculations, sources }) {
         const { panel_args } = calculations;
         return (
-          <InfographicPanel title={"ID METHODS TODO"} sources={sources}>
+          <InfographicPanel
+            title={text_maker("identification_methods")}
+            sources={sources}
+          >
             <ServicesIdMethodsPanel panel_args={panel_args} />
           </InfographicPanel>
         );
