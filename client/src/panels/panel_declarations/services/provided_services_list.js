0173f5e662eab3e1d6857c03611715de312a3a92
GarvinH <garvinh2001@hotmail.com>
2020-07-09 13:07:53 -0400

    Move changes from ServiceList branch to here: provided_services_list.js

--- client/src/panels/panel_declarations/services/provided_services_list.js
+++ client/src/panels/panel_declarations/services/provided_services_list.js
@@ -1,30 +1,83 @@
 import text from "./services.yaml";
 import { Service } from "../../../models/services.js";
-
 import {
   create_text_maker_component,
   InfographicPanel,
   declare_panel,
 } from "../shared.js";
 
+import { FancyUL } from "../../../components";
+
 const { text_maker, TM } = create_text_maker_component(text);
 
-const ProvidedServicesListPanel = ({ panel_args }) => (
-  <div>
-    <TM
-      k="list_of_provided_services_desc"
-      args={{
-        subject_name: panel_args.subject.name,
-        num_of_services: panel_args.services.length,
-      }}
-    />
-    {_.map(panel_args.services, ({ name, id, org_id }) => (
-      <a key={id} href={`#dept/${org_id}/service-panels/${id}`}>
-        {name}
-      </a>
-    ))}
-  </div>
-);
+class ProvidedServicesListPanel extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { service_query: "" };
+  }
+
+  render() {
+    const { panel_args } = this.props;
+    const { service_query } = this.state;
+
+    return (
+      <div>
+        <TM
+          k="list_of_provided_services_desc"
+          args={{
+            subject_name: panel_args.subject.name,
+            num_of_services: panel_args.services.length,
+          }}
+        />
+        <input
+          aria-label={text_maker("explorer_search_is_optional")}
+          className="form-control input-lg"
+          type="text"
+          style={{ width: "100%", marginBottom: "1rem", marginTop: "2rem" }}
+          placeholder={text_maker("filter_results_service")}
+          onChange={(evt) => this.setState({ service_query: evt.target.value })}
+          value={service_query}
+        />
+        <FancyUL>
+          {_.map(
+            _.filter(
+              panel_args.services,
+              (service) =>
+                _.includes(
+                  service.name.toLowerCase(),
+                  service_query.toLowerCase()
+                ) ||
+                _.includes(
+                  service.service_type.toLowerCase(),
+                  service_query.toLowerCase()
+                )
+            ),
+            ({ name, id, org_id, service_type, description }) => (
+              <React.Fragment key={id}>
+                <a href={`#dept/${org_id}/service-panels/${id}`}>{name}</a>
+                <p>{description}</p>
+                <div
+                  style={{
+                    display: "flex",
+                    fontSize: "14px",
+                    justifyContent: "space-between",
+                  }}
+                >
+                  <div className="tag-badge">{service_type}</div>
+                  <a href={`#dept/${org_id}/service-panels/${id}`}>
+                    <button className="btn-ib-primary">
+                      <TM k="view_service" />
+                    </button>
+                  </a>
+                </div>
+              </React.Fragment>
+            )
+          )}
+        </FancyUL>
+      </div>
+    );
+  }
+}
 
 export const declare_provided_services_list_panel = () =>
   declare_panel({
