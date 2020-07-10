0d687e16123ed36bce86551c733bafc5105204f1
GarvinH <garvinh2001@hotmail.com>
2020-07-10 14:25:36 -0400

    Update determine_text_align

--- client/src/components/DisplayTable/DisplayTable.js
+++ client/src/components/DisplayTable/DisplayTable.js
@@ -116,13 +116,10 @@ export class DisplayTable extends React.Component {
       const current_col_formatter = col_configs_with_defaults[col].formatter;
       const current_col_raw_formatter =
         col_configs_with_defaults[col].raw_formatter;
-      if (current_col_formatter && !_.isFunction(current_col_formatter)) {
-        if (_.isString(current_col_formatter) && _.isNumber(row[col])) {
-          return "right";
-        }
-      } else if (_.isNumber(current_col_raw_formatter(row[col]))) {
-        return "right";
-      }
+      return (_.isString(current_col_formatter) && _.isNumber(row[col])) ||
+        _.isNumber(current_col_raw_formatter(row[col]))
+        ? "right"
+        : "left";
     };
 
     const clean_search_string = (search_string) =>
