2f34fc4dc19059daf5a1159c5a8e706fe46c5e04
Taewan Kang <tkang042@uottawa.ca>
2020-07-13 15:12:31 -0400

    quick internal feedbacks, minor text, slight viz, icon, orientation changes

--- client/src/panels/panel_declarations/services/single_service_panels/service_digital_status.js
+++ client/src/panels/panel_declarations/services/single_service_panels/service_digital_status.js
@@ -4,24 +4,41 @@ import {
   Panel,
   DisplayTable,
 } from "../../../../components";
-import { digital_status_keys, available_icons } from "../shared.js";
+import {
+  digital_status_keys,
+  available_icons,
+  available_keys,
+} from "../shared.js";
 
 const { text_maker, TM } = create_text_maker_component(text);
 
 export class ServiceDigitalStatus extends React.Component {
   render() {
     const { service } = this.props;
+
     const column_configs = {
       overview_digital_status_desc: {
         index: 0,
         header: text_maker("overview_digital_status_desc"),
-        is_searchable: true,
+        formatter: (key) => (
+          <div>
+            <TM k={key} el="h4" />
+            <TM k={`${key}_desc`} />
+          </div>
+        ),
+        raw_formatter: (key) =>
+          `${text_maker(key)} ${text_maker(`${key}_desc`)}}`,
       },
       digital_status: {
         index: 1,
         header: text_maker("online_status"),
-        formatter: (value) => available_icons[value],
-        raw_formatter: (value) => String(value),
+        formatter: (value) => (
+          <div>
+            {available_icons[available_keys[value]]}
+            <TM style={{ marginLeft: 5 }} k={available_keys[value]} />
+          </div>
+        ),
+        raw_formatter: (value) => available_keys[value],
       },
     };
 
@@ -30,7 +47,7 @@ export class ServiceDigitalStatus extends React.Component {
         <TM className="medium_panel_text" k="overview_digital_status_title" />
         <DisplayTable
           data={_.map(digital_status_keys, (key) => ({
-            overview_digital_status_desc: text_maker(`${key}_desc`),
+            overview_digital_status_desc: key,
             digital_status: service[`${key}_status`],
           }))}
           column_configs={column_configs}
