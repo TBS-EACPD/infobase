8d45b4da23ed432b481a79891e670e8558bab928
GarvinH <garvinh2001@hotmail.com>
2020-07-14 12:01:56 -0400

    Clean up code.

--- client/src/panels/panel_declarations/intro/profile_panels.js
+++ client/src/panels/panel_declarations/intro/profile_panels.js
@@ -188,8 +188,6 @@ export const declare_profile_panel = () =>
           .filter(([label, item]) => item)
           .value();
 
-        console.log(labels_and_items);
-
         return (
           <TextPanel title={text_maker(`profile`)}>
             <LabeledTombstone labels_and_items={labels_and_items} />
