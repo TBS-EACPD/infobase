370580f1caf0415564d9cce3bd00054cde8efe62
Stephen-ONeil <stephenlevioneil@gmail.com>
2020-07-16 09:43:58 -0400

    Hmm, not sure this is the culprit this time, but make sure that the email_backend cloud function is explicitly public

--- email_backend/deploy_scripts/prod_deploy_email_backend_function.sh
+++ email_backend/deploy_scripts/prod_deploy_email_backend_function.sh
@@ -8,6 +8,7 @@ source deploy_scripts/set_prod_env_vars.sh
 source deploy_scripts/set_transient_secrets.sh # these trap EXIT to handle their own cleanup
 
 gcloud beta functions deploy prod-email-backend --max-instances 1 --project ${PROJECT} --region us-central1 --entry-point email_backend --stage-bucket email-backend-staging-bucket --runtime nodejs10 --trigger-http --env-vars-file $scratch/envs.yaml > /dev/null
+gcloud alpha functions add-iam-policy-binding prod-email-backend --project ${PROJECT} --region us-central1 --member allUsers --role roles/cloudfunctions.invoker
 
 source deploy_scripts/unset_prod_env_vars.sh
 
