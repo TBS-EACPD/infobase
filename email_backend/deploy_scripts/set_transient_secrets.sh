97a0b775d4c98e6b25de5635fa6e78aba26c5ca8
Stephen-ONeil <stephenlevioneil@gmail.com>
2020-07-16 12:04:27 -0400

    Switch email backend from 3-legged OAuth2 authentication to 2LO authetication with a service account

--- email_backend/deploy_scripts/set_transient_secrets.sh
+++ email_backend/deploy_scripts/set_transient_secrets.sh
@@ -31,18 +31,17 @@ export PROJECT=$(lpass show EMAIL_BACKEND_PROJECT_ID --notes)
 
 touch $scratch/envs.yaml
 echo "IS_PROD_SERVER: '$IS_PROD_SERVER'" >> $scratch/envs.yaml
+echo "USE_REMOTE_DB: '$USE_REMOTE_DB'" >> $scratch/envs.yaml
 echo "CURRENT_SHA: '$CURRENT_SHA'" >> $scratch/envs.yaml
 
 export EMAIL_BACKEND_SENDING_ADDRESS=$(lpass show EMAIL_BACKEND_SENDING_ADDRESS --notes)
 export EMAIL_BACKEND_RECEIVING_ADDRESS=$(lpass show EMAIL_BACKEND_RECEIVING_ADDRESS --notes)
-export EMAIL_BACKEND_CLIENT_ID=$(lpass show EMAIL_BACKEND_CLIENT_ID --notes)
-export EMAIL_BACKEND_CLIENT_SECRET=$(lpass show EMAIL_BACKEND_CLIENT_SECRET --notes)
-export EMAIL_BACKEND_REFRESH_TOKEN=$(lpass show EMAIL_BACKEND_REFRESH_TOKEN --notes)
+export EMAIL_BACKEND_OAUTH_SERVICE_ID=$(lpass show EMAIL_BACKEND_OAUTH_SERVICE_ID --notes)
+export EMAIL_BACKEND_OAUTH_SERVICE_PRIVATE_KEY=$(lpass show EMAIL_BACKEND_OAUTH_SERVICE_PRIVATE_KEY --notes)
 echo "EMAIL_BACKEND_SENDING_ADDRESS: '$EMAIL_BACKEND_SENDING_ADDRESS'" >> $scratch/envs.yaml
 echo "EMAIL_BACKEND_RECEIVING_ADDRESS: '$EMAIL_BACKEND_RECEIVING_ADDRESS'" >> $scratch/envs.yaml
-echo "EMAIL_BACKEND_CLIENT_ID: '$EMAIL_BACKEND_CLIENT_ID'" >> $scratch/envs.yaml
-echo "EMAIL_BACKEND_CLIENT_SECRET: '$EMAIL_BACKEND_CLIENT_SECRET'" >> $scratch/envs.yaml
-echo "EMAIL_BACKEND_REFRESH_TOKEN: '$EMAIL_BACKEND_REFRESH_TOKEN'" >> $scratch/envs.yaml
+echo "EMAIL_BACKEND_OAUTH_SERVICE_ID: '$EMAIL_BACKEND_OAUTH_SERVICE_ID'" >> $scratch/envs.yaml
+echo "EMAIL_BACKEND_OAUTH_SERVICE_PRIVATE_KEY: '$EMAIL_BACKEND_OAUTH_SERVICE_PRIVATE_KEY'" >> $scratch/envs.yaml
 
 export MDB_NAME=$(lpass show EMAIL_BACKEND_MDB_NAME --notes)
 export MDB_CONNECT_STRING=$(lpass show EMAIL_BACKEND_MDB_CONNECT_STRING_3 --notes)
@@ -52,5 +51,3 @@ echo "MDB_NAME: '$MDB_NAME'" >> $scratch/envs.yaml
 echo "MDB_CONNECT_STRING: '$MDB_CONNECT_STRING'" >> $scratch/envs.yaml
 echo "MDB_USERNAME: '$MDB_USERNAME'" >> $scratch/envs.yaml
 echo "MDB_PW: '$MDB_PW'" >> $scratch/envs.yaml
-
-echo "USE_REMOTE_DB: '1'" >> $scratch/envs.yaml
