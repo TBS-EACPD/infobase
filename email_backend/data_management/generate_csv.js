80ae2b34ae7a6306253756d561af44cc05315625
Garvin <garvinh2001@hotmail.com>
2020-07-17 11:17:45 -0400

    Destructure email_meta

--- email_backend/data_management/generate_csv.js
+++ email_backend/data_management/generate_csv.js
@@ -1,7 +1,38 @@
 const fs = require("fs");
 const _ = require("lodash");
 var json = JSON.parse(fs.readFileSync("email_data.json", "utf8"));
+const csv = [
+  [
+    "ID",
+    "Issue types",
+    "Issue Details",
+    "Commit SHA",
+    "Route",
+    "Language",
+    "App Version",
+    "Client ID",
+    "Recipient",
+    "Sender",
+    "Referer",
+    "Server Time",
+    "Date",
+    "Time",
+  ],
+];
 
-_.map(json, function (report) {
-  console.log(report);
+const reduced_json = _.map(json, function (report) {
+  return _.reduce(
+    report,
+    function (result, value, key) {
+      if (_.isPlainObject(value)) {
+        return { ...result, ...value };
+      } else {
+        result[key] = value;
+        return result;
+      }
+    },
+    {}
+  );
 });
+
+console.log(reduced_json);
