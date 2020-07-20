0f40dc2b2ce5915fcd6bef07455486f823bedf0f
Garvin <garvinh2001@hotmail.com>
2020-07-20 09:13:04 -0400

    Clean up data and produce array of csv

--- email_backend/data_management/generate_csv.js
+++ email_backend/data_management/generate_csv.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 const _ = require("lodash");
 var json = JSON.parse(fs.readFileSync("email_data.json", "utf8"));
-const csv = [
+const csv_format = [
   [
     "ID",
     "Issue types",
@@ -24,15 +24,30 @@ const reduced_json = _.map(json, function (report) {
   return _.reduce(
     report,
     function (result, value, key) {
-      if (_.isPlainObject(value)) {
-        return { ...result, ...value };
-      } else {
-        result[key] = value;
+      if (key === "__v") {
         return result;
       }
+      if (_.isPlainObject(value)) {
+        return { ...result, ...value };
+      } else if (_.isArray(value)) {
+        result[key] = value.join(" & ");
+      } else {
+        result[key] = value;
+      }
+      return result;
     },
     {}
   );
 });
 
-console.log(reduced_json);
+_.map(reduced_json, function (report) {
+  report[server_time] = new Date(report[server_time]).csv_format.push(
+    _.values(report)
+  );
+});
+
+const csv = _.map(csv_format, function (format) {
+  return format.join(",");
+});
+
+console.log(csv);
