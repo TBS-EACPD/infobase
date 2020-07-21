b603d0b7605c5297672ae477530ce064ba15c176
Garvin <garvinh2001@hotmail.com>
2020-07-21 16:23:12 -0400

    Loop through and collect all needed data (excluding junk)

--- email_backend/data_management/generate_csv.js
+++ email_backend/data_management/generate_csv.js
@@ -1,58 +1,122 @@
-const fs = require("fs");
-const _ = require("lodash");
-const json = JSON.parse(
-  fs.readFileSync("./data_management/email_data.json", "utf8")
-);
-const csv_format = [
-  [
-    "ID",
-    "Issue types",
-    "Issue Details",
-    "Commit SHA",
-    "Route",
-    "Language",
-    "App Version",
-    "Client ID",
-    "Recipient",
-    "Sender",
-    "Referer",
-    "Server Time",
-    "Date",
-    "Time",
-  ],
-];
+// const fs = require("fs");
+// const _ = require("lodash");
+// const json = JSON.parse(
+//   fs.readFileSync("./data_management/email_data.json", "utf8")
+// );
+// const csv_format = [
+//   [
+//     "ID",
+//     "Issue types",
+//     "Issue Details",
+//     "Commit SHA",
+//     "Route",
+//     "Language",
+//     "App Version",
+//     "Client ID",
+//     "Recipient",
+//     "Sender",
+//     "Referer",
+//     "Server Time",
+//     "Date",
+//     "Time",
+//   ],
+// ];
 
-const reduced_json = _.map(json, function (report) {
-  return _.reduce(
-    report,
-    function (result, value, key) {
-      if (key === "__v") {
-        return result;
-      }
-      if (_.isPlainObject(value)) {
-        return { ...result, ...value };
-      } else if (_.isArray(value)) {
-        result[key] = value.join(" & ");
-      } else {
-        result[key] = value;
-      }
-      return result;
-    },
-    {}
-  );
-});
+// const reduced_json = _.map(json, function (report) {
+//   return _.reduce(
+//     report,
+//     function (result, value, key) {
+//       if (key === "__v") {
+//         return result;
+//       }
+//       if (_.isPlainObject(value)) {
+//         return { ...result, ...value };
+//       } else if (_.isArray(value)) {
+//         result[key] = value.join(" & ");
+//       } else {
+//         result[key] = value;
+//       }
+//       return result;
+//     },
+//     {}
+//   );
+// });
 
-_.map(reduced_json, function (report) {
-  report["server_time"] = new Date(report["server_time"]).toUTCString();
-  csv_format.push(_.values(report));
-});
+// _.map(reduced_json, function (report) {
+//   report["server_time"] = new Date(report["server_time"]).toUTCString();
+//   csv_format.push(_.values(report));
+// });
 
-const csv = _.map(csv_format, function (format) {
-  return format.join(",");
-});
+// const csv = _.map(csv_format, function (format) {
+//   return format.join(",");
+// });
 
-fs.writeFile("./data_management/email_data.csv", csv.join("\r\n"), function (
-  err
-) {
-  console.log(err || "Successfully Saved.");
+// fs.writeFile("./data_management/email_data.csv", csv.join("\r\n"), function (
+//   err
+// ) {
+//   console.log(err || "Successfully Saved.");
+// });
+
+import { connect_db } from "../transpiled_build/db_utils/connect_db";
+import { get_templates } from "../transpiled_build/template_utils";
+import { make_mongoose_model_from_original_template } from "../transpiled_build/db_utils/log_email_and_meta_to_db";
+import _ from "lodash";
+connect_db();
+const templates = get_templates();
+_.map(templates, (template_value, template_name) => {
+  const collection = make_mongoose_model_from_original_template({
+    original_template: template_value,
+    template_name,
+  });
+
+  // if (template_name === "report_a_problem") {
+  //   collection.find({}, function (err, report) {
+  //     console.log(report);
+  //   });
+  // }
+
+  //if it happens that later on we add another key with a json value,
+  //change reduced json to handle "key.value". For the time being, the CSV
+  //will be cleaner if we flatten the "key.value" to "value"
+  const csv_format = _.map(_.keys(collection.schema.paths), (key) => {
+    const key_split = key.split(".");
+    return key_split.length > 1 ? key_split[1] : key;
+  });
+  const csv = [csv_format];
+
+  collection.find({}, function (err, submissions) {
+    const reduced_json = _.map(submissions, function (sub) {
+      return _.reduce(
+        sub._doc,
+        function (result, value, key) {
+          if (key === "__v") {
+            return result;
+          }
+          if (_.isPlainObject(value)) {
+            return { ...result, ...value };
+          } else if (_.isArray(value)) {
+            result[key] = value.join(" & ");
+          } else {
+            result[key] = value;
+          }
+          return result;
+        },
+        {}
+      );
+    });
+
+    console.log(reduced_json);
+
+    // console.log(csv);
+  });
 });
+// const original_template = templates["report_a_problem"];
+// const template_name = "report_a_problem";
+// const Report = make_mongoose_model_from_original_template({
+//   original_template,
+//   template_name,
+// });
+
+// Report.find({}, function (err, report) {
+//   console.log(report);
+// });
