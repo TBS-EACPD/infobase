cdcffcb16242b9df2ef6dff10fd23b490a023d41
Garvin <garvinh2001@hotmail.com>
2020-07-27 16:07:39 -0400

    Move all redux functions to index.js for RPB

--- client/src/rpb/index.js
+++ client/src/rpb/index.js
@@ -10,12 +10,7 @@ import { TextMaker, text_maker } from "./rpb_text_provider.js";
 import "./rpb.scss";
 
 //data and state stuff
-import {
-  reducer,
-  mapDispatchToProps,
-  create_mapStateToProps,
-  naive_to_real_state,
-} from "./state_and_data.js";
+import { reducer, mapDispatchToProps } from "./state_and_data.js";
 import { createStore } from "redux";
 import { Provider, connect } from "react-redux";
 import { ensure_loaded } from "../core/lazy_loader.js";
@@ -29,12 +24,50 @@ import { AccessibleTablePicker, TablePicker } from "./TablePicker.js";
 import { GranularView } from "./granular_view.js";
 import { ShareReport } from "./shared.js";
 import { Table } from "../core/TableClass.js";
+import { Subject } from "../models/subject.js";
+import Footnote from "../models/footnotes/footnotes.js";
 
 //misc app stuff
 import { rpb_link } from "./rpb_link.js";
 import { SafeJSURL } from "../general_utils.js";
 
 const sub_app_name = "_rpb";
+
+function get_all_data_columns_for_table(table) {
+  return _.chain(table.unique_headers)
+    .map((nick) => table.col_from_nick(nick))
+    .filter((col) => !col.hidden && !col.key && col.not_for_display !== true)
+    .value();
+}
+
+function get_default_dimension_for_table(table) {
+  return table.dimensions[0].title_key;
+}
+
+//returns a the proposed new slice of state that will change when a new table is selected
+function get_default_state_for_new_table(table_id) {
+  const table = Table.lookup(table_id);
+  const columns = _.map(get_all_data_columns_for_table(table), "nick");
+  return {
+    table: table_id,
+    columns,
+    dimension: get_default_dimension_for_table(table),
+    filter: text_maker("all"),
+    broken_url: false,
+  };
+}
+
+function naive_to_real_state(naive_state) {
+  const { table } = naive_state;
+
+  return {
+    //default state
+    subject: "gov_gov",
+    ...//tables imply their own default state
+    (table ? get_default_state_for_new_table(naive_state.table) : {}),
+    ...naive_state, //whatever state is already defined takes precedence.
+  };
+}
 
 const url_state_selector = createSelector(_.identity, (str) => {
   const state = (() => {
@@ -65,36 +98,204 @@ class Root extends React.Component {
   constructor(props) {
     super(props);
 
-    const { state } = this.props;
-
-    const mapStateToProps = create_mapStateToProps();
-    /* eslint-disable-next-line no-use-before-define */
-    const Container = connect(mapStateToProps, mapDispatchToProps)(RPB);
-
-    const store = createStore(reducer, state);
+    const { state } = this.props; //done
+    console.log(state);
 
     this.state = {
-      store,
-      Container,
+      ...props.state,
     };
   }
-  static getDerivedStateFromProps(nextProps, prevState) {
-    prevState.store.dispatch({
-      type: "navigate_to_new_state",
-      payload: nextProps.state,
-    });
-    return null;
-  }
+
   shouldComponentUpdate(newProps) {
-    return rpb_link(this.state.store.getState()) !== rpb_link(newProps.state);
+    //done
+    return rpb_link(this.state) !== rpb_link(newProps.state);
   }
   render() {
-    const { Container, store } = this.state;
+    const on_set_filter = ({ dimension, filter }) => {
+      this.setState((prevState, props) => {
+        return { url_state: { ...prevState.url_state, dimension, filter } };
+      });
+    };
+
+    const on_set_dimension = (dim_key) => {
+      this.setState((prevState, props) => {
+        return {
+          url_state: {
+            ...prevState.url_state,
+            dim_key,
+            filter: text_maker("all"),
+          },
+        };
+      });
+    };
+
+    const on_switch_table = (table_id) => {
+      console.log(this.state);
+      this.setState(
+        (prevState, props) => {
+          return {
+            url_state: {
+              ...prevState.url_state,
+              ...get_default_state_for_new_table(table_id),
+            },
+          };
+        },
+        () => {
+          console.log(this.state);
+        }
+      );
+    };
+
+    function get_key_columns_for_table(table) {
+      return _.chain(table.unique_headers)
+        .map((nick) => table.col_from_nick(nick))
+        .filter((col) => (col.key && !col.hidden) || col.nick === "dept")
+        .value();
+    }
+
+    //Note that this will cause a memory leak as this closure will never get GC'd
+    const get_filters_for_dim = _.memoize(
+      (table, dim_key) =>
+        _.uniq([text_maker("all"), ..._.keys(table[dim_key]("*", true))]),
+      (table, dim_key) => `${table.id}-${dim_key}`
+    );
+
+    const get_table = createSelector(_.property("table"), (table) =>
+      Table.lookup(table)
+    );
+
+    const get_subject = createSelector(_.property("subject"), (guid) =>
+      Subject.get_by_guid(guid)
+    );
+
+    const get_all_data_columns = createSelector(
+      get_table,
+      get_all_data_columns_for_table
+    );
+
+    const get_sorted_columns = createSelector(
+      [get_table, _.property("columns")],
+      (table, col_nicks) =>
+        _.filter(get_all_data_columns_for_table(table), ({ nick }) =>
+          _.includes(col_nicks, nick)
+        )
+    );
+
+    const get_sorted_key_columns = createSelector(
+      get_table,
+      get_key_columns_for_table
+    );
+
+    const get_def_ready_cols = createSelector(
+      get_table,
+      get_sorted_columns,
+      (table, sorted_data_columns) =>
+        _.chain(sorted_data_columns)
+          .map((col) => ({
+            name: col.fully_qualified_name,
+            def: table.column_description(col.nick),
+          }))
+          .value()
+    );
+
+    //array of html footnotes
+    const get_footnotes = createSelector(
+      [get_table, get_subject],
+      (table, subject) => {
+        const topics = table.tags.concat(["MACHINERY"]);
+        const subject_footnotes = Footnote.get_for_subject(subject, topics);
+        return subject_footnotes;
+      }
+    );
+
+    const get_dimensions = createSelector(get_table, (table) =>
+      _.chain(table.dimensions)
+        .filter("include_in_report_builder")
+        .map(({ title_key }) => ({
+          id: title_key,
+          display: text_maker(title_key),
+        }))
+        .value()
+    );
+
+    const get_all_filters = createSelector(
+      [get_table, _.property("dimension")],
+      (table, dim_key) => get_filters_for_dim(table, dim_key)
+    );
+
+    const get_filters_by_dimension = createSelector(
+      [get_table, get_dimensions],
+      (table, dimensions) =>
+        _.map(dimensions, ({ id: dim_key, display }) => ({
+          display,
+          id: dim_key,
+          children: _.map(get_filters_for_dim(table, dim_key), (filter) => ({
+            filter: filter,
+            dimension: dim_key,
+            display: filter,
+          })),
+        }))
+    );
+
+    const get_table_data = createSelector(get_table, (table) => {
+      table.fill_dimension_columns();
+      return table.data;
+    });
+
+    const get_cat_filter_func = createSelector(
+      [_.property("dimension"), _.property("filter")],
+      (dim_key, filter_val) =>
+        filter_val === text_maker("all")
+          ? _.constant(true)
+          : { [dim_key]: filter_val }
+    );
+
+    const get_zero_filter_func = createSelector(
+      [_.property("columns")],
+      (col_nicks) => (row) =>
+        _.chain(col_nicks)
+          .map((nick) => row[nick])
+          .compact()
+          .isEmpty()
+          .value()
+    );
+    const get_flat_data = createSelector(
+      [get_table_data, get_cat_filter_func, get_zero_filter_func],
+      (table_data, cat_filter_func, zero_filter_func) =>
+        _.chain(table_data)
+          .filter(cat_filter_func)
+          .reject(zero_filter_func)
+          .value()
+    );
 
     return (
-      <Provider store={store}>
-        <Container />
-      </Provider>
+      <RPB
+        table={this.state.table && get_table(this.state)}
+        subject={this.state.subject && get_subject(this.state)}
+        columns={
+          !_.isEmpty(this.state.columns) && get_sorted_columns(this.state)
+        }
+        dimensions={this.state.table && get_dimensions(this.state)}
+        filters={this.state.table && get_all_filters(this.state)}
+        footnotes={this.state.table && get_footnotes(this.state)}
+        def_ready_columns={
+          !_.isEmpty(this.state.columns) && get_def_ready_cols(this.state)
+        }
+        all_data_columns={
+          !_.isEmpty(this.state.columns) && get_all_data_columns(this.state)
+        }
+        flat_data={this.state.table && get_flat_data(this.state)}
+        //granular props
+        filters_by_dimension={
+          this.state.table && get_filters_by_dimension(this.state)
+        }
+        sorted_key_columns={
+          !_.isEmpty(this.state.columns) && get_sorted_key_columns(this.state)
+        }
+        on_set_filter={on_set_filter}
+        on_set_dimension={on_set_dimension}
+        on_switch_table={on_switch_table}
+      />
     );
   }
 }
@@ -105,12 +306,14 @@ class RPB extends React.Component {
     if (props.table) {
       this.state = {
         table_picking: false,
+        ...props.state,
       };
     } else {
       this.state = {
         loading: false,
         table_picking: true,
         selected_subject: null,
+        ...props.state,
       };
     }
   }
@@ -270,6 +473,7 @@ const URLSynchronizer = withRouter(
     }
     shouldComponentUpdate(new_props) {
       // return rpb_link(this.props.state) !== rpb_link(new_props.state);
+      console.log(new_props.state, window.location.hash);
       return rpb_link(new_props.state) !== window.location.hash;
     }
     componentDidMount() {
@@ -331,9 +535,11 @@ export default class ReportBuilder extends React.Component {
     }
   }
   shouldComponentUpdate(nextProps, nextState) {
+    console.log(this.state.url_state, nextState.url_state);
     return (
       this.state.loading !== nextState.loading ||
-      this.state.config_str !== nextState.config_str
+      this.state.config_str !== nextState.config_str ||
+      rpb_link(this.state.url_state) !== rpb_link(nextState.url_state)
     );
   }
   componentDidUpdate() {
@@ -343,6 +549,7 @@ export default class ReportBuilder extends React.Component {
   }
   render() {
     const { url_state } = this.state;
+    console.log(url_state);
 
     return (
       <StandardRouteContainer
