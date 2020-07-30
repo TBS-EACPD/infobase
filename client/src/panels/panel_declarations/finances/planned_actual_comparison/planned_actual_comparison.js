6fc6ef58406d6ada3d2d198aae5ae5d9bf537d50
Amanda <plant.amnda@gmail.com>
2020-07-30 12:45:05 -0400

    Fix CRSO planned_actual_comparison text

--- client/src/panels/panel_declarations/finances/planned_actual_comparison/planned_actual_comparison.js
+++ client/src/panels/panel_declarations/finances/planned_actual_comparison/planned_actual_comparison.js
@@ -56,6 +56,7 @@ export const declare_planned_actual_comparison_panel = () =>
           planned_ftes: planned_ftes,
           actual_spend: actual_spend,
           actual_ftes: actual_ftes,
+          crso_prg_num: subject.level === "crso" && subject.programs.length,
         };
 
         return {
