7017e4157826cd18d842b26529223e520ddd62ce
Amanda <plant.amnda@gmail.com>
2020-07-30 15:14:44 -0400

    Remove info_deps from personel_spend

--- client/src/panels/panel_declarations/finances/sobj/personel_spend.js
+++ client/src/panels/panel_declarations/finances/sobj/personel_spend.js
@@ -20,31 +20,92 @@ export const declare_personnel_spend_panel = () =>
     panel_config_func: (level, panel_key) => ({
       depends_on: ["orgSobjs"],
 
-      info_deps: ["orgSobjs_gov_info"],
+      calculate(subject, data) {
+        const { orgSobjs } = this.tables;
 
-      calculate(subject, info, data) {
+        const series = _.map(
+          std_years,
+          (year) => orgSobjs.horizontal(year, false)[sos[1].text]
+        );
+
+        const five_year_avg = _.sum(series) / series.length;
+
+        const max_spend = _.max(series);
+        const max_year = run_template(
+          `${std_years[series.indexOf(max_spend)]}`
+        );
+
+        const min_spend = _.min(series);
+        const min_year = run_template(
+          `${std_years[series.indexOf(min_spend)]}`
+        );
+
+        const text_calculations = {
+          five_year_avg,
+          max_spend,
+          max_year,
+          min_spend,
+          min_year,
+        };
+
         return {
-          series: {
-            "0": std_years.map(
-              (year) =>
-                this.tables.orgSobjs.horizontal(year, false)[sos[1].text]
-            ),
-          },
+          series,
+          text_calculations,
         };
       },
 
       render({ calculations, footnotes, sources }) {
-        const { info, panel_args } = calculations;
+        const { panel_args } = calculations;
 
-        const personnel_data = [
-          {
-            id: "Personnel",
-            data: _.map(panel_args.series[0], (spending_data, year_index) => ({
-              y: spending_data,
-              x: run_template(std_years[year_index]),
-            })),
-          },
-        ];
+        const { text_calculations } = panel_args;
+
+        let graph_content;
+        if (window.is_a11y_mode) {
+          graph_content = (
+            <A11yTable
+              data_col_headers={[text_maker("spending")]}
+              data={_.chain(std_years)
+                .map(run_template)
+                .zip(panel_args.series["0"])
+                .map(([label, amt]) => ({
+                  label,
+                  data: <Format type="compact1_written" content={amt} />,
+                }))
+                .value()}
+            />
+          );
+        } else {
+          const personnel_data = () => [
+            {
+              id: "Personnel",
+              data: _.map(
+                panel_args.series[0],
+                (spending_data, year_index) => ({
+                  y: spending_data,
+                  x: run_template(std_years[year_index]),
+                })
+              ),
+            },
+          ];
+
+          graph_content = (
+            <div position="relative">
+              <GraphOverlay>
+                <WrappedNivoLine
+                  raw_data={panel_args.series}
+                  data={personnel_data()}
+                  margin={{
+                    top: 50,
+                    right: 40,
+                    bottom: 50,
+                    left: 65,
+                  }}
+                  colors={window.infobase_color_constants.primaryColor}
+                />
+              </GraphOverlay>
+            </div>
+          );
+        }
 
         return (
           <StdPanel
@@ -52,7 +113,7 @@ export const declare_personnel_spend_panel = () =>
             {...{ footnotes, sources }}
           >
             <Col size={5} isText>
-              <TM k="personnel_spend_text" args={info} />
+              <TM k="personnel_spend_text" args={text_calculations} />
             </Col>
             <Col size={7} isGraph>
               <div position="relative">
