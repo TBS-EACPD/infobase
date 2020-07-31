45a8e6377e9a686fb8d0bf2842c245c17cc333df
Garvin <garvinh2001@hotmail.com>
2020-07-31 14:48:39 -0400

    Remove createSelector from Root component

--- client/src/rpb/index.js
+++ client/src/rpb/index.js
@@ -105,113 +105,92 @@ class Root extends React.Component {
   shouldComponentUpdate(newProps, newState) {
     return rpb_link(this.state) !== rpb_link(newState);
   }
+
+  on_set_filter = ({ dimension, filter }) => {
+    this.setState((prevState, props) => {
+      return { ...prevState, dimension, filter };
+    });
+  };
+
+  on_set_dimension = (dim_key) => {
+    this.setState((prevState, props) => {
+      return {
+        ...prevState,
+        dim_key,
+        filter: text_maker("all"),
+      };
+    });
+  };
+
+  on_switch_table = (table_id) => {
+    this.setState((prevState, props) => {
+      return {
+        ...prevState,
+        ...get_default_state_for_new_table(table_id),
+      };
+    });
+  };
+
+  get_key_columns_for_table = (table) => {
+    return _.chain(table.unique_headers)
+      .map((nick) => table.col_from_nick(nick))
+      .filter((col) => (col.key && !col.hidden) || col.nick === "dept")
+      .value();
+  };
+
+  get_filters_for_dim = (table, dim_key) =>
+    _.uniq([text_maker("all"), ..._.keys(table[dim_key]("*", true))]);
+
   render() {
-    const on_set_filter = ({ dimension, filter }) => {
-      this.setState((prevState, props) => {
-        return { ...prevState, dimension, filter };
-      });
-    };
+    const table = this.state.table ? Table.lookup(this.state.table) : undefined;
 
-    const on_set_dimension = (dim_key) => {
-      this.setState((prevState, props) => {
-        return {
-          ...prevState,
-          dim_key,
-          filter: text_maker("all"),
-        };
-      });
-    };
+    const subject = this.state.subject
+      ? Subject.get_by_guid(this.state.subject)
+      : undefined;
 
-    const on_switch_table = (table_id) => {
-      this.setState((prevState, props) => {
-        return {
-          ...prevState,
-          ...get_default_state_for_new_table(table_id),
-        };
-      });
-    };
+    const all_data_columns = this.state.table
+      ? get_all_data_columns_for_table(table)
+      : undefined;
 
-    function get_key_columns_for_table(table) {
-      return _.chain(table.unique_headers)
-        .map((nick) => table.col_from_nick(nick))
-        .filter((col) => (col.key && !col.hidden) || col.nick === "dept")
-        .value();
-    }
-
-    //Note that this will cause a memory leak as this closure will never get GC'd
-    const get_filters_for_dim = _.memoize(
-      (table, dim_key) =>
-        _.uniq([text_maker("all"), ..._.keys(table[dim_key]("*", true))]),
-      (table, dim_key) => `${table.id}-${dim_key}`
-    );
-
-    const get_table = createSelector(_.property("table"), (table) =>
-      Table.lookup(table)
-    );
-
-    const get_subject = createSelector(_.property("subject"), (guid) =>
-      Subject.get_by_guid(guid)
-    );
-
-    const get_all_data_columns = createSelector(
-      get_table,
-      get_all_data_columns_for_table
-    );
-
-    const get_sorted_columns = createSelector(
-      [get_table, _.property("columns")],
-      (table, col_nicks) =>
-        _.filter(get_all_data_columns_for_table(table), ({ nick }) =>
-          _.includes(col_nicks, nick)
+    const columns = !_.isEmpty(all_data_columns)
+      ? _.filter(all_data_columns, ({ nick }) =>
+          _.includes(this.state.columns, nick)
         )
-    );
+      : undefined;
 
-    const get_sorted_key_columns = createSelector(
-      get_table,
-      get_key_columns_for_table
-    );
+    const sorted_key_columns = this.state.table
+      ? this.get_key_columns_for_table(table)
+      : undefined;
 
-    const get_def_ready_cols = createSelector(
-      get_table,
-      get_sorted_columns,
-      (table, sorted_data_columns) =>
-        _.chain(sorted_data_columns)
-          .map((col) => ({
-            name: col.fully_qualified_name,
-            def: table.column_description(col.nick),
+    const def_ready_columns = !_.isEmpty(columns)
+      ? _.map(columns, (col) => ({
+          name: col.fully_qualified_name,
+          def: table.column_description(col.nick),
+        }))
+      : undefined;
+
+    const footnotes =
+      subject && table
+        ? Footnote.get_for_subject(subject, table.tags.concat(["MACHINERY"]))
+        : undefined;
+
+    const dimensions = this.state.table
+      ? _.chain(table)
+          .filter("include_in_report_builder")
+          .map(({ title_key }) => ({
+            id: title_key,
+            display: text_maker(title_key),
           }))
           .value()
-    );
+      : undefined;
 
-    //array of html footnotes
-    const get_footnotes = createSelector(
-      [get_table, get_subject],
-      (table, subject) => {
-        const topics = table.tags.concat(["MACHINERY"]);
-        const subject_footnotes = Footnote.get_for_subject(subject, topics);
-        return subject_footnotes;
-      }
-    );
+    const filters =
+      this.state.table && this.state.dimension
+        ? this.get_filters_for_dim(table, this.state.dimension)
+        : undefined;
 
-    const get_dimensions = createSelector(get_table, (table) =>
-      _.chain(table.dimensions)
-        .filter("include_in_report_builder")
-        .map(({ title_key }) => ({
-          id: title_key,
-          display: text_maker(title_key),
-        }))
-        .value()
-    );
-
-    const get_all_filters = createSelector(
-      [get_table, _.property("dimension")],
-      (table, dim_key) => get_filters_for_dim(table, dim_key)
-    );
-
-    const get_filters_by_dimension = createSelector(
-      [get_table, get_dimensions],
-      (table, dimensions) =>
-        _.map(dimensions, ({ id: dim_key, display }) => ({
+    const filters_by_dimension = !_.isEmpty(dimensions)
+      ? _.map(dimensions, ({ id: dim_key, display }) => ({
           display,
           id: dim_key,
           children: _.map(get_filters_for_dim(table, dim_key), (filter) => ({
@@ -220,67 +199,58 @@ class Root extends React.Component {
             display: filter,
           })),
         }))
-    );
+      : undefined;
 
-    const get_table_data = createSelector(get_table, (table) => {
-      table.fill_dimension_columns();
-      return table.data;
-    });
+    const table_data = this.state.table
+      ? (() => {
+          table.fill_dimension_columns();
+          return table.data;
+        })()
+      : undefined;
 
-    const get_cat_filter_func = createSelector(
-      [_.property("dimension"), _.property("filter")],
-      (dim_key, filter_val) =>
-        filter_val === text_maker("all")
+    const cat_filter_func =
+      this.state.filter && this.state.dimension
+        ? this.state.filter === text_maker("all")
           ? _.constant(true)
-          : { [dim_key]: filter_val }
-    );
+          : { [this.state.dimension]: this.state.filter }
+        : undefined;
 
-    const get_zero_filter_func = createSelector(
-      [_.property("columns")],
-      (col_nicks) => (row) =>
-        _.chain(col_nicks)
-          .map((nick) => row[nick])
+    const zero_filter_func = this.state.columns
+      ? _.chain(this.state.columns)
+          .map((nick) => this.state.columns[nick])
           .compact()
           .isEmpty()
           .value()
-    );
-    const get_flat_data = createSelector(
-      [get_table_data, get_cat_filter_func, get_zero_filter_func],
-      (table_data, cat_filter_func, zero_filter_func) =>
-        _.chain(table_data)
+      : undefined;
+
+    const flat_data = !_.isEmpty(table_data)
+      ? _.chain(table_data)
           .filter(cat_filter_func)
           .reject(zero_filter_func)
           .value()
-    );
+      : undefined;
+
+    const options = {
+      table,
+      subject,
+      columns,
+      dimensions,
+      filters,
+      footnotes,
+      def_ready_columns,
+      all_data_columns,
+      flat_data,
+      filters_by_dimension,
+      sorted_key_columns,
+    };
 
     return (
       <RPB
         {...this.state}
-        table={this.state.table && get_table(this.state)}
-        subject={this.state.subject && get_subject(this.state)}
-        columns={
-          !_.isEmpty(this.state.columns) && get_sorted_columns(this.state)
-        }
-        dimensions={this.state.table && get_dimensions(this.state)}
-        filters={this.state.table && get_all_filters(this.state)}
-        footnotes={this.state.table && get_footnotes(this.state)}
-        def_ready_columns={
-          !_.isEmpty(this.state.columns) && get_def_ready_cols(this.state)
-        }
-        all_data_columns={
-          !_.isEmpty(this.state.columns) && get_all_data_columns(this.state)
-        }
-        flat_data={this.state.table && get_flat_data(this.state)}
-        //granular props
-        filters_by_dimension={
-          this.state.table && get_filters_by_dimension(this.state)
-        }
-        sorted_key_columns={
-          !_.isEmpty(this.state.columns) && get_sorted_key_columns(this.state)
-        }
-        on_set_filter={on_set_filter}
-        on_set_dimension={on_set_dimension}
-        on_switch_table={on_switch_table}
+        {...options}
+        on_set_filter={this.on_set_filter}
+        on_set_dimension={this.on_set_dimension}
+        on_switch_table={this.on_switch_table}
       />
     );
   }
