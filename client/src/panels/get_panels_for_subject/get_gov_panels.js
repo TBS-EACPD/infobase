580cff78640ac76c0e4202d5fb818249526bdc5b
Taewan Kang <tkang042@uottawa.ca>
2020-08-04 13:11:18 -0400

    dept and gov wide standards panel, some text tweaks

--- client/src/panels/get_panels_for_subject/get_gov_panels.js
+++ client/src/panels/get_panels_for_subject/get_gov_panels.js
@@ -28,6 +28,7 @@ import {
   declare_services_digital_status_panel,
   declare_services_fees_panel,
   declare_services_id_methods_panel,
+  declare_services_standards_panel,
   declare_results_intro_panel,
   declare_covid_key_concepts_panel,
   declare_covid_intro_panel,
@@ -83,6 +84,7 @@ export const get_gov_panels = (subject) => ({
     declare_services_digital_status_panel(),
     declare_services_fees_panel(),
     declare_services_id_methods_panel(),
+    declare_services_standards_panel(),
   ],
   results: [
     declare_results_key_concepts_panel(),
