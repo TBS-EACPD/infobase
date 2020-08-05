13ac49e675933c954d529382b1f6bc5c0bb08ca8
Garvin <garvinh2001@hotmail.com>
2020-08-05 16:03:43 -0400

    Add snapshot testing to json that is built before parsing to CSV

--- email_backend/data_management/generate_csv.integration-test.js
+++ email_backend/data_management/generate_csv.integration-test.js
@@ -1,7 +1,4 @@
-import { connect_db } from "../src/db_utils/connect_db";
-import { get_templates } from "../src/template_utils";
-import { make_mongoose_model_from_original_template } from "../src/db_utils/log_email_and_meta_to_db";
-import mongoose from "mongoose";
+import _ from "lodash";
 import axios from "axios";
 import { get_output } from "./generate_csv";
 
@@ -11,8 +8,8 @@ const completed_test_template = {
   enums: ["bug", "other"],
   radio: ["yes"],
   text: "a",
-  number: 1,
-  json: { bleh: "bleh", a: 1 },
+  number: 2,
+  json: { bleh: "bleh", a: 2 },
 
   required_automatic: "blah",
   optional_automatic: "bluh",
@@ -23,16 +20,42 @@ const instance = axios.create({
     referer: "http://localhost:8080/build/InfoBase/index-eng.html",
   },
 });
+let csv, json;
 
-const post_time = new Date().getTime();
-instance
-  .post("http://127.0.0.1:7331/submit_email", {
-    template_name: test_template_name,
-    completed_template: completed_test_template,
-  })
-  .then(() => {
-    const res_time = new Date().getTime();
-    const output = get_output().then(console.log);
-    console.log(output);
-    console.log("test");
+beforeAll((done) => {
+  instance
+    .post("http://127.0.0.1:7331/submit_email", {
+      template_name: test_template_name,
+      completed_template: completed_test_template,
+    })
+    .then(() => {
+      get_output().then(({ csv_output, json_output }) => {
+        csv = csv_output;
+        json = json_output;
+        done();
+      });
+    });
+});
+
+afterAll((done) => {});
+
+describe("Check that CSV output and JSON output are correct", () => {
+  it.only("Snapshot of JSON output", () => {
+    expect(json).toMatchSnapshot({
+      _id: expect.any(Object),
+      a: 1,
+      bleh: "bleh",
+      enums: "bug, other",
+      from: "Sender Name <sender@example.com>",
+      number: 1,
+      optional_automatic: "bluh",
+      radio: "yes",
+      referer: "http://localhost:8080/build/InfoBase/index-eng.html",
+      required_automatic: "blah",
+      server_time: expect.any(String),
+      text: "a",
+      time: expect.any(String),
+      to: "Recipient <recipient@example.com>",
+    });
   });
+});
