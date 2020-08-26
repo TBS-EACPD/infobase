44718cdaeb56e24d626ac54b6f297ec0ea45bb11
Garvin <garvinh2001@hotmail.com>
2020-08-26 13:45:15 -0400

    Replace svg files with yaml; cover case of small window

--- client/src/components/RotateLandscape/RotateLandscape.js
+++ client/src/components/RotateLandscape/RotateLandscape.js
@@ -1,7 +1,7 @@
-import MediaQuery, { useMediaQuery } from "react-responsive";
+import MediaQuery from "react-responsive";
 import "./RotateLandscape.scss";
 import text from "./RotateLandscape.yaml";
-import RotateIcon from "./RotateLandscape.svg";
+import svgs from "./RotateLandscapeSVG.yaml";
 import { create_text_maker_component } from "../misc_util_components";
 import { Fragment } from "react";
 
@@ -19,13 +19,24 @@ export default class RotateLandscape extends React.Component {
               <TM k="rotate_text" el="h2" style={{ color: "white" }} />
               <div
                 className="icon"
-                dangerouslySetInnerHTML={{ __html: RotateIcon }}
+                dangerouslySetInnerHTML={{ __html: svgs.rotate_icon }}
               />
             </div>
           </MediaQuery>
           <MediaQuery orientation="landscape">{children}</MediaQuery>
         </MediaQuery>
-        <MediaQuery minDeviceWidth={568}>{children}</MediaQuery>
+        <MediaQuery minDeviceWidth={568}>
+          <MediaQuery maxWidth={567.98}>
+            <div className="overlay">
+              <TM k="expand_width_text" el="h2" style={{ color: "white" }} />
+              <div
+                className="icon"
+                dangerouslySetInnerHTML={{ __html: svgs.expand_width_icon }}
+              />
+            </div>
+          </MediaQuery>
+          <MediaQuery minWidth={568}>{children}</MediaQuery>
+        </MediaQuery>
       </Fragment>
     ) : (
       children
