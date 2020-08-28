045e823bddf2609a89eeb677a9ab2d0dd96350c3
Garvin <garvinh2001@hotmail.com>
2020-08-28 16:33:52 -0400

    Add redux to close button

--- client/src/components/RotateLandscape/RotateLandscape.js
+++ client/src/components/RotateLandscape/RotateLandscape.js
@@ -3,34 +3,61 @@ import "./RotateLandscape.scss";
 import text from "./RotateLandscape.yaml";
 import { IconRotatePhone, IconExpandWindowWidth } from "../../icons/icons.js";
 import { create_text_maker_component } from "../misc_util_components";
-import { get_static_url } from "../../request_utils";
+import { connect } from "react-redux";
+import { rotate_landscape_off } from "../../InfoBase/App";
+import { Fragment } from "react";
 
 const { TM, text_maker } = create_text_maker_component(text);
 
-export default class RotateLandscape extends React.Component {
+class RotateLandscape extends React.Component {
   render() {
-    const { children } = this.props;
+    const { children, rotate_landscape, rotate_landscape_off } = this.props;
 
     return (
       <div style={{ position: "relative" }}>
         {children}
-        <MediaQuery maxDeviceWidth={567.98} orientation="portrait">
-          <div className="overlay">
-            <TM k="rotate_text" el="h2" style={{ color: "white" }} />
-            <IconRotatePhone width="50%" />
-            <button className="btn btn-ib-primary">
-              {text_maker("close")}
-            </button>
-          </div>
-        </MediaQuery>
-        <MediaQuery minDeviceWidth={568} maxWidth={567.98}>
-          <div className="overlay">
-            <TM k="expand_width_text" el="h2" style={{ color: "white" }} />
-            <IconExpandWindowWidth width="50%" />
-          </div>
-          <button className="btn btn-ib-primary">{text_maker("close")}</button>
-        </MediaQuery>
+        {rotate_landscape ? (
+          <Fragment>
+            <MediaQuery maxDeviceWidth={567.98} orientation="portrait">
+              <div className="overlay">
+                <TM k="rotate_text" el="h2" style={{ color: "white" }} />
+                <IconRotatePhone width="50%" />
+                <button
+                  className="btn btn-ib-primary"
+                  onClick={rotate_landscape_off}
+                >
+                  {text_maker("close")}
+                </button>
+              </div>
+            </MediaQuery>
+            <MediaQuery minDeviceWidth={568} maxWidth={567.98}>
+              <div className="overlay">
+                <TM k="expand_width_text" el="h2" style={{ color: "white" }} />
+                <IconExpandWindowWidth width="50%" />
+                <button
+                  className="btn btn-ib-primary"
+                  onClick={rotate_landscape_off}
+                >
+                  {text_maker("close")}
+                </button>
+              </div>
+              <button className="btn btn-ib-primary">
+                {text_maker("close")}
+              </button>
+            </MediaQuery>
+          </Fragment>
+        ) : null}
       </div>
     );
   }
 }
+
+const mapStateToProps = (state) => ({
+  rotate_landscape: state.show_rotate_landscape,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  rotate_landscape_off: () => dispatch(rotate_landscape_off()),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(RotateLandscape);
