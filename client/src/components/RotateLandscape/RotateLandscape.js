d83f4ba30283592bd6ba5f14ef1a84a88598d5dd
Garvin <garvinh2001@hotmail.com>
2020-08-28 13:23:48 -0400

    Change back to overlay

--- client/src/components/RotateLandscape/RotateLandscape.js
+++ client/src/components/RotateLandscape/RotateLandscape.js
@@ -12,32 +12,27 @@ export default class RotateLandscape extends React.Component {
     const { children } = this.props;
 
     return (
-      <Fragment>
-        <MediaQuery maxDeviceWidth={567.98}>
-          <MediaQuery orientation="portrait">
-            <div className="overlay">
-              <TM k="rotate_text" el="h2" style={{ color: "white" }} />
-              <div
-                className="icon"
-                dangerouslySetInnerHTML={{ __html: svgs.rotate_icon }}
-              />
-            </div>
-          </MediaQuery>
-          <MediaQuery orientation="landscape">{children}</MediaQuery>
+      <div style={{ position: "relative" }}>
+        {children}
+        <MediaQuery maxDeviceWidth={567.98} orientation="portrait">
+          <div className="overlay">
+            <TM k="rotate_text" el="h2" style={{ color: "white" }} />
+            <div
+              className="icon"
+              dangerouslySetInnerHTML={{ __html: svgs.rotate_icon }}
+            />
+          </div>
         </MediaQuery>
-        <MediaQuery minDeviceWidth={568}>
-          <MediaQuery maxWidth={567.98}>
-            <div className="overlay">
-              <TM k="expand_width_text" el="h2" style={{ color: "white" }} />
-              <div
-                className="icon"
-                dangerouslySetInnerHTML={{ __html: svgs.expand_width_icon }}
-              />
-            </div>
-          </MediaQuery>
-          <MediaQuery minWidth={568}>{children}</MediaQuery>
+        <MediaQuery minDeviceWidth={568} maxWidth={567.98}>
+          <div className="overlay">
+            <TM k="expand_width_text" el="h2" style={{ color: "white" }} />
+            <div
+              className="icon"
+              dangerouslySetInnerHTML={{ __html: svgs.expand_width_icon }}
+            />
+          </div>
         </MediaQuery>
-      </Fragment>
+      </div>
     );
   }
 }
