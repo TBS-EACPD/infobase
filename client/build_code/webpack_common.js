d4566d4af361d98657d5095fd3ceec8ceefba327
AlexCLeduc <ledualex@gmail.com>
2020-08-29 09:13:41 -0400

    add decorator syntax

--- client/build_code/webpack_common.js
+++ client/build_code/webpack_common.js
@@ -20,6 +20,10 @@ const get_rules = ({ should_use_babel, language, is_prod_build }) => {
         plugins: [
           "@babel/plugin-proposal-object-rest-spread",
           "@babel/plugin-syntax-dynamic-import",
+          [
+            "@babel/plugin-proposal-decorators",
+            { decoratorsBeforeExport: false },
+          ],
           "@babel/plugin-proposal-class-properties",
         ],
         presets: [
