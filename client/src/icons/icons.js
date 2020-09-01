2b2cffcd0e5af3a4859046581a27f65e61ecaaa4
Taewan Kang <tkang042@uottawa.ca>
2020-09-01 10:17:47 -0400

    timescrubber with arrow

--- client/src/icons/icons.js
+++ client/src/icons/icons.js
@@ -154,6 +154,19 @@ const IconAbout = (props) => {
   return <_IconWrapper {...props} viewbox_width={24} ChildSVG={SVGAbout} />;
 };
 
+const IconArrowDown = (props) => {
+  const SVGArrowDown = () => {
+    return (
+      <path
+        className="icon-fill icon-stroke"
+        d="M413.1 222.5l22.2 22.2c9.4 9.4 9.4 24.6 0 33.9L241 473c-9.4 9.4-24.6 9.4-33.9 0L12.7 278.6c-9.4-9.4-9.4-24.6 0-33.9l22.2-22.2c9.5-9.5 25-9.3 34.3.4L184 343.4V56c0-13.3 10.7-24 24-24h32c13.3 0 24 10.7 24 24v287.4l114.8-120.5c9.3-9.8 24.8-10 34.3-.4z"
+      />
+    );
+  };
+
+  return <_IconWrapper {...props} viewbox_width={24} ChildSVG={SVGArrowDown} />;
+};
+
 const IconGlossary = (props) => {
   const SVGGlossary = () => {
     return (
@@ -938,6 +951,7 @@ export {
   IconZoomOut,
   IconFilterColumns,
   IconCheckmark,
+  IconArrowDown,
   IconCheck,
   IconAttention,
   IconNotApplicable,
