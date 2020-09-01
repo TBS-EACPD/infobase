1f1b0e593fbe6c1ebfe8d5bed022e8d9ab18deb6
Taewan Kang <tkang042@uottawa.ca>
2020-09-01 10:17:47 -0400

    timescrubber with arrow

--- client/src/components/SliderRange.js
+++ client/src/components/SliderRange.js
@@ -1,15 +1,21 @@
 import "./SliderRange.scss";
+import { IconArrowDown } from "../icons/icons.js";
 
 export class SliderRange extends React.Component {
-  componentDidMount() {
-    const rangeBullet = document.getElementsByClassName("slider-label")[0];
-    const slider = document.getElementsByClassName("slider")[0];
+  constructor(props) {
+    super(props);
+    this.calculatePosition = this.calculatePosition.bind(this);
+  }
+  calculatePosition(slider, slider_label) {
     const range_width = slider.getBoundingClientRect().width - 15;
     const percent = slider.value / (this.props.slider_data.length - 1);
+    slider_label.style.left = `${range_width * percent}px`;
+  }
+  componentDidMount() {
+    const slider_label = document.getElementsByClassName("slider-label")[0];
+    const slider = document.getElementsByClassName("slider")[0];
 
-    // the position of the output
-    const newPosition = range_width * percent;
-    rangeBullet.style.left = `${newPosition}px`;
+    this.calculatePosition(slider, slider_label);
   }
   render() {
     const {
@@ -23,7 +29,22 @@ export class SliderRange extends React.Component {
     return (
       <div className="slider-container">
         <span className="slider-label">
-          {value_formatter(selected_data_index)}
+          <span
+            style={{
+              display: "inline-block",
+              width: "100%",
+              paddingLeft: "30px",
+            }}
+          >
+            {value_formatter(selected_data_index)}
+          </span>
+          <IconArrowDown
+            color={window.infobase_color_constants.secondaryColor}
+            alternate_color={false}
+            width={2}
+            height={2}
+            vertical_align={8}
+          />
         </span>
         <input
           className="slider"
@@ -33,16 +54,10 @@ export class SliderRange extends React.Component {
           defaultValue={slider_default_value || slider_data.length - 1}
           step={1}
           onChange={(e) => {
-            const rangeBullet = document.getElementsByClassName(
+            const slider_label = document.getElementsByClassName(
               "slider-label"
             )[0];
-            const range_width =
-              e.currentTarget.getBoundingClientRect().width - 15;
-            const percent = e.target.value / (slider_data.length - 1);
-
-            // the position of the output
-            const newPosition = range_width * percent;
-            rangeBullet.style.left = `${newPosition}px`;
+            this.calculatePosition(e.target, slider_label);
 
             slider_callback(e.target.value);
           }}
