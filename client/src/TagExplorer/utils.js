2b345d74daf0f9aac85ccc117d3be4b105dc0d41
AlexCLeduc <ledualex@gmail.com>
2020-09-09 07:48:01 -0400

    move export at bottom of file

--- client/src/TagExplorer/utils.js
+++ client/src/TagExplorer/utils.js
@@ -6,8 +6,6 @@ import text from "./TagExplorer.yaml";
 
 const { text_maker, TM } = create_text_maker_component(text);
 
-export { text_maker, TM };
-
 const { std_years, planning_years } = year_templates;
 const actual_year = _.last(std_years);
 const planning_year = _.first(planning_years);
@@ -19,6 +17,8 @@ const route_arg_to_year_map = {
 const year_to_route_arg_map = _.invert(route_arg_to_year_map);
 
 export {
+  text_maker,
+  TM,
   planning_year,
   actual_year,
   route_arg_to_year_map,
