ce25a17fc9dfa4da5d7603820cde1d72e661afbc
Garvin <garvinh2001@hotmail.com>
2020-09-18 14:29:56 -0400

    Fix rebase issues

--- client/src/InfoBase/AppState.js
+++ client/src/InfoBase/AppState.js
@@ -1,14 +1,13 @@
-export default function app_reducer(
-  //export not needed but dev tools will throw error about no key
+export const app_reducer = (
   state = { lang: window.lang, is_showing_graph_overlay: true },
   { type, payload }
-) {
+) => {
   switch (type) {
     case "graph_overlay":
       return { ...state, is_showing_graph_overlay: false };
   }
   return state;
-}
+};
 
 export const hide_graph_overlay = () => {
   return {
