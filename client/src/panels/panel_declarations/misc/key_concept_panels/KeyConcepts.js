42902642a0fc07c81af0e1a04ad40c51fb7e1b6e
Garvin <garvinh2001@hotmail.com>
2020-10-02 09:01:44 -0400

    Add popup alert to click on some things to keep in mind

--- client/src/panels/panel_declarations/misc/key_concept_panels/KeyConcepts.js
+++ client/src/panels/panel_declarations/misc/key_concept_panels/KeyConcepts.js
@@ -2,12 +2,13 @@ import classNames from "classnames";
 import { Fragment } from "react";
 import MediaQuery from "react-responsive";
 import { withRouter } from "react-router";
-
-import { StatelessModal } from "src/components/index.js";
+import { ButtonToolbar, Overlay, Popover } from "react-bootstrap";
 
 import { has_local_storage } from "src/core/feature_detection.js";
 
 import { util_components, breakpoints } from "../../shared.js";
+
+import "./KeyConcepts.scss";
 
 import common_lang from "./common_questions.yaml";
 import fin_lang from "./financial_questions.yaml";
@@ -21,20 +22,29 @@ const {
   KeyConceptList,
 } = util_components;
 
+const text = {
+  click_text: {
+    en: "Click below!",
+    fr: "Cliquer ci-dessous!",
+  },
+};
+
 const { text_maker, TM } = create_text_maker_component([
   common_lang,
   fin_lang,
   ppl_lang,
   results_lang,
   tag_lang,
+  text,
 ]);
 
 class KeyConcepts_ extends React.Component {
   state = {
-    is_showing_modal: false,
+    is_showing_tooltip: false,
   };
   constructor(props) {
     super(props);
+    this.accordionRef = React.createRef(null);
 
     const {
       match: {
@@ -49,8 +59,8 @@ class KeyConcepts_ extends React.Component {
 
       if (_.isNull(do_not_display)) {
         this.timeout = setTimeout(
-          () => this.setState({ is_showing_modal: true }),
-          30000
+          () => this.setState({ is_showing_tooltip: true }),
+          1000
         );
       }
     }
@@ -65,11 +75,11 @@ class KeyConcepts_ extends React.Component {
       },
     } = this.props;
 
-    const { is_showing_modal } = this.state;
+    const { is_showing_tooltip } = this.state;
 
     const disable_popup = () => {
       this.timeout && clearTimeout(this.timeout);
-      this.setState({ is_showing_modal: false });
+      this.setState({ is_showing_tooltip: false });
       has_local_storage &&
         localStorage.setItem(
           `hide_${active_bubble_id}_key_concepts_popup`,
@@ -84,37 +94,45 @@ class KeyConcepts_ extends React.Component {
             <div
               className={classNames("mrgn-bttm-md", matches && "mrgn-tp-md")}
               onClick={disable_popup}
+              style={{ position: "relative" }}
             >
-              <AutoAccordion title={text_maker("some_things_to_keep_in_mind")}>
-                <div style={{ paddingLeft: "10px", paddingRight: "10px" }}>
-                  <KeyConceptList
-                    question_answer_pairs={_.map(rendered_q_a_keys, (key) => [
-                      <TM key={key + "_q"} k={key + "_q"} args={{ subject }} />,
-                      <TM key={key + "_a"} k={key + "_a"} args={{ subject }} />,
-                    ])}
-                  />
-                </div>
-              </AutoAccordion>
+              <ButtonToolbar>
+                <AutoAccordion
+                  title={text_maker("some_things_to_keep_in_mind")}
+                  ref={this.accordionRef}
+                >
+                  <div style={{ paddingLeft: "10px", paddingRight: "10px" }}>
+                    <KeyConceptList
+                      question_answer_pairs={_.map(rendered_q_a_keys, (key) => [
+                        <TM
+                          key={key + "_q"}
+                          k={key + "_q"}
+                          args={{ subject }}
+                        />,
+                        <TM
+                          key={key + "_a"}
+                          k={key + "_a"}
+                          args={{ subject }}
+                        />,
+                      ])}
+                    />
+                  </div>
+                </AutoAccordion>
+
+                <Overlay
+                  show={is_showing_tooltip}
+                  target={this.accordionRef.current}
+                  container={this}
+                  placement="top"
+                >
+                  <Popover id="keep_in_mind_tooltip" style={{}}>
+                    {text_maker("click_text")}
+                  </Popover>
+                </Overlay>
+              </ButtonToolbar>
             </div>
           )}
         </MediaQuery>
-        <StatelessModal
-          show={is_showing_modal}
-          title={text_maker("some_things_to_keep_in_mind")}
-          body={_.map(rendered_q_a_keys, (key) => [
-            <TM
-              key={key + "_q"}
-              el="h2"
-              k={key + "_q"}
-              args={{ subject }}
-              style={{ fontWeight: 700 }}
-            />,
-            <TM key={key + "_a"} el="p" k={key + "_a"} args={{ subject }} />,
-            <br key={key} />,
-          ])}
-          on_close_callback={disable_popup}
-          additional_dialog_class={"modal-responsive"}
-        />
       </Fragment>
     );
   }
