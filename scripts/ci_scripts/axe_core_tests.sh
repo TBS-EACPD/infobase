e72817bddd07a162edeeb080edec00d4d256ffcc
Taewan Kang <tkang042@uottawa.ca>
2020-10-15 12:26:05 -0300

    update executable file

--- scripts/ci_scripts/axe_core_tests.sh
+++ scripts/ci_scripts/axe_core_tests.sh
@@ -1,4 +1,4 @@
-set -e
+set -e 
 
 axe https://dev.ea-ad.ca/master/index-eng.html#start --disable color-contrast
 axe https://dev.ea-ad.ca/master/index-eng.html#compare_estimates --disable color-contrast
