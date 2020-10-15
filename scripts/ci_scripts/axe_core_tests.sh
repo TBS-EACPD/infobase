1c0218c1cd23c2380b83502ef731d5da06992485
Taewan Kang <tkang042@uottawa.ca>
2020-10-15 12:00:10 -0300

    try adding more tests

---
+++ scripts/ci_scripts/axe_core_tests.sh
@@ -0,0 +1,7 @@
+set -e
+
+axe https://dev.ea-ad.ca/master/index-eng.html#start --disable color-contrast
+axe https://dev.ea-ad.ca/master/index-eng.html#compare_estimates --disable color-contrast
+axe https://dev.ea-ad.ca/master/index-eng.html#igoc --disable color-contrast
+axe https://dev.ea-ad.ca/master/index-eng.html#treemap --disable color-contrast
+axe https://dev.ea-ad.ca/master/index-eng.html#tag-explorer --disable color-contrast
