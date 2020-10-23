4152fa34cafc9ad1e9e27b51775e36eec80e9a5b
Garvin <garvinh2001@hotmail.com>
2020-10-23 14:11:19 -0400

    Text rework; Correcting handlebars to correct location

--- client/src/panels/panel_declarations/finances/vote_stat/in_year_vote_stat_split.js
+++ client/src/panels/panel_declarations/finances/vote_stat/in_year_vote_stat_split.js
@@ -20,6 +20,8 @@ const render_w_options = ({ graph_col, text_col, text_key }) => ({
     id: data_set.label,
   }));
 
+  console.log(info);
+
   return (
     <StdPanel
       title={text_maker("in_year_voted_stat_split_title")}
