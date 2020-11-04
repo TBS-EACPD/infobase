1ea5139ba8f1b9f93ae59b0c06ccf2abcf57c070
GarvinH <garvinh2001@hotmail.com>
2020-11-04 11:07:02 -0500

    Revert "Show menu by state rule; add close button to menu"
    This reverts commit 86d7b9d6c6a080c9e21aadfa7fc7e7a5ed9559db.

--- client/src/search/Typeahead/TypeaheadMenu.js
+++ client/src/search/Typeahead/TypeaheadMenu.js
@@ -28,7 +28,6 @@ export class TypeaheadMenu extends React.Component {
       queried_results,
       config_groups,
       on_select_item,
-      hide_menu,
     } = this.props;
 
     const menu_item_selected = (selected) => {
@@ -164,14 +163,6 @@ export class TypeaheadMenu extends React.Component {
                     <span className="aria-hidden">▼</span>
                   </ListGroupItem>
                 ),
-                <ListGroupItem
-                  key={pagination_down_item_index + 1}
-                  id={`rbt-menu-item-${pagination_down_item_index + 1}`}
-                  className="rbt-menu-pagination-option rbt-menu-pagination-option--next dropdown-item"
-                  onClick={hide_menu}
-                >
-                  Close menu
-                </ListGroupItem>,
               ];
             })
             .value()}
