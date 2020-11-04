5718ef74779a810f7f6efd6e046f0daa4c0b40e2
Stephen-ONeil <stephenlevioneil@gmail.com>
2020-11-04 10:11:16 -0500

    Fix query info logs from server resulting in multiple logs with text payloads rather than one log with a JSON payload, I believe

--- server/src/app.js
+++ server/src/app.js
@@ -23,35 +23,30 @@ app.use(compression());
 app.use(cors());
 
 app.use("/", function (req, res, next) {
-  try {
-    res.header("cache-control", "public, max-age=31536000");
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header(
-      "Access-Control-Allow-Headers",
-      "Content-Type, Authorization, Content-Length, X-Requested-With, encoded-compressed-query"
-    );
-    if (req.method === "OPTIONS") {
-      res.sendStatus(200);
-    } else {
-      // Often want to use GET to leverage http caching, but some of out longer queries are too long for a query parameter
-      // Instead, the client makes a GET with a header containing a compressed and base 64 encoded copy of the query (the query parameter becomes a hash, for cache busting)
-      // In that case, we mutate the request here to recover the query and let the server pretend it received a normal POST
-      if (
-        req.method === "GET" &&
-        !_.isEmpty(req.headers["encoded-compressed-query"])
-      ) {
-        convert_GET_with_compressed_query_to_POST(req); // mutates req, changes made persist to subsequent middleware
-      }
-
-      // eslint-disable-next-line no-console
-      process.env.USE_REMOTE_DB &&
-        console.log(JSON.stringify(get_log_object_for_request(req), null, 2));
-
-      next();
+  res.header("cache-control", "public, max-age=31536000");
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization, Content-Length, X-Requested-With, encoded-compressed-query"
+  );
+  if (req.method === "OPTIONS") {
+    res.sendStatus(200);
+  } else {
+    // Often want to use GET to leverage http caching, but some of out longer queries are too long for a query parameter
+    // Instead, the client makes a GET with a header containing a compressed and base 64 encoded copy of the query (the query parameter becomes a hash, for cache busting)
+    // In that case, we mutate the request here to recover the query and let the server pretend it received a normal POST
+    if (
+      req.method === "GET" &&
+      !_.isEmpty(req.headers["encoded-compressed-query"])
+    ) {
+      convert_GET_with_compressed_query_to_POST(req); // mutates req, changes made persist to subsequent middleware
     }
-  } catch (e) {
-    console.log(e);
-    res.sendStatus(404);
+
+    // eslint-disable-next-line no-console
+    process.env.USE_REMOTE_DB &&
+      console.log(JSON.stringify(get_log_object_for_request(req)));
+
+    next();
   }
 });
 
