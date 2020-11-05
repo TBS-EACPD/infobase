4d4d1cd31902752eb4f28c09f24102f658385168
Taewan Kang <tkang042@uottawa.ca>
2020-11-05 14:53:03 -0500

    updating webpack, eslint. still need to resolve graphql stuff

--- client/src/EstimatesComparison/utils.js
+++ client/src/EstimatesComparison/utils.js
@@ -2,7 +2,6 @@ import { create_text_maker_component } from "../components/index.js";
 
 import text from "./EstimatesComparison.yaml";
 
-
 export const { text_maker, TM } = create_text_maker_component(text);
 
 export const current_doc_is_mains = false; // Update this when switching between displaying mains and sups!
