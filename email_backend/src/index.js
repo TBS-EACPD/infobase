f210dff1b858d0462306924bf3557af681967dc2
Stephen-ONeil <stephenlevioneil@gmail.com>
2020-11-09 14:24:00 -0500

    Hotfix dumb typo in email_backend entrypoint

--- email_backend/src/index.js
+++ email_backend/src/index.js
@@ -2,6 +2,6 @@
 
 import { run_email_backend } from "./email_backend.js";
 
-const email_backend = (() => run_email_backend)();
+const email_backend = (() => run_email_backend())();
 
 export { email_backend };
