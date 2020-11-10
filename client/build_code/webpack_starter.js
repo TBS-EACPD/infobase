5e7ea8b5f0e526358d981c2017ec66bddacc9c28
Taewan Kang <tkang042@uottawa.ca>
2020-11-10 12:10:58 -0500

    fix terser, output names, better logging

--- client/build_code/webpack_starter.js
+++ client/build_code/webpack_starter.js
@@ -42,7 +42,7 @@ const options_by_app = {
     entry: common_entry.concat(["./src/InfoBase/root.js"]),
     get_output: (language) => ({
       path: path.resolve(__dirname, `../${build_dir_name}/InfoBase/app/`),
-      filename: `app-a11y-${language}.min.js`,
+      filename: `[name]-app-a11y-${language}.min.js`,
       chunkFilename: `[name].app-ally-${language}${
         prod ? ".[chunkhash]" : ""
       }.min.js`,
@@ -52,7 +52,7 @@ const options_by_app = {
     entry: common_entry.concat(["./src/InfoBase/root.js"]),
     get_output: (language) => ({
       path: path.resolve(__dirname, `../${build_dir_name}/InfoBase/app/`),
-      filename: `app-${language}.min.js`,
+      filename: `[name]-app-${language}.min.js`,
       chunkFilename: `[name].app-${language}${
         prod ? ".[chunkhash]" : ""
       }.min.js`,
@@ -100,6 +100,7 @@ gitsha(function (err, commit_sha) {
     webpack(config, function (err, stats) {
       console.log(stats.toString({ cached: true, modules: true }));
       if (err || stats.hasErrors()) {
+        console.err(err);
         process.exitCode = 1;
       }
     });
@@ -112,6 +113,9 @@ gitsha(function (err, commit_sha) {
         ignored: /node_modules/,
       },
       function (err, stats) {
+        if (err) {
+          console.err(err);
+        }
         console.log(stats.toString({ cached: true, modules: true }));
       }
     );
