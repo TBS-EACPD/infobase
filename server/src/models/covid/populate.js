e78784a070b65cdb42ff713812b7886c52945286
Stephen-ONeil <stephenlevioneil@gmail.com>
2020-11-13 16:46:24 -0500

    Shuffle around nesting of mongo and GraphQL models for covid

--- server/src/models/covid/populate.js
+++ server/src/models/covid/populate.js
@@ -3,36 +3,40 @@ import _ from "lodash";
 import { get_standard_csv_file_rows } from "../load_utils.js";
 
 export default async function ({ models }) {
-  const { CovidMeasure, CovidInitiative, CovidInitiativeEstimates } = models;
+  const { CovidMeasure, CovidInitiative } = models;
 
-  const CovidMeasure_records = _.chain(
+  const covid_measure_records = _.chain(
     get_standard_csv_file_rows("covid_measures.csv")
   )
     .map((row) => new CovidMeasure(row))
     .value();
 
-  const CovidInitiative_records = _.chain(
+  const covid_initative_estimates_rows = _.map(
+    get_standard_csv_file_rows("covid_initative_estimates.csv"),
+    (row) => ({
+      ...row,
+      covid_measure_ids: _.split(row.covid_measure_ids, ","),
+    })
+  );
+  const covid_initiative_records = _.chain(
     get_standard_csv_file_rows("covid_initiatives.csv")
   )
-    .map((row) => new CovidInitiative(row))
-    .value();
-
-  const CovidInitiativeEstimates_records = _.chain(
-    get_standard_csv_file_rows("covid_initative_estimates.csv")
-  )
     .map((row) => {
-      const cleaned_row = {
-        ...row,
-        covid_measure_ids: _.split(row.covid_measure_ids, ","),
-      };
+      const initiative_estimates_rows = _.filter(
+        covid_initative_estimates_rows,
+        ({ covid_initiative_id }) =>
+          covid_initiative_id === row.covid_initiative_id
+      );
 
-      return new CovidInitiativeEstimates(cleaned_row);
+      return new CovidInitiative({
+        ...row,
+        estimates: initiative_estimates_rows,
+      });
     })
     .value();
 
   return await Promise.all([
-    CovidMeasure.insertMany(CovidMeasure_records),
-    CovidInitiative.insertMany(CovidInitiative_records),
-    CovidInitiativeEstimates.insertMany(CovidInitiativeEstimates_records),
+    CovidMeasure.insertMany(covid_measure_records),
+    CovidInitiative.insertMany(covid_initiative_records),
   ]);
 }
