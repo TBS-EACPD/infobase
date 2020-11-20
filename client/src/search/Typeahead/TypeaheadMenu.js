cd3bd6870ed6b88158a40745790a30938acf4041
Garvin <garvinh2001@hotmail.com>
2020-11-20 09:02:08 -0500

    Re-add pagination into typeahead

--- client/src/search/Typeahead/TypeaheadMenu.js
+++ client/src/search/Typeahead/TypeaheadMenu.js
@@ -1,3 +1,5 @@
+import { Fragment } from "react";
+
 import { create_text_maker_component } from "../../components";
 
 import text from "./Typeahead.yaml";
@@ -5,19 +7,35 @@ import text from "./Typeahead.yaml";
 const { text_maker, TM } = create_text_maker_component(text);
 
 class TypeaheadMenu_ extends React.Component {
+  state = {
+    pagination_index: 0,
+  };
+
   handleWindowClick = (e) => {
     const { hide_menu, rbtRef } = this.props;
-    if (!rbtRef.current.contains(e.target)) {
+    if (
+      !rbtRef.current.contains(e.target) &&
+      !this.rbtMenuRef.current.contains(e.target)
+    ) {
       hide_menu();
     }
   };
   componentDidMount() {
-    window.addEventListener("click", this.handleWindowClick);
+    document.body.addEventListener("click", this.handleWindowClick);
+    this.rbtMenuRef = React.createRef();
+  }
+
+  componentDidUpdate(prev_props) {
+    if (prev_props.search_text !== this.props.search_text) {
+      this.setState({ pagination_index: 0 });
+    }
   }
 
   render() {
+    const { pagination_index } = this.state;
     const {
       search_text,
+      pagination_size,
       queried_results,
       config_groups,
       on_select_item,
@@ -26,11 +44,42 @@ class TypeaheadMenu_ extends React.Component {
       menuId,
     } = this.props;
 
+    const menu_item_selected = (selected) => {
+      on_select_item(selected);
+      this.setState({ pagination_index: 0 });
+    };
+
+    const paginate_results = (option, index) => {
+      if (option.pagination_placeholder) {
+        if (option.paginate_direction === "previous") {
+          return pagination_index > 0;
+        } else if (option.paginate_direction === "next") {
+          return true; // can't yet tell if next button's needed at this point, so always pass it's placeholder through
+        }
+      }
+      const page_start = pagination_size * pagination_index;
+      const page_end = page_start + pagination_size;
+      const is_on_displayed_page = !(index < page_start || index >= page_end);
+
+      return is_on_displayed_page;
+    };
+
+    const paginated_results = _.filter(
+      queried_results,
+      (queried_result, index) => paginate_results(queried_result, index)
+    );
+
+    const page_range_start = pagination_index * pagination_size + 1;
+    const page_range_end = page_range_start + paginated_results.length - 1;
+
     const total_matching_results = queried_results.length;
 
-    let index_key_counter = 0;
+    const remaining_results =
+      total_matching_results - (pagination_index + 1) * pagination_size;
+    const next_page_size =
+      remaining_results < pagination_size ? remaining_results : pagination_size;
 
-    if (_.isEmpty(queried_results)) {
+    if (_.isEmpty(paginated_results)) {
       return (
         <ul className="rbt-menu dropdown-menu show" id={menuId} role="listbox">
           <li className="dropdown-header">{text_maker("no_matches_found")}</li>
@@ -38,59 +87,116 @@ class TypeaheadMenu_ extends React.Component {
       );
     } else {
       return (
-        <ul className="rbt-menu dropdown-menu show" id={menuId} role="listbox">
-          {_.chain(queried_results)
+        <ul
+          className="rbt-menu dropdown-menu show"
+          id={menuId}
+          role="listbox"
+          ref={this.rbtMenuRef}
+        >
+          {_.chain(paginated_results)
             .groupBy("config_group_index")
             .thru((grouped_results) => {
-              return [
-                <li key={`header-pagination-info`} className="dropdown-header">
-                  <TM
-                    k="paginate_status"
-                    args={{
-                      total_matching_results,
-                    }}
-                  />
-                </li>,
-                ..._.flatMap(grouped_results, (results, group_index) => (
-                  <div
-                    key={`group-${group_index}`}
-                    role="group"
-                    aria-label={config_groups[group_index].group_header}
+              const needs_pagination_up_control = pagination_index > 0;
+              const needs_pagination_down_control =
+                page_range_end < total_matching_results;
+
+              const pagination_down_item_index = needs_pagination_up_control
+                ? paginated_results.length + 1
+                : paginated_results.length;
+
+              let index_key_counter = needs_pagination_up_control ? 1 : 0;
+              return (
+                <Fragment>
+                  <li
+                    key={`header-pagination-info`}
+                    className="dropdown-header"
                   >
-                    <li
-                      key={`header-${group_index}`}
-                      className="dropdown-header"
-                    >
-                      {config_groups[group_index].group_header}
-                    </li>
-                    {[
-                      ..._.map(results, (result) => {
+                    <TM
+                      k="paginate_status"
+                      args={{
+                        page_range_start,
+                        page_range_end,
+                        total_matching_results,
+                      }}
+                    />
+                  </li>
+                  {needs_pagination_up_control && (
+                    <div>
+                      <button
+                        key={`rbt-menu-item-${pagination_down_item_index}`}
+                        id={`rbt-menu-item-${pagination_down_item_index}`}
+                        className="rbt-menu-pagination-option dropdown-item list-group-item"
+                        onClick={(e) => {
+                          this.setState((prev_state) => ({
+                            pagination_index: prev_state.pagination_index - 1,
+                          }));
+                        }}
+                      >
+                        <span className="aria-hidden">▲</span>
+                        <br />
+                        <TM
+                          k="paginate_previous"
+                          args={{ page_size: pagination_size }}
+                        />
+                      </button>
+                    </div>
+                  )}
+
+                  {_.flatMap(grouped_results, (results, group_index) => (
+                    <div key={`header-${group_index}`}>
+                      <li className="dropdown-header">
+                        {config_groups[group_index].group_header}
+                      </li>
+                      {_.map(results, (result) => {
                         const index = index_key_counter++;
                         return (
                           <button
-                            key={index}
+                            key={`rbt-menu-item-${index}`}
+                            id={`rbt-menu-item-${index}`}
                             role="link"
                             className="dropdown-item list-group-item"
-                            onClick={() => on_select_item(result)}
+                            onClick={() => menu_item_selected(result)}
                             ref={index == 0 && firstMenuItemRef}
                           >
                             {result.menu_content(search_text)}
                           </button>
                         );
-                      }),
-                    ]}
+                      })}
+                    </div>
+                  ))}
+                  {needs_pagination_down_control && (
+                    <div>
+                      <button
+                        key={`rbt-menu-item-${pagination_down_item_index}`}
+                        id={`rbt-menu-item-${pagination_down_item_index}`}
+                        className="rbt-menu-pagination-option dropdown-item list-group-item"
+                        onClick={(e) => {
+                          this.setState((prev_state) => ({
+                            pagination_index: prev_state.pagination_index + 1,
+                          }));
+                        }}
+                      >
+                        <TM
+                          k="paginate_next"
+                          args={{ next_page_size: next_page_size }}
+                        />
+                        <br />
+                        <span className="aria-hidden">▼</span>
+                      </button>
+                    </div>
+                  )}
+                  <div>
+                    <button
+                      key={`rbt-menu-item-${pagination_down_item_index + 1}`}
+                      id={`rbt-menu-item-${pagination_down_item_index + 1}`}
+                      className="rbt-menu-close-menu-button dropdown-item list-group-item"
+                      onClick={hide_menu}
+                    >
+                      {text_maker("close_menu")}
+                    </button>
                   </div>
-                )),
-                <div key={`div_${total_matching_results + 1}`}>
-                  <button
-                    key={total_matching_results}
-                    className="rbt-menu-close-menu-button dropdown-item list-group-item"
-                    onClick={hide_menu}
-                  >
-                    {text_maker("close_menu")}
-                  </button>
-                </div>,
-              ];
+                </Fragment>
+              );
             })
             .value()}
         </ul>
@@ -99,7 +205,7 @@ class TypeaheadMenu_ extends React.Component {
   }
 
   componentWillUnmount() {
-    window.removeEventListener("click", this.handleWindowClick);
+    document.body.removeEventListener("click", this.handleWindowClick);
   }
 }
 
