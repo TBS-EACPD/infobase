5f92497a2e1848d0464bbed7f4d967bf1fe507fd
Garvin <garvinh2001@hotmail.com>
2020-11-23 13:48:21 -0500

    Add status notification

--- client/src/search/Typeahead/TypeaheadMenu.js
+++ client/src/search/Typeahead/TypeaheadMenu.js
@@ -1,5 +1,7 @@
 import { Fragment } from "react";
 
+import { Status } from "./Status.js";
+
 import { create_text_maker_component } from "../../components";
 
 import text from "./Typeahead.yaml";
@@ -9,6 +11,12 @@ const { text_maker, TM } = create_text_maker_component(text);
 class TypeaheadMenu_ extends React.Component {
   state = {
     pagination_index: 0,
+  };
+
+  handle_menu_select = (selected) => {
+    const { on_select_item } = this.props;
+    on_select_item(selected);
+    this.setState({ pagination_index: 0 });
   };
 
   handleWindowClick = (e) => {
@@ -34,25 +42,14 @@ class TypeaheadMenu_ extends React.Component {
       pagination_size,
       queried_results,
       config_groups,
-      on_select_item,
       hide_menu,
       firstMenuItemRef,
       menuId,
+      show_menu,
+      min_length,
     } = this.props;
 
-    const menu_item_selected = (selected) => {
-      on_select_item(selected);
-      this.setState({ pagination_index: 0 });
-    };
-
     const paginate_results = (option, index) => {
-      if (option.pagination_placeholder) {
-        if (option.paginate_direction === "previous") {
-          return pagination_index > 0;
-        } else if (option.paginate_direction === "next") {
-          return true; // can't yet tell if next button's needed at this point, so always pass it's placeholder through
-        }
-      }
       const page_start = pagination_size * pagination_index;
       const page_end = page_start + pagination_size;
       const is_on_displayed_page = !(index < page_start || index >= page_end);
@@ -75,124 +72,162 @@ class TypeaheadMenu_ extends React.Component {
     const next_page_size =
       remaining_results < pagination_size ? remaining_results : pagination_size;
 
-    if (_.isEmpty(paginated_results)) {
-      return (
-        <ul className="rbt-menu dropdown-menu show" id={menuId} role="listbox">
-          <li className="dropdown-header">{text_maker("no_matches_found")}</li>
-        </ul>
-      );
-    } else {
-      return (
-        <ul className="rbt-menu dropdown-menu show" id={menuId} role="listbox">
-          {_.chain(paginated_results)
-            .groupBy("config_group_index")
-            .thru((grouped_results) => {
-              const needs_pagination_up_control = pagination_index > 0;
-              const needs_pagination_down_control =
-                page_range_end < total_matching_results;
+    const status_props = {
+      min_length,
+      total_matching_results,
+      page_range_start,
+      page_range_end,
+      query_length: search_text.length,
+    };
 
-              const pagination_down_item_index = needs_pagination_up_control
-                ? paginated_results.length + 1
-                : paginated_results.length;
-
-              let index_key_counter = needs_pagination_up_control ? 1 : 0;
+    return (
+      <Fragment>
+        <Status {...status_props} />
+        {show_menu &&
+          (() => {
+            if (_.isEmpty(paginated_results)) {
               return (
-                <Fragment>
-                  <li
-                    key={`header-pagination-info`}
-                    className="dropdown-header"
-                  >
-                    <TM
-                      k="paginate_status"
-                      args={{
-                        page_range_start,
-                        page_range_end,
-                        total_matching_results,
-                      }}
-                    />
+                <ul
+                  className="rbt-menu dropdown-menu show"
+                  id={menuId}
+                  role="listbox"
+                >
+                  <li className="dropdown-header">
+                    {text_maker("no_matches_found")}
                   </li>
-                  {needs_pagination_up_control && (
-                    <div>
-                      <button
-                        key={`rbt-menu-item-${pagination_down_item_index}`}
-                        id={`rbt-menu-item-${pagination_down_item_index}`}
-                        className="rbt-menu-pagination-option dropdown-item list-group-item"
-                        onClick={(e) => {
-                          this.setState((prev_state) => ({
-                            pagination_index: prev_state.pagination_index - 1,
-                          }));
-                        }}
-                      >
-                        <span className="aria-hidden">▲</span>
-                        <br />
-                        <TM
-                          k="paginate_previous"
-                          args={{ page_size: pagination_size }}
-                        />
-                      </button>
-                    </div>
-                  )}
-
-                  {_.flatMap(grouped_results, (results, group_index) => (
-                    <div key={`header-${group_index}`}>
-                      <li className="dropdown-header">
-                        {config_groups[group_index].group_header}
-                      </li>
-                      {_.map(results, (result) => {
-                        const index = index_key_counter++;
-                        return (
-                          <button
-                            key={`rbt-menu-item-${index}`}
-                            id={`rbt-menu-item-${index}`}
-                            role="link"
-                            className="dropdown-item list-group-item"
-                            onClick={() => menu_item_selected(result)}
-                            ref={index == 0 && firstMenuItemRef}
-                          >
-                            {result.menu_content(search_text)}
-                          </button>
-                        );
-                      })}
-                    </div>
-                  ))}
-                  {needs_pagination_down_control && (
-                    <div>
-                      <button
-                        key={`rbt-menu-item-${pagination_down_item_index}`}
-                        id={`rbt-menu-item-${pagination_down_item_index}`}
-                        className="rbt-menu-pagination-option dropdown-item list-group-item"
-                        onClick={(e) => {
-                          this.setState((prev_state) => ({
-                            pagination_index: prev_state.pagination_index + 1,
-                          }));
-                        }}
-                      >
-                        <TM
-                          k="paginate_next"
-                          args={{ next_page_size: next_page_size }}
-                        />
-                        <br />
-                        <span className="aria-hidden">▼</span>
-                      </button>
-                    </div>
-                  )}
-                  <div>
-                    <button
-                      key={`rbt-menu-item-${pagination_down_item_index + 1}`}
-                      id={`rbt-menu-item-${pagination_down_item_index + 1}`}
-                      className="rbt-menu-close-menu-button dropdown-item list-group-item"
-                      onClick={hide_menu}
-                    >
-                      {text_maker("close_menu")}
-                    </button>
-                  </div>
-                </Fragment>
+                </ul>
               );
-            })
-            .value()}
-        </ul>
-      );
-    }
+            } else {
+              return (
+                <ul
+                  className="rbt-menu dropdown-menu show"
+                  id={menuId}
+                  role="listbox"
+                >
+                  {_.chain(paginated_results)
+                    .groupBy("config_group_index")
+                    .thru((grouped_results) => {
+                      const needs_pagination_up_control = pagination_index > 0;
+                      const needs_pagination_down_control =
+                        page_range_end < total_matching_results;
+
+                      const pagination_down_item_index = needs_pagination_up_control
+                        ? paginated_results.length + 1
+                        : paginated_results.length;
+
+                      let index_key_counter = needs_pagination_up_control
+                        ? 1
+                        : 0;
+                      return (
+                        <Fragment>
+                          <li
+                            key={`header-pagination-info`}
+                            className="dropdown-header"
+                          >
+                            <TM
+                              k="paginate_status"
+                              args={{
+                                page_range_start,
+                                page_range_end,
+                                total_matching_results,
+                              }}
+                            />
+                          </li>
+                          {needs_pagination_up_control && (
+                            <div>
+                              <a
+                                key="rbt-menu-item-0"
+                                id="rbt-menu-item-0"
+                                className="rbt-menu-pagination-option dropdown-item list-group-item"
+                                onClick={(e) => {
+                                  this.setState((prev_state) => ({
+                                    pagination_index:
+                                      prev_state.pagination_index - 1,
+                                  }));
+                                }}
+                              >
+                                <span className="aria-hidden">▲</span>
+                                <br />
+                                <TM
+                                  k="paginate_previous"
+                                  args={{ page_size: pagination_size }}
+                                />
+                              </a>
+                            </div>
+                          )}
+
+                          {_.flatMap(
+                            grouped_results,
+                            (results, group_index) => (
+                              <div key={`header-${group_index}`}>
+                                <li className="dropdown-header">
+                                  {config_groups[group_index].group_header}
+                                </li>
+                                {_.map(results, (result) => {
+                                  const index = index_key_counter++;
+                                  return (
+                                    <a
+                                      key={`rbt-menu-item-${index}`}
+                                      id={`rbt-menu-item-${index}`}
+                                      className="dropdown-item list-group-item"
+                                      onClick={() =>
+                                        this.handle_menu_select(result)
+                                      }
+                                      ref={index == 0 && firstMenuItemRef}
+                                    >
+                                      {result.menu_content(search_text)}
+                                    </a>
+                                  );
+                                })}
+                              </div>
+                            )
+                          )}
+                          {needs_pagination_down_control && (
+                            <div>
+                              <a
+                                key={`rbt-menu-item-${pagination_down_item_index}`}
+                                id={`rbt-menu-item-${pagination_down_item_index}`}
+                                className="rbt-menu-pagination-option dropdown-item list-group-item"
+                                onClick={(e) => {
+                                  this.setState((prev_state) => ({
+                                    pagination_index:
+                                      prev_state.pagination_index + 1,
+                                  }));
+                                }}
+                              >
+                                <TM
+                                  k="paginate_next"
+                                  args={{ next_page_size: next_page_size }}
+                                />
+                                <br />
+                                <span className="aria-hidden">▼</span>
+                              </a>
+                            </div>
+                          )}
+                          <div>
+                            <a
+                              key={`rbt-menu-item-${
+                                pagination_down_item_index + 1
+                              }`}
+                              id={`rbt-menu-item-${
+                                pagination_down_item_index + 1
+                              }`}
+                              className="rbt-menu-close-menu-button dropdown-item list-group-item"
+                              onClick={hide_menu}
+                            >
+                              {text_maker("close_menu")}
+                            </a>
+                          </div>
+                        </Fragment>
+                      );
+                    })
+                    .value()}
+                </ul>
+              );
+            }
+          })()}
+      </Fragment>
+    );
   }
 
   componentWillUnmount() {
