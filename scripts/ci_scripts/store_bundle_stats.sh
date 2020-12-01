7b8008521224e0cdca9edf72104c9fb92d52d04b
Stephen-ONeil <stephenlevioneil@gmail.com>
2020-12-01 13:35:38 -0500

    Update up some bundle stats related CI scripts

--- scripts/ci_scripts/store_bundle_stats.sh
+++ scripts/ci_scripts/store_bundle_stats.sh
@@ -6,7 +6,7 @@ set -e # will exit if any command has non-zero exit value
 source ~/InfoBase/scripts/ci_scripts/redact_env_vars_from_logging.sh "redact-start"
 
 # save a copy for trend analysis purposes
-gsutil cp ./client/$BUILD_DIR/bundle-stats.json $GCLOUD_BUNDLE_STATS_BUCKET_URL/$CIRCLE_BRANCH-$(echo "$CIRCLE_SHA1" | cut -c 1-7).json
+gsutil cp ./client/$BUILD_DIR/InfoBase/bundle-stats.json $GCLOUD_BUNDLE_STATS_BUCKET_URL/$CIRCLE_BRANCH-$(echo "$CIRCLE_SHA1" | cut -c 1-7).json
 
 if [[ $CIRCLE_BRANCH == "master" ]]; then
   # ... the baseline stats lives in a weird place, node_modules/.cache. No options in the API for controling this, pain to manage
