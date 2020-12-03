10cfe4980351a0b00b7074b1f2c7ccbdcb7df9e9
Stephen-ONeil <stephenlevioneil@gmail.com>
2020-12-03 15:13:23 -0500

    Start cleaning up bubble_definitions, before adding a new one. This is ancient, pre-text_maker even, damn

--- client/src/infographic/bubble_definitions.js
+++ client/src/infographic/bubble_definitions.js
@@ -1,85 +1,60 @@
-import { trivial_text_maker as text_maker } from "../models/text.js";
+import { create_text_maker } from "../models/text.js";
 
 import text from "./bubble_definitions.yaml";
 import svgs from "./bubble_svgs.yaml";
+
+const text_maker = create_text_maker(text);
 
 export const bubble_defs = {
   intro: {
     ix: 0,
     id: "intro",
-    tables: [],
     svg_content: svgs.intro.text,
-    title(subject) {
-      switch (subject.level) {
-        case "program":
-          return text.about_program_title[lang];
-        case "gov":
-          return text.about_gov_title[lang];
-        case "tag":
-          return text.about_tag_title[lang];
-        case "crso":
-          return text.about_cr_title[lang];
-        default:
-          return "Introduction";
-      }
-    },
+    title: ({ level }) => text_maker(`about_${level}_title`),
     description: _.constant(""),
   },
   structure: {
     ix: 3,
     id: "structure",
-    title(subject) {
-      if (subject.level === "tag") {
-        return text_maker("tagged_programs");
-      } else {
-        throw "TODO";
-      }
-    },
+    title: () => text_maker("tagged_programs"),
     description: _.constant(""),
-    tables: [],
     svg_content: svgs.structure.text,
   },
   financial: {
     ix: 5,
     id: "financial",
-    title: _.constant(text.fin_title[lang]),
-    // slightly modify the description for Financial data for ib plus
-    description: _.constant(text.fin_desc[lang]),
-    tables: [],
+    title: () => text_maker("fin_title"),
+    description: () => text_maker("fin_desc"),
     svg_content: svgs.financial.text,
   },
   people: {
     ix: 10,
     id: "people",
-    title: _.constant(text.people_title[lang]),
-    description: _.constant(text.people_desc[lang]),
-    tables: [],
+    title: () => text_maker("people_title"),
+    description: () => text_maker("people_desc"),
     svg_content: svgs.people.text,
   },
   results: {
     ix: 15,
     id: "results",
-    title: _.constant(text.planning_title[lang]),
-    description: _.constant(text.planning_desc[lang]),
-    tables: [],
+    title: () => text_maker("results_title"),
+    description: () => text_maker("results_desc"),
     svg_content: svgs.results.text,
   },
   related: {
     ix: 20,
     id: "related",
-    title: _.constant(text.where_can_i_go_title[lang]),
-    description: _.constant(text.where_can_i_go_desc[lang]),
-    tables: [],
+    title: () => text_maker("where_can_i_go_title"),
+    description: () => text_maker("where_can_i_go_desc"),
     color: "#114B5F",
     svg_content: svgs.related.text,
   },
   all_data: {
     ix: 25,
     id: "all_data",
-    tables: [],
     color: "#009652",
-    title: ({ level }) => text[`all_data_${level}_title`][lang],
-    description: _.constant(text.all_data_description[lang]),
+    title: () => text_maker("datasets"),
+    description: () => text_maker("all_data_description"),
     svg_content: svgs.all_data.text,
   },
 };
