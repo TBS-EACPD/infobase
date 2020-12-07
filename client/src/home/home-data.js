afbcfa6ac75731543b640203d3d744e6051d9e44
Stephen-ONeil <stephenlevioneil@gmail.com>
2020-12-07 09:43:04 -0500

    Condense and reconfigure featured content list

--- client/src/home/home-data.js
+++ client/src/home/home-data.js
@@ -1,23 +1,18 @@
 const featured_content_items = _.compact([
   {
-    text_key: "quick_link_DRR_1920",
-    href: "#orgs/gov/gov/infograph/results/.-.-(panel_key.-.-'gov_drr)",
-    is_new: "true",
-  },
-  {
     text_key: "quick_link_covid_estimates",
     href:
       "#orgs/gov/gov/infograph/financial/.-.-(panel_key.-.-'covid_estimates_panel)",
     is_new: "true",
   },
   {
-    text_key: "quick_link_spending_by_program",
-    href: "#partition/dept/exp",
+    text_key: "quick_link_DRR_1920",
+    href: "#orgs/gov/gov/infograph/results/.-.-(panel_key.-.-'gov_drr)",
     is_new: "true",
   },
   {
-    text_key: "quick_link_ftes_by_program",
-    href: "#partition/dept/fte",
+    text_key: "quick_link_gov_spending",
+    href: "#orgs/gov/gov/infograph/financial/.-.-(panel_key.-.-'welcome_mat)",
     is_new: "true",
   },
   {
@@ -27,14 +22,6 @@ const featured_content_items = _.compact([
   {
     text_key: "quick_link_tp_by_region",
     href: "#orgs/gov/gov/infograph/financial/.-.-(panel_key.-.-'tp_by_region)",
-  },
-  {
-    text_key: "quick_link_DP_2021",
-    href: "#orgs/gov/gov/infograph/results/.-.-(panel_key.-.-'gov_dp)",
-  },
-  {
-    text_key: "quick_link_main_estimates",
-    href: "#partition/est_doc_mains/estimates",
   },
   {
     text_key: "igoc",
