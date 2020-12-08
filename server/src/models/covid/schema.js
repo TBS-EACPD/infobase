e4b04ce34afeed693abe6a5ccd5dd9034c15eaf8
Stephen-ONeil <stephenlevioneil@gmail.com>
2020-12-08 13:57:20 -0500

    Reworking the covid measure API

--- server/src/models/covid/schema.js
+++ server/src/models/covid/schema.js
@@ -4,14 +4,19 @@ import { bilingual_field } from "../schema_utils";
 
 const schema = `
   extend type Root{
-    covid_estimates: [CovidEstimates]
-    covid_initiatives: [CovidInitiative]
     covid_measures: [CovidMeasure]
+    covid_measure(covid_measure_id: String!): CovidMeasure
   }
 
   extend type Gov{
     covid_estimates_summary: [CovidEstimatesSummary]
   }
+
+  extend type Org{
+    covid_measures: [CovidMeasure]
+    covid_estimates_summary: [CovidEstimatesSummary]
+  }
+
   type CovidEstimatesSummary{
     id: String
 
@@ -21,92 +26,58 @@ const schema = `
     stat: Float
   }
 
-  extend type Org{
+  type CovidMeasure{
+    id: String
+    name: String
+
     covid_estimates: [CovidEstimates]
-    covid_initiatives: [CovidInitiative]
   }
 
   type CovidEstimates{
     id: String
     
     org_id: Int
+    org: Org
 
     fiscal_year: String
     est_doc: String
     vote: Float
     stat: Float
-  }
-
-  type CovidInitiative{
-    id: String
-    name: String
-
-    estimates: [CovidInitiativeEstimates]
-  }
-
-  type CovidInitiativeEstimates{
-    id: String
-
-    org_id: Int
-
-    covid_initiative_id: [String]
-    covid_initiative: CovidInitiative
-
-    fiscal_year: String
-    est_doc: String
-    vote: Float
-    stat: Float
-
-    covid_measure_ids: [String]
-    covid_measures: [CovidMeasure]
-  }
-
-  type CovidMeasure{
-    id: String
-    name: String
   }
 `;
 
 export default function ({ models, loaders }) {
-  const { CovidInitiative, CovidMeasure, CovidEstimates } = models;
+  const { CovidMeasure } = models;
 
   const {
-    covid_initiatives_by_org_id_loader,
+    org_id_loader,
+    covid_measures_by_org_id_loader,
     covid_measure_loader,
-    covid_estimates_by_org_id_loader,
+    covid_estimates_summary_by_org_id_loader,
   } = loaders;
 
   const resolvers = {
     Root: {
-      covid_initiatives: () => CovidInitiative.find({}),
       covid_measures: () => CovidMeasure.find({}),
-      covid_estimates: () =>
-        CovidEstimates.find({ org_id: { $not: { $eq: "gov" } } }),
+      covid_measure: (_x, { covid_measure_id }) =>
+        covid_measure_loader.load(covid_measure_id),
     },
     Gov: {
       covid_estimates_summary: () =>
-        covid_estimates_by_org_id_loader.load("gov"),
+        covid_estimates_summary_by_org_id_loader.load("gov"),
     },
     Org: {
-      covid_initiatives: ({ org_id }) =>
-        covid_initiatives_by_org_id_loader.load(org_id),
-      covid_estimates: ({ org_id }) =>
-        covid_estimates_by_org_id_loader.load(org_id),
-    },
-    CovidInitiative: {
-      id: _.property("covid_initiative_id"),
-      name: bilingual_field("name"),
-    },
-    CovidInitiativeEstimates: {
-      covid_measures: ({ covid_measure_ids }) => {
-        return covid_measure_ids
-          ? covid_measure_loader.loadMany(covid_measure_ids)
-          : [];
-      },
+      covid_measures: ({ org_id }) =>
+        covid_measures_by_org_id_loader.load(org_id),
+      covid_estimates_summary: ({ org_id }) =>
+        covid_estimates_summary_by_org_id_loader.load(org_id),
     },
     CovidMeasure: {
-      id: _.property("measure_id"),
+      id: _.property("covid_measure_id"),
       name: bilingual_field("name"),
+    },
+    CovidEstimates: {
+      org: ({ org_id }) => org_id_loader.load(org_id),
     },
   };
 
