a430b45bde0b053e28773ce01a82b4ce44d68b4c
Stephen-ONeil <stephenlevioneil@gmail.com>
2020-12-08 13:57:20 -0500

    Reworking the covid measure API

--- server/src/models/covid/models.js
+++ server/src/models/covid/models.js
@@ -10,16 +10,10 @@ import {
   number_type,
   str_type,
   parent_fkey_type,
-  sparse_parent_fkey_type,
   bilingual,
 } from "../model_utils.js";
 
 export default function (model_singleton) {
-  const CovidMeasureSchema = mongoose.Schema({
-    measure_id: pkey_type(),
-    ...bilingual("name", { ...str_type, required: true }),
-  });
-
   const CovidEstimatesSchema = mongoose.Schema({
     org_id: parent_fkey_type(),
 
@@ -29,49 +23,29 @@ export default function (model_singleton) {
     stat: number_type,
   });
 
-  const CovidInitiativeEstimatesSchema = mongoose.Schema({
-    org_id: parent_fkey_type(),
-    covid_initiative_id: parent_fkey_type(),
-
-    fiscal_year: number_type,
-    est_doc: str_type,
-    vote: number_type,
-    stat: number_type,
-
-    covid_measure_ids: [sparse_parent_fkey_type()],
-  });
-  const CovidInitiativeSchema = mongoose.Schema({
-    covid_initiative_id: pkey_type(),
+  const CovidMeasureSchema = mongoose.Schema({
+    covid_measure_id: pkey_type(),
     ...bilingual("name", { ...str_type, required: true }),
 
-    estimates: [CovidInitiativeEstimatesSchema],
+    estimates: [CovidEstimatesSchema],
   });
 
   model_singleton.define_model("CovidMeasure", CovidMeasureSchema);
-  model_singleton.define_model("CovidInitiative", CovidInitiativeSchema);
-  model_singleton.define_model("CovidEstimates", CovidEstimatesSchema);
+  model_singleton.define_model("CovidEstimatesSummary", CovidEstimatesSchema);
 
-  const {
-    CovidMeasure,
-    CovidInitiative,
-    CovidEstimates,
-  } = model_singleton.models;
+  const { CovidMeasure, CovidEstimatesSummary } = model_singleton.models;
 
   const loaders = {
     covid_measure_loader: create_resource_by_id_attr_dataloader(
       CovidMeasure,
       "measure_id"
     ),
-    covid_initiative_loader: create_resource_by_id_attr_dataloader(
-      CovidInitiative,
-      "covid_initiative_id"
-    ),
-    covid_initiatives_by_org_id_loader: create_resource_by_foreignkey_attr_dataloader(
-      CovidInitiative,
+    covid_measures_by_org_id_loader: create_resource_by_foreignkey_attr_dataloader(
+      CovidMeasure,
       "estimates.org_id"
     ),
-    covid_estimates_by_org_id_loader: create_resource_by_foreignkey_attr_dataloader(
-      CovidEstimates,
+    covid_estimates_summary_by_org_id_loader: create_resource_by_foreignkey_attr_dataloader(
+      CovidEstimatesSummary,
       "org_id"
     ),
   };
--- server/src/models/covid/populate.js
+++ server/src/models/covid/populate.js
@@ -3,94 +3,78 @@ import _ from "lodash";
 import { get_standard_csv_file_rows } from "../load_utils.js";
 
 export default async function ({ models }) {
-  const { CovidMeasure, CovidInitiative, CovidEstimates } = models;
+  const { CovidMeasure, CovidEstimatesSummary } = models;
 
-  const covid_measure_records = _.chain(
-    get_standard_csv_file_rows("covid_measures.csv")
-  )
-    .map((row) => new CovidMeasure(row))
-    .value();
-
-  const covid_initiative_estimates_rows = _.map(
-    get_standard_csv_file_rows("covid_initiative_estimates.csv"),
-    (row) => ({
-      ...row,
-      covid_measure_ids: _.chain(row.covid_measure_ids)
-        .split(",")
-        .map(_.trim)
-        .thru((ids) => (ids.length === 1 && _.isEmpty(ids[0]) ? null : ids))
-        .value(),
-    })
+  const covid_estimates_rows = get_standard_csv_file_rows(
+    "covid_estimates.csv"
   );
-  const covid_estimates_records = _.chain(covid_initiative_estimates_rows)
+
+  const covid_measure_records = _.map(
+    get_standard_csv_file_rows("covid_measures.csv"),
+    (row) =>
+      new CovidMeasure({
+        ...row,
+        estimates: _.filter(
+          covid_estimates_rows,
+          ({ covid_measure_id }) => covid_measure_id === row.covid_measure_id
+        ),
+      })
+  );
+
+  const coivd_estimates_summary_records = _.chain(covid_estimates_rows)
     .groupBy("org_id")
-    .flatMap((org_group, org_id) =>
-      _.chain(org_group)
+    .map((org_rows, org_id) =>
+      _.chain(org_rows)
         .groupBy("fiscal_year")
-        .flatMap((year_group, fiscal_year) =>
-          _.chain(year_group)
+        .map((year_rows, fiscal_year) =>
+          _.chain(year_rows)
             .groupBy("est_doc")
-            .flatMap((doc_group, est_doc) =>
-              _.reduce(
-                doc_group,
-                (roll_up, row) => ({
-                  ...roll_up,
-                  vote: roll_up.vote + +row.vote,
-                  stat: roll_up.stat + +row.stat,
+            .map((doc_rows, est_doc) => ({
+              org_id,
+              fiscal_year,
+              est_doc,
+              ..._.reduce(
+                doc_rows,
+                ({ memo_vote, memo_stat }, { row_vote, row_stat }) => ({
+                  vote: memo_vote + row_vote,
+                  stat: memo_stat + row_stat,
                 }),
-                { org_id, fiscal_year, est_doc, vote: 0, stat: 0 }
-              )
-            )
+                { vote: 0, stat: 0 }
+              ),
+            }))
             .value()
         )
         .value()
     )
-    .thru((rolled_up_rows) => {
-      const gov_rows = _.chain(rolled_up_rows)
+    .thru((org_summary_rows) => [
+      ...org_summary_rows,
+      ..._.chain(org_summary_rows)
         .groupBy("fiscal_year")
-        .flatMap((year_group, fiscal_year) =>
-          _.chain(year_group)
+        .map((year_rows, fiscal_year) =>
+          _.chain(year_rows)
             .groupBy("est_doc")
-            .flatMap((doc_group, est_doc) =>
-              _.reduce(
-                doc_group,
-                (roll_up, row) => ({
-                  ...roll_up,
-                  vote: roll_up.vote + +row.vote,
-                  stat: roll_up.stat + +row.stat,
+            .map((doc_rows, est_doc) => ({
+              org_id: "gov",
+              fiscal_year,
+              est_doc,
+              ..._.reduce(
+                doc_rows,
+                ({ memo_vote, memo_stat }, { row_vote, row_stat }) => ({
+                  vote: memo_vote + row_vote,
+                  stat: memo_stat + row_stat,
                 }),
-                { org_id: "gov", fiscal_year, est_doc, vote: 0, stat: 0 }
-              )
-            )
+                { vote: 0, stat: 0 }
+              ),
+            }))
             .value()
         )
-        .value();
-
-      return [...rolled_up_rows, ...gov_rows];
-    })
-    .map((rolled_up_row) => new CovidEstimates(rolled_up_row))
-    .value();
-
-  const covid_initiative_records = _.chain(
-    get_standard_csv_file_rows("covid_initiatives.csv")
-  )
-    .map((row) => {
-      const initiative_estimates_rows = _.filter(
-        covid_initiative_estimates_rows,
-        ({ covid_initiative_id }) =>
-          covid_initiative_id === row.covid_initiative_id
-      );
-
-      return new CovidInitiative({
-        ...row,
-        estimates: initiative_estimates_rows,
-      });
-    })
+        .value(),
+    ])
+    .map((row) => new CovidEstimatesSummary(row))
     .value();
 
   return await Promise.all([
     CovidMeasure.insertMany(covid_measure_records),
-    CovidInitiative.insertMany(covid_initiative_records),
-    CovidEstimates.insertMany(covid_estimates_records),
+    CovidEstimatesSummary.insertMany(coivd_estimates_summary_records),
   ]);
 }
