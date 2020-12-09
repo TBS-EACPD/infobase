7f22ec28b920fc21271440a887824da8c864d443
Stephen-ONeil <stephenlevioneil@gmail.com>
2020-12-09 13:42:22 -0500

    Add estimates summary queries to models/covid/queries.js. Split up build_base_query and build_org_query, so that build_base_query doesn't need to include the unnecessary id variable

--- client/src/models/covid/queries.js
+++ client/src/models/covid/queries.js
@@ -1,20 +1,23 @@
 import gql from "graphql-tag";
 
 const build_base_query = (inner_fragment) => gql`
-  query($lang: String! $id: String!) {
+  query($lang: String!) {
     root(lang: $lang) {
       ${inner_fragment}
     }
   }
 `;
 
-const build_org_query = (inner_fragment) =>
-  build_base_query(`
-  org(org_id: $id) {
-    id
-    ${inner_fragment}
+const build_org_query = (inner_fragment) => gql`
+  query($lang: String! $id: String!) {
+    root(lang: $lang) {
+      org(org_id: $id) {
+        id
+        ${inner_fragment}
+      }
+    }
   }
-`);
+`;
 
 export const org_has_covid_data_query = build_org_query("has_covid_data");
 
@@ -51,4 +54,21 @@ export const all_covid_estimates_by_measure_query = build_base_query(
 );
 export const org_covid_estimates_by_measure_query = build_org_query(
   covid_estimates_by_measure_query_fragment
+);
+
+const covid_estimates_summary_query_fragment = `
+  covid_estimates_summary {
+    fiscal_year
+    est_doc
+    vote
+    stat
+  }
+`;
+export const gov_covid_estimates_summary_query = build_base_query(`
+  gov {
+    ${covid_estimates_by_measure_query_fragment}
+  }
+`);
+export const org_covid_estimates_summary_query = build_org_query(
+  covid_estimates_summary_query_fragment
 );
