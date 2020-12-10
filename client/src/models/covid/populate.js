d93a8ff7a68995376c24f2566a7915101ad099e5
Stephen-ONeil <stephenlevioneil@gmail.com>
2020-12-10 11:02:27 -0500

    Fix typo bugs in api_load_has_covid_response

--- client/src/models/covid/populate.js
+++ client/src/models/covid/populate.js
@@ -20,7 +20,7 @@ export const api_load_has_covid_response = (subject) => {
   } catch (e) {
     return get_client()
       .query({
-        org_has_covid_data_query,
+        query: org_has_covid_data_query,
         variables: {
           lang: window.lang,
           id: subject.id,
@@ -29,7 +29,7 @@ export const api_load_has_covid_response = (subject) => {
       })
       .then((response) =>
         subject.set_has_data(
-          "covid_repsonse",
+          "covid_response",
           response.data.root.org.has_covid_data
         )
       );
