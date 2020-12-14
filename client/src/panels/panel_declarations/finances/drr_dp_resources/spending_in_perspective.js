c705dfc2a9b418af23a0ef06ab167f01f7b48044
Taewan Kang <tkang042@uottawa.ca>
2020-12-14 09:24:19 -0500

    hotfix spending in perspective without any programspending

--- client/src/panels/panel_declarations/finances/drr_dp_resources/spending_in_perspective.js
+++ client/src/panels/panel_declarations/finances/drr_dp_resources/spending_in_perspective.js
@@ -125,7 +125,7 @@ export const declare_spending_in_tag_perspective_panel = () =>
         //analysis: as of writing this (oct 2016) the max number of tags encountered is 13.
         const prog_row = _.first(programSpending.programs.get(subject));
 
-        if (!(prog_row[col] > 0)) {
+        if (!prog_row || !(prog_row[col] > 0)) {
           return false;
         }
 
