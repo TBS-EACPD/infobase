2463f5fb28233f0adfd92c9bf32f4a1b9e1e4a0b
Taewan Kang <tkang042@uottawa.ca>
2020-12-17 15:37:29 -0500

    expenditures by department

--- client/src/models/covid/CovidMeasure.js
+++ client/src/models/covid/CovidMeasure.js
@@ -8,29 +8,29 @@ import {
 } from "src/models/storeMixins.js";
 import { trivial_text_maker } from "src/models/text.js";
 
-const flatten_estimates_rows = (measures) =>
-  _.flatMap(measures, ({ id: measure_id, estimates }) =>
-    _.map(estimates, (estimate_row) => ({
-      measure_id,
-      ...estimate_row,
+const flatten_data_rows = (measures, data_type) =>
+  _.flatMap(measures, (measure) =>
+    _.map(measure[data_type], (row) => ({
+      measure_id: measure.id,
+      ...row,
     }))
   );
 
-const roll_up_estimates_by_property = (estimates_by_measure, property_name) =>
-  _.chain(estimates_by_measure)
+const roll_up_data_by_property = (data_by_measure, group_key, property_name) =>
+  _.chain(data_by_measure)
     .groupBy(property_name)
     .flatMap((grouped_rows, group_value) =>
       _.chain(grouped_rows)
         .groupBy("fiscal_year")
         .flatMap((year_group, fiscal_year) =>
           _.chain(year_group)
-            .groupBy("est_doc")
-            .flatMap((doc_group, est_doc) => ({
+            .groupBy(group_key)
+            .flatMap((group, key) => ({
               [property_name]: group_value,
-              est_doc,
+              [group_key]: key,
               fiscal_year,
               ..._.reduce(
-                doc_group,
+                group,
                 (memo, row) => ({
                   stat: memo.stat + row.stat,
                   vote: memo.vote + row.vote,
@@ -85,27 +85,30 @@ class CovidMeasure extends mix().with(
     this.extend(measure_id, { [data_key]: extended_data_set });
   }
 
-  static get_all_estimates_by_measure = () =>
-    flatten_estimates_rows(this.get_all());
+  static get_all_data_by_measure = (data_type) =>
+    flatten_data_rows(this.get_all(), data_type);
   static org_lookup_estimates_by_measure = (org_id) =>
     _.filter(
-      this.get_all_estimates_by_measure(),
+      this.get_all_data_by_measure("estimates"),
       ({ org_id: row_org_id }) => row_org_id === org_id
     );
   static gov_estimates_by_measure = () =>
-    roll_up_estimates_by_property(
-      this.get_all_estimates_by_measure(),
+    roll_up_data_by_property(
+      this.get_all_data_by_measure("estimates"),
+      "est_doc",
       "measure_id"
     );
 
-  static get_all_estimates_by_org = () =>
-    roll_up_estimates_by_property(
-      this.get_all_estimates_by_measure(),
+  static get_all_data_by_org = (data_type, group_key) =>
+    roll_up_data_by_property(
+      this.get_all_data_by_measure(data_type),
+      group_key,
       "org_id"
     );
   static org_lookup_estimates_by_org = (org_id) =>
-    roll_up_estimates_by_property(
+    roll_up_data_by_property(
       this.org_lookup_estimates_by_measure(org_id),
+      "est_doc",
       "org_id"
     );
 }
