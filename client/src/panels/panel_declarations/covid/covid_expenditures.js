71049512c9620d22e9dd31b527b2bf5d96a47132
Stephen-ONeil <stephenlevioneil@gmail.com>
2020-12-18 12:11:36 -0500

    Rearranging and tweaking more covid panel text. Rename covid_tab_contents.js to covid_Common_components.js, move the above tab footnote list in to it.

--- client/src/panels/panel_declarations/covid/covid_expenditures.js
+++ client/src/panels/panel_declarations/covid/covid_expenditures.js
@@ -19,15 +19,21 @@ import {
   SummaryTab,
   ByDepartmentTab,
   ByMeasureTab,
-} from "./covid_tab_contents.js";
+  AboveTabFootnoteList,
+} from "./covid_common_components.js";
 
 import text2 from "./covid_common_lang.yaml";
 import text1 from "./covid_expenditures.yaml";
 
-const { CovidMeasure, Gov } = Subject;
+const { CovidMeasure } = Subject;
 
 const { text_maker, TM } = create_text_maker_component([text1, text2]);
-const { TabbedContent, SpinnerWrapper, TabLoadingWrapper } = util_components;
+const {
+  TabbedContent,
+  SpinnerWrapper,
+  TabLoadingWrapper,
+  AlertBanner,
+} = util_components;
 
 const client = get_client();
 
@@ -158,6 +164,7 @@ class CovidExpendituresPanel extends React.Component {
   render() {
     const { loading, gov_covid_expenditures_in_year } = this.state;
     const { panel_args } = this.props;
+    const { level } = panel_args.subject;
 
     if (loading) {
       return <SpinnerWrapper config_name={"tabbed_content"} />;
@@ -176,9 +183,10 @@ class CovidExpendituresPanel extends React.Component {
           <div className="frow">
             <div className="fcol-md-12 fcol-xs-12 medium-panel-text text">
               <TM
-                k={`covid_expenditures_above_tab_text_${panel_args.subject.level}`}
+                k={`covid_expenditures_above_tab_text_${level}`}
                 args={extended_panel_args}
               />
+              <AboveTabFootnoteList list_text_key="covid_estimates_above_tab_footnote_list" />
             </div>
           </div>
           <TabbedContent {...tabbed_content_props} />
@@ -198,24 +206,14 @@ export const declare_covid_expenditures_panel = () =>
         dept: { org_covid_summary_query },
       }[level_name],
       footnotes: false,
-      depends_on: ["orgVoteStatPa"],
       source: (subject) => [],
       calculate: function (subject, options) {
         if (level_name === "dept" && !subject.has_data("covid_response")) {
           return false;
         }
 
-        const { orgVoteStatPa } = this.tables;
-        const gov_total_expenditures_in_year = orgVoteStatPa
-          .q(Gov)
-          .sum("{{pa_last_year}}exp");
-
-        return {
-          subject,
-          gov_total_expenditures_in_year,
-        };
+        return { subject };
       },
-
       render: ({ calculations, footnotes, sources }) => {
         const { panel_args } = calculations;
         return (
@@ -226,6 +224,11 @@ export const declare_covid_expenditures_panel = () =>
               footnotes,
             }}
           >
+            <AlertBanner banner_class="danger">
+              {
+                "Not real data! Faked based on the already fake dev-purposes covid estimates. Scale and distribution of values does not reflect what is expected in the real expendture data. For development purposes only!"
+              }
+            </AlertBanner>
             <CovidExpendituresPanel panel_args={panel_args} />
           </InfographicPanel>
         );
