8261769885a6f1aba924a608a364bb38ff185a0f
Taewan Kang <tkang042@uottawa.ca>
2021-01-07 10:09:04 -0500

    Revert "add footnotes filter"
    This reverts commit 957cc31e0d1ad94628ba385dd5ed63165fbe13b0.

--- client/src/infographic/InfographicFilter.js
+++ client/src/infographic/InfographicFilter.js
@@ -8,14 +8,9 @@ import {
 } from "../components/index.js";
 import { Table } from "../core/TableClass.js";
 
-import footnote_topics_text from "../models/footnotes/footnote_topics.yaml";
-
 import text from "./InfographicFilter.yaml";
 
-const { text_maker, TM } = create_text_maker_component([
-  text,
-  footnote_topics_text,
-]);
+const { text_maker, TM } = create_text_maker_component(text);
 
 export default class InfographicFilter extends React.Component {
   render() {
@@ -24,37 +19,13 @@ export default class InfographicFilter extends React.Component {
       total_number_of_panels,
       panel_filter_by_table,
       set_panel_filter_by_table,
-      panel_filter_by_footnotes,
-      set_panel_filter_by_footnotes,
     } = this.props;
 
-    const table_tags = _.map(panel_filter_by_table, (checked, dependency) => ({
+    const tags = _.map(panel_filter_by_table, (checked, dependency) => ({
       id: dependency,
       label: Table.lookup(dependency).name,
       active: checked,
     }));
-    const footnotes_tags = _.map(
-      panel_filter_by_footnotes,
-      (checked, topic_key) => ({
-        id: topic_key,
-        label: text_maker(topic_key),
-        active: checked,
-      })
-    );
-    const infographic_filter_controls = [
-      {
-        label: text_maker("filter_by_datasets"),
-        tags: table_tags,
-        set_panel_filter: set_panel_filter_by_table,
-        panel_filter: panel_filter_by_table,
-      },
-      {
-        label: text_maker("filter_by_footnotes"),
-        tags: footnotes_tags,
-        set_panel_filter: set_panel_filter_by_footnotes,
-        panel_filter: panel_filter_by_footnotes,
-      },
-    ];
 
     return (
       <Details
@@ -73,49 +44,41 @@ export default class InfographicFilter extends React.Component {
         }
         persist_content={true}
         content={
-          <div>
-            {_.map(
-              infographic_filter_controls,
-              ({ label, tags, set_panel_filter, panel_filter }) => (
-                <LabeledBox
-                  label={label}
-                  key={label}
-                  children={
-                    <div>
-                      <TagCloud
-                        tags={tags}
-                        onSelectTag={(evt) =>
-                          set_panel_filter({
-                            ...panel_filter,
-                            [evt]: !panel_filter[evt],
-                          })
-                        }
-                      />
-                      <div
-                        style={{
-                          borderTop: `1px dashed ${window.infobase_color_constants.tertiaryColor}`,
-                          padding: "10px 0px 10px 5px",
-                        }}
-                      >
-                        <SelectAllControl
-                          SelectAllOnClick={() =>
-                            set_panel_filter(
-                              _.mapValues(panel_filter, () => true)
-                            )
-                          }
-                          SelectNoneOnClick={() =>
-                            set_panel_filter(
-                              _.mapValues(panel_filter, () => false)
-                            )
-                          }
-                        />
-                      </div>
-                    </div>
+          <LabeledBox
+            label={text_maker("filter_panels_description")}
+            children={
+              <div>
+                <TagCloud
+                  tags={tags}
+                  onSelectTag={(evt) =>
+                    set_panel_filter_by_table({
+                      ...panel_filter_by_table,
+                      [evt]: !panel_filter_by_table[evt],
+                    })
                   }
                 />
-              )
-            )}
-          </div>
+                <div
+                  style={{
+                    borderTop: `1px dashed ${window.infobase_color_constants.tertiaryColor}`,
+                    padding: "10px 0px 10px 5px",
+                  }}
+                >
+                  <SelectAllControl
+                    SelectAllOnClick={() =>
+                      set_panel_filter_by_table(
+                        _.mapValues(panel_filter_by_table, () => true)
+                      )
+                    }
+                    SelectNoneOnClick={() =>
+                      set_panel_filter_by_table(
+                        _.mapValues(panel_filter_by_table, () => false)
+                      )
+                    }
+                  />
+                </div>
+              </div>
+            }
+          />
         }
       />
     );
