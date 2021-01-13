a92b91dd9e0ade8ef59a0cb5dd8c6014317e4325
Stephen-ONeil <stephenlevioneil@gmail.com>
2021-01-13 16:09:10 -0500

    Move remaining contents of src/app_bootstrap in to src/InfoBase, rename bootstrap.js to bootstrapper.js (to differentiate more clearly from the css framework)

--- client/src/InfoBase/root.js
+++ client/src/InfoBase/root.js
@@ -1,8 +1,8 @@
 import pre_load from "./pre_load.js";
 
 const kill_spinner = pre_load();
-import("../app_bootstrap/bootstrap.js").then(({ bootstrap }) => {
+import("./bootstrapper.js").then(({ bootstrapper }) => {
   import("./App.js").then(({ App, app_reducer }) => {
-    bootstrap(App, app_reducer, kill_spinner);
+    bootstrapper(App, app_reducer, kill_spinner);
   });
 });
