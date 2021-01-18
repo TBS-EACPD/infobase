52228b8b16866b3caa66f4e7aa95f3941a33f310
Stephen-ONeil <stephenlevioneil@gmail.com>
2021-01-18 12:33:13 -0500

    Roll back approach to toggling vote/stat in covid panel tables for now, tangent to add Funding Used column to expenditures tables

--- client/src/panels/panel_declarations/covid/covid_common_components.js
+++ client/src/panels/panel_declarations/covid/covid_common_components.js
@@ -1,12 +1,10 @@
 import { Fragment } from "react";
 
-import { CheckBox } from "src/components/CheckBox.js";
-
 import { create_text_maker_component } from "../shared.js";
 
 import common_covid_text from "./covid_common_lang.yaml";
 
-const { text_maker, TM } = create_text_maker_component(common_covid_text);
+const { TM } = create_text_maker_component(common_covid_text);
 
 const AboveTabFootnoteList = ({ children }) => (
   <Fragment>
@@ -15,30 +13,4 @@ const AboveTabFootnoteList = ({ children }) => (
   </Fragment>
 );
 
-class ToggleVoteStatProvider extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      show_vote_stat: !!this.props.initial_show_vote_stat,
-    };
-  }
-  toggle_show_vote_stat = () =>
-    this.setState({ show_vote_stat: !this.state.show_vote_stat });
-  render() {
-    const { show_vote_stat } = this.state;
-    const { Inner, inner_props } = this.props;
-
-    const ToggleVoteStat = () => (
-      <CheckBox
-        active={show_vote_stat}
-        onClick={this.toggle_show_vote_stat}
-        label={text_maker("show_vote_stat_split")}
-        container_style={{ justifyContent: "flex-end", marginBottom: "-15px" }}
-      />
-    );
-
-    return <Inner {...{ ...inner_props, show_vote_stat, ToggleVoteStat }} />;
-  }
-}
-
-export { AboveTabFootnoteList, ToggleVoteStatProvider };
+export { AboveTabFootnoteList };
