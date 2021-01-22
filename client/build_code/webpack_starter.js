2049c22eaf36544c4db8c3432b3df8da59cec6ec
Stephen-ONeil <stephenlevioneil@gmail.com>
2021-01-22 12:43:47 -0500

    Hotfix for #870, bundle stats uses chunk ID not chunk filename, so no reason to disable contenthash when building with bundle stats output. Was just messing up dev link cache busting

--- client/build_code/webpack_starter.js
+++ client/build_code/webpack_starter.js
@@ -42,7 +42,7 @@ const options_by_app = {
       path: path.resolve(__dirname, `../${build_dir_name}/InfoBase/app/`),
       filename: `app-a11y-${language}.min.js`,
       chunkFilename: `[name].app-ally-${language}${
-        prod && !bundle_stats ? ".[contenthash]" : ""
+        prod ? ".[contenthash]" : ""
       }.min.js`,
     }),
   },
@@ -51,7 +51,7 @@ const options_by_app = {
       path: path.resolve(__dirname, `../${build_dir_name}/InfoBase/app/`),
       filename: `app-${language}.min.js`,
       chunkFilename: `[name].app-${language}${
-        prod && !bundle_stats ? ".[contenthash]" : ""
+        prod ? ".[contenthash]" : ""
       }.min.js`,
     }),
   },
