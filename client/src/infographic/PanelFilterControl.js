625c412214f1b0b3622d31a02ec4e49c5c71bdb7
Stephen-ONeil <stephenlevioneil@gmail.com>
2021-01-22 15:45:38 -0500

    Add a space between the filter panel label and the displayed panel count

--- client/src/infographic/PanelFilterControl.js
+++ client/src/infographic/PanelFilterControl.js
@@ -85,7 +85,7 @@ export default class PanelFilterControl extends React.Component {
       <Details
         summary_content={
           <div>
-            <TM style={{ fontSize: 16 }} k="filter_panels" />
+            <TM style={{ fontSize: 16 }} k="filter_panels" />{" "}
             <TM
               className="panel-status-text"
               k="panels_status"
