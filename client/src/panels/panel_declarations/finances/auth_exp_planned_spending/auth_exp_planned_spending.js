5acfbb3c87c487eea65b9ec73b5afe5e799fa847
Taewan Kang <tkang042@uottawa.ca>
2021-02-04 10:48:22 -0500

    break out authorities into stat and voted

--- client/src/panels/panel_declarations/finances/auth_exp_planned_spending/auth_exp_planned_spending.js
+++ client/src/panels/panel_declarations/finances/auth_exp_planned_spending/auth_exp_planned_spending.js
@@ -160,7 +160,8 @@ class AuthExpPlannedSpendingGraph extends React.Component {
     const should_mark_gap_year =
       gap_year &&
       active_series.budgetary_expenditures &&
-      !active_series.authorities && // authorities always span the gap year, so don't mark it when displaying them
+      !active_series.stat_authorities &&
+      !active_series.voted_authorities && // authorities always span the gap year, so don't mark it when displaying them
       active_series.planned_spending;
 
     const nivo_props = {
@@ -177,7 +178,12 @@ class AuthExpPlannedSpendingGraph extends React.Component {
         left: 100,
       },
       table_ordered_column_keys: _.map(
-        ["authorities", "budgetary_expenditures", "planned_spending"],
+        [
+          "voted_authorities",
+          "stat_authorities",
+          "budgetary_expenditures",
+          "planned_spending",
+        ],
         (key) => text_maker(key)
       ),
       ...(should_mark_gap_year && {
@@ -539,15 +545,49 @@ const calculate = function (subject, options) {
     (est_year) => !_.includes(history_years_written, run_template(est_year))
   );
 
+  const historical_stat_auth_values = queried_subject.sum(auth_cols, {
+    as_object: false,
+    filter: (row) => row.votenum === "S",
+  });
+  const historical_voted_auth_values = queried_subject.sum(auth_cols, {
+    as_object: false,
+    filter: (row) => row.votenum !== "S",
+  });
+  const estimates_query = orgVoteStatEstimates.q(query_subject);
+  const future_stat_auth_values = _.map(
+    future_auth_year_templates,
+    (future_auth_year_template) =>
+      estimates_query.sum(`${future_auth_year_template}_estimates`, {
+        as_object: false,
+        filter: (row) => row.votenum === "S",
+      })
+  );
+  const future_voted_auth_values = _.map(
+    future_auth_year_templates,
+    (future_auth_year_template) =>
+      estimates_query.sum(`${future_auth_year_template}_estimates`, {
+        as_object: false,
+        filter: (row) => row.votenum !== "S",
+      })
+  );
+
   const historical_auth_values = queried_subject.sum(auth_cols, {
     as_object: false,
   });
   const future_auth_values = _.map(
     future_auth_year_templates,
     (future_auth_year_template) =>
-      orgVoteStatEstimates
-        .q(query_subject)
-        .sum(`${future_auth_year_template}_estimates`, { as_object: false })
+      estimates_query.sum(`${future_auth_year_template}_estimates`, {
+        as_object: false,
+      })
+  );
+  const auth_stat_values = _.concat(
+    historical_stat_auth_values,
+    future_stat_auth_values
+  );
+  const auth_voted_values = _.concat(
+    historical_voted_auth_values,
+    future_voted_auth_values
   );
 
   const auth_values = _.concat(historical_auth_values, future_auth_values);
@@ -563,9 +603,14 @@ const calculate = function (subject, options) {
       untrimmed_values: exp_values,
     },
     {
-      key: "authorities",
+      key: "stat_authorities",
       untrimmed_year_templates: _.concat(std_years, future_auth_year_templates),
-      untrimmed_values: auth_values,
+      untrimmed_values: auth_stat_values,
+    },
+    {
+      key: "voted_authorities",
+      untrimmed_year_templates: _.concat(std_years, future_auth_year_templates),
+      untrimmed_values: auth_voted_values,
     },
     subject.has_planned_spending && {
       key: "planned_spending",
