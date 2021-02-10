1bf14d9df880531488ab4888157683d58dfff417
Stephen-ONeil <stephenlevioneil@gmail.com>
2021-02-10 14:43:00 -0500

    Take the hackiness just a little further, but make the ux of the covid infographic links to the key concept panels much nicer

--- client/src/panels/panel_declarations/covid/covid_key_concepts.js
+++ client/src/panels/panel_declarations/covid/covid_key_concepts.js
@@ -50,10 +50,29 @@ export const declare_covid_key_concepts_panel = () =>
     }),
   });
 
+// TODO, can this same UX be acheived with less hackyness?
 export const scroll_to_covid_key_concepts = () => {
+  // Doing this imparitevly, better behaviour for users than alternative, i.e. using a panel_key url option and the router
+  // More brittle this way, but the assumption that panels have their panel key as an id, and that the key concept panel will
+  // be rendered (given it's calculate of _.constant(true)) are fundamental infographic assumptions, so safe enough
   const covid_key_concept_panel = document.querySelector(`#${panel_key}`);
+
   if (!_.isNull(covid_key_concept_panel)) {
     window.scrollTo(0, covid_key_concept_panel.offsetTop);
     covid_key_concept_panel.focus();
+
+    // Ok, this is where it gets more brittle/hacky. Leaking up a bunch of knowledge about SomeThingsToKeepInMind/AutoAccordion
+    // implementation. Should fail softly if those change, and as long as they don't it's a solid UX improvement
+    const accordion_header_button = covid_key_concept_panel.querySelector(
+      ".pull-down-accordion-header > button"
+    );
+    const accordion_body = covid_key_concept_panel.querySelector(
+      ".pull-down-accordion-body"
+    );
+
+    const accordion_exists_and_is_not_expanded =
+      !_.isNull(accordion_header_button) && _.isNull(accordion_body);
+
+    accordion_exists_and_is_not_expanded && accordion_header_button.click();
   }
 };
