5bc39132fbe40195d8ba82c8cb0cc4663c47eee1
Stephen-ONeil <stephenlevioneil@gmail.com>
2021-02-15 09:23:38 -0500

    Fresh covid data from pipeline

--- client/src/models/covid/covid_consts.js
+++ client/src/models/covid/covid_consts.js
@@ -4,3 +4,5 @@ export const COVID_LAST_REFRESHED_DATE = {
   en: "December 31, 2020",
   fr: "31 décembre 2020",
 }[lang];
+
+// TODO key concept talks about "up to December 2020", so need to be a bit more flexible in handling the last refreshed date to make that dynamic too
