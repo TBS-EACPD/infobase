a1a38c86ea1381bc77080d580a8a1344d802d356
Stephen-ONeil <stephenlevioneil@gmail.com>
2021-02-15 12:49:29 -0500

    Change COVID_LAST_REFRESHED_DATE to COVID_LAST_REFRESHED_MONTH, eliminate portions of covid_questions_up_to_date_a that would require manual updates

--- client/src/models/covid/covid_consts.js
+++ client/src/models/covid/covid_consts.js
@@ -1,8 +1,8 @@
 import { lang } from "src/core/injected_build_constants.js";
 
-export const COVID_LAST_REFRESHED_DATE = {
-  en: "December 31, 2020",
-  fr: "31 décembre 2020",
+export const COVID_LAST_REFRESHED_MONTH = {
+  en: "December 2020",
+  fr: "décembre 2020",
 }[lang];
 
 // TODO key concept talks about "up to December 2020", so need to be a bit more flexible in handling the last refreshed date to make that dynamic too
