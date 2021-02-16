1eef4e77b39f12db7e499553b46ca38814636d9b
Taewan Kang <tkang042@uottawa.ca>
2021-02-16 10:18:04 -0500

    supps c preparation changes

--- client/src/EstimatesComparison/utils.js
+++ client/src/EstimatesComparison/utils.js
@@ -5,4 +5,4 @@ import text from "./EstimatesComparison.yaml";
 export const { text_maker, TM } = create_text_maker_component(text);
 
 export const current_doc_is_mains = false; // Update this when switching between displaying mains and sups!
-export const current_sups_letter = "B"; // Update this on each new sups release!
+export const current_sups_letter = "C"; // Update this on each new sups release!
--- client/src/home/home-data.js
+++ client/src/home/home-data.js
@@ -2,6 +2,11 @@ import _ from "lodash";
 
 const featured_content_items = _.compact([
   {
+    text_key: "supps_c",
+    href: "#compare_estimates",
+    is_new: "true",
+  },
+  {
     text_key: "quick_link_covid_estimates",
     href:
       "#orgs/gov/gov/infograph/financial/.-.-(panel_key.-.-'covid_estimates_panel)",
@@ -10,16 +15,10 @@ const featured_content_items = _.compact([
   {
     text_key: "quick_link_DRR_1920",
     href: "#orgs/gov/gov/infograph/results/.-.-(panel_key.-.-'gov_drr)",
-    is_new: "true",
   },
   {
     text_key: "quick_link_gov_spending",
     href: "#orgs/gov/gov/infograph/financial/.-.-(panel_key.-.-'welcome_mat)",
-    is_new: "true",
-  },
-  {
-    text_key: "supps_b",
-    href: "#compare_estimates",
   },
   {
     text_key: "quick_link_tp_by_region",
