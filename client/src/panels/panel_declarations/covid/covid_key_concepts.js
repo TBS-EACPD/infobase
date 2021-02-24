0e13cac5139f945d841975fa32198c172b95035b
Stephen-ONeil <stephenlevioneil@gmail.com>
2021-02-24 13:08:16 -0500

    Add covid FAQ item covid_questions_additional_estimates_measures_q/a

--- client/src/panels/panel_declarations/covid/covid_key_concepts.js
+++ client/src/panels/panel_declarations/covid/covid_key_concepts.js
@@ -40,6 +40,16 @@ export const declare_covid_key_concepts_panel = () =>
                 <TM key={"q"} k={"covid_questions_financial_q"} />,
                 <TM key={"a"} k={"covid_questions_financial_a"} />,
               ],
+              [
+                <TM
+                  key={"q"}
+                  k={"covid_questions_additional_estimates_measures_q"}
+                />,
+                <TM
+                  key={"a"}
+                  k={"covid_questions_additional_estimates_measures_a"}
+                />,
+              ],
               COVID_FUNDING_FEATURE_FLAG &&
                 level === "gov" && [
                   <TM key={"q"} k={"covid_questions_funding_value_q"} />,
