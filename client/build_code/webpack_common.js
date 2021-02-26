51ec43f68c93e9c197be2570de98fa8c8b507ee0
Taewan Kang <tkang042@uottawa.ca>
2021-02-26 17:16:57 -0500

    polyfill? No idea if this is gonna do it for d3-scale

--- client/build_code/webpack_common.js
+++ client/build_code/webpack_common.js
@@ -60,7 +60,7 @@ const get_rules = ({ language, target_ie11, is_prod_build }) => {
     },
     {
       // node modules that specifically require transpilation...
-      include: /node_modules\/(graphiql|graphql-language-service-.*|codemirror-graphql|codemirror)/,
+      include: /node_modules\/(graphiql|graphql-language-service-.*|codemirror-graphql|codemirror|d3-scale)/,
       test: /\.js$/,
       use: js_module_loader_rules,
     },
