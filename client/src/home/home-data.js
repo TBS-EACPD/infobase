f55258108ff2219f82eb9838ce63f9db7095c57d
Stephen-ONeil <stephenlevioneil@gmail.com>
2021-02-26 12:20:45 -0500

    Move COVID-19 featured content item above mains and DPs on homepage. Might want a more descriptive name for it there

--- client/src/home/home-data.js
+++ client/src/home/home-data.js
@@ -2,6 +2,11 @@ import _ from "lodash";
 
 const featured_content_items = _.compact([
   {
+    text_key: "quick_link_covid_estimates",
+    href: "#orgs/gov/gov/infograph/covid",
+    is_new: "true",
+  },
+  {
     text_key: "quick_link_people_2020",
     href: "#orgs/gov/gov/infograph/people",
     is_new: "true",
@@ -14,11 +19,6 @@ const featured_content_items = _.compact([
   {
     text_key: "quick_link_main_estimates",
     href: "#compare_estimates",
-    is_new: "true",
-  },
-  {
-    text_key: "quick_link_covid_estimates",
-    href: "#orgs/gov/gov/infograph/covid",
     is_new: "true",
   },
   {
