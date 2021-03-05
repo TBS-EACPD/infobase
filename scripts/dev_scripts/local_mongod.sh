6601309f5510fbef7d22ed42673204635b2c6d60
Chetan Mandur <ChetanMandur45@gmail.com>
2021-03-04 19:16:27 -0500

    changed local_mongod.sh to work better with WSL2

--- scripts/dev_scripts/local_mongod.sh
+++ scripts/dev_scripts/local_mongod.sh
@@ -11,18 +11,18 @@ while getopts ":p:" opt; do
       ;;
   esac
 done
-if [[ -z "$port" ]]; then
+if [ -z "$port" ]; then
    port="27017"
    echo "No -p option, defaulting to port 27017"
 fi
 
 
-if [[ ! -e ./local_mongo ]]; then 
+if [ ! -e ./local_mongo ]; then 
   mkdir -p ./local_mongo/db
 fi
 
 
-if [[ -e ./local_mongo/db/mongod.lock ]]; then 
+if [ -e ./local_mongo/db/mongod.lock ]; then 
   running_mongod_pid=$(lsof ./local_mongo/db/mongod.lock | awk 'NR == 2 {print $2}')
 
   # sticks in loop until the process is actually dead, otherwise it's a race between the 
