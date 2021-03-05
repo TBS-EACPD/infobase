48fd4452623f1f9645b490df589ec927344c97c5
Taewan Kang <tkang042@uottawa.ca>
2021-03-05 12:56:03 -0500

    was syntax wrong? putting it back to original transpilation

--- client/build_code/webpack_common.js
+++ client/build_code/webpack_common.js
@@ -65,7 +65,7 @@ const get_rules = ({ language, target_ie11, is_prod_build }) => {
     },
     {
       // node modules that specifically require transpilation...
-      include: /node_modules\/(graphiql|graphql-language-service-.*|codemirror-graphql|codemirror|d3-scale|@nivo\/(bar|core|circle-packing|line|pie|treemap))/,
+      include: /node_modules\/(graphiql|graphql-language-service-.*|codemirror-graphql|codemirror|d3-scale|@nivo\/bar|@nivo\/circle-packing|@nivo\/core|@nivo\/line|@nivo\/pie|)/,
       test: /\.js$/,
       use: js_module_loader_rules,
     },
