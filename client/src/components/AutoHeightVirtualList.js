d84a58607fffe43193d24fae1c50365154ad3f22
Garvin <garvinh2001@hotmail.com>
2021-03-10 10:38:28 -0500

    Refactor no ref provided situation

--- client/src/components/AutoHeightVirtualList.js
+++ client/src/components/AutoHeightVirtualList.js
@@ -1,4 +1,3 @@
-import PropTypes from "prop-types";
 import React, { Component } from "react";
 import { List } from "react-virtualized";
 
@@ -7,6 +6,7 @@ export class AutoHeightVirtualList extends React.Component {
     super(props);
 
     this.max_height = props.max_height || 400;
+    this.list_ref = this.props.list_ref || React.createRef();
 
     this.state = {
       list_height: this.max_height,
@@ -14,10 +14,9 @@ export class AutoHeightVirtualList extends React.Component {
   }
 
   componentDidUpdate(prev_props, prev_state) {
-    const { list_ref } = this.props;
-    if (list_ref && list_ref.current) {
-      list_ref.current.Grid.measureAllCells();
-      const list_height = list_ref.current.Grid.getTotalRowsHeight();
+    if (this.list_ref && this.list_ref.current) {
+      this.list_ref.current.Grid.measureAllCells();
+      const list_height = this.list_ref.current.Grid.getTotalRowsHeight();
       if (list_height !== this.state.list_height) {
         if (list_height < this.max_height) {
           this.setState({ list_height: list_height });
@@ -33,17 +32,10 @@ export class AutoHeightVirtualList extends React.Component {
       <List
         {...{
           ...this.props,
-          ref: this.props.list_ref,
+          ref: this.list_ref,
           height: this.state.list_height,
         }}
       />
     );
   }
 }
-
-AutoHeightVirtualList.propTypes = {
-  list_ref: PropTypes.oneOfType([
-    PropTypes.func,
-    PropTypes.shape({ current: PropTypes.instanceOf(Component) }),
-  ]).isRequired,
-};
