72c21da7f88ecfb3ebd38fdb228ea6f8190ba806
Garvin <garvinh2001@hotmail.com>
2021-03-11 15:54:10 -0500

    Remove this.height from autoheight virtual list

--- client/src/components/AutoHeightVirtualList.js
+++ client/src/components/AutoHeightVirtualList.js
@@ -5,29 +5,24 @@ export class AutoHeightVirtualList extends React.Component {
   constructor(props) {
     super(props);
 
-    this.max_height = props.max_height;
     this.list_ref = this.props.list_ref || React.createRef();
 
     this.state = {
-      list_height: this.max_height,
+      list_height: props.max_height,
     };
   }
 
   componentDidUpdate(prev_props, prev_state) {
     const { max_height } = this.props;
 
-    if (max_height !== this.max_height) {
-      this.max_height = max_height;
-    }
-
     if (this.list_ref && this.list_ref.current) {
       this.list_ref.current.Grid.measureAllCells();
       const list_height = this.list_ref.current.Grid.getTotalRowsHeight();
       if (list_height !== this.state.list_height) {
-        if (list_height < this.max_height) {
+        if (list_height < max_height) {
           this.setState({ list_height: list_height });
-        } else if (this.state.list_height < this.max_height) {
-          this.setState({ list_height: this.max_height });
+        } else if (this.state.list_height < max_height) {
+          this.setState({ list_height: max_height });
         }
       }
     }
