b85560b13bb3a8e4d5edd590b780f777a6711577
Garvin <garvinh2001@hotmail.com>
2021-03-12 16:49:26 -0500

    Add logic for getting page range to display

--- client/src/components/DisplayTable/DisplayTableUtils.js
+++ client/src/components/DisplayTable/DisplayTableUtils.js
@@ -70,6 +70,108 @@ export const SelectPage = ({
   change_page,
   num_col,
 }) => {
+  const get_shifted_option_range = (
+    page_count,
+    current_page,
+    option_window
+  ) => {
+    const window_start = current_page - option_window;
+    const window_end = current_page + option_window;
+    if (window_start < 1) {
+      return _.range(1, window_end + 1 - (window_start - 1));
+    } else if (window_end > page_count) {
+      return _.range(window_start - (window_end - page_count), page_count + 1);
+    } else {
+      return _.range(window_start, window_end + 1);
+    }
+  };
+  const extend_options_left = (option_range, page_count, current_page) => {
+    if (_.first(option_range) === 1) {
+      return [
+        ...option_range,
+        _.last(option_range) + 1,
+        _.last(option_range) + 2,
+      ];
+    } else if (_.first(option_range) === 2) {
+      return [1, ...option_range, _.last(option_range) + 1];
+    } else if (_.first(option_range) === 3) {
+      return [1, 2, ...option_range];
+    } else if (current_page === page_count - 3) {
+      return [
+        1,
+        _.first(option_range) - 2,
+        _.first(option_range) - 1,
+        ...option_range,
+      ];
+    } else if (current_page > page_count - 3) {
+      return [
+        1,
+        _.first(option_range) - 3,
+        _.first(option_range) - 2,
+        _.first(option_range) - 1,
+        ...option_range,
+      ];
+    } else {
+      return option_range;
+    }
+  };
+  const extend_options_right = (option_range, page_count, option_window) => {
+    if (
+      _.includes(option_range, page_count - option_window) &&
+      _.last(option_range) !== page_count
+    ) {
+      return [
+        ...option_range,
+        ..._.range(_.last(option_range) + 1, page_count + 1),
+      ];
+    } else {
+      return option_range;
+    }
+  };
+  const get_page_options = (page_count, current_page, option_window = 2) => {
+    const raw_page_options = _.chain(
+      //without ellipsis
+      get_shifted_option_range(page_count, current_page, option_window)
+    )
+      .thru((option_range) =>
+        extend_options_left(option_range, page_count, current_page)
+      )
+      .thru((option_range) =>
+        extend_options_right(option_range, page_count, option_window)
+      )
+      .filter((page_index) => _.inRange(page_index, 1, page_count + 1))
+      .uniq()
+      .sortBy()
+      .value();
+
+    if (page_count > option_window * 2 + 5) {
+      if (raw_page_options.length === option_window * 2 + 5) {
+        return raw_page_options;
+      } else if (raw_page_options.length === option_window * 2 + 1) {
+        return [
+          1,
+          _.first(raw_page_options) - 1,
+          ...raw_page_options,
+          _.last(raw_page_options) + 1,
+          page_count,
+        ];
+      } else {
+        if (!_.includes(raw_page_options, 1)) {
+          return [1, _.first(raw_page_options) - 1, ...raw_page_options];
+        } else if (!_.includes(raw_page_options, page_count)) {
+          return [
+            ...raw_page_options,
+            _.last(raw_page_options) + 1,
+            page_count,
+          ];
+        }
+      }
+    } else {
+      return raw_page_options;
+    }
+  };
+
+  console.log(get_page_options(num_pages, current_page + 1));
   return (
     <tr className="page-selector-controls">
       <td colSpan={num_col}>
