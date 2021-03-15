9e053953e5d8ae27e005bc0a6d46e0aa416e9cc1
Stephen-ONeil <stephenlevioneil@gmail.com>
2021-03-15 11:36:36 -0400

    Add end of month date to COVID_LAST_REFRESHED_MONTH value

--- client/src/models/covid/covid_config.js
+++ client/src/models/covid/covid_config.js
@@ -1,8 +1,8 @@
 import { lang } from "src/core/injected_build_constants.js";
 
 export const COVID_LAST_REFRESHED_MONTH = {
-  en: "January 2021",
-  fr: "janvier 2021",
+  en: "January 31, 2021",
+  fr: "31 janvier 2021",
 }[lang];
 
 export const COVID_CURRENT_YEAR = 2020;
