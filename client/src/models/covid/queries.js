0d532fb1a89ea0ff4695c93c07e3c41168080d01
Stephen-ONeil <stephenlevioneil@gmail.com>
2021-03-18 15:17:17 -0400

    Update client COVID queries... hmm, some of these like top_spending_orgs, etc, would make a lot more sense if they could take fiscal_year as an arg. Maybe do that for all these queries, client will know what fiscal years to ask for based on the has_covid_data schema anyway

--- client/src/models/covid/queries.js
+++ client/src/models/covid/queries.js
@@ -21,8 +21,8 @@ const build_org_query = (inner_fragment) => gql`
 
 const has_covid_data_fields = `
   has_covid_data {
-    has_estimates
-    has_expenditures
+    years_with_estimates
+    years_with_expenditures
   }
 `;
 export const org_has_covid_data_query = build_org_query(has_covid_data_fields);
@@ -46,7 +46,6 @@ export const org_covid_measure_query = build_org_query(
 );
 
 const covid_estimates_fields = `
-  fiscal_year
   est_doc
   vote
   stat
@@ -55,9 +54,13 @@ const covid_estimates_by_measure_query_fragment = `
   covid_estimates_by_measure: covid_measures {
     ${covid_measure_fields}
   
-    covid_estimates {
-      org_id
-      ${covid_estimates_fields}
+    covid_data {
+      fiscal_year
+
+      covid_estimates {
+        org_id
+        ${covid_estimates_fields}
+      }
     }
   }
 `;
@@ -69,7 +72,6 @@ export const org_covid_estimates_by_measure_query = build_org_query(
 );
 
 const covid_expenditures_fields = `
-  fiscal_year
   vote
   stat
 `;
@@ -77,10 +79,13 @@ const covid_expenditures_by_measure_query_fragment = `
   covid_expenditures_by_measure: covid_measures {
     ${covid_measure_fields}
   
-    covid_expenditures {
-      org_id
-      
-      ${covid_expenditures_fields}
+    covid_data {
+      fiscal_year
+
+      covid_expenditures {
+        org_id
+        ${covid_estimates_fields}
+      }
     }
   }
 `;
@@ -92,11 +97,14 @@ export const org_covid_expenditures_by_measure_query = build_org_query(
 );
 
 const covid_count_query_fields = `
-  fiscal_year
   with_authorities
   with_spending
 `;
 const common_covid_summary_query_fragment = `
+  id
+
+  fiscal_year
+
   covid_estimates {
     ${covid_estimates_fields}
   }
@@ -108,8 +116,8 @@ export const gov_covid_summary_query = build_base_query(`
   gov {
     id
     covid_summary {
-      id
       ${common_covid_summary_query_fragment}
+      
       measure_counts {
         ${covid_count_query_fields}
       }
@@ -133,10 +141,14 @@ export const top_covid_spending_query = gql`
         id
         covid_summary {
           id
+          fiscal_year
+
           top_spending_orgs(top_x: ${top_x}) {
             id
             name
             covid_summary {
+              fiscal_year
+
               covid_expenditures {
                 ${covid_expenditures_fields}
               }
@@ -145,10 +157,14 @@ export const top_covid_spending_query = gql`
           top_spending_measures(top_x: ${top_x}) {
             id
             name
-            covid_expenditures {
-              org_id
 
-              ${covid_expenditures_fields}
+            covid_data {
+              fiscal_year
+        
+              covid_expenditures {
+                org_id
+                ${covid_expenditures_fields}
+              }
             }
           }
         }
