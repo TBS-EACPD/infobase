354ae84e16e3ca8a3415915da5dc0111b8fc76d7
Garvin <garvinh2001@hotmail.com>
2021-03-19 10:59:14 -0400

    Smoothen pinned content scrolling

--- client/src/components/PinnedContent.js
+++ client/src/components/PinnedContent.js
@@ -17,85 +17,107 @@ import text from "./PinnedContent.yaml";
 
 const text_maker = create_text_maker(text);
 
-export const PinnedContent = withRouter(
-  class extends React.Component {
-    constructor(props) {
-      super(props);
+class _PinnedContent extends React.Component {
+  constructor(props) {
+    super(props);
 
-      const {
-        match: {
-          params: { options: panel_link },
-        },
-        local_storage_name,
-      } = props;
+    const {
+      match: {
+        params: { options: panel_link },
+      },
+      local_storage_name,
+    } = props;
 
-      const user_has_enabled_pinning = (() => {
-        if (panel_link) {
-          // TODO hack for a specific gotcha from infographics, where we want to disable the pinned content there when directly linking to a panel,
-          // because the pinned content otherwise hides the linked panel title/is confusing... should be handled somewhere else though, not a PinnedContent concern
-          return false;
-        } else if (has_local_storage && local_storage_name) {
-          try {
-            const storage_value = JSON.parse(
-              localStorage.getItem(local_storage_name)
-            );
-            return _.isBoolean(storage_value) ? storage_value : true;
-          } catch {
-            return true;
-          }
+    const user_has_enabled_pinning = (() => {
+      if (panel_link) {
+        // TODO hack for a specific gotcha from infographics, where we want to disable the pinned content there when directly linking to a panel,
+        // because the pinned content otherwise hides the linked panel title/is confusing... should be handled somewhere else though, not a PinnedContent concern
+        return false;
+      } else if (has_local_storage && local_storage_name) {
+        try {
+          const storage_value = JSON.parse(
+            localStorage.getItem(local_storage_name)
+          );
+          return _.isBoolean(storage_value) ? storage_value : true;
+        } catch {
+          return true;
         }
-      })();
-
-      this.state = {
-        user_has_enabled_pinning,
-      };
-    }
-
-    pin_pressed = () => {
-      const { local_storage_name } = this.props;
-      const { user_has_enabled_pinning } = this.state;
-
-      has_local_storage &&
-        local_storage_name &&
-        localStorage.setItem(local_storage_name, !user_has_enabled_pinning);
-
-      this.setState({
-        user_has_enabled_pinning: !user_has_enabled_pinning,
-      });
-    };
-
-    handleKeyDown = (e) => {
-      if (e.key === "Tab") {
-        this.setState({ user_has_enabled_pinning: false });
       }
+    })();
+
+    this.content_ref = React.createRef();
+
+    this.state = {
+      user_has_enabled_pinning,
     };
+  }
 
-    render() {
-      const { user_has_enabled_pinning } = this.state;
-      const { children } = this.props;
+  pin_pressed = () => {
+    const { local_storage_name } = this.props;
+    const { user_has_enabled_pinning } = this.state;
 
-      return !is_a11y_mode ? (
-        <ReactResizeDetector handleWidth>
-          {({ width }) => (
-            <InView>
-              {({ inView, ref, entry }) => (
-                // this div is for the intersection check
+    has_local_storage &&
+      local_storage_name &&
+      localStorage.setItem(local_storage_name, !user_has_enabled_pinning);
+
+    this.setState({
+      user_has_enabled_pinning: !user_has_enabled_pinning,
+      content_height: 0,
+    });
+  };
+
+  handleKeyDown = (e) => {
+    if (e.key === "Tab") {
+      this.setState({ user_has_enabled_pinning: false });
+    }
+  };
+
+  update_content_height = _.debounce(
+    () =>
+      this.setState({
+        content_height: this.content_ref.current.clientHeight,
+      }),
+    this.props.height_update_delay
+  );
+
+  componentDidMount() {
+    this.update_content_height();
+  }
+
+  render() {
+    const { user_has_enabled_pinning, content_height } = this.state;
+    const { children } = this.props;
+
+    return !is_a11y_mode ? (
+      <ReactResizeDetector handleWidth>
+        {({ width }) => (
+          <InView>
+            {({ inView, ref, entry }) => {
+              const is_stickied =
+                user_has_enabled_pinning &&
+                !inView &&
+                entry &&
+                entry.boundingClientRect.top < 0;
+
+              return (
+                // this div with ref is for the intersection check
                 <div ref={ref}>
-                  {/* this div is for sticky styline */}
+                  {/* this conditional div acts as a placeholder to make scrolling smoother */}
+                  {is_stickied && <div style={{ height: content_height }} />}
+                  {/* this div is for sticky styline, must be flex to include margins onto height */}
                   <div
                     style={{
-                      width: width,
-                      ...(user_has_enabled_pinning &&
-                        !inView &&
-                        entry &&
-                        entry.boundingClientRect.top < 0 && {
-                          position: "fixed",
-                          top: 0,
-                          zIndex: 2001,
-                        }),
+                      display: "flex",
+                      ...(is_stickied && {
+                        position: "fixed",
+                        top: 0,
+                        zIndex: 2001,
+                      }),
                     }}
+                    ref={this.content_ref}
+                    onClick={this.update_content_height}
                   >
-                    <div style={{ position: "relative" }}>
+                    <div style={{ position: "relative", width: width }}>
                       {children}
                       <div
                         style={{
@@ -137,13 +159,19 @@ export const PinnedContent = withRouter(
                     </div>
                   </div>
                 </div>
-              )}
-            </InView>
-          )}
-        </ReactResizeDetector>
-      ) : (
-        children
-      );
-    }
+              );
+            }}
+          </InView>
+        )}
+      </ReactResizeDetector>
+    ) : (
+      children
+    );
   }
-);
+}
+
+_PinnedContent.defaultProps = {
+  height_update_delay: 1000,
+};
+
+export const PinnedContent = withRouter(_PinnedContent);
