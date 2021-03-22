570612da87816c3857697141a8cc336eb83bf76f
Garvin <garvinh2001@hotmail.com>
2021-03-22 15:47:00 -0400

    Create story for icon grid

---
+++ client/src/components/IconGrid/IconGrid.stories.js
@@ -0,0 +1,41 @@
+import React from "react";
+
+import { IconGrid } from "./IconGrid.js";
+
+import {
+  IconHTML,
+  IconNodeJS,
+  IconReact,
+  IconGit,
+  IconGitHub,
+  IconPython,
+  IconSass,
+  IconGraphQL,
+  IconBaselineCloud,
+} from "src/icons/icons.js";
+
+export default {
+  title: "IconGrid",
+  component: IconGrid,
+};
+
+const tech_icon_list = _.chain([
+  IconHTML,
+  IconNodeJS,
+  IconReact,
+  IconGit,
+  IconGitHub,
+  IconPython,
+  IconSass,
+  IconGraphQL,
+  IconBaselineCloud,
+])
+  .map((SVG) => ({ svg: <SVG alternate_color={false} width="1.25em" /> }))
+  .value();
+
+const Template = (args) => <IconGrid {...args} />;
+
+export const Basic = Template.bind({});
+Basic.args = {
+  icons: tech_icon_list,
+};
