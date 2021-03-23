d1d102866560c43b70af964260851071e75bf21e
Garvin <garvinh2001@hotmail.com>
2021-03-23 14:10:08 -0400

    Add border to top of footer

--- client/src/components/BackToTop/BackToTop.stories.js
+++ client/src/components/BackToTop/BackToTop.stories.js
@@ -4,18 +4,19 @@ import React, { Fragment } from "react";
 
 import { BackToTop } from "./BackToTop.js";
 
-
 const BackToTopTemplate = ({ focusId }) => (
   <Fragment>
     <div id="ib-site-header-area">Header</div>
     {focusId && <input id={focusId} placeholder="input for focus..." />}
     <div style={{ height: "500vh" }}> Scroll down to footer...</div>
-    <div id="wb-info" style={{ height: "287px" }}>
+    <div id="wb-info" style={{ height: "300px", borderTop: "2px black solid" }}>
       Footer
     </div>
-    <BackToTop focus={() => {
-      document.getElementById("#test").focus();
-    }} />
+    <BackToTop
+      focus={() => {
+        document.getElementById("#test").focus();
+      }}
+    />
   </Fragment>
 );
 
@@ -23,7 +24,6 @@ export default {
   title: "BackToTop",
   component: BackToTopTemplate,
 };
-
 
 export const Basic = BackToTopTemplate.bind({});
 
