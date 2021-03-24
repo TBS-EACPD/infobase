c4c794926ff22af8d2dba67a35c5892eefb7d8a1
Garvin <garvinh2001@hotmail.com>
2021-03-24 15:50:20 -0400

    Re-add resource numbers

--- client/src/TagExplorer/hierarchy_scheme_configs.js
+++ client/src/TagExplorer/hierarchy_scheme_configs.js
@@ -25,7 +25,7 @@ const tag_configs = _.chain(Tag.tag_roots)
         id: tag.guid,
         data: {
           name: tag.name,
-          resources: is_m2m ? null : get_resources_for_subject(tag, year),
+          resources: get_resources_for_subject(tag, year),
           subject: tag,
           defs:
             tag.is_lowest_level_tag &&
