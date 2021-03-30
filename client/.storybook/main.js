1b336b95cf50cbc7a33e8ccb2de10e7173839119
Stephen-ONeil <stephenlevioneil@gmail.com>
2021-03-30 13:15:45 -0400

    Fully override default storybook webpack rules with the client's rule set. Merging the two turned out to be problematic for some CSS files, seems storybook was outputting some PostCSS artifacts that then choked up later client CSS loaders (we don't use PostCSS). Clean up in the process (return new extended config, no more mutating with pushes etc)

--- client/.storybook/main.js
+++ client/.storybook/main.js
@@ -6,6 +6,8 @@ const {
   bundle_extended_bootstrap_css,
 } = require("../build_code/bundle_extended_bootstrap_css.js");
 
+const LANG = "en";
+
 module.exports = {
   stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: [
@@ -13,49 +15,46 @@ module.exports = {
     "@storybook/addon-essentials",
     "@storybook/addon-a11y",
   ],
-  webpackFinal: async (config, { configType }) => {
-    // `configType` has a value of 'DEVELOPMENT' or 'PRODUCTION'
-    // You can change the configuration based on that.
-    // 'PRODUCTION' is used when building the static version of storybook.
+  webpackFinal: async (config) => ({
+    ...config,
 
-    config.resolve = {
-      ...config.resolve,
-      modules: [std_lib_path.resolve(__dirname, "../"), "node_modules/"],
-    };
-
-    // TODO hacky, this is essentially to set injected_build_constants.js's lang export.
-    // Going to have components that need other injected build constants though, and might
-    // even want some that have stories for different sets of build constants (a11y vs standard mode, etc.)
-    // so we need a less hacky and more exstensible way to mock these constants (in general + case by case?)
-    config.plugins.push(
-      new webpack.DefinePlugin({
-        APPLICATION_LANGUAGE: JSON.stringify("en"),
-        IS_A11Y_MODE: process.env.STORYBOOK_A11Y,
-      })
-    );
-
-    // As in the real client, the extended bootstrap stylesheet is precompiled and loaded by the initial html file
-    // (see prieview-head.html) rather than part of the app bundles themselves. This hook ensures the extended bootstrap
-    // stylesheet is updated* and exists where the storybook index.html expects it
-    // * small caveat, unlike normal builds nothing is watching the extended bootstrap files themselves, only output when
-    // something changes in a file the storybook webpack IS watching
-    config.plugins.push({
-      apply: (compiler) => {
-        compiler.hooks.emit.tap("EmitPlugin", () =>
-          bundle_extended_bootstrap_css(config.output.path)
-        );
-      },
-    });
-
-    config.module.rules = [
-      ...config.module.rules,
-      ...get_rules({
-        language: "en",
+    module: {
+      ...config.module,
+      // fully override default storybook rules with the client's rules
+      rules: get_rules({
+        language: LANG,
         is_prod_build: false,
         target_ie11: true,
       }),
-    ];
+    },
 
-    return config;
-  },
+    resolve: {
+      ...config.resolve,
+      modules: [std_lib_path.resolve(__dirname, "../"), "node_modules/"],
+    },
+
+    plugins: [
+      ...config.plugins,
+      new webpack.DefinePlugin({
+        // TODO hacky, this is essentially to set injected_build_constants.js's lang export.
+        // Going to have components that need other injected build constants though, and might
+        // even want some that have stories for different sets of build constants (a11y vs standard mode, etc.)
+        // so we need a less hacky and more exstensible way to mock these constants (in general + case by case?)
+        APPLICATION_LANGUAGE: JSON.stringify(LANG),
+        IS_A11Y_MODE: process.env.STORYBOOK_A11Y,
+      }),
+      {
+        // As in the real client, the extended bootstrap stylesheet is precompiled and loaded by the initial html file
+        // (see prieview-head.html) rather than part of the app bundles themselves. This hook ensures the extended bootstrap
+        // stylesheet is updated* and exists where the storybook index.html expects it
+        // * small caveat, unlike normal builds nothing is watching the extended bootstrap files themselves, only output when
+        // something changes in a files the storybook webpack IS watching
+        apply: (compiler) => {
+          compiler.hooks.emit.tap("EmitPlugin", () =>
+            bundle_extended_bootstrap_css(config.output.path)
+          );
+        },
+      },
+    ],
+  }),
 };
