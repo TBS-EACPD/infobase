26b0ada52cebce1f0ca008eb53bf934fb79cdd9c
Chetan Mandur <ChetanMandur45@gmail.com>
2021-04-06 11:23:54 -0400

    reverted change on val_display

--- client/src/explorer_common/explorer_components.js
+++ client/src/explorer_common/explorer_components.js
@@ -181,7 +181,7 @@ export const ExplorerNode = ({
                 }
               >
                 {_.isFunction(val_display)
-                  ? val_display(get_val(node))
+                  ? val_display(get_val(node), node)
                   : get_val(node)}
               </div>
             ))}
