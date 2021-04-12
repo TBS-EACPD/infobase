e6a4ae1261cdda69e70973350fac61f715e236ac
Taewan Kang <tkang042@uottawa.ca>
2021-04-12 17:09:16 -0400

    switch fully to hooks for all services panels (for orgs)

--- client/src/panels/panel_declarations/services/top10_services_application_volume.js
+++ client/src/panels/panel_declarations/services/top10_services_application_volume.js
@@ -12,7 +12,7 @@ import {
   create_text_maker_component,
 } from "src/components/index.js";
 
-import { Service } from "src/models/services.js";
+import { useServices } from "src/models/populate_services.js";
 
 import { newIBLightCategoryColors } from "src/core/color_schemes.ts";
 import { formatter } from "src/core/format.ts";
@@ -20,7 +20,10 @@ import { is_a11y_mode } from "src/core/injected_build_constants.ts";
 
 import { WrappedNivoHBar } from "src/charts/wrapped_nivo/index.js";
 
-import { delivery_channels_keys } from "./shared.js";
+import {
+  delivery_channels_keys,
+  delivery_channels_query_fragment,
+} from "./shared.js";
 
 import text from "./services.yaml";
 
@@ -31,8 +34,38 @@ const total_volume = text_maker("applications_and_calls");
 const volume_formatter = (val) =>
   formatter("compact", val, { raw: true, noMoney: true });
 
-const Top10ServicesApplicationVolumePanel = ({ panel_args }) => {
-  const { data, subject } = panel_args;
+const Top10ServicesApplicationVolumePanel = ({ subject }) => {
+  const { loading, data } = useServices({
+    id: subject.id,
+    service_fragments: `
+    id
+    name
+    ${delivery_channels_query_fragment}
+    `,
+  });
+  if (loading) {
+    return <span>loading</span>;
+  }
+
+  const processed_data = _.chain(data)
+    .map(({ id, service_report }) => ({
+      id,
+      [total_volume]: _.reduce(
+        delivery_channels_keys,
+        (sum, key) => sum + _.sumBy(service_report, `${key}_count`) || 0,
+        0
+      ),
+    }))
+    .filter(total_volume)
+    .sortBy(total_volume)
+    .takeRight(10)
+    .value();
+
+  const data_name_lookup = _.chain(data)
+    .map(({ id, name }) => [id, name])
+    .fromPairs()
+    .value();
+
   const column_configs = {
     id: {
       index: 0,
@@ -40,10 +73,10 @@ const Top10ServicesApplicationVolumePanel = ({ panel_args }) => {
       is_searchable: true,
       formatter: (id) => (
         <a href={`#dept/${subject.id}/service-panels/${id}`}>
-          {Service.lookup(id).name}
+          {data_name_lookup[id]}
         </a>
       ),
-      raw_formatter: (id) => Service.lookup(id).name,
+      raw_formatter: (id) => data_name_lookup[id],
     },
     [total_volume]: {
       index: 1,
@@ -54,7 +87,7 @@ const Top10ServicesApplicationVolumePanel = ({ panel_args }) => {
   };
   const table_content = (
     <DisplayTable
-      data={[...data].reverse()}
+      data={[...processed_data].reverse()}
       column_configs={column_configs}
       unsorted_initial={true}
     />
@@ -66,9 +99,9 @@ const Top10ServicesApplicationVolumePanel = ({ panel_args }) => {
         className="medium-panel-text"
         k="top10_services_volume_text"
         args={{
-          highest_service_name: Service.lookup(_.last(data).id).name,
-          highest_service_value: _.last(data)[total_volume],
-          num_of_services: data.length,
+          highest_service_name: data_name_lookup[_.last(data).id],
+          highest_service_value: _.last(processed_data)[total_volume],
+          num_of_services: processed_data.length,
         }}
       />
       {is_a11y_mode ? (
@@ -84,7 +117,7 @@ const Top10ServicesApplicationVolumePanel = ({ panel_args }) => {
               enableLabel={true}
               labelSkipWidth={30}
               label={(d) => volume_formatter(d.value)}
-              data={data}
+              data={processed_data}
               is_money={false}
               colors={(d) => colors(d.id)}
               padding={0.1}
@@ -118,7 +151,7 @@ const Top10ServicesApplicationVolumePanel = ({ panel_args }) => {
                         dominantBaseline="end"
                       >
                         <TspanLineWrapper
-                          text={Service.lookup(tick.value).name}
+                          text={data_name_lookup[tick.value]}
                           width={70}
                         />
                       </text>
@@ -141,36 +174,13 @@ export const declare_top10_services_application_volume_panel = () =>
     levels: ["dept", "program"],
     panel_config_func: (level, panel_key) => ({
       title: text_maker("top10_services_volume_title"),
-      requires_services: true,
-      calculate: (subject) => ({
-        subject,
-        services:
-          level === "dept"
-            ? Service.get_by_dept(subject.id)
-            : Service.get_by_prog(subject.id),
-      }),
       footnotes: false,
       render({ title, calculations, sources }) {
-        const { panel_args } = calculations;
-        const data = _.chain(panel_args.services)
-          .map(({ id, service_report }) => ({
-            id,
-            [total_volume]: _.reduce(
-              delivery_channels_keys,
-              (sum, key) => sum + _.sumBy(service_report, `${key}_count`) || 0,
-              0
-            ),
-          }))
-          .filter(total_volume)
-          .sortBy(total_volume)
-          .takeRight(10)
-          .value();
+        const { subject } = calculations;
 
         return (
           <InfographicPanel title={title} sources={sources}>
-            <Top10ServicesApplicationVolumePanel
-              panel_args={{ ...panel_args, data }}
-            />
+            <Top10ServicesApplicationVolumePanel subject={subject} />
           </InfographicPanel>
         );
       },
