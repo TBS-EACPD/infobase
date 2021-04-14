ab5af012e71ea2a54c429088aa72bf43f9da42a7
Stephen-ONeil <stephenlevioneil@gmail.com>
2021-04-14 18:06:34 -0400

    Move a bunch of common logic for derived values from Glossary/EverythingSearch dwon in to SearchConfigTypeahead

--- client/src/glossary/GlossarySearch.js
+++ client/src/glossary/GlossarySearch.js
@@ -7,7 +7,6 @@ import { lang } from "src/core/injected_build_constants.js";
 import { glossary_href } from "src/link_utils.js";
 
 import { glossary as glossary_search_config } from "src/search/search_configs.js";
-import { InfoBaseHighlighter } from "src/search/search_utils.js";
 import { SearchConfigTypeahead } from "src/search/SearchConfigTypeahead.js";
 
 const glossary_placeholder = {
@@ -24,8 +23,6 @@ const GlossarySearch = withRouter(
         <SearchConfigTypeahead
           placeholder={glossary_placeholder}
           search_configs={[glossary_search_config]}
-          config_groups={this.config_groups}
-          all_options={this.all_options}
           on_select={({ id }) =>
             history.push(glossary_href(id).replace("#", "/"))
           }
@@ -33,37 +30,6 @@ const GlossarySearch = withRouter(
         />
       );
     }
-    get_all_options = _.memoize((search_configs) =>
-      _.flatMap(search_configs, (search_config, ix) =>
-        _.map(search_config.get_data(), (data) => ({
-          data,
-          name: search_config.name_function(data),
-          menu_content: (search) =>
-            _.isFunction(search_config.menu_content_function) ? (
-              search_config.menu_content_function(data, search)
-            ) : (
-              <InfoBaseHighlighter
-                search={search}
-                content={search_config.name_function(data)}
-              />
-            ),
-          config_group_index: ix,
-        }))
-      )
-    );
-    get_config_groups = _.memoize((search_configs) =>
-      _.map(search_configs, (search_config, ix) => ({
-        group_header: search_config.header_function(),
-        group_filter: search_config.filter,
-      }))
-    );
-    get all_options() {
-      return this.get_all_options([glossary_search_config]);
-    }
-    get config_groups() {
-      return this.get_config_groups([glossary_search_config]);
-    }
-  }
 );
 
 export { GlossarySearch };
--- client/src/search/SearchConfigTypeahead.js
+++ client/src/search/SearchConfigTypeahead.js
@@ -5,6 +5,8 @@ import { Typeahead } from "src/components/index.js";
 
 import { log_standard_event } from "src/core/analytics.js";
 
+import { InfoBaseHighlighter } from "src/search/search_utils.js";
+
 export class SearchConfigTypeahead extends React.Component {
   constructor(props) {
     super(props);
@@ -28,13 +30,28 @@ export class SearchConfigTypeahead extends React.Component {
     this.setState({ query_value });
   };
 
-  get results() {
-    const { all_options, config_groups, on_select } = this.props;
+  render() {
     const { query_value } = this.state;
 
     return (
-      query_value !== "" &&
-      _.chain(all_options)
+      <Typeahead
+        {...this.props}
+        on_query={this.on_query}
+        query_value={query_value}
+        results={this.results}
+      />
+    );
+  }
+
+  get results() {
+    const { on_select, search_configs } = this.props;
+    const { query_value } = this.state;
+
+    if (query_value) {
+      const all_options = this.get_all_options(search_configs);
+      const config_groups = this.get_config_groups(search_configs);
+
+      return _.chain(all_options)
         .filter(({ config_group_index, data }) =>
           config_groups[config_group_index].group_filter(query_value, data)
         )
@@ -67,20 +84,33 @@ export class SearchConfigTypeahead extends React.Component {
           plain_text: result.name,
         }))
         .compact()
-        .value()
-    );
+        .value();
+    } else {
+      return [];
+    }
   }
-
-  render() {
-    const { query_value } = this.state;
-
-    return (
-      <Typeahead
-        {...this.props}
-        on_query={this.on_query}
-        query_value={query_value}
-        results={this.results}
-      />
-    );
-  }
+  get_all_options = _.memoize((search_configs) =>
+    _.flatMap(search_configs, (search_config, ix) =>
+      _.map(search_config.get_data(), (data) => ({
+        data,
+        name: search_config.name_function(data),
+        menu_content: (search) =>
+          _.isFunction(search_config.menu_content_function) ? (
+            search_config.menu_content_function(data, search)
+          ) : (
+            <InfoBaseHighlighter
+              search={search}
+              content={search_config.name_function(data)}
+            />
+          ),
+        config_group_index: ix,
+      }))
+    )
+  );
+  get_config_groups = _.memoize((search_configs) =>
+    _.map(search_configs, (search_config, ix) => ({
+      group_header: search_config.header_function(),
+      group_filter: search_config.filter,
+    }))
+  );
 }
