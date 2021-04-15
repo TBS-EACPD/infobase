8155c3360fb1dcb3904a5ca0c2cf1a007d889464
Stephen-ONeil <stephenlevioneil@gmail.com>
2021-04-15 11:26:21 -0400

    Put services metadata panel behind services_feature_flag

--- client/src/metadata/data_sources.js
+++ client/src/metadata/data_sources.js
@@ -5,7 +5,10 @@ import { GlossaryEntry } from "src/models/glossary.js";
 
 import { create_text_maker } from "src/models/text.js";
 
-import { lang } from "src/core/injected_build_constants.js";
+import {
+  lang,
+  services_feature_flag,
+} from "src/core/injected_build_constants.js";
 
 //circular dependency hack..
 import { Table } from "src/core/TableClass.js";
@@ -73,200 +76,205 @@ const infobase_open_data_page = {
     "https://ouvert.canada.ca/data/fr/dataset/a35cf382-690c-4221-a971-cf0fd189a46f",
 };
 
-const sources = _.mapValues(
+const sources = _.chain([
   {
-    PA: {
-      ix: 1,
-      description() {
-        return desc_from_gloss_keys("PA");
-      },
-      title() {
-        return tm("pa_title");
-      },
-      frequency: frequencies.y,
-      open_data: infobase_open_data_page,
-      report_link: {
-        en: "http://www.tpsgc-pwgsc.gc.ca/recgen/cpc-pac/index-eng.html",
-        fr: "http://www.tpsgc-pwgsc.gc.ca/recgen/cpc-pac/index-fra.html",
-      },
-      items() {
-        return _.map(tables_from_source_key("PA"), table_to_row_item);
-      },
+    key: "PA",
+    description() {
+      return desc_from_gloss_keys("PA");
     },
-    ESTIMATES: {
-      ix: 2,
-      description() {
-        return desc_from_gloss_keys("MAINS", "SUPPS");
-      },
-      title() {
-        return tm("estimates_title");
-      },
-      frequency: frequencies.q,
-      open_data: infobase_open_data_page,
-      report_link: {
-        en:
-          "https://www.canada.ca/en/treasury-board-secretariat/services/planned-government-spending/government-expenditure-plan-main-estimates.html",
-        fr:
-          "https://www.canada.ca/fr/secretariat-conseil-tresor/services/depenses-prevues/plan-depenses-budget-principal.html",
-      },
-      items() {
-        return _.map(tables_from_source_key("ESTIMATES"), table_to_row_item);
-      },
+    title() {
+      return tm("pa_title");
     },
-    CFMRS: {
-      ix: 3,
-      description() {
-        return desc_from_gloss_keys("CFMRS");
-      },
-      title() {
-        return tm("cfmrs_title");
-      },
-      open_data: {
-        en:
-          "http://open.canada.ca/data/en/dataset/5e6dcf6b-dbed-4b51-84e5-1f4926ad7fdf",
-        fr:
-          "http://ouvert.canada.ca/data/fr/dataset/5e6dcf6b-dbed-4b51-84e5-1f4926ad7fdf",
-      },
-      frequency: frequencies.y,
-      items() {
-        return _.map(tables_from_source_key("CFMRS"), table_to_row_item);
-      },
+    frequency: frequencies.y,
+    open_data: infobase_open_data_page,
+    report_link: {
+      en: "http://www.tpsgc-pwgsc.gc.ca/recgen/cpc-pac/index-eng.html",
+      fr: "http://www.tpsgc-pwgsc.gc.ca/recgen/cpc-pac/index-fra.html",
     },
-    RPS: {
-      description() {
-        return desc_from_gloss_keys("PEOPLE_DATA");
-      },
-      ix: 0,
-      title() {
-        return tm("rps_title");
-      },
-      frequency: frequencies.y,
-      items() {
-        return _.map(tables_from_source_key("RPS"), table_to_row_item);
-      },
-    },
-    DP: {
-      description() {
-        return desc_from_gloss_keys("DP");
-      },
-      title() {
-        return tm("dp_title");
-      },
-      frequency: frequencies.y,
-      open_data: infobase_open_data_page,
-      report_link: {
-        en:
-          "https://www.canada.ca/en/treasury-board-secretariat/services/planned-government-spending/reports-plans-priorities.html",
-        fr:
-          "https://www.canada.ca/fr/secretariat-conseil-tresor/services/depenses-prevues/rapports-plans-priorites.html",
-      },
-      items() {
-        return _.map(tables_from_source_key("DP"), table_to_row_item).concat([
-          {
-            id: "dp_results",
-            text: tm("dp_results_item_name"),
-            inline_link:
-              "#orgs/gov/gov/infograph/results/.-.-(panel_key.-.-'gov_dp)",
-          },
-        ]);
-      },
-    },
-    DRR: {
-      title() {
-        return tm("drr_title");
-      },
-      description() {
-        return desc_from_gloss_keys("DRR");
-      },
-      frequency: frequencies.y,
-      open_data: infobase_open_data_page,
-      report_link: {
-        en:
-          "https://www.canada.ca/en/treasury-board-secretariat/services/departmental-performance-reports.html",
-        fr:
-          "https://www.canada.ca/fr/secretariat-conseil-tresor/services/rapports-ministeriels-rendement.html",
-      },
-      items() {
-        return _.map(tables_from_source_key("DRR"), table_to_row_item).concat([
-          {
-            id: "drr_results",
-            text: tm("drr_results_item_name"),
-            inline_link:
-              "#orgs/gov/gov/infograph/results/.-.-(panel_key.-.-'gov_drr)",
-          },
-        ]);
-      },
-    },
-    IGOC: {
-      title() {
-        return tm("igoc_source_title");
-      },
-      description() {
-        return tm("igoc_source_desc");
-      },
-      frequency: frequencies.y,
-      open_data: infobase_open_data_page,
-      items() {
-        return [
-          {
-            id: "igoc",
-            text: tm("igoc_item_name"),
-            inline_link: "#igoc",
-          },
-        ];
-      },
-    },
-    RTP: {
-      title() {
-        return tm("transfer_payments_source_title");
-      },
-      description() {
-        return tm("transfer_payments_source_desc");
-      },
-      frequency: frequencies.y,
-      open_data: {
-        en:
-          "https://open.canada.ca/data/en/dataset/69bdc3eb-e919-4854-bc52-a435a3e19092",
-        fr:
-          "https://ouvert.canada.ca/data/fr/dataset/69bdc3eb-e919-4854-bc52-a435a3e19092",
-      },
-      items() {
-        return [
-          {
-            id: "rtp",
-            text: tm("transfer_payments_source_title"),
-            inline_link: rpb_link({
-              table: "orgTransferPaymentsRegion",
-              mode: "details",
-            }),
-          },
-        ];
-      },
-    },
-    SERVICES: {
-      title() {
-        return tm("services_title");
-      },
-      description() {
-        return tm("services_desc");
-      },
-      frequency: frequencies.y,
-      items() {
-        return [
-          {
-            id: "service",
-            text: tm("service_inventory"),
-            external_link:
-              lang === "en"
-                ? "https://open.canada.ca/data/en/dataset/3ac0d080-6149-499a-8b06-7ce5f00ec56c"
-                : "https://ouvert.canada.ca/data/fr/dataset/3ac0d080-6149-499a-8b06-7ce5f00ec56c",
-          },
-        ];
-      },
+    items() {
+      return _.map(tables_from_source_key("PA"), table_to_row_item);
     },
   },
-  (obj, key) => ({ ...obj, key })
-);
+  {
+    key: "ESTIMATES",
+    description() {
+      return desc_from_gloss_keys("MAINS", "SUPPS");
+    },
+    title() {
+      return tm("estimates_title");
+    },
+    frequency: frequencies.q,
+    open_data: infobase_open_data_page,
+    report_link: {
+      en:
+        "https://www.canada.ca/en/treasury-board-secretariat/services/planned-government-spending/government-expenditure-plan-main-estimates.html",
+      fr:
+        "https://www.canada.ca/fr/secretariat-conseil-tresor/services/depenses-prevues/plan-depenses-budget-principal.html",
+    },
+    items() {
+      return _.map(tables_from_source_key("ESTIMATES"), table_to_row_item);
+    },
+  },
+  {
+    key: "CFMRS",
+    description() {
+      return desc_from_gloss_keys("CFMRS");
+    },
+    title() {
+      return tm("cfmrs_title");
+    },
+    open_data: {
+      en:
+        "http://open.canada.ca/data/en/dataset/5e6dcf6b-dbed-4b51-84e5-1f4926ad7fdf",
+      fr:
+        "http://ouvert.canada.ca/data/fr/dataset/5e6dcf6b-dbed-4b51-84e5-1f4926ad7fdf",
+    },
+    frequency: frequencies.y,
+    items() {
+      return _.map(tables_from_source_key("CFMRS"), table_to_row_item);
+    },
+  },
+  {
+    key: "RPS",
+    description() {
+      return desc_from_gloss_keys("PEOPLE_DATA");
+    },
+    title() {
+      return tm("rps_title");
+    },
+    frequency: frequencies.y,
+    items() {
+      return _.map(tables_from_source_key("RPS"), table_to_row_item);
+    },
+  },
+  {
+    key: "DP",
+    description() {
+      return desc_from_gloss_keys("DP");
+    },
+    title() {
+      return tm("dp_title");
+    },
+    frequency: frequencies.y,
+    open_data: infobase_open_data_page,
+    report_link: {
+      en:
+        "https://www.canada.ca/en/treasury-board-secretariat/services/planned-government-spending/reports-plans-priorities.html",
+      fr:
+        "https://www.canada.ca/fr/secretariat-conseil-tresor/services/depenses-prevues/rapports-plans-priorites.html",
+    },
+    items() {
+      return _.map(tables_from_source_key("DP"), table_to_row_item).concat([
+        {
+          id: "dp_results",
+          text: tm("dp_results_item_name"),
+          inline_link:
+            "#orgs/gov/gov/infograph/results/.-.-(panel_key.-.-'gov_dp)",
+        },
+      ]);
+    },
+  },
+  {
+    key: "DRR",
+    title() {
+      return tm("drr_title");
+    },
+    description() {
+      return desc_from_gloss_keys("DRR");
+    },
+    frequency: frequencies.y,
+    open_data: infobase_open_data_page,
+    report_link: {
+      en:
+        "https://www.canada.ca/en/treasury-board-secretariat/services/departmental-performance-reports.html",
+      fr:
+        "https://www.canada.ca/fr/secretariat-conseil-tresor/services/rapports-ministeriels-rendement.html",
+    },
+    items() {
+      return _.map(tables_from_source_key("DRR"), table_to_row_item).concat([
+        {
+          id: "drr_results",
+          text: tm("drr_results_item_name"),
+          inline_link:
+            "#orgs/gov/gov/infograph/results/.-.-(panel_key.-.-'gov_drr)",
+        },
+      ]);
+    },
+  },
+  {
+    key: "IGOC",
+    title() {
+      return tm("igoc_source_title");
+    },
+    description() {
+      return tm("igoc_source_desc");
+    },
+    frequency: frequencies.y,
+    open_data: infobase_open_data_page,
+    items() {
+      return [
+        {
+          id: "igoc",
+          text: tm("igoc_item_name"),
+          inline_link: "#igoc",
+        },
+      ];
+    },
+  },
+  {
+    key: "RTP",
+    title() {
+      return tm("transfer_payments_source_title");
+    },
+    description() {
+      return tm("transfer_payments_source_desc");
+    },
+    frequency: frequencies.y,
+    open_data: {
+      en:
+        "https://open.canada.ca/data/en/dataset/69bdc3eb-e919-4854-bc52-a435a3e19092",
+      fr:
+        "https://ouvert.canada.ca/data/fr/dataset/69bdc3eb-e919-4854-bc52-a435a3e19092",
+    },
+    items() {
+      return [
+        {
+          id: "rtp",
+          text: tm("transfer_payments_source_title"),
+          inline_link: rpb_link({
+            table: "orgTransferPaymentsRegion",
+            mode: "details",
+          }),
+        },
+      ];
+    },
+  },
+  services_feature_flag && {
+    key: "SERVICES",
+    title() {
+      return tm("services_title");
+    },
+    description() {
+      return tm("services_desc");
+    },
+    frequency: frequencies.y,
+    items() {
+      return [
+        {
+          id: "service",
+          text: tm("service_inventory"),
+          external_link:
+            lang === "en"
+              ? "https://open.canada.ca/data/en/dataset/3ac0d080-6149-499a-8b06-7ce5f00ec56c"
+              : "https://ouvert.canada.ca/data/fr/dataset/3ac0d080-6149-499a-8b06-7ce5f00ec56c",
+        },
+      ];
+    },
+  },
+])
+  .compact()
+  .reduce((sources, source) => ({ ...sources, [source.key]: source }), {})
+  .value();
 
 const get_source_links = (source_keys) =>
   _.chain(source_keys)
