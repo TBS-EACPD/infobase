61780a6d3b4c44d84b8775021d1e01c91401063b
Chetan Mandur <ChetanMandur45@gmail.com>
2021-04-21 13:37:24 -0400

    deleted hook scripts

--- scripts/dev_scripts/init_hooks.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# Symlinks all git hooks in the hooks directory to .git/hooks.
-# This includes hooks that, on merge and rebase, re-run this script to capture any new hooks
-
-chmod 555 $PWD/scripts/dev_scripts/hooks/*
-ln -sf $PWD/scripts/dev_scripts/hooks/* .git/hooks
