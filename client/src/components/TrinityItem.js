f0743619a2901f666798e6591ffbff1bdb238d67
Garvin <garvinh2001@hotmail.com>
2021-04-21 12:04:00 -0400

    Fix display of trinity items on larger screens

--- client/src/components/TrinityItem.js
+++ client/src/components/TrinityItem.js
@@ -3,10 +3,8 @@ import React from "react";
 import "./TrinityItem.scss";
 
 export const TrinityItem = ({ svg, title, href, onClick }) => (
-  <div className="col-12 col-lg-4">
-    <a href={href} className="TrinityItem" onClick={onClick}>
-      <div className="TrinityItem__Title">{title}</div>
-      <div className="TrinityItem__Img">{svg}</div>
-    </a>
-  </div>
+  <a href={href} className="TrinityItem" onClick={onClick}>
+    <div className="TrinityItem__Title">{title}</div>
+    <div className="TrinityItem__Img">{svg}</div>
+  </a>
 );
