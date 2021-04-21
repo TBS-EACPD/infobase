8a5cd5f5f88d05a532012dfbab98ffe1bf217f77
Garvin <garvinh2001@hotmail.com>
2021-04-21 10:37:20 -0400

    Adjust margins on trinity items; Adjust format on treemap

--- client/src/components/TrinityItem.js
+++ client/src/components/TrinityItem.js
@@ -3,8 +3,10 @@ import React from "react";
 import "./TrinityItem.scss";
 
 export const TrinityItem = ({ svg, title, href, onClick }) => (
-  <a href={href} className="TrinityItem col-12 col-lg-4" onClick={onClick}>
-    <div className="TrinityItem__Title">{title}</div>
-    <div className="TrinityItem__Img">{svg}</div>
-  </a>
+  <div className="col-12 col-lg-4">
+    <a href={href} className="TrinityItem" onClick={onClick}>
+      <div className="TrinityItem__Title">{title}</div>
+      <div className="TrinityItem__Img">{svg}</div>
+    </a>
+  </div>
 );
