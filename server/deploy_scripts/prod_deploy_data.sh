53ff3ca2238376415653359ce9168a75952d2a50
Stephen-ONeil <stephenlevioneil@gmail.com>
2021-04-29 19:01:13 -0400

    Remove old babel-node from server prod_deploy_data.sh

--- server/deploy_scripts/prod_deploy_data.sh
+++ server/deploy_scripts/prod_deploy_data.sh
@@ -5,7 +5,7 @@ set -e # will exit if any command has non-zero exit value
 source deploy_scripts/set_prod_env_vars.sh
 source deploy_scripts/set_transient_data_secrets.sh # these trap EXIT to handle their own cleanup
 
-node_modules/@babel/node/bin/babel-node.js src/populate_db.js
+node src/populate_db.js
 
 source deploy_scripts/unset_prod_env_vars.sh
 
