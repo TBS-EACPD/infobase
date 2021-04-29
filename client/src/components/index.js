efa1810fc25f7e4f0a9d70f5ce73ba4023ba5bea
Garvin <garvinh2001@hotmail.com>
2021-04-29 10:13:09 -0400

    Change two level select to tsx

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -14,7 +14,7 @@ export { SortDirections } from "./SortDirection.js";
 export { TabbedControls, TabbedContent } from "./TabbedContent.js";
 export { LabeledBox } from "./LabeledBox.js";
 export { TextMaker, TM } from "./TextMaker.tsx";
-export { TwoLevelSelect } from "./TwoLevelSelect.js";
+export { TwoLevelSelect } from "./TwoLevelSelect";
 export { CardTopImage } from "./CardTopImage.js";
 export { CardLeftImage } from "./CardLeftImage.js";
 export { CardCenteredImage } from "./CardCenteredImage.js";
