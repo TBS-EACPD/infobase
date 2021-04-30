70ab06b897606fed58784707ced445c49f5dee6b
Stephen-ONeil <stephenlevioneil@gmail.com>
2021-04-29 22:00:07 -0400

    Ugh, ok, fine. Add prod_build scripts to server and email_backend package.json. Didn't want to, since they're really only needed for the deploy scripts (which aren't run through package.json), but it's the most trustworthy way to run the local bode_modules babel in CI (since npx isn't installed)

--- email_backend/scripts/deploy/prod_deploy_email_backend_function.sh
+++ email_backend/scripts/deploy/prod_deploy_email_backend_function.sh
@@ -5,7 +5,7 @@ set -e # will exit if any command has non-zero exit value
 source scripts/util/set_prod_env_vars.sh
 source scripts/util/set_transient_secrets.sh # these trap EXIT to handle their own cleanup
 
-scripts/util/build.sh
+npm run prod_build
 
 gcloud beta functions deploy prod-email-backend --max-instances 1 --project ${PROJECT} --region us-central1 --entry-point email_backend --stage-bucket email-backend-staging-bucket --runtime nodejs14 --trigger-http --env-vars-file $scratch/envs.yaml > /dev/null
 gcloud alpha functions add-iam-policy-binding prod-email-backend --project ${PROJECT} --region us-central1 --member allUsers --role roles/cloudfunctions.invoker
