73d8a84568769b2f9508555bbad66d6857d7ba79
Stephen-ONeil <stephenlevioneil@gmail.com>
2021-04-29 22:22:43 -0400

    Duh, late-working brain, the deploy server job uses a slim google cloud sdk image, shouldn't be trying to run babel in there. Lift server transpile up in to the main build job, persist to the workspace with everything else

--- server/deploy_scripts/ci_deploy_function.sh
+++ server/deploy_scripts/ci_deploy_function.sh
@@ -11,8 +11,6 @@ echo "USE_REMOTE_DB: '1'" >> ./envs.yaml
 
 source ../scripts/ci_scripts/redact_env_vars_from_logging.sh "redact-start"
 
-npm run prod_build
-
 gcloud functions deploy $CIRCLE_BRANCH --entry-point app --stage-bucket api-staging-bucket --runtime nodejs14 --trigger-http --env-vars-file ./envs.yaml
 gcloud alpha functions add-iam-policy-binding $CIRCLE_BRANCH --member allUsers --role roles/cloudfunctions.invoker
 
