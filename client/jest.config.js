3d55df75f1a0b7d6436a625899f85f3ee4f933c8
Stephen-ONeil <stephenlevioneil@gmail.com>
2021-05-03 13:23:56 -0400

    Add TypeScript support to client babel/jest configs

--- client/jest.config.js
+++ client/jest.config.js
@@ -1,13 +1,13 @@
 module.exports = {
   moduleDirectories: ["./", "node_modules"],
   setupFilesAfterEnv: ["./src/testing/set_up_tests.js"],
-  testRegex: "src\\/.+\\.test\\.js?$",
+  testRegex: "src\\/.+\\.test\\.(js|ts)?$",
   moduleNameMapper: {
     "\\.(css|scss)$": "<rootDir>/src/testing/styleMock.js",
   },
   transform: {
     "^.+\\.yaml$": "./src/testing/yaml-lang-transform.js",
-    "^.+\\.js$": "babel-jest",
+    "^.+\\.(js|tsx|ts)$": "babel-jest",
     "^.+\\.csv$": "./src/testing/raw-transform.js",
     //Note that webpack's svg-inline-loader applies minor transformations, let's hope they don't matter to tests, though
     "^.+\\.(csv|svg)$": "./src/testing/raw-transform.js",
