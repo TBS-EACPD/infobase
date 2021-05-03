d1edba325118d9734c0890d4a53d97f96ce26c8b
Stephen-ONeil <stephenlevioneil@gmail.com>
2021-05-03 13:36:08 -0400

    Prettier-up some new jest related files

--- client/__mocks__/spin.js.js
+++ client/__mocks__/spin.js.js
@@ -1,4 +1,4 @@
 //The spin.js author uses ES module in their code (which is weird, because it's written in TS and can easily be output to cjs or umd)
 module.exports = {
   Spinner: {},
-}
+};
--- client/src/testing/transforms-example.test.js
+++ client/src/testing/transforms-example.test.js
@@ -1,25 +1,25 @@
-import { get_yaml_content, get_parsed_csv } from './transforms-example.js';
-import yaml_content from './sample_yaml.yaml';
+import { get_yaml_content, get_parsed_csv } from "./transforms-example.js";
 
+import yaml_content from "./sample_yaml.yaml";
 
 const sample_text_entry_matcher = {
-  __file_name__:"/src/testing/sample_yaml.yaml",
+  __file_name__: "/src/testing/sample_yaml.yaml",
   some_key: {
     en: "some value",
     fr: "le multiline vàlûe\n",
   },
 };
 
-test("yaml import work within test modules themselves",()=>{
+test("yaml import work within test modules themselves", () => {
   expect(yaml_content).toEqual(sample_text_entry_matcher);
 });
 
-test("yaml import work within tested modules",()=>{
+test("yaml import work within tested modules", () => {
   const content = get_yaml_content();
   expect(content).toEqual(sample_text_entry_matcher);
 });
 
-test("csv works in test env",()=>{
+test("csv works in test env", () => {
   const content = get_parsed_csv();
   expect(content[0]).toEqual({
     key: "some_key",
