c0bb1025574bc204ab493ef92605ae7cb5a81eeb
Stephen-ONeil <stephenlevioneil@gmail.com>
2021-05-10 10:06:33 -0400

    Update home page featured content, add covid exp, drop new tag from older items

--- client/src/home/home-data.js
+++ client/src/home/home-data.js
@@ -68,24 +68,27 @@ const infographic_link_items = _.compact([
 
 const featured_content_items = _.compact([
   {
-    text_key: "covid_measure_spending_auth",
-    href: "#orgs/gov/gov/infograph/covid",
+    text_key: "covid_expenditures_estimated_exp",
+    href:
+      "#orgs/gov/gov/infograph/covid/.-.-(panel_key.-.-'covid_expenditures_panel)",
     is_new: "true",
+  },
+  {
+    text_key: "covid_measure_spending_auth",
+    href:
+      "#orgs/gov/gov/infograph/covid/.-.-(panel_key.-.-'covid_estimates_panel)",
   },
   {
     text_key: "quick_link_people_2020",
     href: "#orgs/gov/gov/infograph/people",
-    is_new: "true",
   },
   {
     text_key: "quick_link_DP_2022",
     href: "#orgs/gov/gov/infograph/results/.-.-(panel_key.-.-'gov_dp)",
-    is_new: "true",
   },
   {
     text_key: "quick_link_main_estimates",
     href: "#compare_estimates",
-    is_new: "true",
   },
   {
     text_key: "quick_link_DRR_1920",
