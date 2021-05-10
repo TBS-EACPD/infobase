e7d6fb44457cac021d00b093c68babcd9e03a41e
Patrick <phuan097@uottawa.ca>
2021-05-10 09:58:50 -0400

    Storybooked Details component, is_open doesn't seem to work

---
+++ client/src/components/Details/Details.stories.js
@@ -0,0 +1,22 @@
+import _ from "lodash";
+import React from "react";
+
+import { Details } from "./Details.js";
+
+export default {
+  title: "Details",
+  component: Details,
+};
+
+const Template = (args) => <Details {...args} />;
+
+const summary_content = <div>Summary</div>;
+const content = <div>Content</div>;
+
+export const Basic = Template.bind({});
+Basic.args = {
+  is_open: false,
+  summary_content,
+  persist_content: false,
+  content,
+};
