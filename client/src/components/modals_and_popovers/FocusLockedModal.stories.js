a8eca1d25d20f6978dd165453c21653b3b059378
Patrick <phuan097@uottawa.ca>
2021-05-10 11:42:06 -0400

    Implemented FocusLockedModal story

--- client/src/components/modals_and_popovers/FocusLockedModal.stories.js
+++ client/src/components/modals_and_popovers/FocusLockedModal.stories.js
@@ -10,11 +10,22 @@ export default {
 
 const Template = (args) => <FocusLockedModal {...args} />;
 
-const children = <div>Hi</div>;
+const children = <div>Children</div>;
+const aria_label = {
+  en: "English",
+  fr: "Francais",
+};
 
 export const Basic = Template.bind({});
 Basic.args = {
+  // text
   children,
+  aria_label,
+
+  // booleans
   mounted: true,
   on_exit: true,
+
+  // css
+  additional_dialogue_class: "additional_dialogue_class",
 };
