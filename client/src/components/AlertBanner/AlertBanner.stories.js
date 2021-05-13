0125426972f03fbfe9b944e3cf9c664ca339d113
Patrick <phuan097@uottawa.ca>
2021-05-13 09:28:09 -0400

    Removed unnecessary Lodash imports

--- client/src/components/AlertBanner/AlertBanner.stories.js
+++ client/src/components/AlertBanner/AlertBanner.stories.js
@@ -1,4 +1,3 @@
-import _ from "lodash";
 import React from "react";
 
 import { AlertBanner } from "./AlertBanner.js";
--- client/src/components/DebouncedTextInput/DebouncedTextInput.stories.js
+++ client/src/components/DebouncedTextInput/DebouncedTextInput.stories.js
@@ -1,4 +1,3 @@
-import _ from "lodash";
 import React from "react";
 
 import { DebouncedTextInput } from "./DebouncedTextInput.js";
@@ -8,7 +7,6 @@ export default {
   component: DebouncedTextInput,
 };
 
-// not sure if the component works or not
 const Template = (args) => <DebouncedTextInput {...args} />;
 
 export const Basic = Template.bind({});
