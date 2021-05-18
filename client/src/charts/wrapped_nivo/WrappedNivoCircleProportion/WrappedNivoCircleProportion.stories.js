08599a679be5e54cc4e95a2f2b95113b2ab8ac57
Patrick <phuan097@uottawa.ca>
2021-05-18 10:02:59 -0400

    Initialized WrappedNivoCircleProportion storybook

---
+++ client/src/charts/wrapped_nivo/WrappedNivoCircleProportion/WrappedNivoCircleProportion.stories.js
@@ -0,0 +1,16 @@
+import _ from "lodash";
+import React from "react";
+
+import { WrappedNivoCircleProportion } from "./WrappedNivoCircleProportion";
+
+export default {
+  title: "charts/WrappedNivoCircleProportion",
+  component: WrappedNivoCircleProportion,
+};
+
+const Template = (args) => <WrappedNivoCircleProportion {...args} />;
+
+export const Basic = Template.bind({});
+Basic.args = {
+  title: "WrappedNivoCircleProportion",
+};
