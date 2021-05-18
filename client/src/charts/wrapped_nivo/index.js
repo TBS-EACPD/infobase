8fdf3a64205e3a02b43f036c722f48d703cf24fa
Patrick <phuan097@uottawa.ca>
2021-05-18 09:54:36 -0400

    Initialized WrappedNivoPie storybook

--- client/src/charts/wrapped_nivo/index.js
+++ client/src/charts/wrapped_nivo/index.js
@@ -4,7 +4,7 @@ import {
 } from "./WrappedNivoBar/wrapped_nivo_bar";
 import { WrappedNivoCircleProportion } from "./WrappedNivoCircleProportion";
 import { WrappedNivoLine } from "./WrappedNivoLine";
-import { WrappedNivoPie } from "./WrappedNivoPie";
+import { WrappedNivoPie } from "./WrappedNivoPie/WrappedNivoPie";
 import { WrappedNivoTreemap } from "./WrappedNivoTreemap";
 
 export { NivoLineBarToggle } from "src/charts/wrapped_nivo/NivoLineBarToggle/NivoLineBarToggle";
