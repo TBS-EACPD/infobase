fda58d1f3f6f5d2f28e0c3561f4b0576a75a944c
Patrick <phuan097@uottawa.ca>
2021-05-19 14:00:40 -0400

    Implemented Cardlist into storybook

---
+++ client/src/components/CardList/CardList.stories.js
@@ -0,0 +1,37 @@
+import _ from "lodash";
+import React from "react";
+
+import { CardList } from "./CardList.js";
+
+export default {
+  title: "CardList",
+  component: CardList,
+};
+
+const Template = (args) => <CardList {...args} />;
+
+const elements = [
+  {
+    display: "Display 1",
+    href: "",
+    children: [
+      { display: "Child 1", href: "" },
+      { display: "Child 2", href: "" },
+      { display: "Child 3", href: "" },
+    ],
+  },
+  {
+    display: "Display 2",
+    href: "",
+    children: [
+      { display: "Child 1", href: "" },
+      { display: "Child 2", href: "" },
+      { display: "Child 3", href: "" },
+    ],
+  },
+];
+
+export const Basic = Template.bind({});
+Basic.args = {
+  elements,
+};
