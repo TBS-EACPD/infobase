56f6e2e7eefa83860edbc86b40b9e0b4c16066ed
Taewan Kang <tkang042@uottawa.ca>
2021-05-25 14:14:10 -0400

    faq data to ts

--- client/src/FAQ/faq_data.ts
+++ client/src/FAQ/faq_data.ts
@@ -7,15 +7,15 @@ import { lang } from "src/core/injected_build_constants";
 /* eslint-disable no-restricted-imports */
 import faq_csv_string from "../../../data/faq.csv";
 
-const DISABLED_QUESTIONS = [];
+const DISABLED_QUESTIONS: string[] = [];
 
 export const faq_data = _.chain(faq_csv_string)
-  .thru(csvParse)
+  .thru((csv_str) => csvParse(csv_str))
   .map((qa_row) => [
     qa_row.id,
     {
       q: qa_row[`q_${lang}`],
-      a: marked(qa_row[`a_${lang}`], { sanitize: false, gfm: true }),
+      a: marked(qa_row[`a_${lang}`]!, { sanitize: false, gfm: true }),
     },
   ])
   .fromPairs()
