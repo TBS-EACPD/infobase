4f4a19b9b42070964c35414f4df2718043345cc1
Patrick <phuan097@uottawa.ca>
2021-05-25 09:46:20 -0400

    Added lorem ipsem to AlertBanner

--- client/src/components/AlertBanner/AlertBanner.stories.js
+++ client/src/components/AlertBanner/AlertBanner.stories.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { AlertBanner } from "./AlertBanner.tsx";
+import { AlertBanner } from "./AlertBanner";
 
 export default {
   title: "AlertBanner",
@@ -9,7 +9,8 @@ export default {
 
 const Template = (args) => <AlertBanner {...args} />;
 
-const children = <div>Children</div>;
+const children =
+  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
 
 export const Basic = Template.bind({});
 Basic.args = {
