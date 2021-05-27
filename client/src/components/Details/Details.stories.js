74ed2c2aba296dbd8301e1962656c925ae3484e9
Patrick <phuan097@uottawa.ca>
2021-05-27 18:15:40 -0400

    Added CountdownCircle to storybook

--- client/src/components/Details/Details.stories.js
+++ client/src/components/Details/Details.stories.js
@@ -1,5 +1,7 @@
 import React from "react";
 
+import { CountdownCircle } from "src/components/CountdownCircle/CountdownCircle";
+
 import { Details } from "./Details";
 
 export default {
@@ -7,15 +9,53 @@ export default {
   component: Details,
 };
 
-const Template = (args) => <Details {...args} />;
+// time: 10000,
+// size: "20em",
+// color: "blue",
+// stroke_width: "1em",
+// show_numbers: true,
+
+const Template = (args) => {
+  return (
+    <div>
+      <Details {...args} />
+      <CountdownCircle {...args} />
+    </div>
+  );
+};
 
 const summary_content = <div>Summary</div>;
 const content = <div>Content</div>;
 
-export const Basic = Template.bind({});
-Basic.args = {
+export const PersistContent = Template.bind({});
+PersistContent.args = {
+  // Details
+  is_open: false,
+  summary_content,
+  persist_content: true,
+  content,
+
+  // CountdownCircle
+  time: 10000,
+  size: "20em",
+  color: "red",
+  stroke_width: "1em",
+  show_numbers: true,
+  on_end_callback: "",
+};
+
+export const NonPersistContent = Template.bind({});
+NonPersistContent.args = {
   is_open: false,
   summary_content,
   persist_content: false,
   content,
+
+  // CountdownCircle
+  time: 10000,
+  size: "20em",
+  color: "red",
+  stroke_width: "1em",
+  show_numbers: true,
+  on_end_callback: "",
 };
