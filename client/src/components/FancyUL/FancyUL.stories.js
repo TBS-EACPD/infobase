c24c2768c6b53edd8673e1c492ccd997e45d24bb
Patrick <phuan097@uottawa.ca>
2021-05-27 18:32:45 -0400

    Changed children into an array in FancyUL component

--- client/src/components/FancyUL/FancyUL.stories.js
+++ client/src/components/FancyUL/FancyUL.stories.js
@@ -9,7 +9,7 @@ export default {
 
 const Template = (args) => <FancyUL {...args} />;
 
-const children = <div>Children</div>;
+const children = ["Child 1", "Child 2", "Child 3"];
 
 export const Basic = Template.bind({});
 Basic.args = {
