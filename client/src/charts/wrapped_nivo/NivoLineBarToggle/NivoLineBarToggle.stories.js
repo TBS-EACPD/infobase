446c887876840c023f39c426531b884e0508607d
csong202 <csong097@uottawa.ca>
2021-05-27 11:06:50 -0400

    Implemented NivoLineBarToggle story

--- client/src/charts/wrapped_nivo/NivoLineBarToggle/NivoLineBarToggle.stories.js
+++ client/src/charts/wrapped_nivo/NivoLineBarToggle/NivoLineBarToggle.stories.js
@@ -1,16 +1,9 @@
 import { scaleOrdinal } from "d3-scale";
-import _ from "lodash";
 import React from "react";
 
-import {
-  primaryColor,
-  secondaryColor,
-  tertiaryColor,
-} from "src/core/color_defs";
 import { newIBCategoryColors } from "src/core/color_schemes";
 
-// eslint-disable-next-line no-restricted-imports
-import { infobase_colors_smart } from "../wrapped_nivo_common";
+import { infobase_colors_smart } from "src/charts/wrapped_nivo/wrapped_nivo_common";
 
 import { NivoLineBarToggle } from "./NivoLineBarToggle";
 
@@ -26,18 +19,35 @@ Basic.args = {
   legend_title: "Legend Title",
   bar: true,
   graph_options: {
-    ticks: ["10", "20", "30", "40", "50", "60", "70", "80", "90", "100"],
+    ticks: ["2000", "2001", "2002", "2003", "2004"],
     y_axis: "y-axis",
   },
-  // get_colors: () =>
-  //   infobase_colors_smart(scaleOrdinal().range(newIBCategoryColors)),
-  // colors: [primaryColor, secondaryColor, tertiaryColor],
-  // get_colors: () => [primaryColor, secondaryColor, tertiaryColor],
+  get_colors: () =>
+    infobase_colors_smart(scaleOrdinal().range(newIBCategoryColors)),
   initial_graph_mode: "bar_grouped",
-  data: [],
+  data: [
+    {
+      active: true,
+      data: [1001, 2003, 1998, 347, 800],
+      label: "group 1",
+    },
+    {
+      active: true,
+      data: [609, 343, 888, 2456, 2232],
+      label: "group 2",
+    },
+    {
+      active: true,
+      data: [2000, 3000, 3333, 778, 1500],
+      label: "group 3",
+    },
+    {
+      active: true,
+      data: [900, 877, 2009, 2700, 777],
+      label: "group 4",
+    },
+  ],
   disable_toggle: false,
   legend_col_full_size: 4,
   graph_col_full_size: 8,
-  legend_class: false,
-  graph_col_class: false,
 };
