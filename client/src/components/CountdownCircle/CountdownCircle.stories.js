65b229a1abbbd04640b9ba83b907f56a12689493
Patrick <phuan097@uottawa.ca>
2021-06-01 10:41:47 -0400

    Added actions to CountdownCircle story

--- client/src/components/CountdownCircle/CountdownCircle.stories.js
+++ client/src/components/CountdownCircle/CountdownCircle.stories.js
@@ -1,17 +1,39 @@
-import React from "react";
+import { useArgs } from "@storybook/client-api";
+import React, { useState, useEffect } from "react";
 
 import { CountdownCircle } from "./CountdownCircle";
 
 export default {
   title: "CountdownCircle",
   component: CountdownCircle,
-  argTypes: {
-    on_end_callback: { time: 20000 },
-  },
 };
 
 const Template = (args) => {
-  return <CountdownCircle {...args} />;
+  const [end, setEnd] = useState(false);
+  const [_, updateArgs] = useArgs();
+
+  function on_end_callback() {
+    console.log("Time is up! Click the button to try again.");
+    setEnd(!end);
+  }
+
+  return (
+    <>
+      <CountdownCircle {...args} on_end_callback={on_end_callback} />
+      {end ? (
+        <button
+          onClick={() => {
+            setEnd(!end);
+
+            // Don't want to reset the page like this, but the timer won't reset by changing the time props
+            window.location.reload();
+          }}
+        >
+          Click to Restart
+        </button>
+      ) : null}
+    </>
+  );
 };
 
 export const Basic = Template.bind({});
