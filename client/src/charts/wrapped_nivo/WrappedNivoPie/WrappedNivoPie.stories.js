d00f7433a86f0f2599ee6b490c577655571677db
csong202 <csong097@uottawa.ca>
2021-06-01 11:08:27 -0400

    Implemented WrappedNivoPie

--- client/src/charts/wrapped_nivo/WrappedNivoPie/WrappedNivoPie.stories.js
+++ client/src/charts/wrapped_nivo/WrappedNivoPie/WrappedNivoPie.stories.js
@@ -1,5 +1,6 @@
-import _ from "lodash";
 import React from "react";
+
+import { newIBCategoryColors } from "src/core/color_schemes";
 
 import { WrappedNivoPie } from "./WrappedNivoPie";
 
@@ -8,7 +9,46 @@ export default {
   component: WrappedNivoPie,
 };
 
+const graph_data = [
+  {
+    id: "Group 1",
+    label: "Group 1",
+    value: 32432084,
+  },
+  {
+    id: "Group 2",
+    label: "Group 2",
+    value: 934234,
+  },
+  {
+    id: "Group 3",
+    label: "Group 3",
+    value: 5573653,
+  },
+  {
+    id: "Group 4",
+    label: "Group 4",
+    value: 7436534,
+  },
+  {
+    id: "Group 5",
+    label: "Group 5",
+    value: 2348634,
+  },
+];
+
+const [blue, teal, orange, purple, light_green] = newIBCategoryColors;
+
 const Template = (args) => <WrappedNivoPie {...args} />;
 
 export const Basic = Template.bind({});
-Basic.args = {};
+Basic.args = {
+  data: graph_data,
+  colors: [blue, teal, orange, purple, light_green],
+  disable_table_view: false,
+  display_horizontal: true,
+  show_legend: true,
+  sort_legend: false,
+  include_percent: true,
+  is_money: false,
+};
