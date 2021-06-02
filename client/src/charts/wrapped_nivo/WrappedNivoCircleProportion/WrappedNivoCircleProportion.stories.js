0eb210e6b64d51f4510562be869eaffe39a6161a
csong202 <csong097@uottawa.ca>
2021-06-02 11:43:34 -0400

    minor adjustments to CircleProportion and Line

--- client/src/charts/wrapped_nivo/WrappedNivoCircleProportion/WrappedNivoCircleProportion.stories.js
+++ client/src/charts/wrapped_nivo/WrappedNivoCircleProportion/WrappedNivoCircleProportion.stories.js
@@ -13,8 +13,8 @@ export const Basic = Template.bind({});
 Basic.args = {
   height: 200,
   is_money: false,
-  child_value: 100,
+  child_value: 117,
   child_name: "Child",
-  parent_value: 500,
+  parent_value: 506,
   parent_name: "Parent",
 };
