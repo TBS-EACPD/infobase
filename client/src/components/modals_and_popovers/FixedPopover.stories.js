b9bdc62353ba48a2741bc2e49205f8af7decd29d
Patrick <phuan097@uottawa.ca>
2021-06-03 10:59:13 -0400

    Added clock story to FixedPopover

--- client/src/components/modals_and_popovers/FixedPopover.stories.js
+++ client/src/components/modals_and_popovers/FixedPopover.stories.js
@@ -1,3 +1,4 @@
+import { useArgs } from "@storybook/client-api";
 import React, { Fragment } from "react";
 
 import { FixedPopover } from "./FixedPopover";
@@ -5,18 +6,30 @@ import { FixedPopover } from "./FixedPopover";
 export default {
   title: "modals and popovers/FixedPopover",
   component: FixedPopover,
+
+  // Need decorators to use useArgs()
+  decorators: [(Story) => <div>{Story()}</div>],
 };
 
 const Template = (args) => {
+  const [_, updateArgs] = useArgs();
+
+  function on_close_callback() {
+    console.log(args.show ? "Closing " : "Opening " + " modal.");
+    updateArgs({ ...args, show: !args.show });
+  }
   return (
     <Fragment>
       <div id="ib-site-header-area" />
       <div style={{ height: "500vh" }}> Scroll down... </div>
+      {args.show ? null : (
+        <button onClick={on_close_callback}>Click to open modal</button>
+      )}
       <div
         id="wb-info"
         style={{ height: "300px", borderTop: "2px black solid" }}
       />
-      <FixedPopover {...args} />
+      <FixedPopover {...args} on_close_callback={on_close_callback} />
     </Fragment>
   );
 };
@@ -28,14 +41,33 @@ Basic.args = {
   // text
   title: "Title",
   body: "Body",
-  header: "Header",
+  header: "FixedPopover Header",
   subtitle: "Subtitle",
   footer: "Footer",
 
+  // dimensions
+  max_body_height: "3em",
+
+  // css
+  dialog_position: "",
+  additional_dialog_position: "",
+};
+
+export const Timer = Template.bind({});
+Timer.args = {
+  show: true,
+
+  // text
+  title: "Title",
+  body: "Hover over the modal before it goes away!",
+  header: "FixedPopover Header",
+  subtitle: "Subtitle",
+  // footer: "Footer",
+  close_text: "Close Text",
+
   // booleans
-  restore_focus: false,
+  restore_focus: true,
   close_button_in_header: false,
-  close_text: false,
 
   // dimensions
   max_body_height: "3em",
