164404a08f2d0a5368c73aa12b2c413fb3188518
csong202 <csong097@uottawa.ca>
2021-06-03 11:52:41 -0400

    Implemented Line, fixed problem with colours

--- client/src/charts/wrapped_nivo/WrappedNivoLine/WrappedNivoLine.stories.js
+++ client/src/charts/wrapped_nivo/WrappedNivoLine/WrappedNivoLine.stories.js
@@ -1,3 +1,4 @@
+import { scaleOrdinal } from "d3-scale";
 import _ from "lodash";
 import React from "react";
 
@@ -19,28 +20,34 @@ const graph_data = [
       { x: "Year 1", y: 2008 },
       { x: "Year 2", y: 1278 },
       { x: "Year 3", y: 909 },
+      { x: "Year 4", y: 925 },
     ],
   },
   {
     id: "Group 2",
     data: [
+      { x: "Year 1", y: 3248 },
       { x: "Year 2", y: 1278 },
       { x: "Year 3", y: 909 },
       { x: "Year 4", y: 2998 },
-      { x: "Year 5", y: 1900 },
+      { x: "Year 5", y: 1367 },
+      { x: "Year 6", y: 3432 },
     ],
   },
   {
     id: "Group 3",
     data: [
+      { x: "Year 4", y: 3247 },
       { x: "Year 5", y: 2008 },
-      { x: "Year 6", y: 1278 },
+      { x: "Year 6", y: 2278 },
     ],
   },
   {
     id: "Group 4",
     data: [
-      { x: "Year 3", y: 128 },
+      { x: "Year 1", y: 1487 },
+      { x: "Year 2", y: 2856 },
+      { x: "Year 3", y: 2728 },
       { x: "Year 4", y: 3878 },
       { x: "Year 5", y: 2769 },
       { x: "Year 6", y: 1111 },
@@ -53,13 +60,13 @@ const raw_graph_data = _.chain(graph_data)
   .flatten()
   .value();
 
-const [blue, teal, orange, purple] = newIBCategoryColors;
+const colors = scaleOrdinal().range(newIBCategoryColors);
 
 export const Basic = Template.bind({});
 Basic.args = {
   data: graph_data,
   raw_data: raw_graph_data,
-  colors: [blue, teal, orange, purple],
+  colors: (d) => colors(d.id),
   is_money: false,
   remove_left_axis: false,
   remove_bottom_axis: false,
