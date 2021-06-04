194a00d2bf5af6009a221eb6bcef07e252babd30
IvanFeng1 <feng.ivan.1235@gmail.com>
2021-06-04 16:33:51 -0400

    Panel in TS

---
+++ client/src/components/Panel.tsx
@@ -0,0 +1,194 @@
+import classNames from "classnames";
+import _ from "lodash";
+import React from "react";
+
+import { textLightColor } from "src/core/color_defs";
+
+import { Details } from "./Details";
+import { FootnoteList } from "./FootnoteList";
+import { GlossaryItem } from "./glossary_components";
+import { create_text_maker_component } from "./misc_util_components";
+
+import text from "./Panel.yaml";
+import "./Panel.scss";
+
+const { text_maker, TM } = create_text_maker_component(text);
+
+interface Link {
+  href: string;
+  html: string;
+}
+interface PanelSourceProps {
+  links: Link[];
+}
+
+interface PanelGlossaryProps {
+  keys: string[];
+}
+
+const PanelSource: React.FC<PanelSourceProps> = ({ links }) => {
+  if (_.isEmpty(links)) {
+    return null;
+  }
+  const last_ix = links.length - 1;
+  return (
+    <span>
+      <span aria-hidden>
+        <TM k="panel_source_link_text" />
+      </span>
+      <span className="sr-only">
+        <TM k="panel_a11y_source_expl" />
+      </span>
+      <ul className="list-unstyled list-inline" style={{ display: "inline" }}>
+        {_.map(links, ({ href, html }, ix) => (
+          <li key={ix} className="list-inline-item">
+            <a className="source-link" href={href}>
+              <span dangerouslySetInnerHTML={{ __html: html }} />
+            </a>
+            {ix !== last_ix && ", "}
+          </li>
+        ))}
+      </ul>
+    </span>
+  );
+};
+
+const PanelGlossary: React.FC<PanelGlossaryProps> = ({ keys }) => {
+  if (_.isEmpty(keys)) {
+    return null;
+  }
+  const last_ix = keys.length - 1;
+  return (
+    <span>
+      <TM k="panel_glossary_text" />
+      <ul className="list-unstyled list-inline" style={{ display: "inline" }}>
+        {_.map(keys, (key, ix) => (
+          <li key={ix} className="list-inline-item">
+            <GlossaryItem id={key} item_class="glossary-link" />
+            {ix !== last_ix && ", "}
+          </li>
+        ))}
+      </ul>
+    </span>
+  );
+};
+/* plural: "Government of Canada"
+singular: "Government of Canada"
+subject_type: "gov"
+*/
+
+interface Constructor {
+  plural: string;
+  singular: string;
+  subject_type: string;
+}
+interface Subject {
+  constructor: Constructor;
+  description: string;
+  guid: string;
+  has_planned_spending: boolean;
+  id: string;
+  is: (comparator: any) => boolean;
+  legal_title: string;
+  level: string;
+  lookup: Function;
+  name: string;
+  plural: string;
+  singular: string;
+  subject_type: string;
+}
+interface Footnote {
+  id: string;
+  subject: Subject;
+  text: string;
+  topic_keys: string[];
+  year1: number;
+}
+interface PanelProps {
+  allowOverflow: boolean;
+  title: string;
+  otherHeaderContent: string;
+  children: React.ReactNode;
+  sources: Link[];
+  glossary_keys: string[];
+  footnotes: Footnote[];
+}
+
+interface PanelState {
+  isOpen: boolean;
+}
+export class Panel extends React.Component<PanelProps, PanelState> {
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      isOpen: true,
+    };
+  }
+  render() {
+    const { isOpen } = this.state;
+    const {
+      allowOverflow,
+      title,
+      otherHeaderContent,
+      children,
+      sources,
+      glossary_keys,
+      footnotes,
+    } = this.props;
+
+    return (
+      <section
+        className={classNames(
+          "panel panel-info mrgn-bttm-md",
+          allowOverflow && "panel-overflow"
+        )}
+      >
+        {(title || otherHeaderContent) && (
+          <header className="panel-heading">
+            {
+              <button
+                className={classNames("panel-heading-utils")}
+                onClick={() => this.setState({ isOpen: !isOpen })}
+                aria-label={
+                  isOpen
+                    ? text_maker("collapse_panel")
+                    : text_maker("expand_panel")
+                }
+              >
+                <span style={{ color: textLightColor }} aria-hidden>
+                  {isOpen ? "▼" : "►"}
+                </span>
+              </button>
+            }
+            {title && <h2 className="panel-title">{title}</h2>}
+            {isOpen && otherHeaderContent}
+          </header>
+        )}
+        {isOpen && (
+          <div className="panel-body">
+            {children}
+            <div className="mrgn-tp-md" />
+            {!_.isEmpty(sources) && (
+              <div>
+                <PanelSource links={sources} />
+              </div>
+            )}
+            {!_.isEmpty(glossary_keys) && (
+              <div className="mrgn-tp-md">
+                <PanelGlossary keys={glossary_keys} />
+              </div>
+            )}
+            {!_.isEmpty(footnotes) && (
+              <div className="mrgn-tp-md">
+                <Details
+                  summary_content={<TM k="footnotes" />}
+                  content={<FootnoteList footnotes={footnotes} />}
+                />
+              </div>
+            )}
+          </div>
+        )}
+      </section>
+    );
+  }
+}
