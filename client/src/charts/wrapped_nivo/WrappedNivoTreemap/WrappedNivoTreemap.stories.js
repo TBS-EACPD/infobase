27659f886ac17d707460b068a9c6991a6a3b5a4f
csong202 <csong097@uottawa.ca>
2021-06-07 10:40:54 -0400

    Finally got Treemap colours to work

--- client/src/charts/wrapped_nivo/WrappedNivoTreemap/WrappedNivoTreemap.stories.js
+++ client/src/charts/wrapped_nivo/WrappedNivoTreemap/WrappedNivoTreemap.stories.js
@@ -2,8 +2,6 @@ import { scaleOrdinal } from "d3-scale";
 import _ from "lodash";
 import React from "react";
 
-import { Subject } from "src/models/subject";
-
 import { newIBLightCategoryColors } from "src/core/color_schemes";
 import { formats } from "src/core/format";
 
@@ -16,11 +14,13 @@ export default {
 
 const graph_data_values = [
   32432432,
-  2324234,
-  6456354,
-  3456456,
+  8324234,
+  9456354,
+  5795646,
   23746845,
   8723476,
+  23435435,
+  324323,
 ];
 
 const graph_data_total_amt = _.sum(graph_data_values);
@@ -28,70 +28,73 @@ const graph_data_total_amt = _.sum(graph_data_values);
 const graph_data = {
   children: [
     {
-      dept: "124",
       desc: "Description 1",
       id: 0,
       total: graph_data_total_amt,
       value: graph_data_values[0],
     },
     {
-      dept: "124",
       desc: "Description 2",
       id: 1,
       total: graph_data_total_amt,
       value: graph_data_values[1],
     },
     {
-      dept: "128",
       desc: "Description 3",
       id: 2,
       total: graph_data_total_amt,
       value: graph_data_values[2],
     },
     {
-      dept: "128",
       desc: "Description 4",
       id: 3,
       total: graph_data_total_amt,
       value: graph_data_values[3],
     },
     {
-      dept: "46",
       desc: "Description 5",
       id: 4,
       total: graph_data_total_amt,
       value: graph_data_values[4],
     },
     {
-      dept: "237",
       desc: "Description 6",
       id: 5,
       total: graph_data_total_amt,
       value: graph_data_values[5],
+    },
+    {
+      desc: "Description 7",
+      id: 6,
+      total: graph_data_total_amt,
+      value: graph_data_values[6],
+    },
+    {
+      desc: "Description 8",
+      id: 7,
+      total: graph_data_total_amt,
+      value: graph_data_values[7],
     },
   ],
   color: "white",
   name: "root",
 };
 
-const d3_scale = scaleOrdinal(newIBLightCategoryColors);
-// const color_scale = (vs) =>
-//   function (d) {
-//     return d3_scale(text_func(vs, d, ""));
-//   };
+const sorted_children = _.reverse(
+  _.sortBy(graph_data.children, function (o) {
+    return o.value;
+  })
+);
 
-const { Dept } = Subject;
+const domain = [];
+_.forEach(sorted_children, function (child) {
+  _.concat(domain, child.value);
+});
 
-const text_func = (vs, d, break_str) => {
-  if (vs == "voted") {
-    return d.dept
-      ? `${Dept.lookup(d.dept).name} ${break_str}  ${d.desc}`
-      : d.desc;
-  } else {
-    return d.dept
-      ? `${d.desc} ${break_str} ${Dept.lookup(d.dept).name}`
-      : d.desc;
-  }
+const scale = scaleOrdinal().domain(domain).range(newIBLightCategoryColors);
+
+const color_scale = (data) => {
+  return scale(data.value);
 };
 
 const Template = (args) => <WrappedNivoTreemap {...args} />;
@@ -99,7 +102,7 @@ const Template = (args) => <WrappedNivoTreemap {...args} />;
 export const Basic = Template.bind({});
 Basic.args = {
   data: graph_data,
-  colorScale: d3_scale,
+  colorScale: color_scale,
   formatter: formats.compact1,
   label_id: "desc",
 };
