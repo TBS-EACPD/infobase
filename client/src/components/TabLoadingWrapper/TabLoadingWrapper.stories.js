47901399dd5578965ae196142f55843132b8b406
Patrick <phuan097@uottawa.ca>
2021-06-07 10:57:49 -0400

    Implemented LabeledTable into storybook, TabLoadingWrapper load_data doesn't work

--- client/src/components/TabLoadingWrapper/TabLoadingWrapper.stories.js
+++ client/src/components/TabLoadingWrapper/TabLoadingWrapper.stories.js
@@ -1,5 +1,7 @@
+import _ from "lodash";
 import React from "react";
 
+import { TabbedContent } from "./TabbedContent";
 import { TabLoadingWrapper } from "./TabLoadingWrapper";
 
 export default {
@@ -8,15 +10,24 @@ export default {
   argTypes: {},
 };
 
-const Template = (props) => {
-  function load_data(data) {
-    return data;
-  }
+const Template = (args) => {
+  // function load_data(data) {
+  //   return data ? data : "Null";
+  // }
 
-  return <TabLoadingWrapper {...props} load_data={load_data} />;
+  const promise = new Promise((resolve, reject) => {
+    const data = "Some data";
+    resolve(data);
+  });
+
+  return (
+    <TabLoadingWrapper
+      {...args}
+      load_data={promise}
+      TabbedContent={<TabbedContent {...args} data={promise} />}
+    />
+  );
 };
 
 export const Basic = Template.bind({});
-Basic.args = {
-  args: {},
-};
+Basic.args = {};
---
+++ client/src/components/TabLoadingWrapper/TabbedContent.js
@@ -0,0 +1,14 @@
+import _ from "lodash";
+import React from "react";
+
+const TabbedContent = ({ args, data }) => {
+  // {
+  //   _.map(data, (text, key) => {
+  //     <div key={key}>{text}</div>;
+  //   });
+  // }
+
+  <div>{data}</div>;
+};
+
+export { TabbedContent };
