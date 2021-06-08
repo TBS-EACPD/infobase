4020a397208b1894ae9017b478e1deba460c3048
Stephen-ONeil <stephenlevioneil@gmail.com>
2021-06-08 14:47:47 -0400

    Move runtime polyfills to pre-bootstrapper import in the root.js load order, will need these loaded before attempting to execute bootstrapper(true for the Intl.NumberFormat polyfill, likely true as often as not for any future polyfills)

--- client/src/InfoBase/root.js
+++ client/src/InfoBase/root.js
@@ -1,8 +1,12 @@
 import pre_load from "./pre_load";
 
 const kill_spinner = pre_load();
-import("./bootstrapper").then(({ bootstrapper }) => {
-  import("./App").then(({ App, app_reducer }) => {
-    bootstrapper(App, app_reducer, kill_spinner);
-  });
-});
+import("./runtime_polyfills").then(({ runtime_polyfills }) =>
+  runtime_polyfills().then(() => {
+    import("./bootstrapper").then(({ bootstrapper }) => {
+      import("./App").then(({ App, app_reducer }) => {
+        bootstrapper(App, app_reducer, kill_spinner);
+      });
+    });
+  })
+);
