daacf215e6277071c88f7cab1fe0ab98e5ecae43
Stephen-ONeil <stephenlevioneil@gmail.com>
2021-06-08 16:33:14 -0400

    Bit of an optimization in client entry point, import bootstrapper.js and App.js in parallel.

--- client/src/InfoBase/root.js
+++ client/src/InfoBase/root.js
@@ -1,12 +1,14 @@
 import pre_load from "./pre_load";
 
 const kill_spinner = pre_load();
-import("./runtime_polyfills").then(({ runtime_polyfills }) =>
-  runtime_polyfills().then(() => {
-    import("./bootstrapper").then(({ bootstrapper }) => {
-      import("./App").then(({ App, app_reducer }) => {
-        bootstrapper(App, app_reducer, kill_spinner);
-      });
-    });
-  })
-);
+
+import("./runtime_polyfills")
+  .then(({ runtime_polyfills }) => runtime_polyfills())
+  .then(
+    Promise.all([
+      import("./bootstrapper"),
+      import("./App"),
+    ]).then(([{ bootstrapper }, { App, app_reducer }]) =>
+      bootstrapper(App, app_reducer, kill_spinner)
+    )
+  );
