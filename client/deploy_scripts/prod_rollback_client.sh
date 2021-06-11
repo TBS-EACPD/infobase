6e068102d69cca79ebc0b29d5d30c327914ce310
Stephen-ONeil <stephenlevioneil@gmail.com>
2021-06-11 11:10:21 -0400

    Make client prod_rollback_client.sh resilient against any deploys that may exit mid-staging

--- client/deploy_scripts/prod_rollback_client.sh
+++ client/deploy_scripts/prod_rollback_client.sh
@@ -4,11 +4,15 @@ set -e # will exit if any command has non-zero exit value
 source ./deploy_scripts/set_transient_secrets.sh
 source ./deploy_scripts/create_prod_env_vars.sh
 
+# Move current prod to staging as swap location (staging SHOULD already be current prod,
+# this is just to cover the case where a deploy aborted mid-stage since the current prod was deployed)
+gsutil -m rsync -a public-read -r -d $ROLLBACK_BUCKET $PROD_BUCKET
+
 # Move rollback bucket contents to prod
 gsutil -m rsync -a public-read -r -d $ROLLBACK_BUCKET $PROD_BUCKET
 
-# Move staging contents to rollback (staging would be the same as prod before rollback was run)
-# NOTE: this means a consequitive rollback rolls back the first rollback
+# Move staging (previous prod) contents to rollback
+# NOTE: this means consequitive rollbacks, without any new deploys inbetween, will toggle between the current and previous deploys
 gsutil -m rsync -a public-read -r -d $STAGING_BUCKET $ROLLBACK_BUCKET
 
 #clear certain cloudflare caches
