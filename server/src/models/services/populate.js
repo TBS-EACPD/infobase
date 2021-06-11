fcc57f94a84eabe8f413cd663986464e85118ea2
Taewan Kang <tkang042@uottawa.ca>
2021-06-11 10:33:14 -0400

    dept and program refactor and cleanups

--- server/src/models/services/populate.js
+++ server/src/models/services/populate.js
@@ -24,22 +24,8 @@ export default async function ({ models }) {
     ServiceReport,
     StandardReport,
     GovServiceSummary,
-    DeptServiceGeneralStats,
-    ProgramServiceGeneralStats,
-    DeptServiceTypeSummary,
-    ProgramServiceTypeSummary,
-    DeptServiceDigitalStatusSummary,
-    ProgramServiceDigitalStatusSummary,
-    DeptServiceIdMethodsSummary,
-    ProgramServiceIdMethodsSummary,
-    DeptServiceStandardsSummary,
-    ProgramServiceStandardsSummary,
-    DeptServiceFeesSummary,
-    ProgramServiceFeesSummary,
-    DeptTopServicesApplicationVolSummary,
-    ProgramTopServicesApplicationVolSummary,
-    DeptTopServicesWebsiteVisitsSummary,
-    ProgramTopServicesWebsiteVisitsSummary,
+    OrgServiceSummary,
+    ProgramServiceSummary,
   } = models;
 
   const service_report_rows = _.map(
@@ -203,20 +189,6 @@ export default async function ({ models }) {
     });
     return result;
   };
-  const dept_general_stats = _.chain(service_rows)
-    .groupBy("org_id")
-    .map((services, org_id) => ({
-      id: org_id,
-      number_of_services: services.length,
-    }))
-    .value();
-  const program_general_stats = _.chain(service_rows)
-    .reduce(group_by_program_id, {})
-    .map((services, org_id) => ({
-      id: org_id,
-      number_of_services: services.length,
-    }))
-    .value();
 
   const service_types_lookup = _.chain(
     get_standard_csv_file_rows("service_types_lookup.csv")
@@ -236,14 +208,6 @@ export default async function ({ models }) {
         value,
       }))
       .value();
-  const dept_type_summary = _.chain(service_rows)
-    .groupBy("org_id")
-    .flatMap(populate_type_summary)
-    .value();
-  const program_type_summary = _.chain(service_rows)
-    .reduce(group_by_program_id, {})
-    .flatMap(populate_type_summary)
-    .value();
   const get_current_status_count = (services, key, value) =>
     _.countBy(services, `${key}_status`)[value] || 0;
   const populate_digital_summary_key = (services, subject_id, level, key) => ({
@@ -255,26 +219,6 @@ export default async function ({ models }) {
     cannot_online: get_current_status_count(services, key, false),
     not_applicable: get_current_status_count(services, key, null),
   });
-
-  const dept_service_digital_summary = _.chain(service_rows)
-    .groupBy("org_id")
-    .flatMap((services, org_id) =>
-      _.flatMap(digital_status_keys, (key) =>
-        populate_digital_summary_key(services, org_id, "dept", key)
-      )
-    )
-    .sortBy("can_online")
-    .value();
-  const program_service_digital_summary = _.chain(service_rows)
-    .reduce(group_by_program_id, {})
-    .flatMap((services, program_id) =>
-      _.flatMap(digital_status_keys, (key) =>
-        populate_digital_summary_key(services, program_id, "program", key)
-      )
-    )
-    .sortBy("can_online")
-    .value();
-
   const get_id_method_count = (services, subject_id) =>
     _.chain(["sin_collected", "cra_business_ids_collected"])
       .map((method) =>
@@ -326,17 +270,6 @@ export default async function ({ models }) {
       }, [])
       .value();
 
-  const dept_id_methods_summary = _.chain(service_rows)
-    .groupBy("org_id")
-    .flatMap((services, org_id) => get_id_method_count(services, org_id))
-    .value();
-  const program_id_methods_summary = _.chain(service_rows)
-    .reduce(group_by_program_id, {})
-    .flatMap((services, program_id) =>
-      get_id_method_count(services, program_id)
-    )
-    .value();
-
   const get_services_w_standards_count = (services) =>
     _.chain(services)
       .countBy("standards")
@@ -362,14 +295,6 @@ export default async function ({ models }) {
       met_standards_count: _.countBy(processed_standards, "is_target_met").true,
     };
   };
-  const dept_standards_summary = _.chain(service_rows)
-    .groupBy("org_id")
-    .flatMap(get_final_standards_summary)
-    .value();
-  const program_standards_summary = _.chain(service_rows)
-    .reduce(group_by_program_id, {})
-    .flatMap(get_final_standards_summary)
-    .value();
 
   const get_fees_summary = (services, subject_id) => {
     const fees_count = _.countBy(services, "collects_fees");
@@ -388,14 +313,6 @@ export default async function ({ models }) {
       },
     ];
   };
-  const dept_fees_summary = _.chain(service_rows)
-    .groupBy("org_id")
-    .flatMap(get_fees_summary)
-    .value();
-  const program_fees_summary = _.chain(service_rows)
-    .reduce(group_by_program_id, {})
-    .flatMap(get_fees_summary)
-    .value();
 
   const get_top_application_vol_summary = (services, subject_id) => {
     return _.chain(services)
@@ -417,15 +334,6 @@ export default async function ({ models }) {
       .takeRight(10)
       .value();
   };
-
-  const dept_top_application_vol_summary = _.chain(service_rows)
-    .groupBy("org_id")
-    .flatMap(get_top_application_vol_summary)
-    .value();
-  const program_top_application_vol_summary = _.chain(service_rows)
-    .reduce(group_by_program_id, {})
-    .flatMap(get_top_application_vol_summary)
-    .value();
 
   const gov_services_high_volume_summary = _.chain(service_rows)
     .groupBy("org_id")
@@ -486,17 +394,6 @@ export default async function ({ models }) {
             .value() || 0,
       }))
       .value();
-
-  const dept_top_website_visits_summary = _.chain(service_rows)
-    .groupBy("org_id")
-    .flatMap(get_top_website_visits_summary)
-    .thru(post_process_website_visits)
-    .value();
-  const program_top_website_visits_summary = _.chain(service_rows)
-    .reduce(group_by_program_id, {})
-    .flatMap(get_top_website_visits_summary)
-    .thru(post_process_website_visits)
-    .value();
   const gov_summary = [
     {
       id: "gov",
@@ -520,38 +417,72 @@ export default async function ({ models }) {
       top_services_website_visits_summary: gov_top_website_visits_summary,
     },
   ];
-
+  const org_summary = _.chain(service_rows)
+    .groupBy("org_id")
+    .flatMap((services, org_id) => ({
+      id: org_id,
+      service_general_stats: {
+        id: org_id,
+        number_of_services: services.length,
+      },
+      service_type_summary: populate_type_summary(services, org_id),
+      service_digital_status_summary: _.chain(digital_status_keys)
+        .flatMap((key) =>
+          populate_digital_summary_key(services, org_id, "dept", key)
+        )
+        .sortBy("can_online")
+        .value(),
+      service_id_methods_summary: get_id_method_count(services, org_id),
+      service_fees_summary: get_fees_summary(services, org_id),
+      service_standards_summary: get_final_standards_summary(services, org_id),
+      top_services_application_vol_summary: get_top_application_vol_summary(
+        services,
+        org_id
+      ),
+      top_services_website_visits_summary: _.chain()
+        .thru(() => get_top_website_visits_summary(services, org_id))
+        .thru(post_process_website_visits)
+        .value(),
+    }))
+    .value();
+  const program_summary = _.chain(service_rows)
+    .reduce(group_by_program_id, {})
+    .flatMap((services, program_id) => ({
+      id: program_id,
+      service_general_stats: {
+        id: program_id,
+        number_of_services: services.length,
+      },
+      service_type_summary: populate_type_summary(services, program_id),
+      service_digital_status_summary: _.chain(digital_status_keys)
+        .flatMap((key) =>
+          populate_digital_summary_key(services, program_id, "program", key)
+        )
+        .sortBy("can_online")
+        .value(),
+      service_id_methods_summary: get_id_method_count(services, program_id),
+      service_fees_summary: get_fees_summary(services, program_id),
+      service_standards_summary: get_final_standards_summary(
+        services,
+        program_id
+      ),
+      top_services_application_vol_summary: get_top_application_vol_summary(
+        services,
+        program_id
+      ),
+      top_services_website_visits_summary: _.chain()
+        .thru(() => get_top_website_visits_summary(services, program_id))
+        .thru(post_process_website_visits)
+        .value(),
+    }))
+    .value();
   return await Promise.all([
-    GovServiceSummary.insertMany(gov_summary),
     ServiceReport.insertMany(service_report_rows),
     StandardReport.insertMany(standard_report_rows),
     ServiceStandard.insertMany(service_standard_rows),
     Service.insertMany(service_rows),
-    DeptServiceGeneralStats.insertMany(dept_general_stats),
-    ProgramServiceGeneralStats.insertMany(program_general_stats),
-    DeptServiceTypeSummary.insertMany(dept_type_summary),
-    ProgramServiceTypeSummary.insertMany(program_type_summary),
-    DeptServiceDigitalStatusSummary.insertMany(dept_service_digital_summary),
-    ProgramServiceDigitalStatusSummary.insertMany(
-      program_service_digital_summary
-    ),
-    DeptServiceIdMethodsSummary.insertMany(dept_id_methods_summary),
-    ProgramServiceIdMethodsSummary.insertMany(program_id_methods_summary),
-    DeptServiceStandardsSummary.insertMany(dept_standards_summary),
-    ProgramServiceStandardsSummary.insertMany(program_standards_summary),
-    DeptServiceFeesSummary.insertMany(dept_fees_summary),
-    ProgramServiceFeesSummary.insertMany(program_fees_summary),
-    DeptTopServicesApplicationVolSummary.insertMany(
-      dept_top_application_vol_summary
-    ),
-    ProgramTopServicesApplicationVolSummary.insertMany(
-      program_top_application_vol_summary
-    ),
-    DeptTopServicesWebsiteVisitsSummary.insertMany(
-      dept_top_website_visits_summary
-    ),
-    ProgramTopServicesWebsiteVisitsSummary.insertMany(
-      program_top_website_visits_summary
-    ),
+    GovServiceSummary.insertMany(gov_summary),
+    OrgServiceSummary.insertMany(org_summary),
+    ProgramServiceSummary.insertMany(program_summary),
   ]);
 }
