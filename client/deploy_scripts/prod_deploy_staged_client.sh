b6f1c901ed78dfb92ab8dc8c5cac9aa1a02d7e5a
Stephen-ONeil <stephenlevioneil@gmail.com>
2021-06-11 13:29:22 -0400

    Set missing env vars in client prod_deploy_staged_client.sh

--- client/deploy_scripts/prod_deploy_staged_client.sh
+++ client/deploy_scripts/prod_deploy_staged_client.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -e # will exit if any command has non-zero exit value
 
+source ./deploy_scripts/set_transient_secrets.sh
+source ./deploy_scripts/create_prod_env_vars.sh
+
 #within the GCloud bucket, backup the live prod directory (/InfoBase) to the /rollback dir, rsync /staging to /InfoBase
 #leaves us a safe version to roll back to in /rollback, and minimizes the mid-deploy downtime for the InfoBase
 gsutil -m rsync -a public-read -r -d $PROD_BUCKET $ROLLBACK_BUCKET
