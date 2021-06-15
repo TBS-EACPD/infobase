2a9f92246d01a93593102f8cafea63abc51ffb57
Stephen-ONeil <stephenlevioneil@gmail.com>
2021-06-15 17:19:39 -0400

    Clean up some legacy styles and html structure from the leaf spinner

--- client/src/components/LeafSpinner/pre_react_leaf_spinner.js
+++ client/src/components/LeafSpinner/pre_react_leaf_spinner.js
@@ -1,24 +1,22 @@
-import leaf_loading_spinner from "./leaf-spinner.svg";
+import leaf_loading_spinner from "./LeafSpinner.svg";
 import "./LeafSpinner.scss";
 
 export const start_spinner = () => {
   const app_el = document.querySelector("#app");
 
-  const containerDiv = document.createElement("div");
-  containerDiv.className = "faded-loading-container--true";
-  containerDiv.style.position = "fixed";
+  const spinner_container = document.createElement("div");
+  spinner_container.className = "leaf-spinner-container";
+  spinner_container.setAttribute(
+    "style",
+    "transform: scale(2); position: fixed"
+  );
+  spinner_container.innerHTML = leaf_loading_spinner;
 
-  const spinnerDiv = document.createElement("div");
-  spinnerDiv.className = "leaf-spinner-container";
-  spinnerDiv.setAttribute("style", "transform: scale(2); position: fixed");
-  spinnerDiv.innerHTML = leaf_loading_spinner;
-
-  containerDiv.appendChild(spinnerDiv);
-  app_el.appendChild(containerDiv);
+  app_el.appendChild(spinner_container);
   app_el.setAttribute("aria-busy", "true");
 
   const stop_spinner = () => {
-    app_el.removeChild(containerDiv);
+    app_el.removeChild(spinner_container);
     app_el.removeAttribute("aria-busy");
   };
 
