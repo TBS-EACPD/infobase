67eda846d252e74bad2f7853cd50995334f8a3d9
Stephen-ONeil <stephenlevioneil@gmail.com>
2021-06-18 16:09:19 -0400

    Trivial ts-lint assign_to_dev_helper_namespace.ts

--- client/src/core/assign_to_dev_helper_namespace.ts
+++ client/src/core/assign_to_dev_helper_namespace.ts
@@ -1,12 +1,14 @@
 import { is_dev, is_dev_link } from "./injected_build_constants";
 
-const assign_to_dev_helper_namespace = (dev_helpers: Object) => {
+const assign_to_dev_helper_namespace = (
+  dev_helpers: Record<string, unknown>
+) => {
   if (!(is_dev || is_dev_link)) {
     return null;
   }
 
   if (!window.__DEV) {
-    window.__DEV = {};
+    window.__DEV = {} as Record<string, unknown>;
   }
 
   Object.assign(window.__DEV, dev_helpers);
