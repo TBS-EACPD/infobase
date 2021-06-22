e93ee73096492730ed95b7de1a0ce69d4fba0e54
Stephen-ONeil <stephenlevioneil@gmail.com>
2021-06-22 13:52:04 -0400

    Minor linting and a lot of explanatory commenting on KnownKeys util type

--- client/src/types/util_types.d.ts
+++ client/src/types/util_types.d.ts
@@ -1,14 +1,29 @@
 // by Klaus Meinhardt @ajafff
 // known from Gerrit Birkeland @Gerrit0
+// comments by @stephen-oneil, so blame me if I've interpeted it wrong
 // https://github.com/Microsoft/TypeScript/issues/25987#issuecomment-408339599
 // https://github.com/microsoft/TypeScript/issues/25987#issuecomment-441224690
+
 export type KnownKeys<T> = {
-  [K in keyof T]: string extends K ? never : number extends K ? never : K;
-} extends { [_ in keyof T]: infer U } // eslint-disable-line no-unused-vars
-  ? {} extends U
+  // map key: value to key: key OR key: never in the case where key is an index signature (e.g. [key: string], [key: number])
+  // future proofed against symbol index types, not released but on master as of https://github.com/microsoft/TypeScript/pull/44512
+  [key in keyof T]: string extends key
     ? never
-    : U
-  : never;
+    : number extends key
+    ? never
+    : symbol extends key
+    ? never
+    : key;
+} extends {
+  // conditional used to infer union  the value types from the left hand side of this extends
+  // (where the value types are either the keys or never, and never is excluded from the infered union)
+  [_ in keyof T]: infer KnowableKeys;
+}
+  ? // above line will infer a type of {} if T has no keys, not what we want. Return never instead
+    {} extends KnowableKeys // eslint-disable-line @typescript-eslint/ban-types
+    ? never
+    : KnowableKeys
+  : never; // not reachable, but conditional expression necessary for construction with desired inference of KnowableKeys
 
 export type WithoutIndexTypes<T> = Pick<T, KnownKeys<T>>;
 
