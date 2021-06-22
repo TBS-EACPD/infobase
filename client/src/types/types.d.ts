88174b7ff50c1b4a5b837c75232fae553180bacc
Stephen-ONeil <stephenlevioneil@gmail.com>
2021-06-22 14:08:25 -0400

    Specify stricter type for yaml TextBundle imports

--- client/src/types/types.d.ts
+++ client/src/types/types.d.ts
@@ -1,5 +1,12 @@
 interface TextBundle {
-  [key: string]: Object;
+  [key: string]: {
+    transform?:
+      | ["handlebars"]
+      | ["markdown"]
+      | ["handlebars", "markdown"]
+      | ["handlebars", "embeded-markdown", "markdown"]; // TODO embeded-markdown transform is legacy, hacky. Remove at some point
+  };
+  [(key in "en") | "fr" | "text"]: string;
 }
 declare module "*.yaml" {
   let val: TextBundle;
