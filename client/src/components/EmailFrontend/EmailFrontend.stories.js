131859e0b90507847f9345860624b2f59f865441
Patrick <phuan097@uottawa.ca>
2021-06-23 17:56:37 -0400

    Installed fetch-mock npm library

--- client/src/components/EmailFrontend/EmailFrontend.stories.js
+++ client/src/components/EmailFrontend/EmailFrontend.stories.js
@@ -1,4 +1,7 @@
+import fetchMock from "fetch-mock";
 import React from "react";
+
+import { is_dev, local_ip, is_ci } from "src/core/injected_build_constants";
 
 import { EmailFrontend } from "./EmailFrontend";
 
@@ -8,6 +11,17 @@ export default {
 };
 
 const Template = (args) => {
+  // const email_backend_url =
+  //   is_dev && !is_ci ? `https://${local_ip || "127.0.0.1"}:7331` : null;
+  // : "https://us-central1-report-a-problem-email-244220.cloudfunctions.net/prod-email-backend";
+
+  // Mocks http requests made using fetch, since storybook doesn't allow server calls directly
+  // fetchMock.get(
+  //   `https://${
+  //     local_ip || "127.0.0.1"
+  //   }:7331/email_template?template_name=feedback_simplified`,
+  //   200
+  // );
   const on_submitted = () => {
     console.log("Submitted");
   };
