423301c9588bae4f694b9bf5149f9e5b21cb174f
IvanFeng1 <feng.ivan.1235@gmail.com>
2021-06-24 15:29:42 -0400

    FancyUL.stories to TS

--- client/src/components/FancyUL/FancyUL.tsx
+++ client/src/components/FancyUL/FancyUL.tsx
@@ -4,7 +4,7 @@ import React from "react";
 
 import "./FancyUL.scss";
 
-interface FancyULProps {
+export interface FancyULProps {
   className: string;
   title?: string;
   TitleComponent?: React.ComponentType;
