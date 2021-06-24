85e111cb94cb8a42446570e6df5435be911a8942
IvanFeng1 <feng.ivan.1235@gmail.com>
2021-06-24 15:45:44 -0400

    TabbedContent.stories to TS

---
+++ client/src/components/TabbedContent/TabbedContent.stories.tsx
@@ -0,0 +1,31 @@
+import { Story, Meta } from "@storybook/react";
+
+import React from "react";
+
+import { TabbedContent, TabbedContentProps } from "./TabbedContent";
+
+export default {
+  title: "TabbedContent",
+  component: TabbedContent,
+} as Meta;
+
+const Template: Story<TabbedContentProps> = (args) => (
+  <TabbedContent {...args} />
+);
+
+export const Basic = Template.bind({});
+Basic.args = {
+  tab_keys: ["NA", "SA", "EU"],
+  tab_labels: {
+    NA: "North America",
+    SA: "South America",
+    EU: "Europe",
+  },
+  tab_pane_contents: {
+    NA: <div>Surrounded by the Arctic, Atlantic, and Pacific Oceans </div>,
+    SA: <div>Surrounded by the Atlantic and Pacific Oceans</div>,
+    EU: <div>Surrounded by the Arctic and Atlantic Oceans</div>,
+  },
+  disabled_tabs: ["EU"],
+  disabled_message: "",
+};
