cbcda067354f98bdf89f270692aa0b09d32e9ccf
Taewan Kang <tkang042@uottawa.ca>
2021-07-19 13:07:36 -0400

    call general stats more explicit

--- client/src/panels/panel_declarations/services/services_fees.js
+++ client/src/panels/panel_declarations/services/services_fees.js
@@ -24,6 +24,10 @@ const ServicesFeesPanel = ({ subject }) => {
   const { loading, data } = useSummaryServices({
     subject,
     query_fragment: `
+    service_general_stats{
+      id
+      number_of_services
+    }
     service_fees_summary {
       id
       label
