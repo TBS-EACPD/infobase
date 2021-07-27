69f2974055211c19067526bed480f74da49fd6f1
Stephen-ONeil <stephenlevioneil@gmail.com>
2021-07-27 16:26:29 -0400

    Bunch of cleanup in DisplayTable to drop Date as a CellValue type and tweak the default sorting. Need to update a bunch of consumers of the component now

--- client/src/components/DisplayTable/sort_utils.js
+++ client/src/components/DisplayTable/sort_utils.js
@@ -1,10 +1,10 @@
 import { Subject } from "src/models/subject";
 const { Dept } = Subject;
 
-export const sort_func_template = (a_name, b_name) => {
-  if (a_name < b_name) {
+export const sort_func_template = (a, b) => {
+  if (a < b) {
     return -1;
-  } else if (a_name > b_name) {
+  } else if (a > b) {
     return 1;
   }
   return 0;
