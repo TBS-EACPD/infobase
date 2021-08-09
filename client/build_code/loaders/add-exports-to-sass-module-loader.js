a4a7c61fb0e204268df98f393c20b83298605eb6
Stephen-ONeil <stephenlevioneil@gmail.com>
2021-08-09 13:08:39 -0400

    Add custom loader to abstract away the need for :export syntax in sass modules, assume all top level variables should always be exported and insert the syntax only at compile time

---
+++ client/build_code/loaders/add-exports-to-sass-module-loader.js
@@ -0,0 +1,60 @@
+const path = require("path");
+
+const _ = require("lodash");
+
+const loader_name = path.basename(__filename); // eslint-disable-line no-undef
+
+/* 
+  TODO prettier will ensure most top level sass variable definitions have no trailing white space BUT there are
+  some exemptions (e.g. variables defined inside sass control flow blocks). That's a gotcha, but probably not worth
+  accounting for right now. Reasonable for the convention within sass modules to be avoiding dynamic top level variables,
+  as that will make reasoning about common style variables more complex
+*/
+const sass_variable_pattern = /^\$(.*?):.*/;
+
+const get_top_level_variable_names = (content) =>
+  _.chain(content)
+    .split("\n")
+    .filter((line) => sass_variable_pattern.test(line))
+    .map((export_line) => export_line.replace(sass_variable_pattern, "$1"))
+    .value();
+
+const test_variable_type_cases = (scss_module_path, variable_names) => {
+  const non_camel_case_names = _.filter(
+    variable_names,
+    (name) => _.camelCase(name) !== name
+  );
+
+  if (!_.isEmpty(non_camel_case_names)) {
+    throw new Error(
+      `${scss_module_path}: for consistency and to ensure interopability between SCSS modules and JS, top level SCSS variables (and preferably all SCSS variables) must be in camelCase. Check the following: ${_.join(
+        non_camel_case_names,
+        "\n  "
+      )}`
+    );
+  }
+};
+
+const get_generated_export_block = (variable_names) =>
+  _.chain(variable_names)
+    .map((name) => `  ${name}: $${name};`)
+    .join("\n")
+    .thru(
+      (exports) =>
+        `// variable exports added by ${loader_name}\n:export {\n${exports}\n}`
+    )
+    .value();
+
+module.exports = function (content) {
+  const top_level_variable_names = get_top_level_variable_names(content);
+  debugger;
+  if (!_.isEmpty(top_level_variable_names)) {
+    test_variable_type_cases(this.resourcePath, top_level_variable_names);
+
+    return `${content}\n${get_generated_export_block(
+      top_level_variable_names
+    )}`;
+  } else {
+    return content;
+  }
+};
