c669a2cb4468db2dc8c053ba1e47016d62a14aa3
Patrick <phuan097@uottawa.ca>
2021-08-11 09:46:24 -0400

    Created LegendContainer.js

--- client/src/charts/legends/LegendContainer.js
+++ client/src/charts/legends/LegendContainer.js
@@ -1,1 +1,8 @@
 import React from "react";
+
+import { StandardLegend } from "./StandardLegend";
+
+export const LegendContainer = ({ title, Controls, legendListProps }) => (
+  // { items, isHorizontal, onClick, checkBoxProps }
+  <StandardLegend title={title} legendListProps={legendListProps} />
+);
