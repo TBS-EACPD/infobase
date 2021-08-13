801cb3aefc8694224843fa8fca3039765367f417
csong202 <csong097@uottawa.ca>
2021-08-13 12:16:59 -0400

    consumers must provide text_maker to StandardFAQ

--- client/src/components/StandardFAQ/StandardFAQ.js
+++ client/src/components/StandardFAQ/StandardFAQ.js
@@ -3,49 +3,17 @@ import React from "react";
 
 import { SomeThingsToKeepInMind } from "src/panels/panel_declarations/common_panel_components";
 
-import common_lang from "src/panels/panel_declarations/misc/key_concept_panels/common_questions.yaml";
-
-import fin_lang from "src/panels/panel_declarations/misc/key_concept_panels/financial_questions.yaml";
-import ppl_lang from "src/panels/panel_declarations/misc/key_concept_panels/people_questions.yaml";
-import results_lang from "src/panels/panel_declarations/misc/key_concept_panels/results_questions.yaml";
-import tag_lang from "src/panels/panel_declarations/misc/key_concept_panels/tagging_questions.yaml";
-
 import { KeyConceptList } from "src/components/KeyConceptList/KeyConceptList";
-import { create_text_maker_component } from "src/components/misc_util_components";
-import sample_lang from "src/components/StandardFAQ/StandardFAQ.yaml";
 
 import { Gov } from "src/models/organizational_entities";
 
 import { secondaryColor } from "src/style_constants/colors.interop.scss";
 
-import common_subapp_lang from "./common_faq_questions.yaml";
-import est_lang from "./estimates_comparison_questions.yaml";
-import igoc_lang from "./igoc_explorer_questions.yaml";
-import table_picker_lang from "./table_picker_questions.yaml";
-import tag_exp_lang from "./tag_explorer_questions.yaml";
-import text_diff_lang from "./text_diff_questions.yaml";
-import treemap_lang from "./treemap_questions.yaml";
-
-const { TM } = create_text_maker_component([
-  common_lang,
-  fin_lang,
-  ppl_lang,
-  results_lang,
-  tag_lang,
-  est_lang,
-  igoc_lang,
-  table_picker_lang,
-  tag_exp_lang,
-  text_diff_lang,
-  treemap_lang,
-  common_subapp_lang,
-  sample_lang,
-]);
-
 export class StandardFAQ extends React.Component {
   render() {
     const {
-      rendered_q_a_keys,
+      q_a_base_keys,
+      TM,
       is_initially_expanded,
       background_color,
       subject,
@@ -57,7 +25,7 @@ export class StandardFAQ extends React.Component {
         background_color={background_color}
       >
         <KeyConceptList
-          question_answer_pairs={_.map(rendered_q_a_keys, (base_text_key) => [
+          question_answer_pairs={_.map(q_a_base_keys, (base_text_key) => [
             <TM key={"q"} k={base_text_key + "_q"} args={{ subject }} />,
             <TM key={"a"} k={base_text_key + "_a"} args={{ subject }} />,
           ])}
--- client/src/components/StandardFAQ/StandardFAQ.stories.tsx
+++ client/src/components/StandardFAQ/StandardFAQ.stories.tsx
@@ -1,9 +1,17 @@
 import { Story, Meta } from "@storybook/react";
 import React from "react";
 
+import common_lang from "src/panels/panel_declarations/misc/key_concept_panels/common_questions.yaml";
+
+import { create_text_maker_component } from "src/components/misc_util_components";
+
 import { ComponentProps } from "src/types/util_types.d";
 
 import { StandardFAQ } from "./StandardFAQ";
+
+import common_subapp_lang from "./common_faq_questions.yaml";
+
+import sample_lang from "./StandardFAQ.yaml";
 
 export default {
   title: "StandardFAQ",
@@ -14,9 +22,16 @@ type StandardFAQProps = ComponentProps<typeof StandardFAQ>;
 
 const Template: Story<StandardFAQProps> = (args) => <StandardFAQ {...args} />;
 
+const { TM } = create_text_maker_component([
+  sample_lang,
+  common_lang,
+  common_subapp_lang,
+]);
+
 const q_a_keys = ["question1", "question2", "question3"];
 
 export const Basic = Template.bind({});
 Basic.args = {
-  rendered_q_a_keys: q_a_keys,
+  q_a_base_keys: q_a_keys,
+  TM: TM,
 };
