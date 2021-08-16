1cbc24159e76549a2b4e7fa7e6476ca3ec2846b5
csong202 <csong097@uottawa.ca>
2021-08-16 12:10:23 -0400

    StandardFAQ to ts, minor adjustment to KeyConceptList

--- client/src/components/StandardFAQ/StandardFAQ.tsx
+++ client/src/components/StandardFAQ/StandardFAQ.tsx
@@ -6,7 +6,17 @@ import { KeyConceptList } from "src/components/KeyConceptList/KeyConceptList";
 
 import { secondaryColor } from "src/style_constants/colors.interop.scss";
 
-export class StandardFAQ extends React.Component {
+type StandardFAQProps = {
+  faq_content: [React.ReactNode, React.ReactNode][];
+  is_initially_expanded?: boolean;
+  background_color?: string;
+} & typeof StandardFAQ.defaultProps;
+
+export class StandardFAQ extends React.Component<StandardFAQProps> {
+  static defaultProps = {
+    background_color: secondaryColor,
+    is_initially_expanded: false,
+  };
   render() {
     const { faq_content, is_initially_expanded, background_color } = this.props;
 
@@ -20,7 +30,3 @@ export class StandardFAQ extends React.Component {
     );
   }
 }
-StandardFAQ.defaultProps = {
-  background_color: secondaryColor,
-  is_initially_expanded: false,
-};
--- client/src/components/StandardFAQ/faq_utils.js
+++ client/src/components/StandardFAQ/faq_utils.js
@@ -8,12 +8,8 @@ import { Gov } from "src/models/organizational_entities";
 export function faq_content_maker(text_bundles, q_a_base_keys, subject = Gov) {
   const { TM } = create_text_maker_component(text_bundles.bundles);
 
-  return (
-    <>
-      {_.map(q_a_base_keys, (base_text_key) => [
-        <TM key={"q"} k={base_text_key + "_q"} args={{ subject }} />,
-        <TM key={"a"} k={base_text_key + "_a"} args={{ subject }} />,
-      ])}
-    </>
-  );
+  return _.map(q_a_base_keys, (base_text_key) => [
+    <TM key={"q"} k={base_text_key + "_q"} args={{ subject }} />,
+    <TM key={"a"} k={base_text_key + "_a"} args={{ subject }} />,
+  ]);
 }
