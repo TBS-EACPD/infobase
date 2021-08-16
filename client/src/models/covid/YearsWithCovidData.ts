76c7fbc56fd4d524770915a1d37c609e715b7f41
Stephen-ONeil <stephenlevioneil@gmail.com>
2021-08-16 16:09:58 -0400

    Clean up YearsWithCovidData, no need for a class, just plain objects

--- client/src/models/covid/YearsWithCovidData.ts
+++ client/src/models/covid/YearsWithCovidData.ts
@@ -10,24 +10,18 @@ type YearsWithCovidDataDef = {
   };
 };
 
-class YearsWithCovidData {
+type YearsWithCovidDataInst = {
   id: string;
   subject_id: string;
   years_with_estimates: number[];
   years_with_expenditures: number[];
-
-  constructor({
-    subject_id,
-    years_with_covid_data: { years_with_estimates, years_with_expenditures },
-  }: YearsWithCovidDataDef) {
-    this.id = subject_id;
-    this.subject_id = subject_id;
-    this.years_with_estimates = years_with_estimates;
-    this.years_with_expenditures = years_with_expenditures;
-  }
-}
+};
 
 export const yearsWithCovidDataStore = StaticStoreFactory<
   YearsWithCovidDataDef,
-  YearsWithCovidData
->((def: YearsWithCovidDataDef) => new YearsWithCovidData(def));
+  YearsWithCovidDataInst
+>(({ subject_id, years_with_covid_data }: YearsWithCovidDataDef) => ({
+  id: subject_id,
+  subject_id,
+  ...years_with_covid_data,
+}));
