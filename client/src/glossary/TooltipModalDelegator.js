f39f7b12b69dfcb77213f6f00ca0454120667842
csong202 <csong097@uottawa.ca>
2021-09-13 13:14:55 -0400

    modal shows glossary item definition

--- client/src/glossary/TooltipModalDelegator.js
+++ client/src/glossary/TooltipModalDelegator.js
@@ -1,9 +1,10 @@
 import _ from "lodash";
 import React from "react";
 import ReactDOM from "react-dom";
-import ReactDOMServer from "react-dom/server";
 
-import { ModalButton, StatelessModal } from "src/components/index";
+import { get_glossary_item_tooltip_html } from "src/models/glossary";
+
+import { StatelessModal } from "src/components/index";
 
 export class TooltipModalDelegator extends React.Component {
   replaceWithModalBtn(e) {
@@ -12,86 +13,52 @@ export class TooltipModalDelegator extends React.Component {
     if (!target) {
       return;
     }
-    console.log("target");
+    console.log("\ntarget");
     console.log(target);
     console.dir(_.pickBy(target, (attr) => !!attr && !_.isFunction(attr)));
-
-    console.log("nextSibling");
-    console.log(target.nextSibling);
     console.log("parentElement");
-    console.log(target.parentElement);
     console.dir(
       _.pickBy(target.parentElement, (attr) => !!attr && !_.isFunction(attr))
     );
 
-    // TODO: maybe just use target.firstChild.data? serach up best practices for a11y link titles
-    const link_title = "";
-
     const glossary_item_key = target.dataset.ibttGlossaryKey;
 
-    const modal_content = (
-      <a
-        href={`https://www.tbs-sct.gc.ca/ems-sgd/edb-bdd/index-basic-eng.html#glossary/${
-          glossary_item_key || ""
-        }`}
-        title={link_title}
-      ></a>
+    const glossary_def_html = get_glossary_item_tooltip_html(glossary_item_key);
+
+    let glossary_def = document.createElement("div");
+    glossary_def.innerHTML = glossary_def_html;
+    console.log("glossary_def");
+    console.log(glossary_def);
+    console.dir(
+      _.pickBy(glossary_def, (attr) => !!attr && !_.isFunction(attr))
     );
+    console.log("textContent:", glossary_def.textContent);
 
     function toggle_modal(bool) {
-      this.setState({ showModal: bool });
+      console.log("toggle_modal");
+      //   this.setState({ showModal: bool });
     }
 
-    // ---- ATTEMPT 2 ----
-
     const modal = React.createElement(StatelessModal, {
+      // not sure why, but modal only shows up when show is string
       show: "false",
-      body: modal_content,
+      body: glossary_def.innerText, // using glossary_def gives an error
+      on_close_callback: () => toggle_modal(false),
     });
+
+    console.log("new child");
+    console.dir(
+      _.pickBy(
+        ReactDOM.render(modal, document.createElement("span")),
+        (attr) => !!attr && !_.isFunction(attr)
+      )
+    );
+
     target.removeAttribute("data-ibtt-glossary-key");
     target.removeAttribute("data-toggle");
-    target.appendChild(ReactDOM.render(modal, document.createElement("span")));
-
-    // ---- ATTEMPT 1 ----
-
-    // const replacement = (
-    //   <ModalButton
-    //     title={""}
-    //     button_text={target.firstChild.data}
-    //     show_condition={{ name: "tbd", value: "" }} // TODO: figure out the show_condition
-    //   >
-    //     {children}
-    //   </ModalButton>
-    // );
-
-    // const replacement = (
-    //   <>
-    //     <button onClick={() => toggle_modal(true)}>
-    //       <p>{target.firstChild.data}</p>
-    //     </button>
-    //     <StatelessModal show={false} body={modal_content}></StatelessModal>
-    //   </>
-    // );
-    // console.log("replacement");
-    // console.log(replacement);
-    // const replacement_node = ReactDOM.render(
-    //   replacement,
-    //   document.createElement("span")
-    // );
-    // console.log(replacement_node.innerHTML);
-    // console.log(ReactDOMServer.renderToString(replacement));
-
-    // const parent_node = target.parentNode; //TODO: handle cases with no parent node (don't think that will happen?)
-
-    // const new_span = document.createElement("span");
-
-    // target.insertAdjacentHTML("afterend", new_span);
-
-    // new_span.appendChild(
-    //   ReactDOM.render(replacement, document.createElement("span"))
-    // );
-
-    // parent_node.removeChild(target);
+    target.replaceChildren(
+      ReactDOM.render(modal, document.createElement("span"))
+    );
   }
 
   componentDidMount() {
