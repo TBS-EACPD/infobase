5c326f008f3ff9c311a554f21b9ddda8fa3c7985
Stephen-ONeil <stephenlevioneil@gmail.com>
2021-09-21 18:07:41 -0400

    A more solid approach to the hacky Gov subject special case, since legacy code wants to treat it as both a subject class and an instance interchangeably instead make it a singelton instance (with all the normal static methods as instance methods, and still pretending to be a class in some cases). Keeping the class type case for the name for now

--- client/src/models/structure/Gov.ts
+++ client/src/models/structure/Gov.ts
@@ -3,9 +3,15 @@ import _ from "lodash";
 import { trivial_text_maker } from "src/models/text";
 import { BaseSubjectFactory } from "src/models/utils/BaseSubjectFactory";
 
+/*
+  Special case subject! Bit of slight of hand, Gov pretends to be both a subject and a subject instance
+  (in reality it's actually a single instance of a not-exported class, but all the normal static subject class methods,
+  including a mocked out store, are accessible from the instance).
+*/
+
 const gov_id = "gov";
 
-const get_gov = _.memoize(() => new Gov({ id: gov_id }));
+const get_gov = _.memoize(() => new _Gov({ id: gov_id }));
 
 const invalid_gov_store_method = () => {
   throw new Error(
@@ -13,38 +19,28 @@ const invalid_gov_store_method = () => {
   );
 };
 
-// TODO big pain, a lot of legacy code wants both the class and instance of Gov to be interchangeable,
-// have to do some trickery with doubling up static and instance decorators and mocking up a store
-export class Gov extends BaseSubjectFactory(
+class _Gov extends BaseSubjectFactory(
   "gov",
   trivial_text_maker("goc"),
   trivial_text_maker("goc")
 ) {
-  static store = {
+  // mocked store, note that it's not static because it will always be referenced from the one Gov instance
+  store = {
     lookup: get_gov,
     get_all: () => [get_gov()],
     create: invalid_gov_store_method,
     create_and_register: invalid_gov_store_method,
   };
 
-  static id = gov_id;
-
-  static description = trivial_text_maker("the_goc");
-  get description() {
-    return Gov.description;
-  }
-
-  static legal_title = trivial_text_maker("goc");
-  get legal_title() {
-    return Gov.legal_title;
-  }
-
-  static has_planned_spending = true;
-  get has_planned_spending() {
-    return Gov.has_planned_spending;
-  }
+  id = gov_id;
+  name = trivial_text_maker("goc");
+  description = trivial_text_maker("the_goc");
+  legal_title = trivial_text_maker("goc");
+  has_planned_spending = true;
 
   constructor(def: { id: string }) {
     super(def);
   }
 }
+
+export const Gov = get_gov();
