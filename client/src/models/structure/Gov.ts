6ea09cdf5a848215c987458131d0bd90777b29cf
Stephen-ONeil <stephenlevioneil@gmail.com>
2021-09-22 10:09:33 -0400

    Small cleanup in Gov.ts

--- client/src/models/structure/Gov.ts
+++ client/src/models/structure/Gov.ts
@@ -4,14 +4,10 @@ import { trivial_text_maker } from "src/models/text";
 import { BaseSubjectFactory } from "src/models/utils/BaseSubjectFactory";
 
 /*
-  Special case subject! Bit of slight of hand, Gov pretends to be both a subject and a subject instance
-  (in reality it's actually a single instance of a not-exported class, but all the normal static subject class methods,
-  including a mocked out store, are accessible from the instance).
+  Special case subject! Bit of slight of hand, to fit legacy assumptions Gov pretends to be both a subject class and a subject instance
+  (in reality it's actually a single instance of a not-exported class, but all the normal static subject class methods, including a mocked 
+  out store, are accessible from the instance).
 */
-
-const gov_id = "gov";
-
-const get_gov = _.memoize(() => new _Gov({ id: gov_id }));
 
 const invalid_gov_store_method = () => {
   throw new Error(
@@ -26,13 +22,13 @@ class _Gov extends BaseSubjectFactory(
 ) {
   // mocked store, note that it's not static because it will always be referenced from the one Gov instance
   store = {
-    lookup: get_gov,
-    get_all: () => [get_gov()],
+    lookup: () => this,
+    get_all: () => [this],
     create: invalid_gov_store_method,
     create_and_register: invalid_gov_store_method,
   };
 
-  id = gov_id;
+  id: string;
   name = trivial_text_maker("goc");
   description = trivial_text_maker("the_goc");
   legal_title = trivial_text_maker("goc");
@@ -40,7 +36,9 @@ class _Gov extends BaseSubjectFactory(
 
   constructor(def: { id: string }) {
     super(def);
+
+    this.id = def.id;
   }
 }
 
-export const Gov = get_gov();
+export const Gov = new _Gov({ id: "gov" });
