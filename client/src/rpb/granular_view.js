0852a49a330c918abcc299b2d67c6a2c520c62a4
csong202 <csong097@uottawa.ca>
2021-09-25 17:00:35 -0400

    set unsorted_initial=false

--- client/src/rpb/granular_view.js
+++ client/src/rpb/granular_view.js
@@ -200,7 +200,7 @@ class GranularView extends React.Component {
         data={table_data}
         column_configs={column_configs}
         util_components={display_table_custom_util}
-        unsorted_initial={true}
+        unsorted_initial={false}
       />
     );
   }
