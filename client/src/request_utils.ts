7a17ad13b8fe65f9f3b1d9061fa73d8015da3bbf
Stephen-ONeil <stephenlevioneil@gmail.com>
2021-09-27 16:10:12 -0400

    Port populate_stores.ts. Could use much more robust data validation though

--- client/src/request_utils.ts
+++ client/src/request_utils.ts
@@ -1,7 +1,7 @@
 import { cdn_url, sha } from "src/core/injected_build_constants";
 
 //no URL should start with "./" or "/"
-export const get_static_url = (url: string, version_query: string) => {
+export const get_static_url = (url: string, version_query?: string) => {
   const query_string = version_query || sha;
 
   return `${cdn_url}/${url}?v=${query_string}`;
