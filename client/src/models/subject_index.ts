9f79a5459ca38baecb1d544d046dc01eecc90d68
Stephen-ONeil <stephenlevioneil@gmail.com>
2021-09-27 20:18:12 -0400

    Simplify and fix the temporary type monkey buisness in get_subject_by_guid

--- client/src/models/subject_index.ts
+++ client/src/models/subject_index.ts
@@ -45,24 +45,14 @@ export const get_subject_by_guid = (guid: string) => {
 
   const subject = _.chain(Subject)
     .values()
-    .find((subject) => {
-      if (!("subject_type" in subject)) {
-        // SUBJECT_TS_TODO type guard for, theoretically, unreachable case. Should be removable once Result and Indicator are updated and in TS
-        throw new Error(
-          "A Subject was missing the subject_type property. This SHOULD be a temporary and unreachable type guard that can be cleaned up once Result and Indicator are modernized"
-        );
-      }
-      return subject.subject_type === type;
-    })
+    .find({ subject_type: type })
     .value();
 
   if (subject) {
-    if ("store" in subject) {
-      subject.store.lookup(id);
-    } else {
-      // SUBJECT_TS_TODO shim for legacy Result and Indicator subjects
-      return subject.lookup(id);
-    }
+    // SUBJECT_TS_TODO temporary shim and loss of type safety until legacy Result and Indicator subjects ar ported to TS and the new Subject factory
+    return _.has(subject, "store.lookup")
+      ? (subject as any).store.lookup(id) // eslint-disable-line @typescript-eslint/no-explicit-any
+      : (subject as any).lookup(id); // eslint-disable-line @typescript-eslint/no-explicit-any
   }
 };
 
