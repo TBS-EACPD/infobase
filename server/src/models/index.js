027fe4e1c92d66cbb4fc63869f5d19ef5050aaf5
Stephen-ONeil <stephenlevioneil@gmail.com>
2021-10-14 16:19:10 -0400

    Full fresh npm 8 installs and audits for email_backend and server, upgrade and sort out some deprecations in the server dependencies

--- server/src/models/index.js
+++ server/src/models/index.js
@@ -1,4 +1,4 @@
-import { makeExecutableSchema } from "graphql-tools";
+import { makeExecutableSchema } from "@graphql-tools/schema";
 import _ from "lodash";
 
 import core_subject from "./core_subject/index.js";
