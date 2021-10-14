1ed2ae52e2e17bdd0aaf65eb1f85cad497c3008e
Stephen-ONeil <stephenlevioneil@gmail.com>
2021-10-14 19:42:55 -0400

    Couldn't get useBuiltIns: usage to work, entry seems to work partially although the site hits a react error right now (enough other dependencies were changed whiel the prod builds were broken that I can't say for sure if that's related)

--- client/src/InfoBase/root.js
+++ client/src/InfoBase/root.js
@@ -1,3 +1,6 @@
+import "core-js";
+import "regenerator-runtime/runtime";
+
 import { start_spinner } from "src/components/LeafSpinner/pre_react_leaf_spinner";
 
 const stop_spinner = start_spinner();
