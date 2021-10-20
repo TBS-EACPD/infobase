d348fb1a53246fe07e7917287e2be185efde567f
Stephen-ONeil <stephenlevioneil@gmail.com>
2021-10-20 16:36:17 -0400

    Delete pre_react_leaf_spinner.ts, move the code directly in to root.js, need to be clear that it needs to lightweight and safe to run pre-pollyfill

--- client/src/InfoBase/root.js
+++ client/src/InfoBase/root.js
@@ -1,8 +1,23 @@
 import "core-js/stable/promise";
 
-import { start_spinner } from "src/components/LeafSpinner/pre_react_leaf_spinner";
+import "src/components/LeafSpinner/LeafSpinner.scss";
+import leaf_loading_spinner from "src/components/LeafSpinner/LeafSpinner.svg";
 
-const stop_spinner = start_spinner();
+// Important reminder: this initial spinner setup is pre-pollyfilling! Keep it simple
+
+const spinner_container = document.createElement("div");
+spinner_container.className = "leaf-spinner-container";
+spinner_container.setAttribute("style", "transform: scale(2); position: fixed");
+spinner_container.innerHTML = leaf_loading_spinner;
+
+const app_el = document.querySelector("#app");
+app_el.appendChild(spinner_container);
+app_el.setAttribute("aria-busy", "true");
+
+const stop_spinner = () => {
+  app_el.removeChild(spinner_container);
+  app_el.removeAttribute("aria-busy");
+};
 
 import("./core_polyfills")
   .then(import("./dynamic_polyfills"))
