a458ab16383dbb1d17e01e1d45caaf9701dd7e66
Stephen-ONeil <stephenlevioneil@gmail.com>
2021-10-22 18:46:17 -0400

    Enable caching for client webpack eslint plugin, add .eslintcache outputs to .gitignore

--- client/build_code/webpack_common.js
+++ client/build_code/webpack_common.js
@@ -166,7 +166,7 @@ function get_plugins({
       IS_CI: JSON.stringify(is_ci),
       LOCAL_IP: JSON.stringify(local_ip),
     }),
-    new ESLintPlugin({ extensions: ["js", "ts", "tsx"] }),
+    new ESLintPlugin({ extensions: ["js", "ts", "tsx"], cache: true }),
     new ForkTsCheckerWebpackPlugin({
       async: true,
       typescript: { configFile: "tsconfig.json" },
