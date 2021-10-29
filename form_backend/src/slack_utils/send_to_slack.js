15b8f2c29e077cccbaec24c359d2590bf71a0a53
Stephen-ONeil <stephenlevioneil@gmail.com>
2021-10-29 10:37:14 -0400

    Clean up and rename form_backend deploy env var names

--- form_backend/src/slack_utils/send_to_slack.js
+++ form_backend/src/slack_utils/send_to_slack.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
 export const send_to_slack = (message) => {
-  if (process.env.FORM_SUBMITTED_BOT_SERVICE_LINK) {
+  if (process.env.SLACK_BOT_SERVICE_LINK) {
     return axios.post(
-      process.env.FORM_SUBMITTED_BOT_SERVICE_LINK,
+      process.env.SLACK_BOT_SERVICE_LINK,
       { text: message, mrkdwn: false },
       {
         headers: { "content-type": "application/json" },
