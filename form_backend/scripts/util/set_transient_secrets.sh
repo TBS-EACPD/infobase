c132ecbd8611a34d89f537ddd8d511c5794657c8
Stephen-ONeil <stephenlevioneil@gmail.com>
2021-10-29 10:37:14 -0400

    Clean up and rename form_backend deploy env var names

--- form_backend/scripts/util/set_transient_secrets.sh
+++ form_backend/scripts/util/set_transient_secrets.sh
@@ -15,47 +15,34 @@ function cleanup {
 
   unset PROJECT
 
-  unset EMAIL_BACKEND_SENDING_ADDRESS
-  unset EMAIL_BACKEND_RECEIVING_ADDRESS
-  unset EMAIL_BACKEND_CLIENT_ID
-  unset EMAIL_BACKEND_CLIENT_SECRET
-  unset EMAIL_BACKEND_REFRESH_TOKEN
+  unset MDB_NAME
+  unset MDB_CONNECT_STRING
+  unset MDB_USERNAME
+  unset MDB_PW
 
-  unset FORM_SUBMITTED_BOT_SERVICE_LINK
+  unset FORM_BACKEND_SLACK_BOT_SERVICE_LINK
 }
 trap cleanup EXIT
 
 
-echo $(lpass show EMAIL_BACKEND_SERVICE_KEY --notes) | base64 --decode > $scratch/key.json
+echo $(lpass show FORM_BACKEND_SERVICE_KEY --notes) | base64 --decode > $scratch/key.json
 gcloud auth activate-service-account --key-file=$scratch/key.json
 
-export PROJECT=$(lpass show EMAIL_BACKEND_PROJECT_ID --notes)
+export PROJECT=$(lpass show FORM_BACKEND_PROJECT_ID --notes)
 
 touch $scratch/envs.yaml
 echo "IS_PROD_SERVER: '$IS_PROD_SERVER'" >> $scratch/envs.yaml
 echo "USE_REMOTE_DB: '$USE_REMOTE_DB'" >> $scratch/envs.yaml
 echo "CURRENT_SHA: '$CURRENT_SHA'" >> $scratch/envs.yaml
 
-export EMAIL_BACKEND_SENDING_ADDRESS=$(lpass show EMAIL_BACKEND_SENDING_ADDRESS --notes)
-export EMAIL_BACKEND_RECEIVING_ADDRESS=$(lpass show EMAIL_BACKEND_RECEIVING_ADDRESS --notes)
-export EMAIL_BACKEND_CLIENT_ID=$(lpass show EMAIL_BACKEND_CLIENT_ID --notes)
-export EMAIL_BACKEND_CLIENT_SECRET=$(lpass show EMAIL_BACKEND_CLIENT_SECRET --notes)
-export EMAIL_BACKEND_REFRESH_TOKEN=$(lpass show EMAIL_BACKEND_REFRESH_TOKEN --notes)
-echo "EMAIL_BACKEND_SENDING_ADDRESS: '$EMAIL_BACKEND_SENDING_ADDRESS'" >> $scratch/envs.yaml
-echo "EMAIL_BACKEND_RECEIVING_ADDRESS: '$EMAIL_BACKEND_RECEIVING_ADDRESS'" >> $scratch/envs.yaml
-echo "EMAIL_BACKEND_CLIENT_ID: '$EMAIL_BACKEND_CLIENT_ID'" >> $scratch/envs.yaml
-echo "EMAIL_BACKEND_CLIENT_SECRET: '$EMAIL_BACKEND_CLIENT_SECRET'" >> $scratch/envs.yaml
-echo "EMAIL_BACKEND_REFRESH_TOKEN: '$EMAIL_BACKEND_REFRESH_TOKEN'" >> $scratch/envs.yaml
-
-
-export MDB_NAME=$(lpass show EMAIL_BACKEND_MDB_NAME --notes)
-export MDB_CONNECT_STRING=$(lpass show EMAIL_BACKEND_MDB_CONNECT_STRING_3 --notes)
-export MDB_USERNAME=$(lpass show EMAIL_BACKEND_MDB_USER --notes)
-export MDB_PW=$(lpass show EMAIL_BACKEND_MDB_PW --notes)
+export MDB_NAME=$(lpass show FORM_BACKEND_MDB_NAME --notes)
+export MDB_CONNECT_STRING=$(lpass show FORM_BACKEND_MDB_CONNECT_STRING_3 --notes)
+export MDB_USERNAME=$(lpass show FORM_BACKEND_MDB_USER --notes)
+export MDB_PW=$(lpass show FORM_BACKEND_MDB_PW --notes)
 echo "MDB_NAME: '$MDB_NAME'" >> $scratch/envs.yaml
 echo "MDB_CONNECT_STRING: '$MDB_CONNECT_STRING'" >> $scratch/envs.yaml
 echo "MDB_USERNAME: '$MDB_USERNAME'" >> $scratch/envs.yaml
 echo "MDB_PW: '$MDB_PW'" >> $scratch/envs.yaml
 
-export FORM_SUBMITTED_BOT_SERVICE_LINK=$(lpass show FORM_SUBMITTED_BOT_SERVICE_LINK --notes)
-echo "FORM_SUBMITTED_BOT_SERVICE_LINK: '$FORM_SUBMITTED_BOT_SERVICE_LINK'" >> $scratch/envs.yaml
+export FORM_BACKEND_SLACK_BOT_SERVICE_LINK=$(lpass show FORM_BACKEND_SLACK_BOT_SERVICE_LINK --notes)
+echo "SLACK_BOT_SERVICE_LINK: '$SLACK_BOT_SERVICE_LINK'" >> $scratch/envs.yaml
