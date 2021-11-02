ada31251a07cca1fbd752dd067e305d7cc280897
Stephen-ONeil <stephenlevioneil@gmail.com>
2021-11-02 10:42:20 -0400

    Fix module resolution problem in generate_typescript_progress_stats.js. Move depcruise execution in to the script and clean up output location

--- client/scripts/typescript_progress/generate_typescript_progress_stats.js
+++ client/scripts/typescript_progress/generate_typescript_progress_stats.js
@@ -3,17 +3,23 @@
 This script is to be run in the client/ dir and requires a dependencies.json file to be created by dep-cruiser:
 
 1. npm install --global dependency-cruiser
-2. depcruise -T json -f dependencies.json --exclude node_modules/  src/
-3. node generate_typescript_progress_stats.js
-4. open up typescript_progress_stats.csv, give it a quick check and paste it in a NEW sheet on the google drive document. Name the sheet based on today's date.
+2. from /client, node scripts/typescript_progress/generate_typescript_progress_stats.js
+3. open up scripts/typescript_progress/typescript_progress_stats.csv, give it a quick check and paste it in a NEW sheet on the google drive document. Name the sheet based on today's date.
   - new sheet is recommended because pasting it over the existing sheet might remove important existing metadata of who's working on what. We could periodically delete old sheets.
 
 */
 
+const { execSync } = require("child_process");
 const fs = require("fs");
 
 const d3_dsv = require("d3-dsv");
 const _ = require("lodash");
+
+fs.mkdirSync(`${__dirname}/output`, { recursive: true });
+
+execSync(
+  `depcruise -T json -f ${__dirname}/output/dependencies.json --webpack-config ${__dirname}/webpack-config-standin.json --ts-config tsconfig.json --exclude node_modules/ src/`
+);
 
 const excludedRe = /\.yaml|\.scss|\.css|\.csv/;
 const isJavaScriptRe = /\.js/;
@@ -34,7 +40,9 @@ const getAllDeps = _.memoize(function (fileName) {
   return new Set([...children.map((c) => c.source), ...descendants]);
 }, _.identity);
 
-const file = fs.readFileSync("./dependencies.json").toString();
+const file = fs
+  .readFileSync(`${__dirname}/output/dependencies.json`)
+  .toString();
 const json = JSON.parse(file);
 json.modules.forEach((node) => {
   if (node.source.match(excludedRe)) {
@@ -62,5 +70,7 @@ const final = _.chain(recordsByName)
   .sortBy("name")
   .value();
 
-fs.writeFileSync("./typescript_progress_stats.csv", d3_dsv.csvFormat(final));
-fs.rmSync("./dependencies.json");
+fs.writeFileSync(
+  `${__dirname}/output/typescript_progress_stats.csv`,
+  d3_dsv.csvFormat(final)
+);
