0a52699e89e8a39918506db547893df81396ba94
Stephen-ONeil <stephenlevioneil@gmail.com>
2021-11-10 17:35:57 -0500

    Fix client unit test config. Still basic (lots of mocks to set up) but works

--- client/jest_configs/integration_tests.config.js
+++ client/jest_configs/integration_tests.config.js
@@ -2,7 +2,7 @@ const { common_config_rules } = require("./common_config");
 
 // TODO, these will need to run additional pre-test setup for the global systems like text_maker+handlebars, etc
 
-module.export = {
+module.exports = {
   ...common_config_rules,
 
   testRegex: "\\.integration-test\\.js$",
