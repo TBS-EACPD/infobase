aeee9566cb9104ab2f22106ba1de8ddc9232d5dc
csong202 <csong097@uottawa.ca>
2021-11-15 15:05:45 -0500

    undid all the changes to table sorting

--- client/src/components/DisplayTable/DisplayTable.tsx
+++ client/src/components/DisplayTable/DisplayTable.tsx
@@ -128,7 +128,7 @@ const get_default_state_from_props = (props: _DisplayTableProps) => {
   return {
     visible_col_keys,
     sort_by,
-    descending: true,
+    descending: false,
     searches,
     initial_props: props,
     page_size: props.page_size_increment,
@@ -164,9 +164,10 @@ export class _DisplayTable extends React.Component<
     if (nextProps !== prevState.initial_props) {
       const new_default_state = get_default_state_from_props(nextProps);
 
-      const prev_sort_still_valid =
-        _.includes(new_default_state.visible_col_keys, prevState.sort_by) &&
-        prevState.sort_by === new_default_state.visible_col_keys[0];
+      const prev_sort_still_valid = _.includes(
+        new_default_state.visible_col_keys,
+        prevState.sort_by
+      );
 
       const reconciled_searches = {
         ...new_default_state.searches,
