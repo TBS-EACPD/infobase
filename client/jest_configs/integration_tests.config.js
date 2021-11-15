5ef75d98e36dd3dfd0bff9ac58e9421a0f84712d
Stephen-ONeil <stephenlevioneil@gmail.com>
2021-11-15 15:01:17 -0500

    Testing empty mock mapper for yaml, csv, and svg modules

--- client/jest_configs/integration_tests.config.js
+++ client/jest_configs/integration_tests.config.js
@@ -1,6 +1,7 @@
 const { common_config_rules } = require("./common_config");
 
 // TODO, these will need to run additional pre-test setup for the global systems like text_maker+handlebars, etc
+// will need to overwrite at least some moduleNameMapper empty mock targets with custom loaders
 
 module.exports = {
   ...common_config_rules,
