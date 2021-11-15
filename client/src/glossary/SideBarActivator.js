b1a6e83308674913fef0a8676cd7e9cdceb1b162
Stephane Rancourt <sranc046@uottawa.ca>
2021-11-14 23:07:36 -0500

    Remove rendering on specific routes

--- client/src/glossary/SideBarActivator.js
+++ client/src/glossary/SideBarActivator.js
@@ -1,89 +1,104 @@
 import React from "react";
 
+import { withRouter } from "react-router-dom";
+
 import { glossaryEntryStore } from "src/models/glossary";
 
 import { BackToTop } from "src/components";
 
 import { GlossaryMenu } from "./GlossaryMenu";
 
-export class SidebarActivator extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      show_sidebar: true,
-      showGlossary: false,
-      glossaryItem: {},
-      showList: true,
-    };
-  }
-  replaceWithModalBtn = (e) => {
-    const target =
-      e.target.getAttribute("data-toggle") === "tooltip"
-        ? e.target
-        : e.target.parentElement.getAttribute("data-toggle") === "tooltip"
-        ? e.target.parentElement
-        : undefined;
+const ROUTES_WITHOUT_GLOSSARY = {
+  "/start": true,
+  "/glossary": true,
+};
 
-    if (!target) {
-      return;
+const SidebarActivator = withRouter(
+  class SidebarActivator extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        show_sidebar: true,
+        showGlossary: false,
+        glossaryItem: {},
+        showList: true,
+      };
+    }
+    replaceWithModalBtn = (e) => {
+      const target =
+        e.target.getAttribute("data-toggle") === "tooltip"
+          ? e.target
+          : e.target.parentElement.getAttribute("data-toggle") === "tooltip"
+          ? e.target.parentElement
+          : undefined;
+
+      if (!target) {
+        return;
+      }
+
+      const glossary_item_key = target.dataset.ibttGlossaryKey;
+      const glossary_item = glossaryEntryStore.lookup(glossary_item_key);
+
+      this.setGlossaryItem(glossary_item.id);
+      this.showList(false);
+
+      this.toggleGlossary(true);
+    };
+
+    componentDidMount() {
+      window.addEventListener("click", this.replaceWithModalBtn);
     }
 
-    const glossary_item_key = target.dataset.ibttGlossaryKey;
-    const glossary_item = glossaryEntryStore.lookup(glossary_item_key);
+    componentWillUnmount() {
+      window.removeEventListener("click", this.replaceWithModalBtn);
+    }
 
-    this.setGlossaryItem(glossary_item.id);
-    this.showList(false);
+    toggleGlossary(value) {
+      this.setState({
+        showGlossary: value,
+      });
+    }
 
-    this.toggleGlossary(true);
-  };
+    setGlossaryItem(key) {
+      this.setState({
+        glossaryItem: glossaryEntryStore.lookup(key),
+        showList: false,
+      });
+    }
 
-  componentDidMount() {
-    window.addEventListener("click", this.replaceWithModalBtn);
+    showList(value) {
+      this.setState({
+        showList: value,
+      });
+    }
+    render() {
+      const currentPage = this.props.location.pathname;
+
+      if (ROUTES_WITHOUT_GLOSSARY[currentPage]) return null;
+
+      return (
+        <div>
+          <GlossaryMenu
+            show={this.state.showGlossary}
+            toggle={(value) => this.toggleGlossary(value)}
+            item={this.state.glossaryItem}
+            setGlossaryItem={(key) => this.setGlossaryItem(key)}
+            showList={this.state.showList}
+            setList={(value) => this.showList(value)}
+          />
+          <BackToTop
+            focus={() => {
+              document.querySelector(".glossary__search-bar > input").focus();
+            }}
+            handleClick={() => {
+              this.setState({ showGlossary: true });
+            }}
+            text={"glossary_button"}
+          />
+        </div>
+      );
+    }
   }
+);
 
-  componentWillUnmount() {
-    window.removeEventListener("click", this.replaceWithModalBtn);
-  }
-
-  toggleGlossary(value) {
-    this.setState({
-      showGlossary: value,
-    });
-  }
-
-  setGlossaryItem(key) {
-    this.setState({
-      glossaryItem: glossaryEntryStore.lookup(key),
-      showList: false,
-    });
-  }
-
-  showList(value) {
-    this.setState({
-      showList: value,
-    });
-  }
-  render() {
-    return (
-      <div>
-        <GlossaryMenu
-          show={this.state.showGlossary}
-          toggle={(value) => this.toggleGlossary(value)}
-          item={this.state.glossaryItem}
-          setGlossaryItem={(key) => this.setGlossaryItem(key)}
-          showList={this.state.showList}
-          setList={(value) => this.showList(value)}
-        />
-        <BackToTop
-          focus={() => {
-            document.querySelector(".glossary__search-bar > input").focus();
-          }}
-          handleClick={() => {
-            this.setState({ showGlossary: true });
-          }}
-          text={"glossary_button"}
-        />
-      </div>
-    );
-  }
-}
+export { SidebarActivator };
