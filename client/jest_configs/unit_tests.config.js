57d736a0a56c72f026da3ac4f85ed89adb176a9a
Stephen-ONeil <stephenlevioneil@gmail.com>
2021-11-16 19:56:46 -0500

    Cleanup sub-repo git ignores, add more consistency to naming convnetion of commonly ignored directories

--- client/jest_configs/unit_tests.config.js
+++ client/jest_configs/unit_tests.config.js
@@ -4,6 +4,6 @@ module.exports = {
   ...common_config_rules,
 
   testRegex: "\\.unit-test\\.(js|ts|tsx)$",
-  coverageDirectory: "<rootDir>/coverage/unit_tests",
+  coverageDirectory: "<rootDir>/.coverage/unit_tests",
   cacheDirectory: "<rootDir>/.cache/jest/unit_tests",
 };
--- scripts/dev_scripts/local_mongod.sh
+++ scripts/dev_scripts/local_mongod.sh
@@ -17,13 +17,13 @@ if [ -z "$port" ]; then
 fi
 
 
-if [ ! -e ./local_mongo ]; then 
-  mkdir -p ./local_mongo/db
+if [ ! -e ./.local_mongo ]; then 
+  mkdir -p ./.local_mongo/db
 fi
 
 
-if [ -e ./local_mongo/db/mongod.lock ]; then 
-  running_mongod_pid=$(lsof ./local_mongo/db/mongod.lock | awk 'NR == 2 {print $2}')
+if [ -e ./.local_mongo/db/mongod.lock ]; then 
+  running_mongod_pid=$(lsof ./.local_mongo/db/mongod.lock | awk 'NR == 2 {print $2}')
 
   # sticks in loop until the process is actually dead, otherwise it's a race between the 
   # subsequent mongod starting and this old one finishing its cleanup
@@ -34,10 +34,10 @@ fi
 
 
 echo "storage:
-  dbPath: ./local_mongo/db
+  dbPath: ./.local_mongo/db
 net:
   bindIp: 127.0.0.1
   port: $port
-" > ./local_mongo/mongod.conf
+" > ./.local_mongo/mongod.conf
 
-mongod --config ./local_mongo/mongod.conf
+mongod --config ./.local_mongo/mongod.conf
