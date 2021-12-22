ebc09034c1a455d675b47d1b7f74290e0d83aba9
Stephane Rancourt <sranc046@uottawa.ca>
2021-12-21 21:49:38 -0500

    First refactoring of glossary menu/sidebar/activator

--- client/src/glossary/GlossarySidebarContent.tsx
+++ client/src/glossary/GlossarySidebarContent.tsx
@@ -2,27 +2,18 @@ import _ from "lodash";
 import React from "react";
 
 import "./GlossaryMenu.scss";
-import { glossaryEntryStore } from "src/models/glossary";
 
-import { lang } from "src/core/injected_build_constants";
-
-import { IconArrow } from "src/icons/icons";
-import { SearchHighlighter } from "src/search/search_utils";
+import { GlossaryDef } from "./GlossaryDef";
+import { GlossaryList } from "./GlossaryList";
 
 interface SidebarContentProps {
-  title: string | null;
-  def: string | null;
+  title: string;
+  def: string;
   results: ResultProps[];
   closeItem: () => void;
   openItem: (key: string) => void;
   showList: boolean;
   query: string;
-}
-
-interface SidebarContentState {
-  title: string | null;
-  def: string | "";
-  scrollEl: string;
 }
 
 export interface ResultProps {
@@ -32,180 +23,26 @@ export interface ResultProps {
   raw_definition: string;
 }
 
-export class SidebarContent extends React.Component<
-  SidebarContentProps,
-  SidebarContentState
-> {
-  listRef = React.createRef<HTMLDivElement>();
+export class SidebarContent extends React.Component<SidebarContentProps> {
   constructor(props: SidebarContentProps) {
     super(props);
-
-    this.state = {
-      title: null,
-      def: "",
-      scrollEl: "",
-    };
-  }
-
-  static getDerivedStateFromProps(
-    nextProps: SidebarContentProps,
-    prevState: SidebarContentState
-  ) {
-    const { title: next_title, def: next_def } = nextProps;
-    const { title: prev_title } = prevState;
-
-    if (next_title !== prev_title) {
-      return {
-        title: next_title,
-        def: next_def,
-      };
-    } else {
-      return null;
-    }
-  }
-
-  closeDefinition() {
-    this.props.closeItem();
-    if (this.state.title) {
-      this.setState({
-        scrollEl: this.state.title.replace(/\s+/g, ""),
-      });
-    }
-  }
-
-  componentDidUpdate() {
-    if (this.state.scrollEl) {
-      const el = document.getElementById(this.state.scrollEl);
-      const scrollDiv = document.getElementById("gloss-sidebar");
-
-      if (el && scrollDiv) {
-        scrollDiv.scrollTop = el.offsetTop;
-        el.focus();
-      }
-    }
-  }
-
-  openDefinition(item: ResultProps) {
-    this.props.openItem(item.id);
-    this.setState({
-      scrollEl: item.title.replace(/\s+/g, ""),
-    });
-  }
-
-  //place holder arguments while I get passing functions to work...
-  handleKeyPress(
-    e: React.KeyboardEvent<HTMLSpanElement>,
-    action: string,
-    item: ResultProps | null
-  ) {
-    if (e.key === "Enter") {
-      switch (action) {
-        case "close":
-          this.closeDefinition();
-          break;
-
-        case "open":
-          if (item) {
-            this.openDefinition(item);
-          }
-          break;
-      }
-    }
-  }
-
-  get_glossary_items_by_letter() {
-    const glossary_items =
-      this.props.results.length == 0
-        ? glossaryEntryStore.get_all()
-        : this.props.results;
-
-    const glossary_items_by_letter = _.chain(glossary_items)
-      .groupBy((item) => {
-        const first_letter = item.title[0];
-        if (_.includes(["É", "È", "Ê", "Ë"], first_letter)) {
-          return "E";
-        }
-        return first_letter;
-      })
-      .map((items, letter) => {
-        const sorted_items = _.sortBy(items, "title");
-        return {
-          items: sorted_items,
-          letter,
-        };
-      })
-      .sortBy("letter")
-      .value();
-    return glossary_items_by_letter;
   }
 
   render() {
-    const back_text = {
-      en: "Full list",
-      fr: "Liste complète",
-    }[lang];
-
-    const items_by_letter = this.get_glossary_items_by_letter();
     return (
       <div className="glossary-sidebar-content" id="gloss-sidebar">
         {!this.props.showList ? (
-          <div className="defintion-wrapper">
-            <div className="item-title">{this.state.title}</div>
-            <div
-              className="item-def"
-              dangerouslySetInnerHTML={{
-                __html: this.state.def,
-              }}
-            />
-            <div>
-              <span
-                role="button"
-                className="back-button"
-                onClick={() => this.closeDefinition()}
-                onKeyDown={(e) => this.handleKeyPress(e, "close", null)}
-                tabIndex={0}
-              >
-                <IconArrow
-                  rotation={180}
-                  width="25px"
-                  color="white"
-                  alternate_color={false}
-                />
-                {back_text}
-              </span>
-            </div>
-          </div>
+          <GlossaryDef
+            closeItem={this.props.closeItem}
+            title={this.props.title}
+            def={this.props.def}
+          />
         ) : (
-          <div>
-            {_.map(items_by_letter, ({ letter, items }) => (
-              <div key={letter}>
-                <span className="glossary-letter" key={letter}>
-                  {letter}
-                </span>
-                <hr />
-                {_.map(items, (item, ix) => (
-                  <div key={ix} className="glossary-title">
-                    <span
-                      role="button"
-                      id={item.title.replace(/\s+/g, "")}
-                      onClick={() => this.openDefinition(item)}
-                      onKeyDown={(e) => this.handleKeyPress(e, "open", item)}
-                      tabIndex={0}
-                    >
-                      {this.props.query ? (
-                        <SearchHighlighter
-                          search={this.props.query}
-                          content={item.title}
-                        />
-                      ) : (
-                        item.title
-                      )}
-                    </span>
-                  </div>
-                ))}
-              </div>
-            ))}
-          </div>
+          <GlossaryList
+            openItem={this.props.openItem}
+            query={this.props.query}
+            items_by_letter={this.props.results}
+          />
         )}
       </div>
     );
