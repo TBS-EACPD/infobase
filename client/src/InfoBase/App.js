ba9dd989d38fdc2ba0698531bf5eb721ec2a4572
Stephane Rancourt <sranc046@uottawa.ca>
2022-01-06 10:33:37 -0500

    Minor tweaks

--- client/src/InfoBase/App.js
+++ client/src/InfoBase/App.js
@@ -22,7 +22,7 @@ import {
 import { InsertRuntimeFooterLinks } from "src/core/InsertRuntimeFooterLinks";
 import { RedirectHeader } from "src/core/RedirectHeader";
 
-import { SidebarActivator } from "src/glossary/SideBarActivator";
+import { GlossaryMenuController } from "src/glossary/GlossaryMenuController";
 import { make_request } from "src/request_utils";
 import { SurveyPopup } from "src/Survey/SurveyPopup";
 
@@ -155,7 +155,8 @@ export class App extends React.Component {
             url_before_redirect_key="pre_redirected_url"
           />
           {has_local_storage && <SurveyPopup />}
-          <GlossaryMenu />
+          <GlossaryMenuController />
+
           <Suspense fallback={<LeafSpinner config_name={"route"} />}>
             <Switch>
               <Route
