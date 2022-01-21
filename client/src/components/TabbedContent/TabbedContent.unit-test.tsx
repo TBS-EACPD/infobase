277dad963754b7ca4fe0d11380a572880dd5adda
Stephen-ONeil <stephenlevioneil@gmail.com>
2022-01-21 10:56:12 -0500

    Refactor TabbedContent API for better type constraints, update unit tests

--- client/src/components/TabbedContent/TabbedContent.unit-test.tsx
+++ client/src/components/TabbedContent/TabbedContent.unit-test.tsx
@@ -12,16 +12,16 @@ import React from "react";
 import { TabbedContent, TabbedContentStateful } from "./TabbedContent";
 
 describe("TabbedContent", () => {
-  const tabs = _.map(["tab1", "tab2", "tab3"], (key) => ({
-    key,
-    label: key,
-  }));
+  const tabs = _.chain(["tab1", "tab2", "tab3"])
+    .map((key) => [key, key])
+    .fromPairs()
+    .value();
 
   it("Has a tablist containing provided tab items, in the provided order", () => {
     render(
       <TabbedContent
         tabs={tabs}
-        open_tab_key={tabs[0].key}
+        open_tab_key={_.keys(tabs)[0]}
         tab_open_callback={_.noop}
       >
         whatever
@@ -33,7 +33,7 @@ describe("TabbedContent", () => {
     const tab_nodes = getAllByRole(tablist_node, "tab");
     _.each(tab_nodes, (tab_node, index) => {
       expect(tab_node).toContainElement(
-        getByText(tablist_node, tabs[index].label)
+        getByText(tablist_node, tabs[_.keys(tabs)[index]])
       );
     });
   });
@@ -41,7 +41,7 @@ describe("TabbedContent", () => {
     render(
       <TabbedContent
         tabs={tabs}
-        open_tab_key={tabs[0].key}
+        open_tab_key={_.keys(tabs)[0]}
         tab_open_callback={_.noop}
       >
         whatever
@@ -58,7 +58,7 @@ describe("TabbedContent", () => {
     render(
       <TabbedContent
         tabs={tabs}
-        open_tab_key={tabs[0].key}
+        open_tab_key={_.keys(tabs)[0]}
         tab_open_callback={_.noop}
       >
         {content}
@@ -69,34 +69,32 @@ describe("TabbedContent", () => {
     expect(visible_panel).toContainElement(getByText(visible_panel, content));
   });
   it("When a non-current label is clicked, calls tab_open_callback with the corresponding key", () => {
-    const open_tab = tabs[0];
-    const target_tab = tabs[1];
+    const open_tab_key = _.keys(tabs)[0];
+    const target_tab_key = _.keys(tabs)[1];
     const mock_tab_open_callback = jest.fn();
 
     render(
       <TabbedContent
         tabs={tabs}
-        open_tab_key={open_tab.key}
+        open_tab_key={open_tab_key}
         tab_open_callback={mock_tab_open_callback}
       >
         whatever
       </TabbedContent>
     );
 
-    fireEvent.click(getByText(screen.getByRole("tablist"), open_tab.label));
+    const tablist = screen.getByRole("tablist");
+
+    fireEvent.click(getByText(tablist, tabs[open_tab_key]));
     expect(mock_tab_open_callback).toBeCalledTimes(0);
 
-    fireEvent.click(getByText(screen.getByRole("tablist"), target_tab.label));
-    expect(mock_tab_open_callback).toBeCalledWith(target_tab.key);
+    fireEvent.click(getByText(tablist, tabs[target_tab_key]));
+    expect(mock_tab_open_callback).toBeCalledTimes(1);
+    expect(mock_tab_open_callback).toBeCalledWith(target_tab_key);
   });
   it("Implements spec compliant arrow key navigation", () => {
-    const tabs = _.map(["tab1", "tab2", "tab3"], (key) => ({
-      key,
-      label: key,
-    }));
-
     const test_arrow_navigation = (
-      open_tab: { key: string; label: string },
+      open_tab_key: string,
       arrow_key: "ArrowLeft" | "ArrowRight",
       expected_callback_key: string
     ) => {
@@ -107,7 +105,7 @@ describe("TabbedContent", () => {
       const { rerender } = render(
         <TabbedContent
           tabs={tabs}
-          open_tab_key={open_tab.key}
+          open_tab_key={open_tab_key}
           tab_open_callback={mock_tab_open_callback}
         >
           whatever
@@ -118,10 +116,11 @@ describe("TabbedContent", () => {
 
       const current_tab = screen.getByRole("tab", { selected: true });
       expect(current_tab).toContainElement(
-        getByText(tablist_node, open_tab.label)
+        getByText(tablist_node, tabs[open_tab_key])
       );
 
       fireEvent.keyDown(current_tab, { key: arrow_key });
+      expect(mock_tab_open_callback).toBeCalledTimes(1);
       expect(mock_tab_open_callback).toBeCalledWith(expected_callback_key);
 
       // when re-rendering after arrow key selection, focus should be moved to the newly selected tab
@@ -138,42 +137,41 @@ describe("TabbedContent", () => {
       expect(next_tab).toHaveFocus();
     };
 
-    test_arrow_navigation(tabs[0], "ArrowRight", tabs[1].key);
-    test_arrow_navigation(tabs[1], "ArrowRight", tabs[2].key);
-    test_arrow_navigation(tabs[2], "ArrowLeft", tabs[1].key);
-    test_arrow_navigation(tabs[1], "ArrowRight", tabs[2].key);
-    test_arrow_navigation(tabs[2], "ArrowRight", tabs[0].key);
-    test_arrow_navigation(tabs[0], "ArrowLeft", tabs[2].key);
+    test_arrow_navigation(_.keys(tabs)[0], "ArrowRight", _.keys(tabs)[1]);
+    test_arrow_navigation(_.keys(tabs)[1], "ArrowRight", _.keys(tabs)[2]);
+    test_arrow_navigation(_.keys(tabs)[2], "ArrowLeft", _.keys(tabs)[1]);
+    test_arrow_navigation(_.keys(tabs)[1], "ArrowRight", _.keys(tabs)[2]);
+    test_arrow_navigation(_.keys(tabs)[2], "ArrowRight", _.keys(tabs)[0]);
+    test_arrow_navigation(_.keys(tabs)[0], "ArrowLeft", _.keys(tabs)[2]);
   });
 });
 
 describe("TabbedContentStateful", () => {
   it("Updates selected tab, and corresponding visible tabpanel, on click", () => {
-    const stateful_tabs = _.map(["tab1", "tab2", "tab3"], (key) => ({
-      key,
-      label: key,
-      content: key,
-    }));
+    const stateful_tabs = _.chain(["tab1", "tab2", "tab3"])
+      .map((key) => [key, { label: key, content: key }])
+      .fromPairs()
+      .value();
 
     render(<TabbedContentStateful tabs={stateful_tabs} />);
 
     const tablist_node = screen.getByRole("tablist");
 
     const click_tab_and_verify_update = (key: string) => {
-      fireEvent.click(getByText(tablist_node, key));
+      fireEvent.click(getByText(tablist_node, stateful_tabs[key].label));
 
       expect(screen.getByRole("tab", { selected: true })).toContainElement(
-        getByText(tablist_node, key)
+        getByText(tablist_node, stateful_tabs[key].label)
       );
 
       const visible_panel = screen.getByRole("tabpanel", { hidden: false });
       expect(screen.getByRole("tabpanel", { hidden: false })).toContainElement(
-        getByText(visible_panel, key)
+        getByText(visible_panel, stateful_tabs[key].content)
       );
     };
 
-    click_tab_and_verify_update(stateful_tabs[0].key);
-    click_tab_and_verify_update(stateful_tabs[1].key);
-    click_tab_and_verify_update(stateful_tabs[0].key);
+    click_tab_and_verify_update(_.keys(stateful_tabs)[0]);
+    click_tab_and_verify_update(_.keys(stateful_tabs)[1]);
+    click_tab_and_verify_update(_.keys(stateful_tabs)[0]);
   });
 });
