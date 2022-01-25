799d1407fbbc749013cdae65fe3f6c0813a90a5a
csong202 <csong097@uottawa.ca>
2022-01-25 12:53:37 -0500

    fixed file naming mistake, added unit test to check colour of the checkbox

--- client/src/components/CheckBox/CheckBox.unit-test.tsx
+++ client/src/components/CheckBox/CheckBox.unit-test.tsx
@@ -7,14 +7,22 @@ import { with_console_error_silenced } from "src/testing_utils";
 
 import { CheckBox } from "./CheckBox";
 
+const non_solid_checkbox_no_onclick = <CheckBox id={"id"} label={"label"} />;
 const solid_checkbox_no_onclick = (
-  <CheckBox id={"id"} label={"label"} isSolidBox={true} />
+  <CheckBox
+    id={"id"}
+    label={"label"}
+    isSolidBox={true}
+    active={true}
+    color={"#000000"}
+  />
 );
 const solid_checkbox_with_onclick = (
   <CheckBox
     id={"id"}
     label={"sample label"}
     isSolidBox={true}
+    active={false}
     onClick={(_id: string) => console.log("clicked!")}
   />
 );
@@ -22,7 +30,7 @@ const solid_checkbox_with_onclick = (
 describe("CheckBox", () => {
   it('Throws an error when a solid checkbox is missing "active" and "onclick"', () => {
     with_console_error_silenced(() => {
-      expect(() => render(solid_checkbox_no_onclick)).toThrow();
+      expect(() => render(non_solid_checkbox_no_onclick)).toThrow();
     });
   });
 
@@ -41,5 +49,30 @@ describe("CheckBox", () => {
       is_a11y_mode ? "div.checkbox label input" : "div span.link-styled"
     );
     expect(checkbox_span && checkbox_span.textContent).toEqual("sample label");
+  });
+
+  it("Displays a coloured background if onClick is undefined or active is truthy", () => {
+    if (!is_a11y_mode) {
+      render(solid_checkbox_with_onclick);
+      const transparent_checkbox = document.getElementsByClassName(
+        "checkbox-span checkbox-span--interactive"
+      )[0];
+      expect(
+        transparent_checkbox &&
+          window
+            .getComputedStyle(transparent_checkbox)
+            .getPropertyValue("background-color")
+      ).toEqual("transparent");
+
+      render(solid_checkbox_no_onclick);
+      const colored_checkbox =
+        document.getElementsByClassName("checkbox-span")[1];
+      expect(
+        colored_checkbox &&
+          window
+            .getComputedStyle(colored_checkbox)
+            .getPropertyValue("background-color")
+      ).not.toEqual("transparent");
+    }
   });
 });
