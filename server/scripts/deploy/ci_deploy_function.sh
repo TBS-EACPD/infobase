71fb441f3b7347fa5f9a2c89324e49dfda07d654
Stephen-ONeil <stephenlevioneil@gmail.com>
2022-02-03 13:08:44 -0500

    Update GCF deploys to specify node 16 targets

--- server/scripts/deploy/ci_deploy_function.sh
+++ server/scripts/deploy/ci_deploy_function.sh
@@ -9,5 +9,5 @@ echo "MDB_PW: '$MDB_PW'" >> ./envs.yaml
 
 echo "USE_REMOTE_DB: '1'" >> ./envs.yaml
 
-gcloud functions deploy $CIRCLE_BRANCH --entry-point app --stage-bucket api-staging-bucket --runtime nodejs14 --trigger-http --env-vars-file ./envs.yaml
+gcloud functions deploy $CIRCLE_BRANCH --entry-point app --stage-bucket api-staging-bucket --runtime nodejs16 --trigger-http --env-vars-file ./envs.yaml
 gcloud alpha functions add-iam-policy-binding $CIRCLE_BRANCH --member allUsers --role roles/cloudfunctions.invoker
