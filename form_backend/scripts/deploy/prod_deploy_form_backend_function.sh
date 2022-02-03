f2b365d1dc511727961eca30770420abd8469664
Stephen-ONeil <stephenlevioneil@gmail.com>
2022-02-03 13:08:44 -0500

    Update GCF deploys to specify node 16 targets

--- form_backend/scripts/deploy/prod_deploy_form_backend_function.sh
+++ form_backend/scripts/deploy/prod_deploy_form_backend_function.sh
@@ -9,7 +9,7 @@ source scripts/util/set_transient_secrets.sh # these trap EXIT to handle their o
 
 scripts/deploy/build.sh
 
-gcloud beta functions deploy prod-email-backend --max-instances 1 --project ${PROJECT} --region us-central1 --entry-point form_backend --stage-bucket email-backend-staging-bucket --runtime nodejs14 --trigger-http --env-vars-file $scratch/envs.yaml > /dev/null
+gcloud beta functions deploy prod-email-backend --max-instances 1 --project ${PROJECT} --region us-central1 --entry-point form_backend --stage-bucket email-backend-staging-bucket --runtime nodejs16 --trigger-http --env-vars-file $scratch/envs.yaml > /dev/null
 gcloud alpha functions add-iam-policy-binding prod-email-backend --project ${PROJECT} --region us-central1 --member allUsers --role roles/cloudfunctions.invoker
 
 source scripts/util/unset_prod_env_vars.sh
--- server/scripts/deploy/prod_deploy_function.sh
+++ server/scripts/deploy/prod_deploy_function.sh
@@ -9,7 +9,7 @@ source scripts/deploy/set_transient_function_secrets.sh # these trap EXIT to han
 
 scripts/deploy/build.sh
 
-gcloud functions deploy prod-api-${CURRENT_SHA} --project ${PROJECT} --region us-central1 --entry-point app --stage-bucket prod-api-staging-bucket --runtime nodejs14 --trigger-http --env-vars-file $scratch/envs.yaml > /dev/null
+gcloud functions deploy prod-api-${CURRENT_SHA} --project ${PROJECT} --region us-central1 --entry-point app --stage-bucket prod-api-staging-bucket --runtime nodejs16 --trigger-http --env-vars-file $scratch/envs.yaml > /dev/null
 gcloud alpha functions add-iam-policy-binding prod-api-${CURRENT_SHA} --project ${PROJECT} --region us-central1 --member allUsers --role roles/cloudfunctions.invoker
 
 source scripts/deploy/unset_prod_env_vars.sh
