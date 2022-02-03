6acd3afd6e710e0e3852032d42111a36a2f759d7
Stephen-ONeil <stephenlevioneil@gmail.com>
2022-02-03 13:55:49 -0500

    GCF has full support for ESModules now, drop transpilation in the server and form_backend deploys

--- form_backend/scripts/deploy/prod_deploy_form_backend_function.sh
+++ form_backend/scripts/deploy/prod_deploy_form_backend_function.sh
@@ -7,8 +7,6 @@ npm ci
 source scripts/util/set_prod_env_vars.sh
 source scripts/util/set_transient_secrets.sh # these trap EXIT to handle their own cleanup
 
-scripts/deploy/build.sh
-
 gcloud beta functions deploy prod-email-backend --max-instances 1 --project ${PROJECT} --region us-central1 --entry-point form_backend --stage-bucket email-backend-staging-bucket --runtime nodejs16 --trigger-http --env-vars-file $scratch/envs.yaml > /dev/null
 gcloud alpha functions add-iam-policy-binding prod-email-backend --project ${PROJECT} --region us-central1 --member allUsers --role roles/cloudfunctions.invoker
 
