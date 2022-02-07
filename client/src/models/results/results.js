c8b51110f452d3da9963e089790fa8ded393d538
Stephen-ONeil <stephenlevioneil@gmail.com>
2022-02-07 16:45:42 -0500

    Fresh DRR data. ISED submission and ACOA fixes. Drop ISED from the late departments list

--- client/src/models/results/results.js
+++ client/src/models/results/results.js
@@ -374,7 +374,7 @@ const drr_docs = build_doc_info_objects("drr", [
     resource_years: ["{{pa_last_year}}"],
     doc_url_en: "DRR_TODO",
     doc_url_fr: "DRR_TODO",
-    late_results_orgs: ["560", "130", "561"],
+    late_results_orgs: ["560", "561"],
     late_resources_orgs: [],
   },
 ]);
