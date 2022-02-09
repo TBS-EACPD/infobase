05c316eb65a2fe4165b9218d9f25edf597712fc6
Stephen-ONeil <stephenlevioneil@gmail.com>
2022-02-09 15:21:40 -0500

    Accordion unit tests

--- client/src/components/Accordion/AccordionAuto.unit-test.tsx
+++ client/src/components/Accordion/AccordionAuto.unit-test.tsx
@@ -17,21 +17,25 @@ const mocked_trivial_text_maker = trivial_text_maker as jest.MockedFunction<
 mocked_trivial_text_maker.mockImplementation((key: string) => key);
 
 describe("AccordionAuto", () => {
+  const accordion_pass_through_props = {
+    max_height: "200px",
+    title: "test",
+    background_color: "#333",
+  };
+
   it("Starts with children rendered if is_initially_expanded={true}", () => {
     const child_text = "test child";
 
     render(
       <AccordionAuto
         is_initially_expanded={true}
-        max_height="200px"
-        title="test"
-        background_color="#333"
+        {...accordion_pass_through_props}
       >
         <div>{child_text}</div>
       </AccordionAuto>
     );
 
-    expect(screen.queryByText(child_text)).toBeVisible();
+    expect(screen.getByText(child_text)).toBeVisible();
   });
   it("Starts with no children rendered if is_initially_expanded={false}", () => {
     const child_text = "test child";
@@ -39,9 +43,7 @@ describe("AccordionAuto", () => {
     render(
       <AccordionAuto
         is_initially_expanded={false}
-        max_height="200px"
-        title="test"
-        background_color="#333"
+        {...accordion_pass_through_props}
       >
         <div>{child_text}</div>
       </AccordionAuto>
@@ -55,9 +57,7 @@ describe("AccordionAuto", () => {
     render(
       <AccordionAuto
         is_initially_expanded={false}
-        max_height="200px"
-        title="test"
-        background_color="#333"
+        {...accordion_pass_through_props}
       >
         <div>{child_text}</div>
       </AccordionAuto>
