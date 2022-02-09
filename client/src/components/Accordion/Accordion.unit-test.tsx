b7621c47e7a982bda00922d90ccf78982d44dcff
Stephen-ONeil <stephenlevioneil@gmail.com>
2022-02-09 15:21:40 -0500

    Accordion unit tests

---
+++ client/src/components/Accordion/Accordion.unit-test.tsx
@@ -0,0 +1,83 @@
+import {
+  render,
+  screen,
+  fireEvent,
+  waitForElementToBeRemoved,
+} from "@testing-library/react";
+import { iteratee } from "lodash";
+import React from "react";
+
+import { trivial_text_maker } from "src/models/text";
+
+import { Accordion } from "./Accordion";
+
+jest.mock("src/models/text");
+const mocked_trivial_text_maker = trivial_text_maker as jest.MockedFunction<
+  typeof trivial_text_maker
+>;
+mocked_trivial_text_maker.mockImplementation((key: string) => key);
+
+describe("Accordion", () => {
+  const testing_default = {
+    title: "title",
+    children: "child",
+    on_toggle: () => undefined,
+    is_expanded: false,
+  };
+
+  it("Renders title text, regardless of is_expanded", () => {
+    const title_text = "test title";
+
+    const { rerender } = render(
+      <Accordion
+        {...{ ...testing_default, title: title_text, is_expanded: false }}
+      />
+    );
+
+    expect(screen.getByText(title_text)).toBeVisible();
+
+    rerender(
+      <Accordion
+        {...{ ...testing_default, title: title_text, is_expanded: true }}
+      />
+    );
+
+    expect(screen.getByText(title_text)).toBeVisible();
+  });
+
+  it("Renders children when is_expanded is true", () => {
+    const children_text = "test title";
+
+    const { rerender } = render(
+      <Accordion
+        {...{ ...testing_default, children: children_text, is_expanded: false }}
+      />
+    );
+
+    expect(screen.queryByText(children_text)).toBeNull();
+
+    rerender(
+      <Accordion
+        {...{ ...testing_default, children: children_text, is_expanded: true }}
+      />
+    );
+
+    expect(screen.getByText(children_text)).toBeVisible();
+  });
+
+  it("Has buttons, which call on_toggle on interaction", () => {
+    const on_toggle = jest.fn();
+
+    render(<Accordion {...{ ...testing_default, on_toggle }} />);
+
+    const some_accordion_buttons = screen.getAllByRole("button");
+
+    expect(some_accordion_buttons.length > 0).toBeTruthy();
+
+    expect(on_toggle).toHaveBeenCalledTimes(0);
+    some_accordion_buttons.forEach((button, index) => {
+      fireEvent.click(button);
+      expect(on_toggle).toHaveBeenCalledTimes(index + 1);
+    });
+  });
+});
