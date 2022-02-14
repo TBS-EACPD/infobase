d53a071604082d56b3e338ebeb3423a58fba6894
Stephane-Rancourt <sranc046@uottawa.ca>
2022-02-14 10:16:12 -0500

    No results found message rather than just the full list

--- client/src/glossary/GlossarySidebar/GlossarySidebarList.js
+++ client/src/glossary/GlossarySidebar/GlossarySidebarList.js
@@ -4,6 +4,10 @@ import React from "react";
 
 import "./GlossarySidebar.scss";
 
+import { create_text_maker_component } from "src/components/index";
+
+import glossary_text from "src/glossary/glossary.yaml";
+
 import { get_glossary_items_by_letter } from "src/glossary/glossary_utils";
 
 import { glossary_lite as glossary_search_config } from "src/search/search_configs";
@@ -12,6 +16,8 @@ import {
   SearchHighlighter,
   get_simplified_search_phrase,
 } from "src/search/search_utils";
+
+const { text_maker } = create_text_maker_component(glossary_text);
 
 export class GlossaryList extends React.Component {
   constructor(props) {
@@ -26,7 +32,6 @@ export class GlossaryList extends React.Component {
   }
 
   static getDerivedStateFromProps(nextProps, prevState) {
-    console.log("maybe");
     const { search_phrase: next_phrase } = nextProps;
     const { search_phrase: prev_phrase } = prevState;
 
@@ -148,34 +153,40 @@ export class GlossaryList extends React.Component {
 
     return (
       <div>
-        {_.map(items_by_letter, ({ letter, items }) => (
-          <div key={letter}>
-            <span className="glossary-sb__letter" key={letter}>
-              {letter}
-            </span>
-            <hr />
-            {_.map(items, (item, ix) => (
-              <div key={ix} className="glossary-sb__title">
-                <span
-                  role="button"
-                  //id={item.title.replace(/\s+/g, "")}
-                  onClick={() => this.openDefinition(item)}
-                  onKeyDown={(e) => this.handleKeyPress(e, item)}
-                  tabIndex={0}
-                >
-                  {search_phrase ? (
-                    <SearchHighlighter
-                      search={search_phrase}
-                      content={item.title}
-                    />
-                  ) : (
-                    item.title
-                  )}
+        {results.length == 0 && search_phrase != "" ? (
+          <div>{text_maker("no_matches_found")}</div>
+        ) : (
+          <div>
+            {_.map(items_by_letter, ({ letter, items }) => (
+              <div key={letter}>
+                <span className="glossary-sb__letter" key={letter}>
+                  {letter}
                 </span>
+                <hr />
+                {_.map(items, (item, ix) => (
+                  <div key={ix} className="glossary-sb__title">
+                    <span
+                      role="button"
+                      //id={item.title.replace(/\s+/g, "")}
+                      onClick={() => this.openDefinition(item)}
+                      onKeyDown={(e) => this.handleKeyPress(e, item)}
+                      tabIndex={0}
+                    >
+                      {search_phrase ? (
+                        <SearchHighlighter
+                          search={search_phrase}
+                          content={item.title}
+                        />
+                      ) : (
+                        item.title
+                      )}
+                    </span>
+                  </div>
+                ))}
               </div>
             ))}
           </div>
-        ))}
+        )}
       </div>
     );
   }
