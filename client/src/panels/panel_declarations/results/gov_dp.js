5fcb864d0b365c0b5ac94443dcc932c0d6af9ec1
Stephen-ONeil <stephenlevioneil@gmail.com>
2022-02-16 17:07:52 -0500

    Add multi year capabilities to gov DP summary panel

--- client/src/panels/panel_declarations/results/gov_dp.js
+++ client/src/panels/panel_declarations/results/gov_dp.js
@@ -1,5 +1,5 @@
 import _ from "lodash";
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 
 import { HeightClippedGraph } from "src/panels/panel_declarations/common_panel_components";
 import { declare_panel } from "src/panels/panel_declarations/common_panel_utils";
@@ -8,6 +8,7 @@ import { InfographicPanel } from "src/panels/panel_declarations/InfographicPanel
 import {
   create_text_maker_component,
   DisplayTable,
+  Tabs,
 } from "src/components/index";
 
 import { Dept } from "src/models/subjects";
@@ -18,7 +19,7 @@ import { LateDepartmentsBanner } from "./result_components";
 import {
   ResultCounts,
   filter_and_genericize_doc_counts,
-  current_dp_key,
+  get_result_doc_keys,
   result_docs,
   link_to_results_infograph,
 } from "./results_common";
@@ -27,21 +28,82 @@ import text from "./gov_dp.yaml";
 
 const { text_maker, TM } = create_text_maker_component(text);
 
-const current_dp_year = result_docs[current_dp_key].year;
-const current_dp_corresponding_drr_year =
-  _.toNumber(result_docs[current_dp_key].year_short) + 1;
+const dp_keys = get_result_doc_keys("dp");
 
-const DpSummary = ({
-  counts,
-  rows_of_counts_by_dept,
-  column_configs,
-  late_dept_count,
-}) => {
-  const current_dp_counts_with_generic_keys = filter_and_genericize_doc_counts(
-    counts,
-    current_dp_key
+const get_dp_year = (dp_key) => result_docs[dp_key].year;
+const get_dp_corresponding_drr_year = (dp_key) =>
+  _.toNumber(result_docs[dp_key].year_short) + 1;
+
+const get_dp_data = (dp_key) => {
+  const dept_counts = _.filter(
+    ResultCounts.get_all_dept_counts(),
+    (row) => row[`${dp_key}_results`] > 0
   );
-  return (
+
+  const subj_map = _.chain(dept_counts)
+    .map((row) => [
+      row.id,
+      link_to_results_infograph(Dept.store.lookup(row.id)),
+    ])
+    .fromPairs()
+    .value();
+
+  const rows_of_counts_by_dept = _.map(dept_counts, (row) => ({
+    subject_name: row.id,
+    [`${dp_key}_results`]: row[`${dp_key}_results`],
+    [`${dp_key}_indicators`]: row[`${dp_key}_indicators`],
+  }));
+  const column_configs = {
+    subject_name: {
+      index: 0,
+      header: text_maker("org"),
+      is_searchable: true,
+      formatter: (value) =>
+        subj_map[value] ? (
+          <a href={subj_map[value]}> {Dept.store.lookup(value).name} </a>
+        ) : (
+          value
+        ),
+      plain_formatter: (value) =>
+        value ? Dept.store.lookup(value).name : value,
+    },
+    [`${dp_key}_results`]: {
+      index: 1,
+      header: text_maker("results"),
+      is_summable: true,
+      formatter: "big_int",
+    },
+    [`${dp_key}_indicators`]: {
+      index: 2,
+      header: text_maker("indicators"),
+      is_summable: true,
+      formatter: "big_int",
+    },
+  };
+  const late_dept_count = result_docs[dp_key].late_results_orgs.length;
+
+  return {
+    column_configs,
+    rows_of_counts_by_dept,
+    late_dept_count,
+    counts_with_generic_keys: filter_and_genericize_doc_counts(
+      ResultCounts.get_gov_counts(),
+      dp_key
+    ),
+  };
+};
+
+const DpSummary = () => {
+  const [dp_key, set_dp_key] = useState(_.last(dp_keys));
+
+  const {
+    rows_of_counts_by_dept,
+    column_configs,
+    late_dept_count,
+    counts_with_generic_keys,
+  } = get_dp_data(dp_key);
+
+  const panel_content = (
     <Fragment>
       <div className="col-12 col-lg-12 medium-panel-text">
         {late_dept_count > 0 && (
@@ -50,10 +112,10 @@ const DpSummary = ({
         <TM
           k="gov_dp_text"
           args={{
-            ...current_dp_counts_with_generic_keys,
+            ...counts_with_generic_keys,
             depts_with_dps: rows_of_counts_by_dept.length,
-            year: current_dp_year,
-            drr_tabling_year: current_dp_corresponding_drr_year,
+            year: get_dp_year(dp_key),
+            drr_tabling_year: get_dp_corresponding_drr_year(dp_key),
           }}
         />
       </div>
@@ -66,6 +128,23 @@ const DpSummary = ({
       </HeightClippedGraph>
     </Fragment>
   );
+
+  if (dp_keys.length > 1) {
+    return (
+      <Tabs
+        tabs={_.chain(dp_keys)
+          .map((dp_key) => [dp_key, get_dp_year(dp_key)])
+          .fromPairs()
+          .value()}
+        open_tab_key={dp_key}
+        tab_open_callback={set_dp_key}
+      >
+        {panel_content}
+      </Tabs>
+    );
+  } else {
+    return panel_content;
+  }
 };
 
 export const declare_gov_dp_panel = () =>
@@ -75,83 +154,16 @@ export const declare_gov_dp_panel = () =>
     panel_config_func: () => ({
       requires_result_counts: true,
       title: text_maker("gov_dp_summary_title", {
-        year: current_dp_year,
+        first_year: get_dp_year(_.first(dp_keys)),
+        last_year: dp_keys.length > 1 && get_dp_year(_.last(dp_keys)),
       }),
-      calculate: () => {
-        const dept_counts = _.filter(
-          ResultCounts.get_all_dept_counts(),
-          (row) => row[`${current_dp_key}_results`] > 0
-        );
-
-        const subj_map = _.chain(dept_counts)
-          .map((row) => [
-            row.id,
-            link_to_results_infograph(Dept.store.lookup(row.id)),
-          ])
-          .fromPairs()
-          .value();
-
-        const rows_of_counts_by_dept = _.map(dept_counts, (row) => ({
-          subject_name: row.id,
-          [`${current_dp_key}_results`]: row[`${current_dp_key}_results`],
-          [`${current_dp_key}_indicators`]: row[`${current_dp_key}_indicators`],
-        }));
-        const column_configs = {
-          subject_name: {
-            index: 0,
-            header: text_maker("org"),
-            is_searchable: true,
-            formatter: (value) =>
-              subj_map[value] ? (
-                <a href={subj_map[value]}> {Dept.store.lookup(value).name} </a>
-              ) : (
-                value
-              ),
-            plain_formatter: (value) =>
-              value ? Dept.store.lookup(value).name : value,
-          },
-          [`${current_dp_key}_results`]: {
-            index: 1,
-            header: text_maker("results"),
-            is_summable: true,
-            formatter: "big_int",
-          },
-          [`${current_dp_key}_indicators`]: {
-            index: 2,
-            header: text_maker("indicators"),
-            is_summable: true,
-            formatter: "big_int",
-          },
-        };
-        const late_dept_count =
-          result_docs[current_dp_key].late_results_orgs.length;
-
-        return {
-          column_configs,
-          rows_of_counts_by_dept,
-          late_dept_count,
-        };
-      },
+      calculate: () => !_.isEmpty(dp_keys),
       footnotes: ["RESULTS", "DP"],
       source: () => get_source_links(["DP"]),
-      render({ title, calculations, sources, footnotes }) {
-        const {
-          panel_args: {
-            rows_of_counts_by_dept,
-            late_dept_count,
-            column_configs,
-          },
-        } = calculations;
-        const counts = ResultCounts.get_gov_counts();
-
+      render({ title, sources, footnotes }) {
         return (
           <InfographicPanel {...{ title, sources, footnotes }} allowOverflow>
-            <DpSummary
-              counts={counts}
-              rows_of_counts_by_dept={rows_of_counts_by_dept}
-              late_dept_count={late_dept_count}
-              column_configs={column_configs}
-            />
+            <DpSummary />
           </InfographicPanel>
         );
       },
