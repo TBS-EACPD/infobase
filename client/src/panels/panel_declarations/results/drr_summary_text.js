36c2c822515e6e308b3ddda9e749124501acd9ab
Stephen-ONeil <stephenlevioneil@gmail.com>
2022-02-17 10:50:13 -0500

    Untangle gov and non-gov DRR summary panels. Mainly move the shared summary component to its own file and isolate the text keys.

--- client/src/panels/panel_declarations/results/drr_summary_text.js
+++
@@ -1,5 +0,0 @@
-import { create_text_maker_component } from "src/components/index";
-
-import text from "./drr_summary_text.yaml";
-
-export const { text_maker, TM } = create_text_maker_component(text);
