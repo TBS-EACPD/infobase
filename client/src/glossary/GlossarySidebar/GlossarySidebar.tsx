168bcfe68e00591fe916dd6e52de35dc513d144f
Stephane-Rancourt <sranc046@uottawa.ca>
2022-02-21 10:22:48 -0500

    Shift GlossarySidebarSearch content into GlossarySidebar

--- client/src/glossary/GlossarySidebar/GlossarySidebar.tsx
+++ client/src/glossary/GlossarySidebar/GlossarySidebar.tsx
@@ -2,23 +2,31 @@ import React from "react";
 
 import "./GlossarySidebar.scss";
 
-import { create_text_maker_component } from "src/components/index";
+import {
+  DebouncedTextInput,
+  create_text_maker_component,
+} from "src/components/index";
 
 import glossary_text from "src/glossary/glossary.yaml";
 
+import { IconSearch } from "src/icons/icons";
+
 import { GlossaryDef } from "./GlossarySidebarDefinition";
 import { GlossaryList } from "./GlossarySidebarList";
-import { SideBarSearch } from "./GlossarySidebarSearch";
 
 const { text_maker } = create_text_maker_component(glossary_text);
 
-interface GlossarySidebarProps {
+const GlossarySidebarSearchDefaultProps = {
+  placeholder: text_maker("glossary_placeholder"),
+};
+
+type GlossarySidebarProps = typeof GlossarySidebarSearchDefaultProps & {
   glossary_item_key: string;
   focus_item_key: string;
   set_glossary_item: (key: string) => void;
   search_phrase: string;
   set_query: (query: string) => void;
-}
+};
 
 interface GlossarySidebarState {
   focus_item_key: string;
@@ -28,6 +36,8 @@ export class GlossarySidebar extends React.Component<
   GlossarySidebarProps,
   GlossarySidebarState
 > {
+  static defaultProps = GlossarySidebarSearchDefaultProps;
+
   constructor(props: GlossarySidebarProps) {
     super(props);
 
@@ -36,18 +46,35 @@ export class GlossarySidebar extends React.Component<
     };
   }
 
+  callback = (query: string) => {
+    this.props.set_query(query);
+    this.props.set_glossary_item("");
+  };
+
   render() {
-    const { glossary_item_key, search_phrase, focus_item_key } = this.props;
+    const { glossary_item_key, search_phrase, focus_item_key, placeholder } =
+      this.props;
     return (
       <div>
         <div className="glossary-sb__header-wrapper">
           <div className="glossary-sb__header">
             <div className="glossary-sb__search-wrapper">
-              <SideBarSearch
-                set_glossary_item={(key) => this.props.set_glossary_item(key)}
-                setQuery={(query) => this.props.set_query(query)}
-                search_phrase={search_phrase}
-              />
+              <div className={"glossary-sb__search-bar"}>
+                <div className={"glossary-sb__icon-container"}>
+                  <span aria-hidden="true">
+                    <IconSearch
+                      width="30px"
+                      color="#2C70C9"
+                      alternate_color={false}
+                    />
+                  </span>
+                </div>
+                <DebouncedTextInput
+                  placeHolder={placeholder}
+                  updateCallback={this.callback}
+                  defaultValue={search_phrase}
+                />
+              </div>
             </div>
             <div className="glossary-sb__example">
               {text_maker("glossary_example")}
