57b5174cc978809240a49c332fd2f32cd514caa3
Stephen-ONeil <stephenlevioneil@gmail.com>
2022-02-24 15:21:36 -0500

    Centralize current estimates document code/name information on the orgVoteStatEstimates table... not going to be its final home most likely, was sticking it here because I'd like it to be data driven, but I realized mid-way through that, in practice, the table data isn't always populated by the time this is needed

--- client/src/EstimatesComparison/utils.js
+++ client/src/EstimatesComparison/utils.js
@@ -1,8 +1,17 @@
+import _ from "lodash";
+
 import { create_text_maker_component } from "src/components/index";
+
+import { Table } from "src/tables/TableClass";
 
 import text from "./EstimatesComparison.yaml";
 
 export const { text_maker, TM } = create_text_maker_component(text);
 
-export const current_doc_is_mains = true; // Update this when switching between displaying mains and sups!
-export const current_sups_letter = "C"; // Update this on each new sups release!
+const current_doc_code = Table.store
+  .lookup("orgVoteStatEstimates")
+  .get_current_doc_code();
+
+export const current_doc_is_mains = current_doc_code === "MAINS";
+export const current_sups_letter =
+  /^SE(A|B|C)$/.test(current_doc_code) && _.last(current_doc_code);
--- client/src/panels/panel_declarations/finances/vote_stat/vote_stat_text_provider.js
+++ client/src/panels/panel_declarations/finances/vote_stat/vote_stat_text_provider.js
@@ -1,5 +1,16 @@
 import { create_text_maker_component } from "src/components/index";
 
+import { lang } from "src/core/injected_build_constants";
+
+import { Table } from "src/tables/TableClass";
+
 import text from "./vote_stat_text.yaml";
 
-export const { text_maker, TM } = create_text_maker_component([text]);
+export const { text_maker, TM } = create_text_maker_component([
+  text,
+  {
+    last_estimates: {
+      [lang]: Table.store.lookup("orgVoteStatEstimates").get_current_doc_name(),
+    },
+  },
+]);
