4810c13873c28b345c70abca6f3af6c9213c54ed
Stephen-ONeil <stephenlevioneil@gmail.com>
2022-02-28 15:29:51 -0500

    Clean up some strange legacy estimates code that seemed to think votes would be mixed in to the dataset's estimates document column

--- client/src/models/businessConstants.ts
+++ client/src/models/businessConstants.ts
@@ -608,74 +608,39 @@ const NA_values = [
 
 const estimates_docs = {
   MAINS: {
-    order: 0,
     en: "Main Estimates",
     fr: "Budget principal",
+    order: 0,
   },
   MYA: {
-    order: 1,
     en: "Multi Year Appropriations",
     fr: "Crédits disponibles des précédents exercices",
+    order: 1,
   },
   VA: {
-    order: 11,
     en: "Voted Adjustments",
     fr: "Réajustement votés",
+    order: 1.1,
   },
   SA: {
-    order: 12,
     en: "Statutory Adjustments",
     fr: "Réajustements législatifs",
+    order: 1.2,
   },
   SEA: {
-    order: 2,
     en: "Supp. Estimates A",
     fr: "Budget supp. A",
+    order: 2,
   },
   SEB: {
-    order: 3,
     en: "Supp. Estimates B",
     fr: "Budget supp. B",
+    order: 3,
   },
   SEC: {
-    order: 4,
     en: "Supp. Estimates C",
     fr: "Budget supp. C",
-  },
-  V5: {
-    order: 6,
-    en: "Government Contingencies",
-    fr: "Éventualités du gouvernement",
-  },
-  V10: {
-    order: 7,
-    en: "Operating Budget Carry Forward",
-    fr: "Report du budget de fonctionnement",
-  },
-  V15: {
-    order: 8,
-    en: "Compensation adjustments",
-    fr: "Rajustements à la rémunération",
-  },
-  V25: {
-    order: 9,
-    en: "Operating Budget Carry Forward",
-    fr: "Report du budget de fonctionnement",
-  },
-  V30: {
-    order: 10,
-    en: "Paylist requirements",
-    fr: "Besoins en matière de rémunération",
-  },
-  V33: {
-    order: 11,
-    en: "Capital Budget Carry Forward",
-    fr: "Report du budget de dépenses en capital",
-  },
-  DEEM: {
-    order: 12,
-    en: "Deemed appropriation",
-    fr: "Crédit réputé",
+    order: 4,
   },
 };
 
--- server/src/models/vote_stat/constants.js
+++ server/src/models/vote_stat/constants.js
@@ -1,83 +1,37 @@
 export const estimates_docs = {
-  IM: {
-    en: "Interim Estimates",
-    fr: "Budget provisoire des dépenses",
-    order: 0,
-  },
   MAINS: {
     en: "Main Estimates",
     fr: "Budget principal",
     order: 0,
   },
   MYA: {
-    // tabled should only be set once MYA is available for the current
-    // year around the end of the summer
-    order: 1,
     en: "Multi Year Appropriations",
     fr: "Crédits disponibles des précédents exercices",
+    order: 1,
   },
   VA: {
-    // tabled should only be set once MYA is available for the current
-    // year around the end of the summer
-    order: 1.1,
     en: "Voted Adjustments",
     fr: "Réajustement votés",
+    order: 1.1,
   },
   SA: {
-    // tabled should only be set once MYA is available for the current
-    // year around the end of the summer
-    order: 1.2,
     en: "Statutory Adjustments",
     fr: "Réajustements législatifs",
+    order: 1.2,
   },
   SEA: {
-    order: 2,
     en: "Supp. Estimates A",
     fr: "Budget supp. A",
+    order: 2,
   },
   SEB: {
-    order: 3,
     en: "Supp. Estimates B",
     fr: "Budget supp. B",
+    order: 3,
   },
   SEC: {
-    order: 4,
     en: "Supp. Estimates C",
     fr: "Budget supp. C",
-  },
-  V5: {
-    order: 6,
-    en: "Government Contingencies",
-    fr: "Éventualités du gouvernement",
-  },
-  V10: {
-    order: 7,
-    en: "Operating Budget Carry Forward",
-    fr: "Report du budget de fonctionnement",
-  },
-  V15: {
-    order: 8,
-    en: "Compensation adjustments",
-    fr: "Rajustements à la rémunération",
-  },
-  V25: {
-    order: 9,
-    en: "Operating Budget Carry Forward",
-    fr: "Report du budget de fonctionnement",
-  },
-  V30: {
-    order: 10,
-    en: "Paylist requirements",
-    fr: "Besoins en matière de rémunération",
-  },
-  V33: {
-    order: 11,
-    en: "Capital Budget Carry Forward",
-    fr: "Report du budget de dépenses en capital",
-  },
-  DEEM: {
-    order: 12,
-    en: "Deemed appropriation",
-    fr: "Crédit réputé",
+    order: 4,
   },
 };
