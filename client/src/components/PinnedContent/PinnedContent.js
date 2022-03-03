2ea675ae8bf6ba05ba2e5906fedd93b87f47c4ab
Stephen-ONeil <stephenlevioneil@gmail.com>
2022-03-03 14:57:25 -0500

    Cleanup and simplify how PinnedContent manages syncing the place holder div's height against the content's height. Actually stay in sync if the content changes height. Note: still certain scroll ranges where the rendering stutters between pinned and unpinned (but MUCH better than hte previous implementation. Also note: I've intentionally broken the width syncing, want a less hacky fix to that too

--- client/src/components/PinnedContent/PinnedContent.js
+++ client/src/components/PinnedContent/PinnedContent.js
@@ -55,129 +55,106 @@ class _PinnedContent extends React.Component {
         : default_pin_state;
     }
   }
+
   set_is_pinned = (is_pinned) => {
     set_pinned_content_local_storage(this.props.local_storage_name, is_pinned);
     this.setState({ is_pinned_local_storage_mirror: is_pinned });
   };
-
   pin_pressed = () => {
     this.set_is_pinned(!this.is_pinned);
-
-    this.setState({
-      content_height: 0,
-    });
   };
-
   handleKeyDown = (e) => {
     if (e.key === "Tab") {
       this.set_is_pinned(false);
     }
   };
 
-  update_content_height = _.debounce(() => {
-    this.setState({
-      content_height: this.content_ref.current.clientHeight,
-    });
-  }, this.props.height_update_delay);
-
   componentDidMount() {
     this.set_is_pinned(this.is_pinned);
-    this.update_content_height();
-  }
-
-  componentWillUnmount() {
-    this.update_content_height.cancel();
   }
 
   render() {
-    const { content_height } = this.state;
     const { children } = this.props;
 
     return (
-      <ReactResizeDetector handleWidth>
-        {({ width }) => (
-          <InView>
-            {({ inView, ref, entry }) => {
-              const should_pin =
-                this.is_pinned &&
-                !inView &&
-                entry &&
-                entry.boundingClientRect.top < 0;
+      <InView>
+        {({ inView, ref, entry }) => {
+          const should_pin =
+            this.is_pinned &&
+            !inView &&
+            entry &&
+            entry.boundingClientRect.top < 0;
 
-              return (
-                <div ref={ref}>
-                  {/* 
-                    this conditional div with height height: content_height acts as a placeholder to make scrolling
-                    smoother. Before adding this, stickying by "position: fixed" would take it out of the DOM block
-                    flow which would bump up the window due to the total block content being shortened.
-                    By adding this placeholder div, the height of the total block content remains the same,
-                    thus no longer causing the window to jump
-                  */}
-                  {should_pin && <div style={{ height: content_height }} />}
-                  {/* this div is for sticky styline, must be flex to include margins onto height */}
-                  <div
-                    style={{
-                      display: "flex",
-                      ...(should_pin && {
-                        position: "fixed",
-                        top: 0,
-                        zIndex: 2001,
-                      }),
-                    }}
-                    ref={this.content_ref}
-                  >
-                    <div style={{ position: "relative", width: width }}>
-                      {children}
-                      <div
-                        style={{
-                          position: "absolute",
-                          top: "1rem",
-                          right: "1rem",
-                        }}
-                      >
-                        <button
-                          onClick={this.pin_pressed}
+          return (
+            <div ref={ref}>
+              <ReactResizeDetector handleHeight>
+                {({ targetRef }) => (
+                  <>
+                    {should_pin && (
+                      <div style={{ height: targetRef.current.offsetHeight }} />
+                    )}
+                    <div
+                      style={{
+                        ...(should_pin && {
+                          position: "fixed",
+                          top: 0,
+                          zIndex: 2001,
+                        }),
+                      }}
+                      ref={targetRef}
+                    >
+                      <div style={{ position: "relative" }}>
+                        {children}
+                        <div
                           style={{
-                            background: "none",
-                            border: "none",
+                            position: "absolute",
+                            top: "1rem",
+                            right: "1rem",
                           }}
-                          aria-label={text_maker(
-                            !this.is_pinned ? "pin" : "unpin"
-                          )}
-                          onKeyDown={this.handleKeyDown}
                         >
-                          {!this.is_pinned ? (
-                            <IconPin
-                              height="25px"
-                              width="25px"
-                              svg_style={{ verticalAlign: "Top" }}
-                              color={backgroundColor}
-                              alternate_color="false"
-                            />
-                          ) : (
-                            <IconUnpin
-                              height="25px"
-                              width="25px"
-                              svg_style={{ verticalAlign: "Top" }}
-                              color={backgroundColor}
-                              alternate_color="false"
-                            />
-                          )}
-                        </button>
+                          <button
+                            onClick={this.pin_pressed}
+                            style={{
+                              background: "none",
+                              border: "none",
+                            }}
+                            aria-label={text_maker(
+                              !this.is_pinned ? "pin" : "unpin"
+                            )}
+                            onKeyDown={this.handleKeyDown}
+                          >
+                            {!this.is_pinned ? (
+                              <IconPin
+                                height="25px"
+                                width="25px"
+                                svg_style={{ verticalAlign: "Top" }}
+                                color={backgroundColor}
+                                alternate_color="false"
+                              />
+                            ) : (
+                              <IconUnpin
+                                height="25px"
+                                width="25px"
+                                svg_style={{ verticalAlign: "Top" }}
+                                color={backgroundColor}
+                                alternate_color="false"
+                              />
+                            )}
+                          </button>
+                        </div>
                       </div>
                     </div>
-                  </div>
-                </div>
-              );
-            }}
-          </InView>
-        )}
-      </ReactResizeDetector>
+                  </>
+                )}
+              </ReactResizeDetector>
+            </div>
+          );
+        }}
+      </InView>
     );
   }
 }
 _PinnedContent.defaultProps = {
-  height_update_delay: 1000,
   default_pin_state: has_local_storage,
 };
 
