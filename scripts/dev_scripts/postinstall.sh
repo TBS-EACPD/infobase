e7e7cb16a9cf7e12f71c1714a38eee84491725d4
Stephen-ONeil <stephenlevioneil@gmail.com>
2022-03-14 18:18:53 -0400

    Per repo-level postinstall.sh script. Fair bit of duplication but simple and easy enough to maintain. Shouldn't be a problem for GCF deploys, and leaves room for future postinstall steps that any of the projects might need

---
+++ scripts/dev_scripts/postinstall.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+set -e # will exit if any command has non-zero exit value 
+
+# Cleanup eslint cache; doesn't invalidate on node_modules changes but _should_ because of rules like no-missing-dependencies 
+rm -f .eslintcache
