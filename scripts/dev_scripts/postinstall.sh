5da2436c308b7e3a7f3ad4e3ad580a0474759dda
Stephen-ONeil <stephenlevioneil@gmail.com>
2022-03-15 10:11:57 -0400

    Go back to a central, root level, script for clearing eslitn caches. In GCF sub-repos, do a check for the script's existence before attempting to run postinstall/ci; it won't exist (or be necessary) inside the GCF build environment (and just checking for existence is better than trying to detect that environment)

--- scripts/dev_scripts/postinstall.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -e # will exit if any command has non-zero exit value 
-
-# Cleanup eslint cache; doesn't invalidate on node_modules changes but _should_ because of rules like no-unresolved
-rm -f .eslintcache
