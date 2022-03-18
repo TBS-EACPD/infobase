948ec9f4c9cd4ca90c7d0f1b1f1cc5dad1a0973b
Stephen-ONeil <stephenlevioneil@gmail.com>
2022-03-18 17:59:25 -0400

    Tweak DataSources model, frame of the DataSets model

--- client/src/models/metadata/DataSources.ts
+++ client/src/models/metadata/DataSources.ts
@@ -1,34 +1,28 @@
 import _ from "lodash";
 
-import { lang } from "src/core/injected_build_constants";
-
 import { Frequencies } from "./Frequencies";
 
 type SourceDef = {
   name: string;
   description: React.ReactNode;
+  open_data_link?: string;
+  other_links?: { text: string; href: string }[];
   frequency_key?: keyof typeof Frequencies;
-  bilingual_links?: { text: string; href: { [key in LangType]: string } }[];
 };
 
-export type SourceKeys = "test" | "etc"; // TODO
+export type SourceKey = "test" | "etc"; // TODO
 
-const source_definitions: { [key in SourceKeys]: SourceDef } = {
+const source_definitions: { [key in SourceKey]: SourceDef } = {
   // TODO
   test: { name: "", description: "" },
   etc: { name: "", description: "" },
 };
 
 export const DataSources = _.mapValues(source_definitions, (def: SourceDef) => {
-  const { frequency_key, bilingual_links } = def;
+  const { frequency_key } = def;
 
   const frequency =
     frequency_key !== undefined ? Frequencies[frequency_key] : undefined;
 
-  const links = _.map(bilingual_links, ({ text, href }) => ({
-    text,
-    href: href[lang],
-  }));
-
-  return { ...def, frequency, links };
+  return { ...def, frequency };
 });
