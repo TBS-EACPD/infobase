44e16a32c2a70427bcbcb573f540ef596a835780
Stephen-ONeil <stephenlevioneil@gmail.com>
2022-03-18 16:55:57 -0400

    Start of centralized models for data source and data set concepts

---
+++ client/src/models/metadata/Frequencies.ts
@@ -0,0 +1,8 @@
+import { lang } from "src/core/injected_build_constants";
+
+export const Frequencies = {
+  yearly: { en: "Yearly", fr: "Annuellement" }[lang],
+  quarterly: { en: "Quarterly", fr: "Trimestriellement" }[lang],
+  mothly: { en: "Monthly", fr: "Mensuellement" }[lang],
+  as_needed: { en: "As needed", fr: "Comme requis" }[lang],
+};
