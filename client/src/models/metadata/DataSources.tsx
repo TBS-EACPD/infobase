f02db9d1110c15233c2171979ce216be992d2181
Stephen-ONeil <stephenlevioneil@gmail.com>
2022-03-21 17:00:56 -0400

    Bring over old description getters and let some data source definitions pull description values from the glossary. A little hacky, potentially not future proof, but not bad as it was before because the new module architecture breaks the related circular dependency

--- client/src/models/metadata/DataSources.tsx
+++ client/src/models/metadata/DataSources.tsx
@@ -3,6 +3,8 @@ import React from "react";
 
 import { create_text_maker_component } from "src/components/misc_util_components";
 
+import { glossaryEntryStore } from "src/models/glossary";
+
 import { lang } from "src/core/injected_build_constants";
 
 import { Frequencies } from "./Frequencies";
@@ -36,9 +38,18 @@ const infobase_open_data_href = {
   fr: "https://ouvert.canada.ca/data/fr/dataset/a35cf382-690c-4221-a971-cf0fd189a46f",
 }[lang];
 
-// TODO old data sources objects occasionally used glossary entries as a source of descriptions, but
-// that required some hacky timing to make sure the glossary was actually loaded in time...
-// That's the source of the "TODO" descriptions below
+// HACK WARNING: because the glossaryEntryStore is populated at run time, we can't safely access glossary definitions on-module-load.
+// Currently, the glossary stores _will_ allways be populated in time for DataSources to be used though, as long as any sources that
+// pull thier definition from the glossary use a getter so that access to the glossary item is lazy-evaluated. Not necessarily future proof
+// TODO consider future proof solutions (worst case, duplicate relevant glossary text in DataSources.yaml)
+function desc_from_glossary_keys(...glossary_keys: string[]) {
+  const definitions = _.map(glossary_keys, (glossary_key) =>
+    glossaryEntryStore.lookup(glossary_key).get_compiled_definition()
+  );
+  return _.map(definitions, (def, ix) => (
+    <div key={ix} dangerouslySetInnerHTML={{ __html: def }} />
+  ));
+}
 
 const source_definitions: { [key in SourceKey]: SourceDef } = {
   IGOC: {
@@ -49,7 +60,9 @@ const source_definitions: { [key in SourceKey]: SourceDef } = {
   },
   PA: {
     name: text_maker("pa_name"),
-    description: "TODO",
+    get description() {
+      return desc_from_glossary_keys("PA");
+    },
     definitive_link: {
       en: "https://www.tpsgc-pwgsc.gc.ca/recgen/cpc-pac/apropos-about-eng.html",
       fr: "https://www.tpsgc-pwgsc.gc.ca/recgen/cpc-pac/apropos-about-fra.html",
@@ -59,7 +72,9 @@ const source_definitions: { [key in SourceKey]: SourceDef } = {
   },
   ESTIMATES: {
     name: text_maker("estimates_name"),
-    description: "TODO",
+    get description() {
+      return desc_from_glossary_keys("MAINS", "SUPPS");
+    },
     definitive_link: {
       en: "https://www.canada.ca/en/treasury-board-secretariat/services/planned-government-spending/government-expenditure-plan-main-estimates.html",
       fr: "https://www.canada.ca/fr/secretariat-conseil-tresor/services/depenses-prevues/plan-depenses-budget-principal.html",
@@ -69,7 +84,9 @@ const source_definitions: { [key in SourceKey]: SourceDef } = {
   },
   CFMRS: {
     name: text_maker("cfmrs_name"),
-    description: "TODO",
+    get description() {
+      return desc_from_glossary_keys("CFMRS");
+    },
     open_data_link: {
       en: "http://open.canada.ca/data/en/dataset/5e6dcf6b-dbed-4b51-84e5-1f4926ad7fdf",
       fr: "http://ouvert.canada.ca/data/fr/dataset/5e6dcf6b-dbed-4b51-84e5-1f4926ad7fdf",
@@ -87,7 +104,9 @@ const source_definitions: { [key in SourceKey]: SourceDef } = {
   },
   DP: {
     name: text_maker("dp_name"),
-    description: "TODO",
+    get description() {
+      return desc_from_glossary_keys("DP");
+    },
     definitive_link: {
       en: "https://www.canada.ca/en/treasury-board-secretariat/services/planned-government-spending/reports-plans-priorities.html",
       fr: "https://www.canada.ca/fr/secretariat-conseil-tresor/services/depenses-prevues/rapports-plans-priorites.html",
@@ -97,7 +116,9 @@ const source_definitions: { [key in SourceKey]: SourceDef } = {
   },
   DRR: {
     name: text_maker("drr_name"),
-    description: "TODO",
+    get description() {
+      return desc_from_glossary_keys("DRR");
+    },
     definitive_link: {
       en: "https://www.canada.ca/en/treasury-board-secretariat/services/departmental-performance-reports.html",
       fr: "https://www.canada.ca/fr/secretariat-conseil-tresor/services/rapports-ministeriels-rendement.html",
@@ -107,7 +128,9 @@ const source_definitions: { [key in SourceKey]: SourceDef } = {
   },
   RPS: {
     name: text_maker("rps_name"),
-    description: "TODO",
+    get description() {
+      return desc_from_glossary_keys("PEOPLE_DATA");
+    },
     frequency_key: "yearly",
     definitive_link: {
       en: "https://www.canada.ca/en/treasury-board-secretariat/services/innovation/human-resources-statistics.html",
