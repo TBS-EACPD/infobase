3c0b3659ac0c8e16a5279c5307bfa22170c1f3ed
Stephen-ONeil <stephenlevioneil@gmail.com>
2022-04-05 18:02:08 -0400

    Drop Frequencies.unit-test.ts, the single non-empty assertion isn't necessary since in practice (although not directly) the type system _will_ complain if it has no keys.

--- client/src/models/metadata/Frequencies.unit-test.ts
+++
@@ -1,9 +0,0 @@
-import _ from "lodash";
-
-import { Frequencies } from "./Frequencies";
-
-describe("Frequencies", () => {
-  it("Is non-empty", () => {
-    expect(Frequencies).toBeTruthy();
-  });
-});
