50bfb5dcdcc2b45449ee065233f623f3c001fea4
Stephen-ONeil <stephenlevioneil@gmail.com>
2022-04-05 17:12:07 -0400

    Drop type assertion tests from Frequencies.unit-test.ts, use InferedKeysRecordHelper in module itself to better enforce the typing

--- client/src/models/metadata/Frequencies.ts
+++ client/src/models/metadata/Frequencies.ts
@@ -1,8 +1,10 @@
 import { lang } from "src/core/injected_build_constants";
 
-export const Frequencies = {
+import { InferedKeysRecordHelper } from "src/types/type_utils";
+
+export const Frequencies = InferedKeysRecordHelper<string>()({
   yearly: { en: "Yearly", fr: "Annuellement" }[lang],
   quarterly: { en: "Quarterly", fr: "Trimestriellement" }[lang],
   mothly: { en: "Monthly", fr: "Mensuellement" }[lang],
   as_needed: { en: "As needed", fr: "Comme requis" }[lang],
-};
+});
