dbda57a8d146342cf5341a3783a80c9f5d77f944
Stephen-ONeil <stephenlevioneil@gmail.com>
2022-04-25 17:33:50 -0400

    Simplify SurveyPopup logic even more, always show it right away unless deactivated or postponed. No need to be too smart about when to show it to users, they can choose to delay themselves with the option to open it in a new tab and the postpone option. Change postpone time to 12 hours

--- client/src/Survey/SurveyPopup.js
+++ client/src/Survey/SurveyPopup.js
@@ -1,6 +1,5 @@
 import _ from "lodash";
 import React, { Fragment } from "react";
-import { withRouter } from "react-router";
 
 import {
   FixedPopover,
@@ -20,15 +19,11 @@ import text from "./SurveyPopup.yaml";
 
 const { TM, text_maker } = create_text_maker_component(text);
 
-const POSTPONE_TIMEOUT_PERIOD = 1000 * 60 * 60 * 24;
+const POSTPONE_TIMEOUT_PERIOD = 1000 * 60 * 60 * 12;
 
 const currentDate = new Date();
 
 const is_survey_campaign_period = (() => {
-  if (is_dev || is_dev_link) {
-    return false;
-  }
-
   // Reminder: months are 0-indexed, years and days aren't
   const is_start_of_trimester = _.includes([0, 4, 8], currentDate.getMonth());
 
@@ -37,152 +32,144 @@ const is_survey_campaign_period = (() => {
   return is_start_of_trimester && is_second_half_of_month;
 })();
 
+// Note: suffix based on year and month to bust cached settings between current, and potential future, campaign periods
 const key_suffix = `--${currentDate.getFullYear()}-${currentDate.getMonth()}`;
-
 const postponed_since_storage_key =
   "infobase_survey_popup_postponed_since" + key_suffix;
-
 const is_deactivated_storage_key =
   "infobase_survey_popup_deactivated" + key_suffix;
 
-export const SurveyPopup = withRouter(
-  class _SurveyPopup extends React.Component {
-    constructor(props) {
-      super(props);
+export class SurveyPopup extends React.Component {
+  constructor(props) {
+    super(props);
 
-      const is_deactivated_storage_value = localStorage.getItem(
-        is_deactivated_storage_key
-      );
-      const is_deactivated_by_user = is_deactivated_storage_value === "true";
+    const is_deactivated_storage_value = localStorage.getItem(
+      is_deactivated_storage_key
+    );
+    const is_deactivated_by_user = is_deactivated_storage_value === "true";
 
-      const postponed_since_storage_value = localStorage.getItem(
-        postponed_since_storage_key
-      );
-      const is_postponed_by_user = !_.isNull(postponed_since_storage_value)
-        ? Date.now() - +postponed_since_storage_value >= POSTPONE_TIMEOUT_PERIOD
-        : false;
+    const postponed_since_storage_value = localStorage.getItem(
+      postponed_since_storage_key
+    );
+    const is_postponed_by_user = !_.isNull(postponed_since_storage_value)
+      ? Date.now() - +postponed_since_storage_value >= POSTPONE_TIMEOUT_PERIOD
+      : false;
 
-      const is_active =
-        is_survey_campaign_period &&
-        !is_postponed_by_user &&
-        !is_deactivated_by_user;
+    const show_popup =
+      !(is_dev || is_dev_link) &&
+      is_survey_campaign_period &&
+      !is_deactivated_by_user &&
+      !is_postponed_by_user;
 
-      this.state = {
-        is_active,
-        landing_page: props.location.pathname,
-      };
-    }
-
-    handlePopupButtonPress = (button_type) => {
-      log_standard_event({
-        SUBAPP: this.props.location.pathname,
-        MISC1: "SURVEY_POPUP",
-        MISC2: `interaction: ${button_type}`,
-      });
-
-      if (_.includes(["yes", "no", "short_survey_submitted"], button_type)) {
-        localStorage.setItem(is_deactivated_storage_key, "true");
-      } else if (button_type === "later") {
-        localStorage.setItem(is_deactivated_storage_key, "false");
-        localStorage.setItem(postponed_since_storage_key, Date.now());
-      }
-
-      this.setState({ is_active: false });
+    this.state = {
+      show_popup,
     };
-
-    render() {
-      const { is_active, landing_page } = this.state;
-
-      const should_show =
-        is_active && this.props.location.pathname !== landing_page;
-
-      if (should_show) {
-        log_standard_event({
-          SUBAPP: this.props.location.pathname,
-          MISC1: "SURVEY_POPUP",
-          MISC2: "displayed",
-        });
-
-        return (
-          <FixedPopover
-            show={should_show}
-            title={
-              <Fragment>
-                <IconFeedback
-                  aria_label={text_maker("suvey_popup_header")}
-                  color={tertiaryColor}
-                  alternate_color={false}
-                />
-                {text_maker("suvey_popup_header")}
-              </Fragment>
-            }
-            body={
-              <Fragment>
-                <div
-                  style={{
-                    fontSize: "0.9em",
-                    display: "flex",
-                    flexDirection: "column",
-                  }}
-                >
-                  <TM k="survey_popup_body" />
-                </div>
-                <div
-                  style={{ display: "flex", justifyContent: "space-evenly" }}
-                >
-                  <a
-                    className="link-unstyled btn btn-ib-primary"
-                    onClick={() => this.handlePopupButtonPress("yes")}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    href={"#survey"}
-                  >
-                    {text_maker(`survey_popup_yes`)}
-                  </a>
-                </div>
-                <div
-                  className="mrgn-tp-md"
-                  style={{
-                    fontSize: "0.9em",
-                    display: "flex",
-                    flexDirection: "column",
-                    textAlign: "left",
-                    borderTop: "1px solid #e5e5e5",
-                  }}
-                >
-                  <TM
-                    k="simplified_survey_header"
-                    style={{ alignSelf: "center", fontSize: "1.4em" }}
-                  />
-                  <FormFrontend
-                    template_name="feedback_simplified"
-                    include_privacy={false}
-                    top_border={false}
-                    on_submitted={() =>
-                      this.handlePopupButtonPress("short_survey_submitted")
-                    }
-                  />
-                </div>
-              </Fragment>
-            }
-            footer={
-              <div style={{ display: "flex", justifyContent: "space-evenly" }}>
-                {_.map(["later", "no"], (button_type) => (
-                  <button
-                    key={button_type}
-                    className="btn btn-ib-primary"
-                    onClick={() => this.handlePopupButtonPress(button_type)}
-                  >
-                    {text_maker(`survey_popup_${button_type}`)}
-                  </button>
-                ))}
-              </div>
-            }
-          />
-        );
-      } else {
-        return null;
-      }
-    }
   }
-);
+
+  handlePopupButtonPress = (button_type) => {
+    log_standard_event({
+      SUBAPP: this.props.location.pathname,
+      MISC1: "SURVEY_POPUP",
+      MISC2: `interaction: ${button_type}`,
+    });
+
+    if (_.includes(["yes", "no", "short_survey_submitted"], button_type)) {
+      localStorage.setItem(is_deactivated_storage_key, "true");
+    } else if (button_type === "later") {
+      localStorage.setItem(is_deactivated_storage_key, "false");
+      localStorage.setItem(postponed_since_storage_key, Date.now());
+    }
+
+    this.setState({ show_popup: false });
+  };
+
+  render() {
+    const { show_popup } = this.state;
+
+    if (!show_popup) {
+      return null;
+    }
+
+    log_standard_event({
+      SUBAPP: this.props.location.pathname,
+      MISC1: "SURVEY_POPUP",
+      MISC2: "displayed",
+    });
+
+    return (
+      <FixedPopover
+        show={show_popup}
+        title={
+          <Fragment>
+            <IconFeedback
+              aria_label={text_maker("suvey_popup_header")}
+              color={tertiaryColor}
+              alternate_color={false}
+            />
+            {text_maker("suvey_popup_header")}
+          </Fragment>
+        }
+        body={
+          <Fragment>
+            <div
+              style={{
+                fontSize: "0.9em",
+                display: "flex",
+                flexDirection: "column",
+              }}
+            >
+              <TM k="survey_popup_body" />
+            </div>
+            <div style={{ display: "flex", justifyContent: "space-evenly" }}>
+              <a
+                className="link-unstyled btn btn-ib-primary"
+                onClick={() => this.handlePopupButtonPress("yes")}
+                target="_blank"
+                rel="noopener noreferrer"
+                href={"#survey"}
+              >
+                {text_maker(`survey_popup_yes`)}
+              </a>
+            </div>
+            <div
+              className="mrgn-tp-md"
+              style={{
+                fontSize: "0.9em",
+                display: "flex",
+                flexDirection: "column",
+                textAlign: "left",
+                borderTop: "1px solid #e5e5e5",
+              }}
+            >
+              <TM
+                k="simplified_survey_header"
+                style={{ alignSelf: "center", fontSize: "1.4em" }}
+              />
+              <FormFrontend
+                template_name="feedback_simplified"
+                include_privacy={false}
+                top_border={false}
+                on_submitted={() =>
+                  this.handlePopupButtonPress("short_survey_submitted")
+                }
+              />
+            </div>
+          </Fragment>
+        }
+        footer={
+          <div style={{ display: "flex", justifyContent: "space-evenly" }}>
+            {_.map(["later", "no"], (button_type) => (
+              <button
+                key={button_type}
+                className="btn btn-ib-primary"
+                onClick={() => this.handlePopupButtonPress(button_type)}
+              >
+                {text_maker(`survey_popup_${button_type}`)}
+              </button>
+            ))}
+          </div>
+        }
+      />
+    );
+  }
+}
