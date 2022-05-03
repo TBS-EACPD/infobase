666263607115f8f3a6014c36a294c04e602eb837
Stephen-ONeil <stephenlevioneil@gmail.com>
2022-05-03 17:36:34 -0400

    Drop the calculated summary counts for DRR and DP from the results intro panel. Not run this by RD, but it's going to be my suggestion

--- client/src/panels/panel_declarations/results/results_intro.js
+++ client/src/panels/panel_declarations/results/results_intro.js
@@ -1,5 +1,3 @@
-import { sum } from "d3-array";
-import _ from "lodash";
 import React from "react";
 
 import { InfographicPanel } from "src/panels/panel_declarations/InfographicPanel";
@@ -7,34 +5,17 @@ import { declare_panel } from "src/panels/PanelRegistry";
 
 import { create_text_maker_component } from "src/components/index";
 
-import { Dept } from "src/models/subjects";
-
 import { lang, is_a11y_mode } from "src/core/injected_build_constants";
 
 import { get_static_url } from "src/request_utils";
 
-import {
-  ResultCounts,
-  GranularResultCounts,
-  result_docs,
-  current_drr_key,
-  current_dp_key,
-} from "./results_common";
+import { result_docs, current_drr_key, current_dp_key } from "./results_common";
 
 import text from "./results_intro_text.yaml";
 
 const { text_maker, TM } = create_text_maker_component(text);
 
-const ResultsIntroPanel = ({
-  subject,
-  is_gov,
-  summary_result_counts,
-  doc_urls,
-  has_current_dp,
-  has_current_drr,
-}) => {
-  const summary_text_args = { subject, is_gov, ...summary_result_counts };
-
+const ResultsIntroPanel = ({ doc_urls }) => {
   return (
     <div className="row align-items-center">
       <div className="col-12 col-lg-7 medium-panel-text">
@@ -60,26 +41,6 @@ const ResultsIntroPanel = ({
       )}
       <div className="col-12 col-lg-12 medium-panel-text">
         <TM k="gba_plus_intro_text" />
-        {has_current_dp && (
-          <TM
-            k="dp_summary_text"
-            args={{
-              ...summary_text_args,
-              year: result_docs[current_dp_key].year,
-              tabling_year:
-                _.toNumber(result_docs[current_dp_key].year_short) + 1,
-            }}
-          />
-        )}
-        {has_current_drr && (
-          <TM
-            k="drr_summary_text"
-            args={{
-              ...summary_text_args,
-              year: result_docs[current_drr_key].year,
-            }}
-          />
-        )}
         <TM k="reports_links_text" args={doc_urls} />
       </div>
     </div>
@@ -90,104 +51,17 @@ export const declare_results_intro_panel = () =>
   declare_panel({
     panel_key: "results_intro",
     subject_types: ["gov", "dept"],
-    panel_config_func: (subject_type) => ({
-      legacy_non_table_dependencies:
-        subject_type === "gov"
-          ? ["requires_result_counts"]
-          : ["requires_granular_result_counts"],
+    panel_config_func: () => ({
       get_dataset_keys: () => ["actual_results", "planned_results"],
       get_title: () => text_maker("results_intro_title"),
-      calculate: ({ subject }) => {
-        const is_gov = subject.subject_type == "gov";
-
-        const verbose_counts = (() => {
-          if (is_gov) {
-            const dept_counts = ResultCounts.get_all_dept_counts();
-            const gov_counts = _.mergeWith({}, ...dept_counts, (val, src) =>
-              _.isNumber(val) ? val + src : src
-            );
-
-            const counts_by_dept = _.chain(dept_counts)
-              .map((row) => ({
-                subject: Dept.store.lookup(row.id),
-                counts: row,
-              }))
-              .map((obj) => ({ ...obj, total: sum(_.values(obj.counts)) }))
-              .value();
-            const depts_with_dps = _.sumBy(counts_by_dept, (dept) =>
-              dept.counts[`${current_dp_key}_results`] > 0 ? 1 : 0
-            );
-            const depts_with_drrs = _.sumBy(counts_by_dept, (dept) =>
-              dept.counts[`${current_drr_key}_results`] > 0 ? 1 : 0
-            );
-
-            return {
-              depts_with_dps,
-              depts_with_drrs,
-              ..._.omit(gov_counts, ["id", "subject_type", "subject_id"]),
-            };
-          } else {
-            return {
-              num_crs: _.chain(subject.crsos)
-                .map(({ id }) =>
-                  _.get(
-                    GranularResultCounts.get_subject_counts(id),
-                    `${current_dp_key}_results`
-                  )
-                )
-                .compact()
-                .size()
-                .value(),
-              num_programs: _.chain(subject.programs)
-                .map(({ id }) =>
-                  _.get(
-                    GranularResultCounts.get_subject_counts(id),
-                    `${current_dp_key}_results`
-                  )
-                )
-                .compact()
-                .size()
-                .value(),
-              ...ResultCounts.get_dept_counts(subject.id),
-            };
-          }
-        })();
-
-        const has_current_dp = verbose_counts[`${current_dp_key}_results`] > 0;
-        const has_current_drr =
-          verbose_counts[`${current_drr_key}_results`] > 0;
-
-        if (!has_current_dp && !has_current_drr) {
-          return false;
-        }
-
-        const summary_result_counts = {
-          dp_results: verbose_counts[`${current_dp_key}_results`],
-          dp_indicators: verbose_counts[`${current_dp_key}_indicators`],
-          drr_results: verbose_counts[`${current_drr_key}_results`],
-          drr_indicators: verbose_counts[`${current_drr_key}_total`],
-          num_crs: is_gov ? false : verbose_counts.num_crs,
-          num_programs: is_gov ? false : verbose_counts.num_programs,
-          depts_with_dps: is_gov ? verbose_counts.depts_with_dps : false,
-          depts_with_drrs: is_gov ? verbose_counts.depts_with_drrs : false,
-        };
-
-        const doc_urls = {
+      calculate: () => ({
+        doc_urls: {
           dp_url_year: result_docs[current_dp_key].year,
           dp_url: result_docs[current_dp_key][`doc_url_${lang}`],
           drr_url_year: result_docs[current_drr_key].year,
           drr_url: result_docs[current_drr_key][`doc_url_${lang}`],
-        };
-
-        return {
-          subject,
-          is_gov,
-          summary_result_counts,
-          doc_urls,
-          has_current_dp,
-          has_current_drr,
-        };
-      },
+        },
+      }),
       render({ title, subject, calculations, sources, datasets, footnotes }) {
         return (
           <InfographicPanel {...{ title, sources, datasets, footnotes }}>
