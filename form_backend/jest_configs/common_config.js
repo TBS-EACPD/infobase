18321e96302303e6bf7669282b89fee3337305ad
Stephen-ONeil <stephenlevioneil@gmail.com>
2022-05-06 16:46:44 -0400

    Set jest maxWorkers of 4 when in CI. May be able to increase that number. This old chestnut AGAIN. The mystery CI jest time outs were because it based it's max worker count on the number of (apparent) available threads. In CircleCI, it sees the threads available to the whole VM and doesn't know it's in a container that only gets a slice of those. During the collection step, jest would just keep piling on more workers till the job was done... sometimes starving the individual workers of resources and ensuring it never actually finished!

--- form_backend/jest_configs/common_config.js
+++ form_backend/jest_configs/common_config.js
@@ -3,6 +3,9 @@ const reporters = !process.env.DO_NOT_PRINT_COVERAGE
   : ["json"];
 
 export const common_config_rules = {
+  // jest tries to spawn # threads - 1 workers, which can lead to it starving itself in CI where it detects way more threads than it can actually use
+  ...(process.env.CIRCLECI && { maxWorkers: 4 }),
+
   rootDir: "..",
 
   coverageReporters: reporters,
