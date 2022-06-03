b518a99575907a397d984952e4c8611b9cf24afc
AlexCLeduc <ledualex@gmail.com>
2022-06-03 14:21:38 -0400

    extract generic getQuery from getSuspensedQuery for promise-based consumers

--- client/src/graphql_utils/getQuery.ts
+++ client/src/graphql_utils/getQuery.ts
@@ -3,16 +3,23 @@ import { suspend } from "suspend-react";
 
 import { get_client } from "./graphql_utils";
 
+export async function getQuery<Query, Variables>(
+  query: DocumentNode,
+  variables?: Variables
+): Promise<Query> {
+  const apolloClient = get_client();
+  const resp = await apolloClient.query({ query, variables });
+
+  return resp.data;
+}
+
 export function getSuspensedQuery<Query, Variables>(
   query: DocumentNode,
   variables?: Variables
 ): Query {
-  const apolloClient = get_client();
   const key = query?.loc?.source?.body + JSON.stringify(variables);
 
   return suspend(async () => {
-    const resp = await apolloClient.query({ query, variables });
-
-    return resp.data;
+    return getQuery<Query, Variables>(query, variables);
   }, [key]);
 }
