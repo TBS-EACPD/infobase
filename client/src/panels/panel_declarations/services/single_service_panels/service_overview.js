96e4ca766c121ad4e47d4513c7887f844cf43415
Stephane-Rancourt <sranc046@uottawa.ca>
2022-06-14 11:07:09 -0400

    Comment out last accessibilty review field in overview, too low level of reporting

--- client/src/panels/panel_declarations/services/single_service_panels/service_overview.js
+++ client/src/panels/panel_declarations/services/single_service_panels/service_overview.js
@@ -160,10 +160,10 @@ export class ServiceOverview extends React.Component {
           <dt>{text_maker("last_gender_analysis")}</dt>
           <dd>{service.last_gender_analysis}</dd>
           <dt>{text_maker("last_improve_from_feedback")}</dt>
-          <dd>{service.last_improve_from_feedback}</dd>*/}
-
+          <dd>{service.last_improve_from_feedback}</dd>
           <dt>{text_maker("last_accessibility_review")}</dt>
           <dd>{service.last_accessibility_review}</dd>
+          */}
 
           <dt>{text_maker("service_link_text")}</dt>
           <dd>
