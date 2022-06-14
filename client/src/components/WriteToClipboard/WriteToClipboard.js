866c34b9d90a21adcd719cfd1b5c202d2f95edb9
Stephen-ONeil <stephenlevioneil@gmail.com>
2022-06-14 19:07:10 -0400

    Clean up WriteToClipboard, add better handling of more types of errors it may encouter

--- client/src/components/WriteToClipboard/WriteToClipboard.js
+++ client/src/components/WriteToClipboard/WriteToClipboard.js
@@ -1,6 +1,6 @@
-import _ from "lodash";
 import React, { Fragment } from "react";
 
+import { AlertBanner } from "src/components/AlertBanner/AlertBanner";
 import { FixedPopover } from "src/components/modals_and_popovers/index";
 
 import { create_text_maker } from "src/models/text";
@@ -15,14 +15,16 @@ import text from "./WriteToClipboard.yaml";
 
 const text_maker = create_text_maker(text);
 
+const default_state = {
+  is_model_open: false,
+  is_successfully_copied: null,
+  keyboard_navigation_detected: false,
+};
 export class WriteToClipboard extends React.Component {
   constructor() {
     super();
 
-    this.state = {
-      copy_status_message: false,
-      keyboard_navigation_detected: false,
-    };
+    this.state = default_state;
   }
   render() {
     const {
@@ -33,25 +35,34 @@ export class WriteToClipboard extends React.Component {
       icon_color,
     } = this.props;
 
-    const { copy_status_message, keyboard_navigation_detected } = this.state;
+    const {
+      is_model_open,
+      is_successfully_copied,
+      keyboard_navigation_detected,
+    } = this.state;
 
-    const modal_active = _.isString(copy_status_message);
-    const copy_success = copy_status_message === text_maker("copy_success");
     return (
       <Fragment>
         <button
           className={button_class_name}
           onClick={() => {
             debounced_log_standard_event("WRITE_TO_CLIPBOARD", text_to_copy);
-            window.navigator.clipboard
-              .writeText(text_to_copy)
-              .then(() =>
+
+            // wraping the API call itself in a promise so that errors where clipboard/writeText itself in undefined are handled by the same copy fail logic
+            new Promise((resolve) =>
+              resolve(window.navigator.clipddddboard.writeText(text_to_copy))
+            )
+              .then(() => {
                 this.setState({
-                  copy_status_message: text_maker("copy_success"),
-                })
-              )
+                  is_model_open: true,
+                  is_successfully_copied: true,
+                });
+              })
               .catch(() =>
-                this.setState({ copy_status_message: text_maker("copy_fail") })
+                this.setState({
+                  is_model_open: true,
+                  is_successfully_copied: false,
+                })
               );
           }}
           onKeyDown={() =>
@@ -65,10 +76,8 @@ export class WriteToClipboard extends React.Component {
           />
         </button>
         <FixedPopover
-          on_close_callback={() =>
-            this.setState({ copy_status_message: false })
-          }
-          show={modal_active}
+          show={is_model_open}
+          on_close_callback={() => this.setState(default_state)}
           title={
             <Fragment>
               <IconCopy
@@ -76,25 +85,28 @@ export class WriteToClipboard extends React.Component {
                 alternate_color={false}
                 aria_hide={true}
               />
-              {modal_active
-                ? copy_success
-                  ? copy_status_message
-                  : text_maker("copy_to_clipboard")
-                : ""}
+              {text_maker("copy_to_clipboard")}
             </Fragment>
           }
-          subtitle={modal_active && !copy_success && copy_status_message}
+          subtitle={
+            is_model_open && (
+              <AlertBanner
+                banner_class={is_successfully_copied ? "success" : "danger"}
+                style={{ marginBottom: "0px" }}
+              >
+                {is_successfully_copied
+                  ? text_maker("copy_success")
+                  : text_maker("copy_fail")}
+              </AlertBanner>
+            )
+          }
           body={
-            modal_active &&
-            copy_success && (
-              /* eslint-disable jsx-a11y/no-noninteractive-tabindex */
-              <div tabIndex={0}>{text_to_copy}</div>
+            is_model_open && (
+              <p style={{ whiteSpace: "pre-wrap" }}>{text_to_copy}</p>
             )
           }
           dialog_position="left"
-          auto_close_time={
-            !is_a11y_mode && modal_active && copy_success && 1900
-          }
+          auto_close_time={!is_a11y_mode && is_successfully_copied && 1900}
           close_button_in_header={!is_a11y_mode}
           restore_focus={is_a11y_mode || keyboard_navigation_detected}
         />
