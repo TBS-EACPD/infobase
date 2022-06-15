1e0b921c583a37b73bcd65de8a043e713054cb9e
Stephen-ONeil <stephenlevioneil@gmail.com>
2022-06-15 10:44:48 -0400

    Post rebase reconcilliation of cypress snapshot changes

--- client/cypress/integration/InfoBase/cypress-snapshots.js
+++ client/cypress/integration/InfoBase/cypress-snapshots.js
@@ -159,10 +159,8 @@ module.exports = {
             "<span>Datasets</span>",
             "<span>Service Inventory</span>",
             "<span>Applications by telephone</span>",
-            "<span class=\"infobase-pie__legend-data\">1,327,740</span>",
-            "<span class=\"text-nowrap\">0.4%</span>",
-            "<strong>2019-20</strong>",
-            "<p>"
+            "<span class=\"infobase-pie__legend-data\">5,675,706</span>",
+            "<span class=\"text-nowrap\">2.1%</span>"
           ]
         }
       },
