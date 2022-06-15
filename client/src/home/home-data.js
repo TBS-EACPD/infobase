029862e8386ea98e8b6ebc9d8a95147024f41abc
Stephane-Rancourt <sranc046@uottawa.ca>
2022-06-15 11:48:06 -0400

    Featured content badge for service inventory 2020-21

--- client/src/home/home-data.js
+++ client/src/home/home-data.js
@@ -70,6 +70,11 @@ const infographic_link_items = _.compact([
 
 const featured_content_items = _.compact([
   {
+    text_key: "quick_link_service_inventory_2020",
+    href: "#infographic/gov/gov/services/.-.-(panel_key.-.-'services_intro)",
+    is_new: "true",
+  },
+  {
     text_key: "supps_a",
     href: "#compare_estimates",
     is_new: "true",
@@ -86,10 +91,6 @@ const featured_content_items = _.compact([
   {
     text_key: "quick_link_DRR20",
     href: "#infographic/gov/gov/results/.-.-(panel_key.-.-'gov_drr)",
-  },
-  {
-    text_key: "quick_link_service_inventory_2019",
-    href: "#infographic/gov/gov/services/.-.-(panel_key.-.-'services_intro)",
   },
   {
     text_key: "quick_link_people_2021",
