d45a94ad955cd3996af212b8bf50a8008ecf0f95
Stephen-ONeil <stephenlevioneil@gmail.com>
2022-07-19 15:47:04 -0400

    Delete (vestigial) server side transfer payments by location code

--- server/src/models/transfer_payments_loc/index.js
+++
@@ -1,9 +0,0 @@
-import define_models from "./models.js";
-import populate_models from "./populate.js";
-import define_schema from "./schema.js";
-
-export default {
-  define_schema,
-  populate_models,
-  define_models,
-};
