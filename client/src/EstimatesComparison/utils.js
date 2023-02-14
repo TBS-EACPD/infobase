6e6563da4401746cae15a39e9315fc14e72d46e0
Eric Germond <egerm010@uottawa.ca>
2023-02-14 14:08:34 -0500

    Ran npx prettier

--- client/src/EstimatesComparison/utils.js
+++ client/src/EstimatesComparison/utils.js
@@ -10,4 +10,4 @@ export const { text_maker, TM } = create_text_maker_component(text);
 
 export const current_doc_is_mains = CURRENT_EST_DOC === "MAINS";
 export const current_sups_letter = "C";
-  ///^SE(A|B|C)$/.test(CURRENT_EST_DOC) && _.last(CURRENT_EST_DOC);
+///^SE(A|B|C)$/.test(CURRENT_EST_DOC) && _.last(CURRENT_EST_DOC);
