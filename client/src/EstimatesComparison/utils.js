20c0acce6212f08f9acb9b69db0967c8c7ca8815
Eric Germond <egerm010@uottawa.ca>
2023-02-14 13:59:27 -0500

    Edited common_lang years,added main link in featured,and hardcoded sups letter C

--- client/src/EstimatesComparison/utils.js
+++ client/src/EstimatesComparison/utils.js
@@ -1,4 +1,4 @@
-import _ from "lodash";
+//import _ from "lodash";
 
 import { create_text_maker_component } from "src/components/index";
 
@@ -9,5 +9,5 @@ import text from "./EstimatesComparison.yaml";
 export const { text_maker, TM } = create_text_maker_component(text);
 
 export const current_doc_is_mains = CURRENT_EST_DOC === "MAINS";
-export const current_sups_letter =
-  /^SE(A|B|C)$/.test(CURRENT_EST_DOC) && _.last(CURRENT_EST_DOC);
+export const current_sups_letter = "C";
+  ///^SE(A|B|C)$/.test(CURRENT_EST_DOC) && _.last(CURRENT_EST_DOC);
