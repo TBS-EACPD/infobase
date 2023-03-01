697cb646d443d90227c9e1094ea5002c8f400e87
Eric Germond <egerm010@uottawa.ca>
2023-03-01 11:31:08 -0500

    Seemingly swapped option_range to proper number array type, ran npx prettier

--- client/src/components/DisplayTable/DisplayTableUtils.tsx
+++ client/src/components/DisplayTable/DisplayTableUtils.tsx
@@ -1,13 +1,19 @@
-import { JSXAttribute } from "@babel/types";
-import { UnknownReturnType } from "d3-scale";
-import _, { Dictionary, List } from "lodash";
-import React, { Fragment, JSXElementConstructor, useState } from "react";
+import _ from "lodash";
+
+import type { List } from "lodash";
+
+import React, { Fragment, useState } from "react";
 
 import { CheckBox } from "src/components/CheckBox/CheckBox";
+
 import { DebouncedTextInput } from "src/components/DebouncedTextInput/DebouncedTextInput";
+
 import { DropdownMenu } from "src/components/DropdownMenu/DropdownMenu";
+
 import { create_text_maker_component } from "src/components/misc_util_components";
+
 import { SelectAllControl } from "src/components/SelectAllControl";
+
 import { WriteToClipboard } from "src/components/WriteToClipboard/WriteToClipboard";
 
 import { IconCopy, IconDownload, IconFilter } from "src/icons/icons";
@@ -17,11 +23,11 @@ import {
   tertiaryColor,
   secondaryColor,
 } from "src/style_constants/index";
-import { _DisplayTable } from "./DisplayTable";
-import { _DisplayTableState } from "./DisplayTable";
-import { FilterOption } from "./DisplayTable";
+
+import type { _DisplayTable, FilterOption } from "./DisplayTable";
 
 import text from "./DisplayTable.yaml";
+
 import "./DisplayTableUtils.scss";
 
 const { text_maker, TM } = create_text_maker_component(text);
@@ -31,7 +37,12 @@ export const DropdownFilter = ({
   set_filter_options,
   filter_options_by_column,
   column_searches,
-}: {column_key: string,set_filter_options: any,filter_options_by_column: {[col_keys:string]: [FilterOption]},column_searches: {[keys:string]: string} }) => {
+}: {
+  column_key: string;
+  set_filter_options: any;
+  filter_options_by_column: { [col_keys: string]: [FilterOption] };
+  column_searches: { [keys: string]: string };
+}) => {
   const [search, set_search] = useState("");
   const clean_search_string = (string: string) =>
     _.chain(string).deburr().toLower().trim().value();
@@ -73,25 +84,38 @@ export const DropdownFilter = ({
           {filtered_list.length > 0 ? (
             <>
               <div className="display-table-dropdown-filter">
-                {_.map(filtered_list, ({ id, label, active, color }: {id: string, label: string, active: boolean, color: string}) => (
-                  <CheckBox
-                    id={id}
-                    key={id}
-                    color={color}
-                    label={label}
-                    active={active}
-                    container_style={{ marginTop: "10px" }}
-                    label_style={{ textAlign: "left" }}
-                    onClick={() =>
-                      set_filter_options({
-                        ...filter_options_by_column,
-                        [column_key]: _.map(list, (item) =>
-                          item.id === id ? { ...item, active: !active } : item
-                        ),
-                      })
-                    }
-                  />
-                ))}
+                {_.map(
+                  filtered_list,
+                  ({
+                    id,
+                    label,
+                    active,
+                    color,
+                  }: {
+                    id: string;
+                    label: string;
+                    active: boolean;
+                    color: string;
+                  }) => (
+                    <CheckBox
+                      id={id}
+                      key={id}
+                      color={color}
+                      label={label}
+                      active={active}
+                      container_style={{ marginTop: "10px" }}
+                      label_style={{ textAlign: "left" }}
+                      onClick={() =>
+                        set_filter_options({
+                          ...filter_options_by_column,
+                          [column_key]: _.map(list, (item) =>
+                            item.id === id ? { ...item, active: !active } : item
+                          ),
+                        })
+                      }
+                    />
+                  )
+                )}
               </div>
               <div style={{ marginBottom: "5px" }}>
                 <SelectAllControl
@@ -125,7 +149,7 @@ export const DropdownFilter = ({
   );
 };
 
-export const DisplayTableCopyCsv = ({ csv_string }: {csv_string: string}) => (
+export const DisplayTableCopyCsv = ({ csv_string }: { csv_string: string }) => (
   <WriteToClipboard
     text_to_copy={csv_string}
     button_class_name={"display_table__util-icon"}
@@ -135,7 +159,11 @@ export const DisplayTableCopyCsv = ({ csv_string }: {csv_string: string}) => (
   />
 );
 
-export const DisplayTableColumnToggle = ({ columns }: {columns: JSX.Element}) => (
+export const DisplayTableColumnToggle = ({
+  columns,
+}: {
+  columns: JSX.Element;
+}) => (
   <DropdownMenu
     opened_button_class_name={"btn-ib-light--reversed"}
     closed_button_class_name={"btn-ib-light"}
@@ -158,7 +186,13 @@ const download_csv = (csv_string: string, table_name: string | undefined) => {
   temporary_anchor.setAttribute("href", uri);
   temporary_anchor.dispatchEvent(new MouseEvent("click"));
 };
-export const DisplayTableDownloadCsv = ({ csv_string, table_name }: {csv_string: string, table_name: string | undefined}) => (
+export const DisplayTableDownloadCsv = ({
+  csv_string,
+  table_name,
+}: {
+  csv_string: string;
+  table_name: string | undefined;
+}) => (
   <Fragment>
     <button
       onClick={() => download_csv(csv_string, table_name)}
@@ -172,7 +206,11 @@ export const DisplayTableDownloadCsv = ({ csv_string, table_name }: {csv_string:
   </Fragment>
 );
 
-const _is_ellipsis = (page_options: number[], page_num: number, index: number) =>
+const _is_ellipsis = (
+  page_options: number[],
+  page_num: number,
+  index: number
+) =>
   index !== 0 &&
   index !== page_options.length - 1 &&
   (page_options[index + 1] !== page_num + 1 ||
@@ -183,7 +221,12 @@ export const SelectPage = ({
   num_pages,
   change_page,
   num_col,
-}: {current_page: number, num_pages: number, change_page: _DisplayTable["change_page"],num_col:number}) => {
+}: {
+  current_page: number;
+  num_pages: number;
+  change_page: _DisplayTable["change_page"];
+  num_col: number;
+}) => {
   /*
    * TODO: refactor/clean up the following logic to be a lot more dense/simple to understand
    * To future devs: you probably don't need to understand how this logic works unless changes
@@ -204,18 +247,21 @@ export const SelectPage = ({
       return _.range(window_start, window_end + 1);
     }
   };
-  const extend_options_left = (option_range: any, page_count: number, current_page: number) => {
-    console.log(typeof(option_range));
+  const extend_options_left = (
+    option_range: number[],
+    page_count: number,
+    current_page: number
+  ) => {
     if (_.first(option_range) === 1) {
       //option range sees page 1, add 2 to the end
       return [
         ...option_range,
-       (_.last(option_range) as any) + 1,
-       (_.last(option_range) as any) + 2,
+        (_.last(option_range) as number) + 1,
+        (_.last(option_range) as number) + 2,
       ];
     } else if (_.first(option_range) === 2) {
       //option range sees page 2 (page 1 is not visible so manually add it)
-      return [1, ...option_range, (_.last(option_range) as any) + 1];
+      return [1, ...option_range, (_.last(option_range) as number) + 1];
     } else if (_.first(option_range) === 3) {
       //option range sees page 3 (page 1 and 2 not visible, manually add it)
       return [1, 2, ...option_range];
@@ -223,24 +269,28 @@ export const SelectPage = ({
       //being on the forth last page manages to be short 1 option
       return [
         1,
-        (_.first(option_range) as any) - 2,
-        (_.first(option_range) as any) - 1,
+        (_.first(option_range) as number) - 2,
+        (_.first(option_range) as number) - 1,
         ...option_range,
       ];
     } else if (current_page > page_count - 3) {
       //being in the last 3 pages manages to be short 2 options
       return [
         1,
-        (_.first(option_range) as any) - 3,
-        (_.first(option_range) as any) - 2,
-        (_.first(option_range) as any) - 1,
+        (_.first(option_range) as number) - 3,
+        (_.first(option_range) as number) - 2,
+        (_.first(option_range) as number) - 1,
         ...option_range,
       ];
     } else {
       return option_range;
     }
   };
-  const extend_options_right = (option_range: any, page_count: number, option_window: number) => {
+  const extend_options_right = (
+    option_range: number[],
+    page_count: number,
+    option_window: number
+  ) => {
     if (
       _.includes(option_range, page_count - option_window) &&
       _.last(option_range) !== page_count
@@ -248,13 +298,17 @@ export const SelectPage = ({
       //being on the 4th or 5th last page doesn't capture the ending extremities
       return [
         ...option_range,
-        ..._.range((_.last(option_range) as any) + 1, page_count + 1),
+        ..._.range((_.last(option_range) as number) + 1, page_count + 1),
       ];
     } else {
       return option_range;
     }
   };
-  const get_page_options = (page_count: number, current_page: number, option_window = 2) => {
+  const get_page_options = (
+    page_count: number,
+    current_page: number,
+    option_window = 2
+  ) => {
     //depending on the sze of the options, we may have to format to ensure the correct number of options are shown
     //the if check below is where we determine if we have the correct number of options
     const raw_page_options: List<number> = _.chain(
@@ -267,7 +321,7 @@ export const SelectPage = ({
       )
       .thru((option_range) =>
         extend_options_right(option_range, page_count, option_window)
-      ) 
+      )
       .filter((page_index: number) => _.inRange(page_index, 1, page_count + 1))
       .uniq()
       .sortBy()
@@ -280,11 +334,11 @@ export const SelectPage = ({
         //correct number of options
         return raw_page_options;
       } else if (raw_page_options.length === option_window * 2 + 1) {
-        //we are missing 4 options (2 ellipses and 2 extremeties)   
+        //we are missing 4 options (2 ellipses and 2 extremeties)
         return [
           1,
           (_.first(raw_page_options) as any) - 1,
-          ...raw_page_options as any,
+          ...(raw_page_options as any),
           (_.last(raw_page_options) as any) + 1,
           page_count,
         ];
@@ -292,7 +346,11 @@ export const SelectPage = ({
         //handling cases for being in the extremities
         if (!_.includes(raw_page_options, 1)) {
           //if we are close to the very front, we must manually add the ellipsis value and the very last value
-          return [1, (_.first(raw_page_options) as any) - 1, ...(raw_page_options as any)];
+          return [
+            1,
+            (_.first(raw_page_options) as any) - 1,
+            ...(raw_page_options as any),
+          ];
         } else if (!_.includes(raw_page_options, page_count)) {
           //same thing here except we are close to the very end
           return [
@@ -328,7 +386,7 @@ export const SelectPage = ({
                   } page-selector`}
                   onClick={() => change_page(page_num - 1)}
                   aria-label={
-                    _is_ellipsis((page_options as any), page_num, index)
+                    _is_ellipsis(page_options as any, page_num, index)
                       ? (page_options as any)[index + 1] !== page_num + 1
                         ? text_maker("ellipsis_end", { page_num: page_num })
                         : text_maker("ellipsis_start", { page_num: page_num })
@@ -337,7 +395,7 @@ export const SelectPage = ({
                   role="tab"
                   aria-selected={page_num - 1 === current_page}
                   dangerouslySetInnerHTML={{
-                    __html: _is_ellipsis((page_options as any), page_num, index)
+                    __html: _is_ellipsis(page_options as any, page_num, index)
                       ? "&hellip;"
                       : page_num,
                   }}
@@ -357,7 +415,13 @@ export const SelectPageSize = ({
   page_size_increment,
   num_items,
   num_options_max,
-}: {selected: number | undefined, on_select: _DisplayTable["change_page_size"], page_size_increment: number,num_items:number,num_options_max:number}) => {
+}: {
+  selected: number | undefined;
+  on_select: _DisplayTable["change_page_size"];
+  page_size_increment: number;
+  num_items: number;
+  num_options_max: number;
+}) => {
   const num_options =
     _.ceil(num_items / page_size_increment) > num_options_max
       ? num_options_max
