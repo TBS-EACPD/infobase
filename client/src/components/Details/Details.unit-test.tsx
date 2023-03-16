dfeb1526889443305bffa03a981c254a2acf3978
Eric Germond <egerm010@uottawa.ca>
2023-03-16 16:43:03 -0400

    First summary content and content rendering tests (tests with both unopened and opened details)

---
+++ client/src/components/Details/Details.unit-test.tsx
@@ -0,0 +1,42 @@
+import { render, screen, fireEvent } from "@testing-library/react";
+
+import _ from "lodash";
+
+import React from "react";
+
+import { Details } from "./Details";
+
+describe("Details", () => {
+  it("initially renders summary content, but not content without opening", async () => {
+    const summary_content = "Details title";
+    const content = "Details content";
+
+    render(
+      <Details
+        {...{
+          summary_content: <div>{summary_content}</div>,
+          content: <div>{content}</div>,
+        }}
+      />
+    );
+    expect(screen.getByText("Details title")).toBeInTheDocument();
+    expect(screen.queryByText("Details content")).toBeNull();
+  });
+
+  it("initially renders summary content, as well as content after opening", async () => {
+    const summary_content = "Details title";
+    const content = "Details content";
+
+    render(
+      <Details
+        {...{
+          summary_content: <div>{summary_content}</div>,
+          content: <div>{content}</div>,
+        }}
+      />
+    );
+    expect(screen.getByText("Details title")).toBeInTheDocument();
+    fireEvent.click(screen.getByRole("button"));
+    expect(screen.getByText("Details content")).toBeVisible();
+  });
+});
