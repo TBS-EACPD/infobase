2c9fb4505c800819ea50382e408018adaac3a666
Eric Germond <egerm010@uottawa.ca>
2023-03-17 14:36:04 -0400

    Interactive child button test

--- client/src/components/LabeledBox/LabeledBox.unit-test.tsx
+++ client/src/components/LabeledBox/LabeledBox.unit-test.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 
 import React from "react";
 
@@ -16,5 +16,27 @@ describe("LabeledBox", () => {
     );
     expect(screen.queryByText("label display")).toBeInTheDocument();
     expect(screen.queryByText("label child")).toBeInTheDocument();
+  });
+
+  it("renders an interactable child", async () => {
+    const on_click = jest.fn();
+
+    render(
+      <LabeledBox
+        {...{
+          label: "label display" as React.ReactNode,
+          children: (
+            <div>
+              <button onClick={on_click}>Test1</button>
+            </div>
+          ),
+        }}
+      />
+    );
+    expect(screen.queryByText("Test1")).toBeInTheDocument();
+    expect(on_click).toHaveBeenCalledTimes(0);
+    const button1 = screen.queryByRole("button");
+    fireEvent.click(button1 as HTMLButtonElement);
+    expect(on_click).toHaveBeenCalledTimes(1);
   });
 });
