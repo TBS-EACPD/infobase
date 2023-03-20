9a1eff87e7a1e330cb9244ee24a557944508d607
Eric Germond <egerm010@uottawa.ca>
2023-03-20 14:29:28 -0400

    Rendering optional attributes and empty content tests, added content display check

--- client/src/components/LabeledTable/LabeledTable.unit-test.tsx
+++ client/src/components/LabeledTable/LabeledTable.unit-test.tsx
@@ -4,6 +4,8 @@ import React from "react";
 
 import { LabeledTable } from "./LabeledTable";
 
+import "client/src/components/LabeledTable/site.scss";
+
 const testing_no_ids = {
   options: [
     {
@@ -17,21 +19,20 @@ const testing_no_ids = {
   ],
 };
 
-/*const testing_with_ids = {
+const testing_with_ids = {
   options: [
     {
       id: "1",
       label: "item1",
-      content: "content1"
+      content: "content1",
     },
     {
       id: "2",
       label: "item2",
-      content: "content2"
+      content: "content2",
     },
   ],
 };
-*/
 
 describe("LabeledTable", () => {
   it("Initially renders labeled table with no optional id's or titlecomponents", async () => {
@@ -43,54 +44,40 @@ describe("LabeledTable", () => {
     expect(screen.queryByText("Test table")).toBeInTheDocument();
     expect(screen.queryByText("item1")).toBeInTheDocument();
     expect(screen.queryByText("item2")).toBeInTheDocument();
+    expect(screen.queryByText("content1")).toBeInTheDocument();
+    expect(screen.queryByText("content2")).toBeInTheDocument();
   });
-  /*
 
   it("Renders labeled table with content's with id's and a TitleComponent for the table", async () => {
-
-  }
-
-  /*
-
-  it("Renders an interactable child", async () => {
-    const on_click = jest.fn();
-
     render(
-      <LabeledBox
+      <LabeledTable
         {...{
-          label: "label display" as React.ReactNode,
-          children: (
-            <div>
-              <button onClick={on_click}>Test1</button>
-            </div>
+          title: "Test table",
+          contents: testing_with_ids.options,
+          TitleComponent: ({ children }) => (
+            <h2 className="heading-unstyled">{children}</h2>
           ),
         }}
       />
     );
-    expect(screen.queryByText("Test1")).toBeInTheDocument();
-    expect(on_click).toHaveBeenCalledTimes(0);
-    const button1 = screen.queryByRole("button");
-    fireEvent.click(button1 as HTMLButtonElement);
-    expect(on_click).toHaveBeenCalledTimes(1);
+    expect(screen.queryByText("Test table")).toBeInTheDocument();
+    expect(screen.queryByText("item1")).toBeInTheDocument();
+    expect(screen.queryByText("item2")).toBeInTheDocument();
+    expect(screen.queryByText("content1")).toBeInTheDocument();
+    expect(screen.queryByText("content2")).toBeInTheDocument();
   });
 
-  it("Renders multiple children", async () => {
+  it("Renders labeledtable with empty content array", async () => {
     render(
-      <LabeledBox
+      <LabeledTable
         {...{
-          label: "label display" as React.ReactNode,
-          children: [
-            <div key="test1">
-              <button>Test1</button>
-            </div>,
-            <div key="test2">
-              <button>Test2</button>
-            </div>,
-          ],
+          title: "Test table",
+          contents: [],
+          TitleComponent: ({ children }) => (
+            <h2 className="heading-unstyled">{children}</h2>
+          ),
         }}
       />
     );
-    expect(screen.queryByText("Test1")).toBeInTheDocument();
-    expect(screen.queryByText("Test2")).toBeInTheDocument();
-    */
+  });
 });
