e244775687b2af826a1dc074c4e0ad530e800f24
Eric Germond <egerm010@uottawa.ca>
2023-04-19 16:28:56 -0400

    Added translation option for text

--- client/src/home/home-data.js
+++ client/src/home/home-data.js
@@ -21,9 +21,31 @@ import {
 
 import infographic_sections_text_bundle from "src/infographic/bubble_definitions.yaml";
 
+import { lang } from "src/core/injected_build_constants";
+
 const { text_maker } = create_text_maker_component([
   infographic_sections_text_bundle,
 ]);
+
+var finance_details;
+var people_details;
+var results_details;
+
+if (lang === "en") {
+  finance_details =
+    ". In 2021-22, the government spent $376.1 billion. The three largest items in terms of spending were: Old Age Security payments (Old Age Security Act) - Employment and Social Development Canada, Canada Health Transfer (Part V.1 - Federal-Provincial Fiscal Arrangements Act) - Department of Finance Canada, Fiscal Equalization (Part I - Federal-Provincial Fiscal Arrangements Act) - Department of Finance Canada. Click to learn more.";
+  people_details =
+    ". As of March 31st, 2021 there were 319,601 employees in the Federal Public Service , with 42.2% located in the National Capital Region. Click to learn more.";
+  results_details =
+    ". In 2021-22, the federal government sought to achieve 1501 results. Progress towards meeting these results was measured using 2,698 indicators . Of those indicators, 1,343 (50%) have met their target. Click to learn more.";
+} else if (lang === "fr") {
+  finance_details =
+    ". En 2021-2022, le gouvernement a dépensé 376,1 milliards de dollars. Les trois items suivant représentaient la plus grande partie des dépenses : Versements de la Sécurité de la vieillesse (Loi sur la sécurité de la vieillesse) - Emploi et Développement social Canada, Transfert canadien en matière de santé (Partie V.1 - Loi sur les arrangements fiscaux entre le gouvernement fédéral et les provinces) - Ministère des Finances Canada, Péréquation fiscale (Partie I - Loi sur les arrangements fiscaux entre le gouvernement fédéral et les provinces) - Ministère des Finances Canada. Cliquez pour en savoir plus.";
+  people_details =
+    ". Au 31 mars 2021, il y avait 319 601 employés au sein de la fonction publique générale , dont 42,2 % travaillaient dans la région de la capitale nationale. Cliquez pour en savoir plus.";
+  results_details =
+    ". En 2021-2022, le gouvernement fédéral a cherché à rencontrer 1516 résultats. Les progrès accomplis dans l'atteinte de ces résultats ont été mesurés à l’aide de 2 731 indicateurs . De ces indicateurs, 1 361 (50 %) ont atteint leur cible. Cliquez pour en savoir plus.";
+}
 
 const infographic_link_items = _.compact([
   {
@@ -35,9 +57,8 @@ const infographic_link_items = _.compact([
       text_maker(
         "financial_title"
       ) /*+ "In 2021-22, the government spent $376.1 billion. The three largest items in terms of spending were: Old Age Security payments (Old Age Security Act) - Employment and Social Development Canada, Canada Health Transfer (Part V.1 - Federal-Provincial Fiscal Arrangements Act) - Department of Finance Canada, Fiscal Equalization (Part I - Federal-Provincial Fiscal Arrangements Act) - Department of Finance Canada"*/,
-    description:
-      text_maker("financial_desc") +
-      ". In 2021-22, the government spent $376.1 billion. The three largest items in terms of spending were: Old Age Security payments (Old Age Security Act) - Employment and Social Development Canada, Canada Health Transfer (Part V.1 - Federal-Provincial Fiscal Arrangements Act) - Department of Finance Canada, Fiscal Equalization (Part I - Federal-Provincial Fiscal Arrangements Act) - Department of Finance Canada. Click to learn more.",
+    description: text_maker("financial_desc") + finance_details,
+    //". In 2021-22, the government spent $376.1 billion. The three largest items in terms of spending were: Old Age Security payments (Old Age Security Act) - Employment and Social Development Canada, Canada Health Transfer (Part V.1 - Federal-Provincial Fiscal Arrangements Act) - Department of Finance Canada, Fiscal Equalization (Part I - Federal-Provincial Fiscal Arrangements Act) - Department of Finance Canada. Click to learn more.",
   },
   {
     href: "#infographic/gov/gov/covid",
@@ -51,9 +72,8 @@ const infographic_link_items = _.compact([
       <IconEmployeesAlt width="100%" color="#FFFFFF" alternate_color={false} />
     ),
     title: text_maker("people_title"),
-    description:
-      text_maker("people_desc") +
-      ". As of March 31st, 2021 there were 319,601 employees in the Federal Public Service , with 42.2% located in the National Capital Region. Click to learn more.",
+    description: text_maker("people_desc") + people_details,
+    //". As of March 31st, 2021 there were 319,601 employees in the Federal Public Service , with 42.2% located in the National Capital Region. Click to learn more.",
   },
   {
     href: "#infographic/gov/gov/results",
@@ -61,9 +81,8 @@ const infographic_link_items = _.compact([
       <IconClipboardAlt width="100%" color="#FFFFFF" alternate_color={false} />
     ),
     title: text_maker("results_title"),
-    description:
-      text_maker("results_desc") +
-      ". In 2021-22, the federal government sought to achieve 1501 results. Progress towards meeting these results was measured using 2,698 indicators . Of those indicators, 1,343 (50%) have met their target. Click to learn more.",
+    description: text_maker("results_desc") + results_details,
+    //". In 2021-22, the federal government sought to achieve 1501 results. Progress towards meeting these results was measured using 2,698 indicators . Of those indicators, 1,343 (50%) have met their target. Click to learn more.",
   },
   services_feature_flag && {
     href: "#infographic/gov/gov/services",
