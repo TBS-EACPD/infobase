577f77b286d6c0e7eb86693de169c42aabd1368a
Eric Germond <egerm010@uottawa.ca>
2023-04-25 12:16:52 -0400

    First test

---
+++ client/src/components/CountdownCircle/CountdownCircle.unit-test.tsx
@@ -0,0 +1,25 @@
+import { render, screen } from "@testing-library/react";
+
+import React from "react";
+
+import { CountdownCircle } from "./CountdownCircle";
+
+describe("CountdownCircle", () => {
+  it("Checks that countdown circle does not display numbers when show_numbers is default (false)", async () => {
+    const on_end_callback = jest.fn();
+    const time = 2000;
+    // render(<CountdownCircle time={2000} on_end_callback={onEnd()} show_numbers={true} />);
+    render(<CountdownCircle {...{ time, on_end_callback }} />);
+    expect(screen.queryByText(2)).toBeNull();
+
+    await wait1Second();
+    expect(screen.queryByText(1)).toBeNull();
+
+    await wait1Second();
+    expect(screen.queryByText(0)).toBeNull();
+
+    function wait1Second() {
+      return new Promise((resolve) => setTimeout(resolve, 1000));
+    }
+  });
+});
