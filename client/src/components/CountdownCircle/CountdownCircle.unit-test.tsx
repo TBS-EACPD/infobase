733445dcd8b4d8fd8c85f73cd81d21810c5a6f15
Eric Germond <egerm010@uottawa.ca>
2023-04-28 09:48:56 -0400

    Testing when countdowncircle has show_numbers as true

--- client/src/components/CountdownCircle/CountdownCircle.unit-test.tsx
+++ client/src/components/CountdownCircle/CountdownCircle.unit-test.tsx
@@ -8,7 +8,7 @@ describe("CountdownCircle", () => {
   it("Checks that countdown circle does not display numbers when show_numbers is default (false)", async () => {
     const on_end_callback = jest.fn();
     const time = 2000;
-    // render(<CountdownCircle time={2000} on_end_callback={onEnd()} show_numbers={true} />);
+    //render(<CountdownCircle time={2000} on_end_callback={onEnd()} show_numbers={true} />);
     render(<CountdownCircle {...{ time, on_end_callback }} />);
     expect(screen.queryByText(2)).toBeNull();
 
@@ -17,6 +17,25 @@ describe("CountdownCircle", () => {
 
     await wait1Second();
     expect(screen.queryByText(0)).toBeNull();
+
+    function wait1Second() {
+      return new Promise((resolve) => setTimeout(resolve, 1000));
+    }
+  });
+
+  it("Checks that countdown circle displays numbers when show_numbers is true", async () => {
+    const on_end_callback = jest.fn();
+    const time = 2000;
+    render(
+      <CountdownCircle {...{ time, on_end_callback, show_numbers: true }} />
+    );
+    expect(screen.queryByText(2)).toBeVisible();
+
+    await wait1Second();
+    expect(screen.queryByText(1)).toBeVisible();
+
+    await wait1Second();
+    expect(screen.queryByText(0)).toBeVisible();
 
     function wait1Second() {
       return new Promise((resolve) => setTimeout(resolve, 1000));
