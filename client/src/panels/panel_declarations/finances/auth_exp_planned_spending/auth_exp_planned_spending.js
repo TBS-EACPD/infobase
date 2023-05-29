c3c72378ed4ecaa04765fc09225b931b8e936723
Hiruni <hirunisenarath@gmail.com>
2023-05-29 12:42:15 -0400

    Add new topic_key for footnotes on five-year trend and use get_topic_key to extend the topic keys for auth,exp,planned_spending pannel to include the new 5YEAR_TREND topic_key

--- client/src/panels/panel_declarations/finances/auth_exp_planned_spending/auth_exp_planned_spending.js
+++ client/src/panels/panel_declarations/finances/auth_exp_planned_spending/auth_exp_planned_spending.js
@@ -753,6 +753,9 @@ export const declare_auth_exp_planned_spending_panel = () =>
         "program_spending",
         "tabled_estimates",
       ],
+      get_topic_keys: ({ derived_topic_keys }) => {
+        return [...derived_topic_keys, "5YEAR_TREND"];
+      },
       glossary_keys: ["BUD_EXP", "NB_EXP"],
       get_title: ({ subject }) =>
         text_maker("auth_exp_planned_spending_title", {
