d8fcd137d0befa22537fe79f022980a2ad5d9ba2
Stephane-Rancourt <rancourt.steph@gmail.com>
2023-10-24 15:42:44 -0400

    Commenting out late banner, only needed for later FTE update

--- client/src/models/footnotes/dynamic_footnotes.ts
+++ client/src/models/footnotes/dynamic_footnotes.ts
@@ -22,15 +22,15 @@ export const PRE_DRR_PUBLIC_ACCOUNTS_LATE_FTE_MOCK_DOC = {
   doc_type: "drr",
   year: run_template("{{pa_last_year}}"),
   late_results_orgs: [
-    "46", // Canada Revenue Agency
+    /*"46", // Canada Revenue Agency
     "221", // Canadian Energy Regulator
     "138", // Department of Transport
     "128", // Department of Employment and Social Development
     "199", // Library and Archives of Canada
-    "326", // Treasury Board Secretariat
+    "326", // Treasury Board Secretariat*/
   ] as string[],
   late_resources_orgs: [
-    "46", // Canada Revenue Agency
+    /*"46", // Canada Revenue Agency
     "221", // Canadian Energy Regulator
     "138", // Department of Transport
     "98", // Canadian Transportation Accident Investigation and Safety Board
@@ -39,7 +39,7 @@ export const PRE_DRR_PUBLIC_ACCOUNTS_LATE_FTE_MOCK_DOC = {
     "133", // Department of National Defence
     "302", // National Security and Intelligence Review Agency Secretariat
     "278", // Privy Council Office
-    "345", // Secretariat of the National Security and Intelligence Committee of Parliamentarians
+    "345", // Secretariat of the National Security and Intelligence Committee of Parliamentarians*/
   ] as string[],
 };
 
