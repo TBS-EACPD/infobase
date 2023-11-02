a1b93ed1c66628b178e55ac8439011af982a11cb
Stephane-Rancourt <rancourt.steph@gmail.com>
2023-11-02 13:56:55 -0400

    Update home page DRR quick links

--- client/src/home/home-data.js
+++ client/src/home/home-data.js
@@ -68,6 +68,11 @@ const infographic_link_items = _.compact([
 
 const featured_content_items = _.compact([
   {
+    text_key: "quick_link_DRR23",
+    href: "#infographic/gov/gov/results/.-.-(panel_key.-.-'gov_drr)",
+    is_new: "true",
+  },
+  {
     text_key: "quick_link_gov_spending",
     href: "#infographic/gov/gov/financial/.-.-(panel_key.-.-'welcome_mat)",
     is_new: "true",
@@ -86,10 +91,6 @@ const featured_content_items = _.compact([
     text_key: "quick_link_service_inventory_2021",
     href: "#infographic/gov/gov/services/.-.-(panel_key.-.-'services_intro)",
     is_new: "true",
-  },
-  {
-    text_key: "quick_link_DRR22",
-    href: "#infographic/gov/gov/results/.-.-(panel_key.-.-'gov_drr)",
   },
   {
     text_key: "covid_expenditures_estimated_exp_2022",
