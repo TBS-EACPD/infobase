5617e6ec2debe602af492aeb106dca21222c448e
Stephane-Rancourt <rancourt.steph@gmail.com>
2023-11-09 10:52:57 -0500

    Supps B configurations

--- client/src/home/home-data.js
+++ client/src/home/home-data.js
@@ -68,6 +68,11 @@ const infographic_link_items = _.compact([
 
 const featured_content_items = _.compact([
   {
+    text_key: "supps_b",
+    href: "#compare_estimates",
+    is_new: "true",
+  },
+  {
     text_key: "quick_link_DRR23",
     href: "#infographic/gov/gov/results/.-.-(panel_key.-.-'gov_drr)",
     is_new: "true",
@@ -78,19 +83,13 @@ const featured_content_items = _.compact([
     is_new: "true",
   },
   {
-    text_key: "supps_a",
-    href: "#compare_estimates",
+    text_key: "quick_link_service_inventory_2021",
+    href: "#infographic/gov/gov/services/.-.-(panel_key.-.-'services_intro)",
     is_new: "true",
   },
   {
     text_key: "quick_link_DP23",
     href: "#infographic/gov/gov/results/.-.-(panel_key.-.-'gov_dp)",
-    is_new: "true",
-  },
-  {
-    text_key: "quick_link_service_inventory_2021",
-    href: "#infographic/gov/gov/services/.-.-(panel_key.-.-'services_intro)",
-    is_new: "true",
   },
   {
     text_key: "covid_expenditures_estimated_exp_2022",
