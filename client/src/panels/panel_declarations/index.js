a8cf57a2dd668d5c51fa23bac0a23fb35364aea4
Hiruni <hirunisenarath@gmail.com>
2025-03-04 14:34:44 -0500

    Remove old version of warning panel for depts that have a gap in reporting

--- client/src/panels/panel_declarations/index.js
+++ client/src/panels/panel_declarations/index.js
@@ -54,7 +54,6 @@ export {
   declare_tags_of_interest_panel,
   declare_tag_progs_by_dept_panel,
   declare_related_tags_panel,
-  declare_no_services_submission_panel,
   declare_services_missing_program_ids_panel,
 } from "./misc/index";
 
