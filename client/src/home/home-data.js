575800ae1d0e7b0bf148557964b543e5842e814f
Stephane-Rancourt <rancourt.steph@gmail.com>
2025-03-20 01:01:57 -0400

    Misc changes that were missed spec war titles and faqs

--- client/src/home/home-data.js
+++ client/src/home/home-data.js
@@ -68,13 +68,13 @@ const infographic_link_items = _.compact([
 
 const featured_content_items = _.compact([
   {
-    text_key: "quick_link_service_inventory_2022",
-    href: "#infographic/gov/gov/services/.-.-(panel_key.-.-'services_intro)",
-    is_new: "true",
+    text_key: "spec_war",
+    href: "#compare_estimates",
+    is_new: true,
   },
   {
-    text_key: "quick_link_gov_spending",
-    href: "#infographic/gov/gov/financial/.-.-(panel_key.-.-'welcome_mat)",
+    text_key: "quick_link_service_inventory_2022",
+    href: "#infographic/gov/gov/services/.-.-(panel_key.-.-'services_intro)",
     is_new: "true",
   },
   {
@@ -83,16 +83,17 @@ const featured_content_items = _.compact([
     is_new: "true",
   },
   {
-    text_key: "supps_b",
-    href: "#compare_estimates",
-  },
-  {
-    text_key: "quick_link_DP24",
-    href: "#infographic/gov/gov/results/.-.-(panel_key.-.-'gov_dp)",
+    text_key: "quick_link_gov_spending",
+    href: "#infographic/gov/gov/financial/.-.-(panel_key.-.-'welcome_mat)",
+    is_new: "true",
   },
   {
     text_key: "quick_link_DRR24",
     href: "#infographic/gov/gov/results/.-.-(panel_key.-.-'gov_drr)",
+  },
+  {
+    text_key: "quick_link_DP24",
+    href: "#infographic/gov/gov/results/.-.-(panel_key.-.-'gov_dp)",
   },
   {
     text_key: "covid_expenditures_estimated_exp_2022",
