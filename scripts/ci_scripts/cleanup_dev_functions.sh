13fe17862072476749de32ac0632a72f2042b920
Chad Atton <chad.atton@tbs-sct.gc.ca>
2025-04-14 15:35:34 -0400

    check functions and delete then check services.

--- scripts/ci_scripts/cleanup_dev_functions.sh
+++ scripts/ci_scripts/cleanup_dev_functions.sh
@@ -4,11 +4,9 @@ set -e
 echo "Starting dev functions cleanup..."
 
 active_branches=$(git branch -r | sed -E 's/(.*\/)(.*)/\2/g')
-current_functions=$(gcloud functions list --format="value(name)")
-current_run_services=$(gcloud run services list --format="value(name)")
 
+current_functions=$(gcloud functions list --format="value(name)")
 to_delete_functions=$(grep -Fxvf <(echo "$active_branches") <(echo "$current_functions") || echo "")
-to_delete_services=$(grep -Fxvf <(echo "$active_branches" | sed 's/_/-/g') <(echo "$current_run_services") || echo "")
 
 if [[ ! -z "$to_delete_functions" ]]; then
     echo "start deleting functions"
@@ -20,6 +18,9 @@ if [[ ! -z "$to_delete_functions" ]]; then
 else
     echo "no functions to delete"
 fi
+
+current_run_services=$(gcloud run services list --format="value(name)")
+to_delete_services=$(grep -Fxvf <(echo "$active_branches" | sed 's/_/-/g') <(echo "$current_run_services") || echo "")
 
 if [[ ! -z "$to_delete_services" ]]; then
     echo "start deleting run services"
