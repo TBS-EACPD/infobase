d37564b3c438befee3ad4e97840e371e58f9590d
Stephane-Rancourt <rancourt.steph@gmail.com>
2025-11-26 09:46:54 -0500

    deploy script tweaks again

--- server/scripts/deploy/prod_deploy_function.sh
+++ server/scripts/deploy/prod_deploy_function.sh
@@ -8,7 +8,7 @@ source scripts/deploy/set_prod_env_vars.sh
 source scripts/deploy/set_transient_function_secrets.sh # these trap EXIT to handle their own cleanup
 
 gcloud functions deploy prod-api-${CURRENT_SHA} --project ${PROJECT} --region us-central1 --entry-point app --stage-bucket prod-api-staging-bucket --runtime nodejs20 --trigger-http --allow-unauthenticated --env-vars-file $scratch/envs.yaml > /dev/null
-gcloud functions add-invoker-policy-binding prod-api-${CURRENT_SHA} --project ${PROJECT} --region us-central1 --member allUsers --no-user-output-enabled
+gcloud functions add-invoker-policy-binding prod-api-${CURRENT_SHA} --project ${PROJECT} --region us-central1 --member allUsers
 
 source scripts/deploy/unset_prod_env_vars.sh
 
