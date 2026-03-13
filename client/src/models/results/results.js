520cecf206e13454c533ea0df3b0406eda255ea3
Stephane-Rancourt <rancourt.steph@gmail.com>
2026-03-13 18:38:17 -0400

    Automated banner fixes, ensure loaded results home page

--- client/src/models/results/results.js
+++ client/src/models/results/results.js
@@ -556,7 +556,7 @@ const dp_docs = build_doc_info_objects("dp", [
       "https://www.canada.ca/en/treasury-board-secretariat/services/planned-government-spending/reports-plans-priorities/2026-27-departmental-plans.html",
     doc_url_fr:
       "https://www.canada.ca/fr/secretariat-conseil-tresor/services/depenses-prevues/rapports-plans-priorites/plans-ministeriels-2026-2027.html",
-    late_results_orgs: ["297", "278"],
+    late_results_orgs: [],
     late_resources_orgs: [],
     late_planned_fte_orgs: [],
     temp_untabled_orgs: [],
